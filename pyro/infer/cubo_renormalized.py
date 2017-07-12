0b6c797d0d12e83bac77e7e2ec1bf9ae98c1c869
Theofanis Karaletsos <theofanis@uber.com>
2017-07-12 14:32:42 -0700

    renormalized sketch updated

--- pyro/infer/cubo_renormalized.py
+++ pyro/infer/cubo_renormalized.py
@@ -6,6 +6,8 @@ import pyro
 import pyro.poutine as poutine
 from pyro.infer.abstract_infer import AbstractInfer
 
+import pdb as pdb
+
 
 def zero_grads(tensors):
     """
@@ -20,7 +22,7 @@ def zero_grads(tensors):
                 p.grad = Variable(data.new().resize_as_(data).zero_())
 
 
-class CUBO(AbstractInfer):
+class CUBO_RW(AbstractInfer):
     """
     A new, Trace and Poutine-based implementation of SVI
     """
@@ -35,7 +37,7 @@ class CUBO(AbstractInfer):
         Call parent class initially, then setup the poutines to run
         """
         # initialize
-        super(CUBO, self).__init__()
+        super(CUBO_RW, self).__init__()
         # TODO init this somewhere else in a more principled way
         self.sites = None
 
@@ -62,6 +64,10 @@ class CUBO(AbstractInfer):
 
         rr_max = torch.max(log_r, 1)
         rr_reduced = log_r - rr_max.expand_as(log_r)
+
+        #key point: what are we renormalizing?
+        pdb.set_trace()
+
         w = torch.exp(rr_reduced)
         w_n = torch.pow(w, self.n_cubo)
 
