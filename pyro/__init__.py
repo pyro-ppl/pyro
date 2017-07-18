83e9def5af3d8f8d6355e6c0faeffa010d04960c
Martin Jankowiak <jankowiak@uber.com>
2017-07-18 10:31:43 -0700

    fixed flake8 errors

--- pyro/__init__.py
+++ pyro/__init__.py
@@ -124,19 +124,20 @@ def module(pyro_name, nn_obj):
 
     current_nn_state = nn_obj.state_dict()
     for name, param in state_dict.items():
-	if name not in current_nn_state:
-	    raise KeyError('unexpected key "{}" in state_dict'
-			   .format(name))
-	if isinstance(param, Parameter):
-	    # backwards compatibility for serialized parameters
-	    param = param.data
+        if name not in current_nn_state:
+            raise KeyError('unexpected key "{}" in state_dict'.format(name))
+        if isinstance(param, Parameter):
+            # backwards compatibility for serialized parameters
+            param = param.data
 
         # only copy if the param has actually changed
-        if id(param)!=id(current_nn_state[name]):
-	    current_nn_state[name].copy_(param)
+        # Note: apart from the following line, the rest of this code
+        # logic is borrowed from torch.nn.Module.load_state_dict
+        if id(param) != id(current_nn_state[name]):
+            current_nn_state[name].copy_(param)
 
     missing = set(current_nn_state.keys()) - set(state_dict.keys())
     if len(missing) > 0:
-	raise KeyError('missing keys in state_dict: "{}"'.format(missing))
+        raise KeyError('missing keys in state_dict: "{}"'.format(missing))
 
     return nn_obj
