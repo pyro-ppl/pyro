7c2f23da5c0a317132eab72da3e138db71aaa0c9
jpchen <jpchen@uber.com>
2017-07-18 21:05:00 -0700

    test cleanup

--- pyro/infer/kl_qp.py
+++ pyro/infer/kl_qp.py
@@ -19,6 +19,7 @@ def zero_grads(tensors):
                 data = p.grad.data
                 p.grad = Variable(data.new().resize_as_(data).zero_())
 
+
 class KL_QP(AbstractInfer):
     """
     A new, Trace and Poutine-based implementation of SVI
