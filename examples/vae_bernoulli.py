8d6917538a5da8b651857ccb350812116900314f
Theofanis Karaletsos <theofanis@uber.com>
2017-09-21 13:08:37 -0700

    adjusted name and vae-bernoulli to use this

--- examples/vae_bernoulli.py
+++ examples/vae_bernoulli.py
@@ -129,7 +129,8 @@ def per_param_args(name, param):
 adam_params = {"lr": .001}
 
 kl_optim = KL_QP(model, guide, pyro.optim(optim.Adam, adam_params), analytic=True)
-kl_eval = KL_QP(model=model, guide=guide, optim_step_fct=pyro.optim(optim.Adam, adam_params), num_particles=10, analytic=True)
+kl_eval = KL_QP(model=model, guide=guide, optim_step_fct=pyro.optim(optim.Adam, adam_params),
+                num_particles=10, analytic=True)
 
 # num_steps = 1
 mnist_data = Variable(train_loader.dataset.train_data.float() / 255.)
