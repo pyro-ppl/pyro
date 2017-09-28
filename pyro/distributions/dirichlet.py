05a4bd94906aee80a3ac20c86ae357a776cc6c53
Fritz Obermeyer <fritzo@uber.com>
2017-09-28 07:36:09 -0700

    Fix indexing bug using scipy.stats.dirichlet.rvs

--- pyro/distributions/dirichlet.py
+++ pyro/distributions/dirichlet.py
@@ -81,11 +81,11 @@ class Dirichlet(Distribution):
             raise ValueError('Expected alpha.dim() in (1,2), actual: {}'.format(alpha.dim()))
         alpha_np = alpha.data.numpy()
         if alpha.dim() == 1:
-            x_np = spr.dirichlet.rvs(alpha_np)
+            x_np = spr.dirichlet.rvs(alpha_np)[0]
         else:
             x_np = np.empty_like(alpha_np)
             for i in range(alpha_np.shape[0]):
-                x_np[i, :] = spr.dirichlet.rvs(alpha_np[i, :])
+                x_np[i, :] = spr.dirichlet.rvs(alpha_np[i, :])[0]
         x = Variable(torch.Tensor(x_np))
         return x
 
