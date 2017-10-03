6cdef2f592d79fb9e61d8ed01b3ac40f37498ceb
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2017-10-02 21:23:08 -0700

    Move set_rng_seed to pyro.util (#181)
    Move set_rng_seed to pyro.util

--- pyro/util.py
+++ pyro/util.py
@@ -40,6 +40,17 @@ def memoize(fn):
     return _fn
 
 
+def set_rng_seed(rng_seed):
+    """
+    Sets seeds of torch, numpy, and torch.cuda (if available).
+    :param int rng_seed: The seed value.
+    """
+    torch.manual_seed(rng_seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(rng_seed)
+    np.random.seed(rng_seed)
+
+
 def ones(*args, **kwargs):
     return Parameter(torch.ones(*args, **kwargs))
 
