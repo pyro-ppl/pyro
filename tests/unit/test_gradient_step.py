4f16c73837e39a76408a9c30ed002f05a8caad7c
Martin Jankowiak <jankowiak@uber.com>
2017-10-03 17:49:58 -0700

    use integer float representation

--- tests/unit/test_gradient_step.py
+++ tests/unit/test_gradient_step.py
@@ -3,6 +3,7 @@ import torch.optim
 from torch.autograd import Variable
 import pytest
 
+import struct
 import pyro
 import pyro.distributions as dist
 from pyro.infer.kl_qp import KL_QP
@@ -11,6 +12,9 @@ from pyro.util import ng_ones, ng_zeros
 
 pytestmark = pytest.mark.init(rng_seed=0)
 
+# we're going to use a representation of a float which is given by 4 integers
+def floatrep(x):
+    return [ord(c) for c in struct.pack('!f', x)]
 
 @pytest.mark.parametrize("kl_qp", [KL_QP, TraceGraph_KL_QP])
 @pytest.mark.parametrize("reparameterized", [True, False])
@@ -19,8 +23,8 @@ def test_kl_qp_gradient_step(kl_qp, reparameterized):
         return
     verbose = True
     pyro.get_param_store().clear()
-    mu_q_expected = {True: -1.2770081289993769, False: -1.2770081290294442}[reparameterized]
-    log_sig_q_expected = {True: -0.40374189711367497, False: -0.40374189244231484}[reparameterized]
+    mu_q_expected = {True: [191, 150, 200, 248], False: [191, 150, 200, 248]}[reparameterized]
+    log_sig_q_expected = {True: [190, 156, 7, 49], False: [190, 156, 7, 33]}[reparameterized]
 
     def model():
         mu_latent = pyro.sample("mu_latent", dist.diagnormal, ng_zeros(1), ng_ones(1))
@@ -33,13 +37,14 @@ def test_kl_qp_gradient_step(kl_qp, reparameterized):
         sig_q = torch.exp(log_sig_q)
         return pyro.sample("mu_latent", dist.diagnormal, mu_q, sig_q, reparameterized=reparameterized)
 
-    kl_optim = kl_qp(model, guide, pyro.optim(torch.optim.Adam, {"lr": 0.001}))
+    kl_optim = kl_qp(model, guide, pyro.optim(torch.optim.Adam, {"lr": 0.1}))
     kl_optim.step()
-    if verbose:
-        print("after one step mu_q was %.12f; expected %.12f" % (pyro.param("mu_q").data.numpy()[0],
-                                                                 mu_q_expected))
-        print("after one step log_sig_q was %.12f; expected %.12f" % (pyro.param("log_sig_q").data.numpy()[0],
-                                                                      log_sig_q_expected))
+    new_mu_q = floatrep(pyro.param("mu_q").data.numpy()[0])
+    new_log_sig_q = floatrep(pyro.param("log_sig_q").data.numpy()[0])
 
-    assert(pyro.param("mu_q").data.numpy()[0] == mu_q_expected)
-    assert(pyro.param("log_sig_q").data.numpy()[0] == log_sig_q_expected)
+    if verbose:
+        print("after one step mu_q was %s; expected %s" % (new_mu_q, mu_q_expected))
+        print("after one step log_sig_q was %s expected %s" % (new_log_sig_q, log_sig_q_expected))
+
+    assert(floatrep(pyro.param("mu_q").data.numpy()[0]) == mu_q_expected)
+    assert(floatrep(pyro.param("log_sig_q").data.numpy()[0]) == log_sig_q_expected)
