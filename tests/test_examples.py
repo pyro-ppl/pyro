3e6b64e0a3075e1780f051fc09c97248c1440115
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2017-10-05 15:54:21 -0700

    Parallelize test examples (#203)
    * Parallelize test examples
    
    * Fix copy-and-paste error
    
    * Use verbose --num-epochs parameter name
    
    * Fix merge conflicts

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -1,35 +1,20 @@
-from __future__ import print_function
-
 import os
+import glob
 import sys
-from os.path import isfile, join
-from subprocess import Popen, PIPE, STDOUT
+from subprocess import check_call
 
+from tests.common import EXAMPLES_DIR
 import pytest
 
-from tests.common import TestCase
+EXAMPLES = [
+    os.path.basename(f)
+    for f in glob.glob(os.path.join(EXAMPLES_DIR, '*.py'))
+    if not f.endswith('__init__.py')
+]
 
 
 @pytest.mark.stage("test_examples")
-class runExample(TestCase):
-    def setUp(self):
-        self.PATH = 'examples/'
-        self.examples = [f for f in os.listdir(self.PATH) if isfile(join(self.PATH, f))]
-        # remove __init___.py
-        self.examples.pop(0)
-        self.num_epochs = 1
-
-    def test(self):
-        print('')
-        for example in self.examples:
-            print('running example ' + example + ' ... ', end='')
-            sys.stdout.flush()  # need to manually do this in Python 2.7
-            cmd = 'python ' + self.PATH + example + ' -n ' + str(self.num_epochs)
-            p = Popen(cmd, shell=True, stdin=PIPE,
-                      stdout=PIPE, stderr=STDOUT, close_fds=True)
-            output = p.stdout.read()
-            if p.wait() == 0:
-                print('ok')
-            else:
-                self.fail(example + ' threw an Error. Stack trace below:\n' + str(output))
-        assert True
+@pytest.mark.parametrize('example', EXAMPLES)
+def test_example(example):
+    example = os.path.join(EXAMPLES_DIR, example)
+    check_call([sys.executable, example, '--num-epochs', '1'])
