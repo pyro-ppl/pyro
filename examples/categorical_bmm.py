fix broadcasting issue

examples/categorical_bmm.py
@@ -12,6 +12,17 @@ import pyro
 from pyro.distributions import Bernoulli, Categorical
 from pyro.infer.kl_qp import KL_QP
 
+import sys, warnings, traceback, torch
+def warn_with_traceback(message, category, filename, lineno, file=None, line=None):
+    sys.stderr.write(warnings.formatwarning(message, category, filename, lineno, line))
+    traceback.print_stack(sys._getframe(2))
+warnings.showwarning = warn_with_traceback; warnings.simplefilter('always', UserWarning);
+# warnings.showwarning = warn_with_traceback; warnings.simplefilter('error', UserWarning);
+torch.utils.backcompat.broadcast_warning.enabled = True
+torch.utils.backcompat.keepdim_warning.enabled = True
+
+
+
 mnist = dset.MNIST(
     root='./data',
     train=True,
@@ -44,7 +55,7 @@ def local_model(i, datum):
     # do MLE for class means
     mu = pyro.param("mean_of_class_" + str(cll[0]), mean_param)
     mu_param = softmax(mu)
-    pyro.observe("obs_" + str(i), Bernoulli(mu_param), datum)
+    pyro.observe("obs_" + str(i), Bernoulli(mu_param), datum.view(1, -1))
     return cll
 
 