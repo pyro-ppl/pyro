97eac68e57dbc11c5f57936e1add39096f1950c1
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2017-10-06 13:36:08 -0700

    Implement map_data() in terms of iarange() (#184)
    * Sketch batched_range() function
    
    * Format comments
    
    * Add comments about a global score factor
    
    * Simplify logic of batch_range()
    
    * Replace batched_range() with iarange()
    
    * Add irange helper function
    
    * Implement map_data in terms of iarange and irange
    
    * Fix map_data_in_terms_of_iarange
    
    * Attempt to implement iarange poutine scaling
    
    * Finer grained unit and integration test matrix (#200)
    
    * bug fix, missing term in surrogate loss (#205)
    
    * Parallelize test examples (#203)
    
    * Parallelize test examples
    
    * Fix copy-and-paste error
    
    * Use verbose --num-epochs parameter name
    
    * Fix merge conflicts
    
    * Minor change to default test stage marker (#211)
    
    * Get tests to pass, yay
    
    * Delete obsolete map_data code
    
    * Simplify LambdaPoutine

--- pyro/poutine/trace_poutine.py
+++ pyro/poutine/trace_poutine.py
@@ -1,7 +1,4 @@
-import pyro
-
 from .poutine import Poutine
-from .lambda_poutine import LambdaPoutine
 from .trace import Trace
 
 
@@ -79,17 +76,3 @@ class TracePoutine(Poutine):
                                                           *args, **kwargs)
         self.trace.add_param(name, retrieved, *args, **kwargs)
         return retrieved
-
-    def _pyro_map_data(self, msg, name, data, fn, batch_size=None, batch_dim=0):
-        """
-        Trace map_data
-        """
-        scale = pyro.util.get_batch_scale(data, batch_size, batch_dim)
-        ret = super(TracePoutine, self)._pyro_map_data(msg, name, data,
-                                                       LambdaPoutine(fn, name, scale),
-                                                       # XXX watch out for changing
-                                                       batch_size=batch_size,
-                                                       batch_dim=batch_dim)
-
-        self.trace.add_map_data(name, fn, batch_size, batch_dim, msg["indices"])
-        return ret
