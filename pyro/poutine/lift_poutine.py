19abac19a10ca5bc3e69df8de2a269d3f8bf1c0e
jpchen <jpchen@uber.com>
2017-10-09 00:43:26 -0700

    moar docs

--- pyro/poutine/lift_poutine.py
+++ pyro/poutine/lift_poutine.py
@@ -5,20 +5,34 @@ from .poutine import Poutine
 
 class LiftPoutine(Poutine):
     """
-    Implements the param->sample lifting operation that turns params into rvs
+    Poutine which "lifts" parameters to random samples.
+    Given a stochastic function with param calls and a prior,
+    creates a stochastic function where all param calls are 
+    replaced by sampling from prior.
+    
+    Prior should be a callable or a dict of names to callables.
     """
 
     def __init__(self, fn, prior):
         """
-        constructor
+        :param fn: stochastic function
+        :param prior: prior used to lift parameters. Prior can be of type 
+                      dict, pyro.distributions, or a python stochastic fn 
+
+        Constructor
         """
         self.prior = prior
         super(LiftPoutine, self).__init__(fn)
 
     def _prepare_site(self, msg):
+        """
+        Sets flags of params that will be overriden so they are not
+        reexecuted in the stack and not added to the param store.
+        """
         name = msg["name"]
         param_name = params.user_param_name(name)
-        if isinstance(self.prior, dict) and param_name in self.prior.keys():
+        if isinstance(self.prior, dict) and param_name in self.prior.keys() \
+                or callable(self.prior):
             if msg["type"] == "param":
                 msg["done"] = True
         return msg
@@ -30,8 +44,6 @@ class LiftPoutine(Poutine):
         pyro.distributions object or a dict of distributions keyed
         on the param names. If the param name does not match the
         name the keys in the prior, that param name is unchanged.
-
-        TODO: any stochastic fn
         """
         name = msg["name"]
         param_name = params.user_param_name(name)
@@ -46,7 +58,7 @@ class LiftPoutine(Poutine):
             msg["fn"] = self.prior
         elif callable(self.prior):
             if not isinstance(self.prior, Distribution):
-                # prior is a stochastic fn. do not add in trace as sample
+                # prior is a stochastic fn. block sample
                 msg["stop"] = True
             msg["fn"] = self.prior
         else:
