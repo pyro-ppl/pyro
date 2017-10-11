790ea40c8f95fe592ac85b0c1cd35bd7a0ab7093
Fritz Obermeyer <fritzo@uber.com>
2017-10-11 13:49:19 -0700

    Add documentation to subsample.py

--- pyro/distributions/subsample.py
+++ pyro/distributions/subsample.py
@@ -10,7 +10,7 @@ class Subsample(Distribution):
     Randomly select a subsample of a range of indices.
 
     :param int size: the size of the range to subsample from
-    :param int batch_size: the size of the returned subsample
+    :param int subsample_size: the size of the returned subsample
     :returns: a random subsample of `range(size)`
     :rtype: torch.autograd.Variable of torch.LongTensor
     """
@@ -23,17 +23,37 @@ class Subsample(Distribution):
         return size, subsample_size
 
     def __init__(self, size=None, subsample_size=None):
+        """
+        :param int size: the size of the range to subsample from
+        :param int subsample_size: the size of the returned subsample
+        """
         self.size = size
         self.subsample_size = subsample_size
 
     def sample(self, size=None, subsample_size=None):
+        """
+        :param int size: the size of the range to subsample from
+        :param int subsample_size: the size of the returned subsample
+        :returns: a random subsample of `range(size)`
+        :rtype: torch.autograd.Variable of torch.LongTensor
+        """
         size, subsample_size = self._sanitize_inputs(size, subsample_size)
+        if subsample_size > size:
+            raise ValueError("expected size < subsample_size, actual {} vs {}".format(
+                size, subsample_size))
         return Variable(torch.randperm(size)[:subsample_size])
 
     def batch_log_pdf(self, x, size=None, subsample_size=None):
+        """
+        :param int size: the size of the range to subsample from
+        :param int subsample_size: the size of the returned subsample
+        :param torch.autograd.Variable x: a subsample of `range(size)`
+        :return: log probability density
+        :rtype: torch.autograd.Variable
+        """
         size, subsample_size = self._sanitize_inputs(size, subsample_size)
         if subsample_size is None:
             subsample_size = x.size(-1)
         elif subsample_size != x.size(-1):
-            raise ValueError('subsample does not match subsample_size')
+            raise ValueError("subsample does not match subsample_size")
         return gammaln(1 + size) - gammaln(1 + size - subsample_size)
