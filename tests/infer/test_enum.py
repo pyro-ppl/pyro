d66c04220a0a427e68d893ad53a2c0919903fbd3
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2017-10-12 14:55:27 -0700

    Sum over discrete latent variables in ELBO (#206)
    * Support sampling of continuous RVs in QueuePoutine
    
    * Add test for QueuePoutine
    
    * Use iterators internally in KLPQP
    
    * Hack enumeration into KL_QP
    
    * Fix: add .enum_discrete field to KL_QP class
    
    * Account for weight in enumeration hack in KL_QP
    
    * Block gradients from enumeration weights
    
    * Use .detach() to block gradients
    
    * Simplify Trace.log_pdf and Trace.batch_log_pdf
    
    * Add site_filter arg to log_pdf and batch_log_pdf
    
    * Use site_filter argument to Trace.log_pdf()
    
    * Refactor sum-over-discrete algorithm into pyro.infer.enum module
    
    * Attempt to generalize to tracegraph
    
    * Add test for pyro.infer.enum
    
    * Add tests for iter_discrete_traces
    
    * Get iter_discrete_traces to work with TraceGraph objects
    
    * Add failing tests of iter_discrete_traces on batched variables
    
    * Implement enum_discrete in Trace_ELBO
    
    * Implement enum_discrete in TraceGraph_ELBO
    
    * Add assertion to test less-shallow Trace.copy()
    
    * Fix typos using iter_discrete_traces
    
    * Fix broadcasting bug in batch_log_pdf
    
    * Add failing test for summed-out ELBO
    
    * Add more failing tests to tests/infer/test_enum.py
    
    * Mark segfaulting test as run=False
    
    * Mark failing tests, add segfault jig
    
    * Mark flaky test as xfail
    
    * Remove debugging cruft

---
+++ tests/infer/test_enum.py
@@ -0,0 +1,153 @@
+import sys
+
+import pytest
+import torch
+from torch.autograd import Variable
+
+import pyro
+import pyro.distributions as dist
+import pyro.optim
+from pyro import poutine
+from pyro.infer import SVI
+from pyro.infer.enum import iter_discrete_traces, scale_trace
+from tests.common import assert_equal
+
+
+# A model with continuous and discrete variables, no batching.
+def model0():
+    p = pyro.param("p", Variable(torch.Tensor([0.1, 0.9])))
+    mu = pyro.param("mu", Variable(torch.Tensor([-1.0, 1.0])))
+    sigma = pyro.param("sigma", Variable(torch.Tensor([2.0, 3.0])))
+    x = pyro.sample("x", dist.Bernoulli(p))
+    y = pyro.sample("y", dist.DiagNormal(mu, sigma))
+    z = pyro.sample("z", dist.DiagNormal(y, sigma))
+    return dict(x=x, y=y, z=z)
+
+
+# A purely discrete model, no batching.
+def model1():
+    p = pyro.param("p", Variable(torch.Tensor([0.05])))
+    ps = pyro.param("ps", Variable(torch.Tensor([0.1, 0.2, 0.3, 0.4])))
+    x = pyro.sample("x", dist.Bernoulli(p))
+    y = pyro.sample("y", dist.Categorical(ps, one_hot=False))
+    return dict(x=x, y=y)
+
+
+# A discrete model with batching.
+def model2():
+    p = pyro.param("p", Variable(torch.Tensor([[0.05], [0.15]])))
+    ps = pyro.param("ps", Variable(torch.Tensor([[0.1, 0.2, 0.3, 0.4],
+                                                 [0.4, 0.3, 0.2, 0.1]])))
+    x = pyro.sample("x", dist.Bernoulli(p))
+    y = pyro.sample("y", dist.Categorical(ps, one_hot=False))
+    assert x.size() == (2, 1)
+    assert y.size() == (2, 1)
+    return dict(x=x, y=y)
+
+
+@pytest.mark.parametrize("model", [
+    model0,
+    pytest.param(model1, marks=pytest.mark.xfail(sys.version_info >= (3, 0),
+                                                 reason="spurrious failure, probably pytorch")),
+    pytest.param(model2, marks=pytest.mark.xfail(reason="https://github.com/uber/pyro/issues/253")),
+])
+@pytest.mark.parametrize("graph_type", ["flat", "dense"])
+def test_scale_trace(graph_type, model):
+    pyro.get_param_store().clear()
+    scale = 1.234
+    tr1 = poutine.trace(model, graph_type=graph_type).get_trace()
+    tr2 = scale_trace(tr1, scale)
+
+    assert tr1 is not tr2, "scale_trace() did not make a copy"
+    assert set(tr1.nodes.keys()) == set(tr2.nodes.keys())
+    for name, site1 in tr1.nodes.items():
+        site2 = tr2.nodes[name]
+        assert site1 is not site2, "Trace.copy() was too shallow"
+        if "scale" in site1:
+            assert_equal(site2["scale"], scale * site1["scale"], msg=(site1, site2))
+
+    # These check that memoized values were cleared.
+    assert_equal(tr2.log_pdf(), scale * tr1.log_pdf())
+    assert_equal(tr2.batch_log_pdf(), scale * tr1.batch_log_pdf())
+
+
+@pytest.mark.parametrize("graph_type", ["flat", "dense"])
+def test_iter_discrete_traces_scalar(graph_type):
+    pyro.get_param_store().clear()
+    traces = list(iter_discrete_traces(graph_type, model1))
+
+    p = pyro.param("p").data
+    ps = pyro.param("ps").data
+    assert len(traces) == 2 * len(ps)
+
+    for scale, trace in traces:
+        x = trace.nodes["x"]["value"].data.long().view(-1)[0]
+        y = trace.nodes["y"]["value"].data.long().view(-1)[0]
+        expected_scale = [1 - p[0], p[0]][x] * ps[y]
+        assert_equal(scale, expected_scale)
+
+
+@pytest.mark.xfail(reason="https://github.com/uber/pyro/issues/220")
+@pytest.mark.parametrize("graph_type", ["flat", "dense"])
+def test_iter_discrete_traces_vector(graph_type):
+    pyro.get_param_store().clear()
+    traces = list(iter_discrete_traces(graph_type, model2))
+
+    p = pyro.param("p").data
+    ps = pyro.param("ps").data
+    assert len(traces) == 2 * ps.size(-1)
+
+    for scale, trace in traces:
+        x = trace.nodes["x"]["value"].data.squeeze().long()[0]
+        y = trace.nodes["y"]["value"].data.squeeze().long()[0]
+        expected_scale = torch.exp(dist.Bernoulli(p).log_pdf(x) *
+                                   dist.Categorical(ps, one_hot=False).log_pdf(y))
+        expected_scale = expected_scale.data.view(-1)[0]
+        assert_equal(scale, expected_scale)
+
+
+# A simple Gaussian mixture model.
+def gmm_model(data):
+    p = Variable(torch.Tensor([0.5]))
+    sigma = Variable(torch.Tensor([1.0]))
+    mus = pyro.param("mus", Variable(torch.Tensor([-1, 1]), requires_grad=True))
+    for i in pyro.irange("data", len(data)):
+        z = pyro.sample("z_{}".format(i), dist.Bernoulli(p))
+        assert z.size() == (1, 1)
+        z = z.long().data[0, 0]
+        print("M{} z_{} = {}".format("  " * i, i, z))
+        pyro.observe("x_{}".format(i), dist.DiagNormal(mus[z], sigma), data[i])
+
+
+def gmm_guide(data):
+    for i in pyro.irange("data", len(data)):
+        p = pyro.param("p_{}".format(i), Variable(torch.Tensor([0.5]), requires_grad=True))
+        z = pyro.sample("z_{}".format(i), dist.Bernoulli(p))
+        assert z.size() == (1, 1)
+        z = z.long().data[0, 0]
+        print("G{} z_{} = {}".format("  " * i, i, z))
+
+
+@pytest.mark.parametrize("data_size", [1, 2, 3])
+@pytest.mark.parametrize("graph_type", ["flat", "dense"])
+@pytest.mark.parametrize("model", [gmm_model, gmm_guide])
+def test_gmm_iter_discrete_traces(model, data_size, graph_type):
+    pyro.get_param_store().clear()
+    data = Variable(torch.arange(0, data_size))
+    traces = list(iter_discrete_traces(graph_type, model, data=data))
+    assert len(traces) == 2 ** data_size
+
+
+@pytest.mark.parametrize("trace_graph", [False, True])
+@pytest.mark.parametrize("enum_discrete", [
+    False,
+    pytest.param(True, marks=pytest.mark.xfail(run=False, reason="segfault")),
+])
+def test_gmm_elbo_single_datum(enum_discrete, trace_graph):
+    pyro.get_param_store().clear()
+    data = Variable(torch.Tensor([0, 1, 9]))
+
+    optimizer = pyro.optim.Adam({"lr": .001})
+    inference = SVI(gmm_model, gmm_guide, optimizer, loss="ELBO",
+                    trace_graph=trace_graph, enum_discrete=enum_discrete)
+    inference.step(data)
