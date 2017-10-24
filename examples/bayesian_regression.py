f6196eb3cb5f1e4718cb9de79fe5008d9b81eb3d
Jonathan <jonathanp.chen@gmail.com>
2017-10-24 15:04:32 -0700

    Write bayesian classifier with `random_module()` (#305)
    * rewrite bayesian regression with random_module
    
    * renaming
    
    * remove logistic clutter
    
    * comments and tweaks
    
    * names
    
    * lint
    
    * more comments
    
    * renaming
    
    * Cuda-ize bayesian_regression.py
    
    * cuda comments

--- examples/bayesian_regression.py
+++ examples/bayesian_regression.py
@@ -7,138 +7,124 @@ from torch.nn.functional import normalize  # noqa: F401
 from torch.autograd import Variable
 
 import pyro
-from pyro import poutine
 from pyro.distributions import DiagNormal, Bernoulli  # noqa: F401
 from pyro.infer import SVI
 from pyro.optim import Adam
 
 """
 Bayesian Regression
+Learning a function of the form:
+    y = wx + b
 """
 
 
 # generate toy dataset
-def build_linear_dataset(N, noise_std=0.1):
-    D = 1
+def build_linear_dataset(N, p, noise_std=0.1):
     X = np.linspace(-6, 6, num=N)
     y = 3 * X + 1 + np.random.normal(0, noise_std, size=N)
-    X = X.reshape((N, D))
+    X = X.reshape((N, p))
     y = y.reshape((N, 1))
     X, y = Variable(torch.Tensor(X)), Variable(torch.Tensor(y))
     return torch.cat((X, y), 1)
 
 
-def build_logistic_dataset(N, noise_std=0.1):
-    D = 1
-    X = np.linspace(-6, 6, num=N)
-    y = np.tanh(X) + np.random.normal(0, noise_std, size=N)
-    y[y < 0.5] = 0
-    y[y >= 0.5] = 1
-    X = (X - 4.0) / 4.0
-    X = X.reshape((N, D))
-    y = y.reshape((N, 1))
-    X, y = Variable(torch.Tensor(X)), Variable(torch.Tensor(y))
-    return torch.cat((X, y), 1)
+# NN with one linear layer
+class RegressionModel(nn.Module):
+    def __init__(self, p):
+        super(RegressionModel, self).__init__()
+        self.linear = nn.Linear(p, 1)
+
+    def forward(self, x):
+        # x * w + b
+        return self.linear(x)
 
 
-def log_reg(x_data):
-    p_w = pyro.param("weight", Variable(torch.zeros(D, 1), requires_grad=True))
-    p_b = pyro.param("bias", Variable(torch.ones(1), requires_grad=True))
-    reg = torch.addmm(p_b, x_data, p_w)
-    latent = sigmoid(reg)
-    return latent
+N = 100  # size of toy data
+p = 1  # number of features
 
-
-def lin_reg(x_data):
-    p_w = pyro.param("weight", Variable(torch.zeros(1), requires_grad=True))
-    p_b = pyro.param("bias", Variable(torch.ones(1), requires_grad=True))
-    latent = p_w.squeeze() * x_data.squeeze() + p_b
-    return latent
-
-
-sigmoid = nn.Sigmoid()
 softplus = nn.Softplus()
-
-N = 1000  # size of toy data
-D = 1  # number of features
-batch_size = 256  # batch size
-
-# type of regression
-reg_fn = lin_reg
+regression_model = RegressionModel(p)
 
 
 def model(data):
-    mu = Variable(torch.zeros(D, 1))
-    sigma = Variable(torch.ones(D, 1))
-    bias_mu = Variable(torch.zeros(1))
-    bias_sigma = Variable(torch.ones(1))
-    w_prior = DiagNormal(mu, sigma)
-    b_prior = DiagNormal(bias_mu, bias_sigma)
-    priors = {'weight': w_prior, 'bias': b_prior}
-    lifted_fn = poutine.lift(reg_fn, priors)
+    # Create unit normal priors over the parameters
+    mu = Variable(torch.zeros(p, 1)).type_as(data)
+    sigma = Variable(torch.ones(p, 1)).type_as(data)
+    bias_mu = Variable(torch.zeros(1)).type_as(data)
+    bias_sigma = Variable(torch.ones(1)).type_as(data)
+    w_prior, b_prior = DiagNormal(mu, sigma), DiagNormal(bias_mu, bias_sigma)
+    priors = {'linear.weight': w_prior, 'linear.bias': b_prior}
+    # wrap regression model that lifts module parameters to random variables
+    # sampled from the priors
+    lifted_module = pyro.random_module("module", regression_model, priors)
+    # sample a nn
+    lifted_nn = lifted_module()
 
     with pyro.iarange("map", N, subsample=data):
         x_data = data[:, :-1]
         y_data = data[:, -1]
-        latent = lifted_fn(x_data)
-        if reg_fn.__name__ == 'lin_reg':
-            pyro.observe("obs", DiagNormal(latent, Variable(torch.ones(data.size(0)))), y_data.squeeze())
-        else:
-            pyro.observe("obs", Bernoulli(latent.squeeze()), y_data)
+        # run the nn with the data
+        latent = lifted_nn(x_data).squeeze()
+        pyro.observe("obs", DiagNormal(latent, Variable(torch.ones(data.size(0))).type_as(data)), y_data.squeeze())
 
 
 def guide(data):
-    x_data = data[:, :-1]
-    w_mu = Variable(torch.randn(D, 1), requires_grad=True)
-    w_log_sig = Variable(-3.0 * torch.ones(D, 1) + 0.05 * torch.randn(D, 1), requires_grad=True)
-    b_mu = Variable(torch.randn(1), requires_grad=True)
-    b_log_sig = Variable(-3.0 * torch.ones(1) + 0.05 * torch.randn(1), requires_grad=True)
+    w_mu = Variable(torch.randn(p, 1).type_as(data.data), requires_grad=True)
+    w_log_sig = Variable((-3.0 * torch.ones(p, 1) + 0.05 * torch.randn(p, 1)).type_as(data.data), requires_grad=True)
+    b_mu = Variable(torch.randn(1).type_as(data.data), requires_grad=True)
+    b_log_sig = Variable((-3.0 * torch.ones(1) + 0.05 * torch.randn(1)).type_as(data.data), requires_grad=True)
+    # register learnable params in the param store
     mw_param = pyro.param("guide_mean_weight", w_mu)
-    sw_param = softplus(pyro.param("guide_sigma_weight", w_log_sig))
+    sw_param = softplus(pyro.param("guide_log_sigma_weight", w_log_sig))
     mb_param = pyro.param("guide_mean_bias", b_mu)
-    sb_param = softplus(pyro.param("guide_sigma_bias", b_log_sig))
+    sb_param = softplus(pyro.param("guide_log_sigma_bias", b_log_sig))
+    # gaussian priors for w and b
     w_prior = DiagNormal(mw_param, sw_param)
     b_prior = DiagNormal(mb_param, sb_param)
-    priors = {'weight': w_prior, 'bias': b_prior}
-    with pyro.iarange("map", N, subsample=x_data):
-        lifted_fn = poutine.lift(reg_fn, priors)
-        lifted_fn(x_data)
+    priors = {'linear.weight': w_prior, 'linear.bias': b_prior}
+    # overloading the parameters in the module with random samples from the prior
+    lifted_module = pyro.random_module("module", regression_model, priors)
+    # sample a nn
+    lifted_module()
 
 
-def load_data(reg_type):
-    global reg_fn
-    if reg_type == 'linear':
-        return build_linear_dataset(N)
-    elif reg_type == 'logistic':
-        reg_fn = log_reg
-        return build_logistic_dataset(N)
-    raise ValueError('data set type should be "logistic" or "linear".')
+# instantiate optim and inference objects
+optim = Adam({"lr": 0.01})
+svi = SVI(model, guide, optim, loss="ELBO")
 
 
-adam = Adam({"lr": 0.01})
-svi = SVI(model, guide, adam, loss="ELBO")
-
-# get batch indices
-all_batches = np.arange(0, N, batch_size)
-if all_batches[-1] != N:
-    all_batches = list(all_batches) + [N]
+# get array of batch indices
+def get_batch_indices(N, batch_size):
+    all_batches = np.arange(0, N, batch_size)
+    if all_batches[-1] != N:
+        all_batches = list(all_batches) + [N]
+    return all_batches
 
 
 def main():
     parser = argparse.ArgumentParser(description="parse args")
     parser.add_argument('-n', '--num-epochs', default=1000, type=int)
-    parser.add_argument('-b', '--batch', default=False, type=bool)
-    parser.add_argument('-t', '--regression-type', default='linear', type=str)
+    parser.add_argument('-b', '--batch-size', default=N, type=int)
+    parser.add_argument('--cuda', action='store_true')
     args = parser.parse_args()
-    data = load_data(args.regression_type)
+    data = build_linear_dataset(N, p)
+    if args.cuda:
+        # make tensors and modules CUDA
+        data = data.cuda()
+        softplus.cuda()
+        regression_model.cuda()
     for j in range(args.num_epochs):
-        if not args.batch:
+        if args.batch_size == N:
+            # use the entire data set
             epoch_loss = svi.step(data)
         else:
             # mini batch
             epoch_loss = 0.0
+            perm = torch.randperm(N) if not args.cuda else torch.randperm(N).cuda()
             # shuffle data
-            data = data[torch.randperm(N)]
+            data = data[perm]
+            # get indices of each batch
+            all_batches = get_batch_indices(N, args.batch_size)
             for ix, batch_start in enumerate(all_batches[:-1]):
                 batch_end = all_batches[ix + 1]
                 batch_data = data[batch_start: batch_end]
