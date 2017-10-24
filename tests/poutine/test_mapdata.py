cf771271d4bb8fa0cdd55e91da0fde82fa9a3b45
Fritz Obermeyer <fritzo@uber.com>
2017-10-24 10:56:27 -0700

    Fix bug in iarange,irange when subsample_size == size

--- tests/poutine/test_mapdata.py
+++ tests/poutine/test_mapdata.py
@@ -160,39 +160,82 @@ def test_nested_map_data():
             assert tr.nodes[name]["scale"] == 4.0 * 2.0
 
 
-def iarange_model():
-    with pyro.iarange('iarange', 20, 5) as batch:
+def iarange_model(subsample_size):
+    mu = Variable(torch.zeros(20))
+    sigma = Variable(torch.ones(20))
+    with pyro.iarange('iarange', 20, subsample_size) as batch:
+        pyro.sample("x", dist.diagnormal, mu[batch], sigma[batch])
         result = list(batch.data)
     return result
 
 
-def irange_model():
+def irange_model(subsample_size):
+    mu = Variable(torch.zeros(20))
+    sigma = Variable(torch.ones(20))
     result = []
-    for i in pyro.irange('irange', 20, 5):
+    for i in pyro.irange('irange', 20, subsample_size):
+        pyro.sample("x_{}".format(i), dist.diagnormal, mu[i], sigma[i])
         result.append(i)
     return result
 
 
-def map_data_model():
-    return pyro.map_data('mapdata', range(20), lambda i, x: i, batch_size=5)
+def map_data_vector_model(subsample_size):
+    mu = Variable(torch.zeros(20))
+    sigma = Variable(torch.ones(20))
+
+    def local_model(batch, unused):
+        pyro.sample("x", dist.diagnormal, mu[batch], sigma[batch])
+        return batch
+
+    ind = Variable(torch.LongTensor(range(20)))
+    batch = pyro.map_data('mapdata', ind, local_model, batch_size=subsample_size)
+    return list(batch.data)
 
 
+def map_data_iter_model(subsample_size):
+    mu = Variable(torch.zeros(20))
+    sigma = Variable(torch.ones(20))
+
+    def local_model(i, unused):
+        pyro.sample("x_{}".format(i), dist.diagnormal, mu[i], sigma[i])
+        return i
+
+    return pyro.map_data('mapdata', range(20), local_model, batch_size=subsample_size)
+
+
+@pytest.mark.parametrize('subsample_size', [5, 20])
 @pytest.mark.parametrize('model', [
     iarange_model,
     irange_model,
-    map_data_model,
-], ids=['iarange', 'irange', 'map_data'])
-def test_replay(model):
+    map_data_vector_model,
+    map_data_iter_model,
+], ids=['iarange', 'irange', 'map_data_vector', 'map_data_iter'])
+def test_map_data_stack(model, subsample_size):
+    tr = poutine.trace(model).get_trace(subsample_size)
+    for name, node in tr.nodes.items():
+        if name.startswith("x"):
+            assert node["map_data_stack"], "missing map_data_stack at node {}".format(name)
+
+
+@pytest.mark.parametrize('subsample_size', [5, 20])
+@pytest.mark.parametrize('model', [
+    iarange_model,
+    irange_model,
+    map_data_vector_model,
+    map_data_iter_model,
+], ids=['iarange', 'irange', 'map_data_vector', 'map_data_iter'])
+def test_replay(model, subsample_size):
     pyro.set_rng_seed(0)
 
     traced_model = poutine.trace(model)
-    original = traced_model()
+    original = traced_model(subsample_size)
 
-    replayed = poutine.replay(model, traced_model.trace)()
+    replayed = poutine.replay(model, traced_model.trace)(subsample_size)
     assert replayed == original
 
-    different = traced_model()
-    assert different != original
+    if subsample_size < 20:
+        different = traced_model(subsample_size)
+        assert different != original
 
 
 def iarange_custom_model(subsample):
@@ -218,44 +261,45 @@ def test_custom_subsample(model):
     assert poutine.trace(model)(subsample) == subsample
 
 
-def iarange_cuda_model():
+def iarange_cuda_model(subsample_size):
     mu = Variable(torch.zeros(20).cuda())
     sigma = Variable(torch.ones(20).cuda())
-    with pyro.iarange("data", 20, 5, use_cuda=True) as batch:
+    with pyro.iarange("data", 20, subsample_size, use_cuda=True) as batch:
         pyro.sample("x", dist.diagnormal, mu[batch], sigma[batch])
 
 
-def irange_cuda_model():
+def irange_cuda_model(subsample_size):
     mu = Variable(torch.zeros(20).cuda())
     sigma = Variable(torch.ones(20).cuda())
-    for i in pyro.irange("data", 20, 5, use_cuda=True):
+    for i in pyro.irange("data", 20, subsample_size, use_cuda=True):
         pyro.sample("x_{}".format(i), dist.diagnormal, mu[i], sigma[i])
 
 
-def map_data_vector_cuda_model():
+def map_data_vector_cuda_model(subsample_size):
     mu = Variable(torch.zeros(20).cuda())
     sigma = Variable(torch.ones(20).cuda())
     pyro.map_data("data", mu,
                   lambda i, mu: pyro.sample("x", dist.diagnormal, mu, sigma[i]),
-                  use_cuda=True)
+                  batch_size=subsample_size, use_cuda=True)
 
 
-def map_data_iter_cuda_model():
+def map_data_iter_cuda_model(subsample_size):
     mu = Variable(torch.zeros(20).cuda())
     sigma = Variable(torch.ones(20).cuda())
     pyro.map_data("data", list(mu),
                   lambda i, mu: pyro.sample("x_{}".format(i), dist.diagnormal, mu, sigma[i]),
-                  use_cuda=True)
+                  batch_size=subsample_size, use_cuda=True)
 
 
 @requires_cuda
+@pytest.mark.parametrize('subsample_size', [5, 20])
 @pytest.mark.parametrize('model', [
     iarange_cuda_model,
     irange_cuda_model,
     map_data_vector_cuda_model,
     map_data_iter_cuda_model,
 ], ids=["iarange", "irange", "map_data_vector", "map_data_iter"])
-def test_cuda(model):
-    tr = poutine.trace(model).get_trace()
+def test_cuda(model, subsample_size):
+    tr = poutine.trace(model).get_trace(subsample_size)
     assert tr.log_pdf().is_cuda
     assert tr.batch_log_pdf().is_cuda
