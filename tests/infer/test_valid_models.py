29e1838037be204af33a530342781a7319641c03
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2017-10-26 18:38:39 -0700

    Add xfailing test for iarange in model but not guide (#368)

--- tests/infer/test_valid_models.py
+++ tests/infer/test_valid_models.py
@@ -18,61 +18,118 @@ def assert_ok(model, guide, **kwargs):
 
 def assert_error(model, guide, **kwargs):
     inference = SVI(model,  guide, Adam({"lr": 1e-3}), "ELBO", **kwargs)
-    with pytest.raises(SyntaxError):
+    with pytest.raises((NotImplementedError, UserWarning, KeyError)):
         inference.step()
 
 
-@pytest.mark.parametrize("trace_graph", [True, False])
-def test_irange_ok(trace_graph):
+@pytest.mark.parametrize("subsample_size", [None, 5], ids=["full", "subsample"])
+@pytest.mark.parametrize("trace_graph", [False, True], ids=["trace", "tracegraph"])
+def test_irange_ok(trace_graph, subsample_size):
 
     def model():
         p = Variable(torch.Tensor([0.5]))
-        for i in pyro.irange("irange", 10, 5):
+        for i in pyro.irange("irange", 10, subsample_size):
             pyro.sample("x_{}".format(i), dist.bernoulli, p)
 
     def guide():
         p = pyro.param("p", Variable(torch.Tensor([0.5]), requires_grad=True))
-        for i in pyro.irange("irange", 10, 5):
+        for i in pyro.irange("irange", 10, subsample_size):
             pyro.sample("x_{}".format(i), dist.bernoulli, p)
 
     assert_ok(model, guide, trace_graph=trace_graph)
 
 
-@pytest.mark.parametrize("trace_graph", [True, False])
-def test_iarange_ok(trace_graph):
+@pytest.mark.parametrize("subsample_size", [None, 5], ids=["full", "subsample"])
+@pytest.mark.parametrize("trace_graph", [False, True], ids=["trace", "tracegraph"])
+def test_iarange_ok(trace_graph, subsample_size):
 
     def model():
         p = Variable(torch.Tensor([0.5]))
-        with pyro.iarange("irange", 10, 5) as ind:
+        with pyro.iarange("irange", 10, subsample_size) as ind:
             pyro.sample("x", dist.bernoulli, p, batch_size=len(ind))
 
     def guide():
         p = pyro.param("p", Variable(torch.Tensor([0.5]), requires_grad=True))
-        with pyro.iarange("irange", 10, 5) as ind:
+        with pyro.iarange("irange", 10, subsample_size) as ind:
             pyro.sample("x", dist.bernoulli, p, batch_size=len(ind))
 
     assert_ok(model, guide, trace_graph=trace_graph)
 
 
-@pytest.mark.parametrize("trace_graph", [True, False])
-def test_irange_irange_ok(trace_graph):
+@pytest.mark.parametrize("subsample_size", [None, 5], ids=["full", "subsample"])
+@pytest.mark.parametrize("trace_graph", [False, True], ids=["trace", "tracegraph"])
+def test_irange_irange_ok(trace_graph, subsample_size):
 
     def model():
         p = Variable(torch.Tensor([0.5]))
-        for i in pyro.irange("irange_0", 10, 5):
-            for j in pyro.irange("irange_1", 10, 5):
+        for i in pyro.irange("irange_0", 10, subsample_size):
+            for j in pyro.irange("irange_1", 10, subsample_size):
                 pyro.sample("x_{}_{}".format(i, j), dist.bernoulli, p)
 
     def guide():
         p = pyro.param("p", Variable(torch.Tensor([0.5]), requires_grad=True))
-        for i in pyro.irange("irange_0", 10, 5):
-            for j in pyro.irange("irange_1", 10, 5):
+        for i in pyro.irange("irange_0", 10, subsample_size):
+            for j in pyro.irange("irange_1", 10, subsample_size):
                 pyro.sample("x_{}_{}".format(i, j), dist.bernoulli, p)
 
     assert_ok(model, guide, trace_graph=trace_graph)
 
 
-@pytest.mark.xfail(reason="error is not caught")
+@pytest.mark.parametrize("subsample_size", [None, 5], ids=["full", "subsample"])
+@pytest.mark.parametrize("trace_graph", [False, True], ids=["trace", "tracegraph"])
+def test_irange_irange_swap_error(trace_graph, subsample_size):
+
+    def model():
+        p = Variable(torch.Tensor([0.5]))
+        for i in pyro.irange("irange_0", 10, subsample_size):
+            for j in pyro.irange("irange_1", 10, subsample_size):
+                pyro.sample("x_{}_{}".format(i, j), dist.bernoulli, p)
+
+    def guide():
+        p = pyro.param("p", Variable(torch.Tensor([0.5]), requires_grad=True))
+        for j in pyro.irange("irange_1", 10, subsample_size):
+            for i in pyro.irange("irange_0", 10, subsample_size):
+                pyro.sample("x_{}_{}".format(i, j), dist.bernoulli, p)
+
+    assert_error(model, guide, trace_graph=trace_graph)
+
+
+@pytest.mark.xfail(reason="raises UserWarning or KeyError")
+@pytest.mark.parametrize("subsample_size", [None, 5], ids=["full", "subsample"])
+@pytest.mark.parametrize("trace_graph", [False, True], ids=["trace", "tracegraph"])
+def test_irange_in_model_not_guide_ok(trace_graph, subsample_size):
+
+    def model():
+        p = Variable(torch.Tensor([0.5]))
+        for i in pyro.irange("irange", 10, subsample_size):
+            pass
+        pyro.sample("x", dist.bernoulli, p)
+
+    def guide():
+        p = pyro.param("p", Variable(torch.Tensor([0.5]), requires_grad=True))
+        pyro.sample("x", dist.bernoulli, p)
+
+    assert_ok(model, guide, trace_graph=trace_graph)
+
+
+@pytest.mark.parametrize("subsample_size", [None, 5], ids=["full", "subsample"])
+@pytest.mark.parametrize("trace_graph", [False, True], ids=["trace", "tracegraph"])
+def test_irange_in_guide_not_model_error(trace_graph, subsample_size):
+
+    def model():
+        p = Variable(torch.Tensor([0.5]))
+        pyro.sample("x", dist.bernoulli, p)
+
+    def guide():
+        p = pyro.param("p", Variable(torch.Tensor([0.5]), requires_grad=True))
+        for i in pyro.irange("irange", 10, subsample_size):
+            pass
+        pyro.sample("x", dist.bernoulli, p)
+
+    assert_error(model, guide, trace_graph=trace_graph)
+
+
+@pytest.mark.xfail(reason="NotImplementedError is not raised")
 def test_iarange_irange_error():
 
     def model():
@@ -90,7 +147,7 @@ def test_iarange_irange_error():
     assert_error(model, guide, trace_graph=True)
 
 
-@pytest.mark.parametrize("trace_graph", [True, False])
+@pytest.mark.parametrize("trace_graph", [False, True], ids=["trace", "tracegraph"])
 def test_irange_iarange_ok(trace_graph):
 
     def model():
