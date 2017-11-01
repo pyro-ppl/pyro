ddd04d5e8dfa5aac96c0a7bd3a6e635a1115f038
Fritz Obermeyer <fritzo@uber.com>
2017-10-31 18:13:20 -0700

    Add checks to ensure enum_discrete can run

--- pyro/infer/enum.py
+++ pyro/infer/enum.py
@@ -1,15 +1,38 @@
-import functools
-
 import torch
 from torch.autograd import Variable
 
+import pyro
 from pyro import poutine, util
+from pyro.poutine import LambdaPoutine
 from pyro.poutine.trace import Trace
 from six.moves.queue import LifoQueue
 
 
 def site_is_discrete(name, site):
     return getattr(site["fn"], "enumerable", False)
+
+
+# Batched .enumerate_support() assumes batched values are independent.
+# This wraps discrete_escape to also check that every batched escape site occurs within an iarange.
+def make_escape_fn(trace):
+
+    def escape_fn(msg):
+        if not util.discrete_escape(trace, msg):
+            return False
+        batch_shape = msg["fn"].batch_shape(msg["value"], *msg["args"], **msg["kwargs"])
+        is_batched = any(size > 1 for size in batch_shape)
+        if not is_batched:
+            return True
+        iranges = set(f.name for f in pyro._PYRO_STACK if isinstance(f, LambdaPoutine) and not f.vectorized)
+        iaranges = set(f.name for f in pyro._PYRO_STACK if isinstance(f, LambdaPoutine) and f.vectorized)
+        iaranges -= iranges  # Needed because irange is implemented in terms of iarange.
+        if not iaranges:
+            raise ValueError(
+                    "poutine.queue encountered a batched pyro.sample site '{}' outiside of a pyro.iarange. "
+                    "To fix, either enclose in a pyro.iarange, or avoid batching.".format(msg["name"]))
+        return True
+
+    return escape_fn
 
 
 def iter_discrete_traces(graph_type, fn, *args, **kwargs):
@@ -30,7 +53,7 @@ def iter_discrete_traces(graph_type, fn, *args, **kwargs):
     queue.put(Trace())
     while not queue.empty():
         partial_trace = queue.get()
-        escape_fn = functools.partial(util.discrete_escape, partial_trace)
+        escape_fn = make_escape_fn(partial_trace)
         traced_fn = poutine.trace(poutine.escape(poutine.replay(fn, partial_trace), escape_fn),
                                   graph_type=graph_type)
         try:
