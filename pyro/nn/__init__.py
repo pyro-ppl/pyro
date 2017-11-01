27ac1c4daa5a4fae13bda3ab32f962385f5ccb48
Rohit Singh <theone.rohit@gmail.com>
2017-11-01 16:54:15 -0700

    added epsilon-scaled clipped versions of Softmax and Sigmoid as utilities to use with Pytorch neural networks

--- pyro/nn/__init__.py
+++ pyro/nn/__init__.py
@@ -1,3 +1,5 @@
 from __future__ import absolute_import, division, print_function
 
 from .auto_reg_nn import AutoRegressiveNN, MaskedLinear  # noqa: F401
+
+from .clipped_nn import ClippedSoftmax, ClippedSigmoid
---
+++ pyro/nn/clipped_nn.py
@@ -0,0 +1,30 @@
+
+import torch
+import torch.nn as nn
+
+class ClippedSoftmax(nn.Softmax):
+    """
+        a wrapper around nn.Softmax that scales its output
+        from [0,1] to [epsilon,1-epsilon]
+    """
+    def __init__(self, epsilon, *args, **kwargs):
+        self.epsilon = epsilon
+        super(ClippedSoftmax, self).__init__(*args, **kwargs)
+
+    def forward(self, val):
+        rval = super(ClippedSoftmax, self).forward(val)
+        return (rval * (1.0 - 2 * self.epsilon)) + self.epsilon
+
+
+class ClippedSigmoid(nn.Sigmoid):
+    """
+        a wrapper around nn.Sigmoid that scales its output
+        from [0,1] to [epsilon,1-epsilon]
+    """
+    def __init__(self, epsilon, *args, **kwargs):
+        self.epsilon = epsilon
+        super(ClippedSigmoid, self).__init__(*args, **kwargs)
+
+    def forward(self, val):
+        rval = super(ClippedSigmoid, self).forward(val)
+        return (rval * (1.0 - 2 * self.epsilon)) + self.epsilon
