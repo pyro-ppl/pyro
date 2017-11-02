c24d45af44a2c4ea6d974ef8839c3f63ffabced8
Jonathan <jonathanp.chen@gmail.com>
2017-11-02 15:36:39 -0700

    extend timeout (#468)

--- tutorial/source/conf.py
+++ tutorial/source/conf.py
@@ -87,6 +87,8 @@ pygments_style = 'sphinx'
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
 
+# extend timeout
+nbsphinx_timeout = 120
 
 # -- Options for HTML output ----------------------------------------------
 
