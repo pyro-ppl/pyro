f984325d252aa92ad0c3104a89bfa8c2a40dd645
Dustin Tran <dustinviettran@gmail.com>
2017-11-01 21:51:45 -0700

    fix for linter

--- pyro/distributions/bernoulli.py
+++ pyro/distributions/bernoulli.py
@@ -1,7 +1,6 @@
 from __future__ import absolute_import, division, print_function
 
 import torch
-import torch.nn.functional as F
 from torch.autograd import Variable
 
 from pyro.distributions.distribution import Distribution
