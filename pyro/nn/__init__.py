76e7a1304f6919b21f6c7585126d63b0945ce71b
Fritz Obermeyer <fritzo@uber.com>
2017-11-02 12:50:01 -0700

    Add tests and format code

--- pyro/nn/__init__.py
+++ pyro/nn/__init__.py
@@ -1,5 +1,4 @@
 from __future__ import absolute_import, division, print_function
 
 from .auto_reg_nn import AutoRegressiveNN, MaskedLinear  # noqa: F401
-
-from .clipped_nn import ClippedSoftmax, ClippedSigmoid   # noqa: F401
+from .clipped_nn import ClippedSigmoid, ClippedSoftmax  # noqa: F401
