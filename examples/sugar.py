2683897a4b88af62a181cf8367451376f8f70035
Fritz Obermeyer <fritzo@uber.com>
2017-11-09 00:52:47 -0800

    Implement pyro.sugar module and example

---
+++ examples/sugar.py
@@ -0,0 +1,97 @@
+from __future__ import absolute_import, division, print_function
+
+import argparse
+
+import torch
+from torch.autograd import Variable
+
+import pyro
+import pyro.distributions as dist
+from pyro import sugar
+from pyro.infer import SVI
+from pyro.optim import Adam
+from pyro.sugar import Latent, LatentList, LatentDict
+from pyro.util import ng_ones, ng_zeros
+
+
+def model(data):
+    latent = Latent("latent")
+    sugar.sample(latent.z, dist.normal, ng_zeros(1), ng_ones(1))
+    model_recurse(data, latent)
+
+
+def model_recurse(data, latent):
+    if isinstance(data, Variable):
+        sugar.observe(latent.x, dist.normal, data, latent.z, ng_ones(1))
+    elif isinstance(data, list):
+        sugar.param(latent.prior_sigma, Variable(torch.ones(1), requires_grad=True))
+        latent.list = LatentList(len(data))
+        for data_i, latent_i in zip(data, latent.list):
+            sugar.sample(latent_i.z, dist.normal, latent.z, latent.prior_sigma)
+            model_recurse(data_i, latent_i)
+    elif isinstance(data, dict):
+        sugar.param(latent.prior_sigma, Variable(torch.ones(1), requires_grad=True))
+        latent.dict = LatentDict()
+        for key, value in data.items():
+            sugar.sample(latent.dict[key].z, dist.normal, latent.z, latent.prior_sigma)
+            model_recurse(value, latent.dict[key])
+    else:
+        raise TypeError("Unsupported type {}".format(type(data)))
+
+
+def guide(data):
+    guide_recurse(data, Latent("latent"))
+
+
+def guide_recurse(data, latent):
+    sugar.param(latent.post_mu, Variable(torch.zeros(1), requires_grad=True))
+    sugar.param(latent.post_sigma, Variable(torch.ones(1), requires_grad=True))
+    sugar.sample(latent.z, dist.normal, latent.post_mu, latent.post_sigma)
+    if isinstance(data, Variable):
+        pass
+    elif isinstance(data, list):
+        latent.list = LatentList(len(data))
+        for data_i, latent_i in zip(data, latent.list):
+            guide_recurse(data_i, latent_i)
+    elif isinstance(data, dict):
+        latent.dict = LatentDict()
+        for key, value in data.items():
+            guide_recurse(value, latent.dict[key])
+    else:
+        raise TypeError("Unsupported type {}".format(type(data)))
+
+
+def main(args):
+    optim = Adam({"lr": 0.1})
+    inference = SVI(model, guide, optim, loss="ELBO")
+
+    # Data is an arbitrary json-like structure with tensors at leaves.
+    one = ng_ones(1)
+    data = {
+        "foo": one,
+        "bar": [0 * one, 1 * one, 2 * one],
+        "baz": {
+            "noun": {
+                "concrete": 4 * one,
+                "abstract": 6 * one,
+            },
+            "verb": 2 * one,
+        },
+    }
+
+    print('Step\tLoss')
+    for step in range(args.num_epochs):
+        if step % 100 == 0:
+            loss = inference.step(data)
+            print('{}\t{:0.5g}'.format(step, loss))
+
+    print('Parameters:')
+    for name in sorted(pyro.get_param_store().get_all_param_names()):
+        print('{} = {}'.format(name, pyro.param(name).data.numpy()))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="parse args")
+    parser.add_argument('-n', '--num-epochs', default=1000, type=int)
+    args = parser.parse_args()
+    main(args)
---
+++ pyro/sugar.py
@@ -0,0 +1,123 @@
+from __future__ import absolute_import, division, print_function
+
+import functools
+
+import pyro
+
+
+class Latent(object):
+    """
+    Object to hold latent state.
+
+    :param str address: The name of the object.
+
+    Example::
+
+        state = Latent("state")
+        state.x = 0
+        state.ys = LatentList(5)
+        state.zs = LatentDict()
+    """
+    def __init__(self, address):
+        super(Latent, self).__setattr__('_address', address)
+
+    def __getattribute__(self, name):
+        try:
+            return super(Latent, self).__getattribute__(name)
+        except AttributeError:
+            address = '{}.{}'.format(self._address, name)
+            value = Latent(address)
+            value._set = lambda value: super(Latent, self).__setattr__(name, value)
+            super(Latent, self).__setattr__(name, value)
+            return value
+
+    def __setattr__(self, name, value):
+        if isinstance(value, (LatentList, LatentDict)):
+            value._bind('{}.{}'.format(self._address, name))
+        super(Latent, self).__setattr__(name, value)
+
+    def __str__(self):
+        return 'Latent({!r})'.format(self._address)
+
+
+class LatentList(list):
+    """
+    List-like object to hold latent state.
+
+    This must be created in expressions like::
+
+        latent = Latent()
+        latent.xs = LatentList(5)  # Must be bound to a Latent before use.
+    """
+    def __init__(self, size):
+        self._size = size
+        self._address = None
+
+    def _bind(self, address):
+        if self:
+            raise RuntimeError("Cannot bind a LatentList in a Latent after data has been added")
+        if self._address is not None:
+            raise RuntimeError("Tried to bind an already-bound LatentList: {}".format(self._address))
+        self._address = address
+        for i in range(self._size):
+            value = Latent('{}[{}]'.format(address, i))
+            value._set = lambda value, i=i: self.__setitem__(i, value)
+            self.append(value)
+
+
+class LatentDict(dict):
+    """
+    Temporary dict-like object to hold latent state.
+
+    This must be created in expressions like::
+
+        latent = Latent()
+        latent.xs = LatentDict()  # Must be bound to a Latent before use.
+    """
+    def __init__(self):
+        self._address = None
+
+    def _bind(self, address):
+        if self:
+            raise RuntimeError("Cannot bind a LatentDict in a Latent after data has been added")
+        if self._address is not None:
+            raise RuntimeError("Tried to bind an already-bound LatentDict: {}".format(self._address))
+        self._address = address
+
+    def __getitem__(self, key):
+        try:
+            return super(LatentDict, self).__getitem__(key)
+        except KeyError:
+            if self._address is None:
+                raise RuntimeError("Cannot access a LatentDict until it is bound to a Latent")
+            value = Latent('{}[{!r}]'.format(self._address, key))
+            value._set = lambda value: self.__setitem__(key, value)
+            self[key] = value
+            return value
+
+
+@functools.wraps(pyro.sample)
+def sample(latent, fn, *args, **kwargs):
+    if not isinstance(latent, Latent):
+        raise TypeError("sugar.sample expected a Latent but got {}".format(repr(latent)))
+    value = pyro.sample(latent._address, fn, *args, **kwargs)
+    latent._set(value)
+    return value
+
+
+@functools.wraps(pyro.observe)
+def observe(latent, fn, obs, *args, **kwargs):
+    if not isinstance(latent, Latent):
+        raise TypeError("sugar.observe expected a Latent but got {}".format(repr(latent)))
+    value = pyro.observe(latent._address, fn, obs, *args, **kwargs)
+    latent._set(value)
+    return value
+
+
+@functools.wraps(pyro.param)
+def param(latent, *args, **kwargs):
+    if not isinstance(latent, Latent):
+        return latent  # value was already set
+    value = pyro.param(latent._address, *args, **kwargs)
+    latent._set(value)
+    return value
