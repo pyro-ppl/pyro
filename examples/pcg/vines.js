4ae9778d7b21b71562bbdeddf50211e98ae80cd3
Paul Szerlip <pas@uber.com>
2017-11-10 15:00:15 -0800

    Procedural base, working towards rendering in python using pycair/cairoffi. This is a translation of the vines wppl demo code into python structure.
    Structure not yet tested.

---
+++ examples/pcg/vines.js
@@ -0,0 +1,291 @@
+// ----------------------------------------------------------------------------
+// Globals / constants
+
+// TODO: once things are working, strip out the use of 'future,' since we're using immediate mode?
+var futurePolicy = 'immediate';
+// var futurePolicy = 'lifo';
+// var futurePolicy = 'fifo';
+// var futurePolicy = 'uniformFromAll';
+// var futurePolicy = 'uniformFromDeepest';
+// var futurePolicy = 'depthWeighted';
+setFuturePolicy(futurePolicy);
+
+
+var viewport = {xmin: -12, xmax: 12, ymin: -22, ymax: 2};
+var norm2world = function(p) {
+	return utils.new(THREE.Vector2,
+		viewport.xmin + p.x*(viewport.xmax - viewport.xmin),
+		viewport.ymin + p.y*(viewport.ymax - viewport.ymin)
+	);
+}
+
+// ----------------------------------------------------------------------------
+// Factor encouraging similarity to target image
+
+
+// Render update
+var renderUpdate = function(geo) {
+	utils.rendering.drawImgToRenderContext(globalStore.genImg);
+	utils.rendering.renderIncr(geo, viewport);
+	globalStore.genImg = utils.rendering.copyImgFromRenderContext();
+};
+
+
+// Basically Gaussian log-likelihood, without the constant factor
+var makescore = function(val, target, tightness) {
+	var diff = val - target;
+	return - (diff * diff) / (tightness * tightness);
+}
+
+var simTightness = 0.02;
+var boundsTightness = 0.001;
+var targetFactor = function() {
+	renderUpdate(globalStore.geo);
+	// Similarity factor
+	var sim = utils.normalizedSimilarity(globalStore.genImg, target);
+	globalStore.sim = sim;
+	var simf = makescore(sim, 1, simTightness);
+	// Bounds factors
+	var bbox = globalStore.bbox;
+	var extraX = (Math.max(viewport.xmin - bbox.min.x, 0) + Math.max(bbox.max.x - viewport.xmax, 0)) / (viewport.xmax - viewport.xmin);
+	var extraY = (Math.max(viewport.ymin - bbox.min.y, 0) + Math.max(bbox.max.y - viewport.ymax, 0)) / (viewport.ymax - viewport.ymin);
+	var boundsfx = makescore(extraX, 0, boundsTightness);
+	var boundsfy = makescore(extraY, 0, boundsTightness);
+	var f = simf + boundsfx + boundsfy;
+	if (globalStore.prevFactor) {
+		factor(f - globalStore.prevFactor);
+	} else {
+		factor(f);
+	}
+	globalStore.prevFactor = f;
+};
+
+
+// ----------------------------------------------------------------------------
+// The program itself
+
+
+var makeProgram = function() {
+
+	var _flip = function(p, localState, name) {
+		return sample(Bernoulli({p: p}));
+	}
+
+	var _discrete3 = function(p0, p1, p2, localState, name) {
+		return sample(Discrete({ps: [p0, p1, p2]}));
+	}
+
+	var group = function(lst, n) {
+		return (lst.length === 0) ? [] : cons(lst.slice(0, n), group(lst.slice(n), n));
+	};
+
+	var _gaussian = function (mu, sigma, localState, name) {
+		return sample(Gaussian({mu: mu, sigma: sigma}));
+	}
+
+	var addBranch = function(newbranch, currState) {
+		// Update model state
+		globalStore.geo = {
+			type: 'branch',
+			branch: newbranch,
+			next: globalStore.geo,
+			parent: currState.prevBranch,
+			n: globalStore.geo ? globalStore.geo.n + 1 : 1
+		};
+		globalStore.bbox = globalStore.bbox.clone().union(utils.bboxes.branch(newbranch));
+
+		// Add new heuristic factor
+		targetFactor();
+	};
+
+	var addLeaf = function(newleaf, currState) {
+		// Update model state
+		globalStore.geo = {
+			type: 'leaf',
+			leaf: newleaf,
+			next: globalStore.geo,
+			parent: currState.prevBranch,
+			n: globalStore.geo ? globalStore.geo.n + 1 : 1
+		};
+		globalStore.bbox = globalStore.bbox.clone().union(utils.bboxes.leaf(newleaf));
+
+		// Add new heuristic factor
+		targetFactor();
+	};
+
+	var addFlower = function(newflower, currState) {
+		// Update model state
+		globalStore.geo = {
+			type: 'flower',
+			flower: newflower,
+			next: globalStore.geo,
+			parent: currState.prevBranch,
+			n: globalStore.geo ? globalStore.geo.n + 1 : 1
+		};
+		globalStore.bbox = globalStore.bbox.clone().union(utils.bboxes.flower(newflower));
+
+		// Add new heuristic factor
+		targetFactor();
+	}
+
+
+	var initialWidth = 0.75;
+	var widthDecay = 0.975;
+	var minWidthPercent = 0.15;
+	var minWidth = minWidthPercent*initialWidth;
+	var leafAspect = 2.09859154929577;
+	var leafWidthMul = 1.3;
+	var flowerRadMul = 1;
+
+	var state = function(obj) {
+		return {
+			depth: obj.depth,
+			pos: obj.pos,
+			angle: obj.angle,
+			width: obj.width,
+			prevBranch: obj.prevBranch,
+			features: undefined
+		};
+	};
+
+	var polar2rect = function(r, theta) {
+		return utils.new(THREE.Vector2, r*Math.cos(theta), r*Math.sin(theta));
+	};
+
+	var lOpts = ['none', 'left', 'right'];
+	var lProbs = [1, 1, 1];
+	var branch = function(currState) {
+
+		// Generate new branch
+		var width = widthDecay * currState.width;
+		var length = 2;
+		var newang = currState.angle + _gaussian(0, Math.PI/8, currState, 'angle');
+		var newbranch = {
+			start: currState.pos,
+			angle: newang,
+			width: width,
+			end: polar2rect(length, newang).add(currState.pos)
+		};
+		addBranch(newbranch, currState);
+
+		var newState = state({
+			depth: currState.depth + 1,
+			pos: newbranch.end,
+			angle: newbranch.angle,
+			width: newbranch.width,
+			prevBranch: globalStore.geo
+		});
+
+		// Generate leaf?
+		future(function() {
+			var leafOpt = lOpts[_discrete3(lProbs[0], lProbs[1], lProbs[2], newState, 'leaf')];
+			if (leafOpt !== 'none') {
+				var lwidth = leafWidthMul * initialWidth;
+				var llength = lwidth * leafAspect;
+				var angmean = (leafOpt === 'left') ? Math.PI/4 : -Math.PI/4;
+				var langle = newbranch.angle + _gaussian(angmean, Math.PI/12, newState, 'leafAngle');
+				var lstart = newbranch.start.clone().lerp(newbranch.end, 0.5);
+				var lend = polar2rect(llength, langle).add(lstart);
+				var lcenter = lstart.clone().add(lend).multiplyScalar(0.5);
+				addLeaf({
+					length: llength,
+					width: lwidth,
+					angle: langle,
+					center: lcenter
+				}, newState);
+			}
+		});
+
+		// Generate flower?
+		future(function() {
+			if (_flip(0.5, newState, 'flower')) {
+				addFlower({
+					center: newbranch.end,
+					radius: flowerRadMul * initialWidth,
+					angle: newbranch.angle
+				}, newState);
+			}
+		});
+
+		// Terminate?
+		future(function() {
+			var terminateProb = 0.5;
+			if (_flip(terminateProb, newState, 'terminate')) {
+				globalStore.terminated = true;
+			} else {
+				// Generate no further branches w/ prob 1/3
+				// Generate one further branch w/ prob 1/3
+				// Generate two further branches w/ prob 1/3
+				future(function() {
+					if (!globalStore.terminated && newState.width > minWidth && _flip(0.66, newState, 'branch1')) {
+						branch(newState);
+						future(function() {
+							if (!globalStore.terminated && newState.width > minWidth && _flip(0.5, newState, 'branch2')) {
+								branch(newState);
+							}
+						});
+					}
+				});
+			}
+		});
+	};
+
+	var generate = function() {
+		var w = target.image.width;
+		var h = target.image.height;
+		globalStore.genImg = utils.new(utils.ImageData2D).fillWhite(w, h);
+		globalStore.geo = undefined;
+		globalStore.bbox = utils.new(THREE.Box2);
+
+		// Determine starting state by inverting viewport transform
+		var starting_world_pos = norm2world(target.startPos);
+		var starting_dir = target.startDir;
+		var starting_ang = Math.atan2(starting_dir.y, starting_dir.x);
+
+		// These are separated like this so that we can have an initial local
+		//    state to feed to the _gaussian for the initial angle.
+		var initState = state({
+			depth: 0,
+			pos: starting_world_pos,
+			angle: 0,
+			width: initialWidth,
+			prevBranch: undefined
+		});
+		var startState = state({
+			depth: initState.depth,
+			pos: initState.pos,
+			angle: _gaussian(starting_ang, Math.PI/6, initState, 'startAngle'),
+			width: initState.width,
+			prevBranch: initState.prevBranch
+		});
+
+		future(function() { branch(startState); });
+		finishAllFutures();
+
+		return globalStore.geo;
+	};
+
+	return generate;
+}
+
+
+// ----------------------------------------------------------------------------
+
+// var generate = makeProgram(false);
+var generate = makeProgram();
+
+// var nParticles = 100;
+var nParticles = 30;
+// var nParticles = 300;
+// var nParticles = 600;
+var dist = Infer({method: 'SMC', particles: nParticles, justSample: true, onlyMAP: true}, generate);
+var samp = sample(dist);
+var ret = {
+	viewport: viewport,
+	samp: samp
+};
+ret;
+
+
+
+
---
+++ examples/pcg/vines.py
@@ -0,0 +1,435 @@
+# Author: Paul Szerlip Converted from Original JS code:
+# https://github.com/probmods/webppl/blob/gh-pages-vinesDemoFreeSketch/demos/vines/wppl/vines.wppl
+from pdb import set_trace as bb
+import numpy as np
+import torch
+from torch.autograd import Variable as V
+from torch import Tensor as T
+import pyro.distributions as dist
+import pyro
+from collections import defaultdict
+
+
+# ----------------------------------------------------------------------------
+# Globals / constants
+
+
+def VT(x):
+    # create torch variable around tesnor with input X
+    return V(T(x))
+
+
+class Vector2():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def add(self, other):
+        self.x += other.x
+        self.y += other.y
+        return self
+
+    def clone(self):
+        return Vector2(self.x, self.y)
+
+    # linear interpolate between two points us and them
+    def lerp(self, other, zero_one):
+        self.x = zero_one * self.x + (1 - zero_one) * other.x
+        self.y = zero_one * self.y + (1 - zero_one) * other.y
+        return self
+
+    # obs multiply x,y by scalar
+    def scalar_mult(self, mult):
+        self.x = self.x * mult
+        self.y = self.y * mult
+        return self
+
+
+class BBOX2():
+    def __init__(self, v2_min, v2_max):
+        self.min = v2_min
+        self.max = v2_max
+
+    def clone(self):
+        return BBOX2(self.min.clone(), self.max.clone())
+
+    # overwrites bbox with combined union -- clone to avoid issues
+    def union(self, other):
+        print("warning, bbox union not verified. \
+                This print statement should annoy you.")
+        self.min = Vector2(min(self.min.x, other.min.x),
+                           min(self.min.y, other.min.y))
+        self.max = Vector2(max(self.max.x, other.max.x),
+                           max(self.max.y, other.max.y))
+        return self
+
+
+def norm2world(p, viewport):
+    return Vector2(	viewport.xmin + p.x * (viewport.xmax - viewport.xmin),
+                    viewport.ymin + p.y * (viewport.ymax - viewport.ymin))
+
+
+def polar2rect(r, theta):
+    return Vector2(r * torch.cos(theta), r * torch.sin(theta))
+
+
+def union_bbox(*bboxes):
+    raise NotImplementedError("Yet to combine bboxes")
+
+# ----------------------------------------------------------------------------
+# Factor encouraging similarity to target image
+
+
+def bbox_branch(new_branch):
+    raise NotImplementedError("Yet to create bbox for branch")
+
+
+def bbox_leaf(new_leaf):
+    raise NotImplementedError("Yet to create bbox for leaf")
+
+
+def bbox_flower(new_flower):
+    raise NotImplementedError("Yet to create bbox for flower")
+
+
+def bbox_for_type(type_name, type_obj):
+    if type_name == "flower":
+        return bbox_flower(type_obj)
+    elif type_name == "branch":
+        return bbox_branch(type_obj)
+    elif type_name == "leaf":
+        return bbox_leaf(type_obj)
+    else:
+        raise NotImplementedError(
+            "Unknown bbox for type: {}".format(type_name))
+
+
+def normalizedSimilarity(img, target):
+    raise NotImplementedError("Yet to measure img similarity")
+
+
+# pull from:
+# https://github.com/probmods/webppl/blob/gh-pages-vinesDemoFreeSketch/demos/vines/js/index.js
+def load_target(target_file):
+    raise NotImplementedError("Yet to load img from file")
+
+# // Render update
+
+
+def renderUpdate(geo, viewport):
+    # TODO: Render to 2D canvas via python TODO^2: Render to Unity
+    raise NotImplementedError("Yet to render to canvas")
+    # return img, viewport
+
+    # utils.rendering.drawImgToRenderContext(globalStore.genImg);
+    # utils.rendering.renderIncr(geo, viewport); globalStore.genImg =
+    # utils.rendering.copyImgFromRenderContext();
+
+
+# Basically Gaussian log-likelihood, without the constant factor
+def makescore(val, target, tightness):
+    diff = val - target
+    return - (diff**2) / (tightness**2)
+
+
+def norm_prob(probs):
+    return probs / probs.sum()
+
+# holds our procedural object
+
+
+class ProceduralVine():
+
+    def __init__(self, initial_state, bbox, viewport, target, target_factor):
+        # TODO, real type here
+        self.target = target
+        self.viewport = viewport
+        self.bbox = bbox
+        self.target_factor
+        self.terminated = False
+
+        # start angle is mu,sigma for sample -- replace is with a pyro sample
+        initial_state['angle'] = self._gaussian(
+            'start_angle', initial_state['angle'][0],
+            initial_state['angle'][1])
+
+        # set the state as an array
+        self.geo_state = [initial_state]
+
+    def _flip(self, name, p):
+        return pyro.sample(name, dist.bernoulli, p)
+
+    def _discrete3(self, name, ps):
+        return pyro.sample(name, dist.categorical, ps)
+
+    def _gaussian(self, name, mu, sigma):
+        return pyro.sample(name, dist.normal, mu, sigma)
+
+    def add_pyro_factor(self, site_name, val):
+        # TODO: Check validity -- this is approximating the factor statement in
+        # webppl with a bernoulli observe
+        logit_input = val
+
+        # factor hack!
+        if self.last_f is not None:
+            logit_input = val - self.last_f
+
+        self.last_f = val
+
+        # factor definitely happened this will add an obs to the graph and
+        # bernoulli likelihood == f^whatever*(1-f)^0 == f*log_p
+        pyro.observe(site_name, dist.bernoulli,
+                     logits=logit_input, obs=VT([1]))
+
+    # generically add the type to our state that will in turn be rendered and
+    # then measured against the target image we'll add that factor to any
+    # relevant sample sites in the model
+    def add_type(self, site_name, type_name, cur_state, new_type_obj):
+        geo = {
+            "type": type_name,
+            "next": self.last_geo,
+            "parent": cur_state["prev_branch"],
+            "n": self.last_geo["n"] + 1 if self.last_geo else 1
+        }
+        # add type object to the geo for later rendering info
+        geo[type_name] = new_type_obj
+
+        # create union of our current bounding box + new object added
+        self.bbox = self.bbox.clone().union(bbox_for_type(type_name,
+                                                          new_type_obj))
+        self.geo_state.append(geo)
+
+        # get target factor from choices
+        f, self.viewport = self.target_factor(
+            self.geo_state, self.bbox, self.viewport, self.target)
+
+        # need to add factor to previous sample sites
+        self.add_pyro_factor(site_name, f)
+
+        # mark the last geo object
+        self.last_geo = geo
+
+    def add_branch(self, site_name, cur_state, new_branch):
+        return self.add_type(site_name, 'branch', cur_state, new_branch)
+
+    def add_leaf(self, site_name, cur_state, new_leaf):
+        return self.add_type(site_name, 'leaf', cur_state, new_leaf)
+
+    def add_flower(self, site_name, cur_state, new_flower):
+        return self.add_type(site_name, 'flower', cur_state, new_flower)
+
+
+def get_state(obj):
+    return {
+        "depth": obj["depth"],
+        "pos": obj["pos"],
+        "angle": obj["angle"],
+        "width": obj["width"],
+        "prev_branch": obj["prev_branch"],
+        "features": None
+    }
+
+
+# Magic item, if you call a property, it will increment that property under
+# the hood and return that id
+class ObjectCounter(object):
+    def __init__(self):
+        self.counters = defaultdict(lambda: 0)
+        pass
+
+    # simple get attribute function return
+    def __getitem__(self, name):
+        count = self.counters[name]
+        self.counters[name] += 1
+        return "{}_{}".format(name, count)
+
+    def __getattr__(self, name):
+        return self.__getitem__(name)
+
+
+#  ----------------------------------------------------------------------------
+#  The program itself
+def main(simTightness=0.02, boundsTightness=0.001,
+         initialWidth=0.75,
+         widthDecay=0.975,
+         minWidthPercent=0.15,
+         leafAspect=2.09859154929577,
+         leafWidthMul=1.3,
+         flowerRadMul=1,
+         target_file=None):
+
+    # for now, target is a global. Later, this can be sent in batches
+    target = load_target(target_file)
+    minWidth = minWidthPercent * initialWidth,
+    start_viewport = {"xmin": -12, "xmax": 12, "ymin": -22, "ymax": 2}
+
+    def target_factor(full_state, bbox, viewport, target):
+
+        # render
+        partial_render, viewport = renderUpdate(full_state, viewport)
+
+        # Similarity factor
+        sim = normalizedSimilarity(partial_render, target)
+
+        # factor to add to observe log_pdf
+        simf = makescore(sim, 1, simTightness)
+
+        # Bounds factors
+        extraX = (max(viewport.xmin - bbox.min.x, 0) +
+                  max(bbox.max.x - viewport.xmax, 0)) / (viewport.xmax -
+                                                         viewport.xmin)
+
+        extraY = (max(viewport.ymin - bbox.min.y, 0) +
+                  max(bbox.max.y - viewport.ymax, 0)) / (viewport.ymax -
+                                                         viewport.ymin)
+
+        boundsfx = makescore(extraX, 0, boundsTightness)
+        boundsfy = makescore(extraY, 0, boundsTightness)
+
+        # full factor = sum of parts
+        f = simf + boundsfx + boundsfy
+
+        # return the likelihood scoring relative to target, and our modified
+        # viewpoint
+        return f, viewport
+
+    def model():
+
+        # create a counting object
+        oc = ObjectCounter()
+
+        # Determine starting state by inverting viewport transform
+        starting_world_pos = norm2world(target["startPos"], start_viewport)
+        starting_dir = target["startDir"]
+
+        starting_ang = torch.atan2(starting_dir["y"], starting_dir["x"])
+
+        # // These are separated like this so that we can have an initial local
+        # //    state to feed to the _gaussian for the initial angle.
+        init_state = get_state({
+            "depth": 0,
+            "pos": starting_world_pos,
+            "angle": 0,
+            "width": initialWidth,
+            "prev_branch": None
+        })
+
+        start_state = get_state({
+            "depth": init_state["depth"],
+            "pos": init_state["pos"],
+            "angle": (starting_ang, np.pi / 6),
+            "width": init_state["width"],
+            "prev_branch": init_state["prev_branch"]
+        })
+
+        # hold our current procedural vine object
+        proc_vines = ProceduralVine(start_state, BBOX2(), start_viewport,
+                                    target, target_factor)
+
+        leaf_opts = ['none', 'left', 'right']
+        leaf_probs = norm_prob(VT([1, 1, 1]))
+
+
+        # clusre around the procedural vines object
+        def create_branch(cur_state):
+
+            # calculate the width
+            width = VT([widthDecay * cur_state["width"]])
+            length = VT([2])
+
+            new_ang = cur_state["angle"] + proc_vines._gaussian(oc.angle,
+                                                                0,
+                                                                np.pi / 8)
+            new_branch = {
+                "start": cur_state["pos"],
+                "angle": new_ang,
+                "width": width,
+                "end": polar2rect(length, new_ang).add(cur_state["pos"])
+            }
+
+            proc_vines.add_branch(oc.branch_obs, cur_state, new_branch)
+
+            new_state = get_state({
+                "depth": cur_state["depth"] + 1,
+                "pos": new_branch["end"],
+                "angle": new_branch["angle"],
+                "width": new_branch["width"],
+                "prev_branch": proc_vines.last_geo
+            })
+
+            # generate leaf (no futures yet) TODO: Add futures/pyro promises
+            leaf_sopt = leaf_opts[proc_vines._discrete3(oc.leaf, leaf_probs)]
+
+            if leaf_sopt != 'none':
+                lwidth = VT([leafWidthMul * initialWidth])
+                llength = lwidth * leafAspect
+                angmean = np.pi / 4 if leaf_sopt == 'left' else -np.pi / 4
+                langle = new_branch["angle"] \
+                    + proc_vines._gaussian(oc.leaf_angle, angmean, np.pi / 12)
+
+                lstart = new_branch["start"].clone().lerp(
+                    new_branch["end"], 0.5)
+
+                lend = polar2rect(llength, langle).add(lstart)
+                lcenter = lstart.clone().add(lend).scalar_mult(0.5)
+                # observe unique leaf ix, send in the state, along with the
+                # leaf_obj
+                # new leaf added!
+                proc_vines.add_leaf(oc.leaf_obs,
+                                    new_state,
+                                    {
+                                        "length": llength,
+                                        "width": lwidth,
+                                        "angle": langle,
+                                        "center": lcenter
+                                    })
+
+            # Generate flower? TODO: Add futures/pyro flower
+            if proc_vines._flip(oc.flower, VT([0.5])):
+                proc_vines.add_flower(oc.flower_obs, new_state, {
+                    "center": new_branch["end"],
+                    "radius": flowerRadMul * initialWidth,
+                    "angle": new_branch["angle"]
+                })
+
+            # Terminate? TODO: Add futures/pyro terminate
+            terminate_prob = 0.5
+            # do we terminate here and now?
+            if proc_vines._flip(oc.terminate,
+                                VT([terminate_prob])):
+                proc_vines.terminated = True
+            else:
+                # not done terminating yet!
+                # Generate no further branches w/ prob 1/3
+                # Generate one further branch w/ prob 1/3
+                # Generate two further branches w/ prob 1/3
+                # TODO: Add as future
+                # pyro.future(lambda x: )
+                if not proc_vines.terminated \
+                        and new_state["width"] > minWidth \
+                        and proc_vines._flip(oc.reup_branch_one, VT([0.66])):
+                    # create another branch!
+                    create_branch(new_state)
+
+                    # TODO: Add as future pyro.future(lambda x: )
+                    if not proc_vines.terminated and \
+                            new_state["width"] > minWidth and \
+                            proc_vines._flip(oc.reup_branch_two, VT([0.5])):
+
+                        # if we fliipped yes, keep the good times going
+                        create_branch(new_state)
+
+    # now we define our inference alg
+    bb()
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    # parser.add_argument('-cfg', "--config_file", type=str, required=True,
+    # help='master configuration file for running the training experiments')
+    args = parser.parse_args()
+
+    # turn args into a list of kwargs sent to main
+    main(**vars(args))
