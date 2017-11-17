4c4d7afac65b159d710a7bf3bec6e8b93537a9f7
Fritz Obermeyer <fritzo@uber.com>
2017-11-17 10:52:15 -0800

    Add MAP to pyro/infer/__init__.py

--- pyro/infer/__init__.py
+++ pyro/infer/__init__.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, division, print_function
 from pyro.infer.abstract_infer import Marginal, TracePosterior
 from pyro.infer.elbo import ELBO
 from pyro.infer.importance import Importance
+from pyro.infer.map import MAP
 from pyro.infer.search import Search
 from pyro.infer.svi import SVI
 
