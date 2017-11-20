a8280afb4c351488784f01b0e3d8569d498652d7
Paul Szerlip <pas@uber.com>
2017-11-20 08:22:40 -0800

    cloning works, but release no longer works. I believe what's happening is that they share the manager objects.
    Instead, each manager needs to be created, and then fetch the object of interest to read. That means that each thread, when released, will have their own local socket that won't be closed on clone.
    
    More expensive? Yes. But may solve the weird proxy object failures (the only known error for this approach right now).

--- examples/process/simple_fork.py
+++ examples/process/simple_fork.py
@@ -8,8 +8,8 @@ from multiprocessing import Process, Manager, Semaphore, Lock, Queue, Array
 # from arrow import get as aget
 from arrow import now as anow
 from uuid import uuid4
-from numpy.random import normal, seed
-
+from numpy.random import normal, seed, choice
+from collections import defaultdict
 
 def get_uuid():
     return uuid4().hex
@@ -18,39 +18,9 @@ def get_uuid():
 def sample(mu, sigma):
     return normal(mu, sigma)
 
-def store_fork_continue(*args, **kwargs):
-    manager = kwargs['manager']
-    trace_dict = kwargs['trace_dict']
-    fct_key = kwargs['fct_key']
-    mu_sigma = kwargs['fct_args']
-
-    shared_semaphore = manager.Semaphore(0)
-
-    if fct_key not in trace_dict and local_uuid not in trace_dict[fct_key]:
-        local_uuid = get_uuid()
-        # mark this spot with a uuid
-        trace_dict[fct_key] = {'uuid': local_uuid}
-
-    # child of the fork
-    if pid == 0:
-        shared_semaphore.acquire()
-        # run it again
-        p = mp.Process(target=store_fork_continue, args=args, kwargs=kwargs)
-        p.start()
-        # store_fork_continue(*args, **kwargs)
-
-    # if not child, we continue on the way
-    else:
-        trace_dict[fct_key]['thread'] = shared_semaphore
-        trace_dict[fct_key]['value'] = sample(*mu_sigma)
-
-
-def foo(local_uuid, lock, shared_dict, manager, main_exit=False):
+def fork(fct_key, local_uuid, trace_dict, shared_dict, thread_dict, manager,
+         clone_op=False, clone_thread=None):
     print("Fork for your life {}".format(os.getpid()))
-    if local_uuid not in shared_dict:
-        shared_dict[local_uuid] = {'value': 0, 'thread': None}
-        # : manager.list()}
-
     pid = os.fork()
     seed()
     print("Post forking {}".format(pid))
@@ -58,61 +28,165 @@ def foo(local_uuid, lock, shared_dict, manager, main_exit=False):
     if pid:
 
         print("Main so hot post pid right now")
-        with lock:
-            try:
-                print("in lock")
-                # old_val = shared_dict.pop(local_uuid, None)
-                # print("old val eradticated")
-                shared_dict.update({local_uuid: {'value': sample(0, 1)}})
-                print("val changed")
-                # shared_dict[local_uuid]['thread'] = shared_semaphore
-                # shared_dict.update({local_uuid: {'value': sample(0, 1), 'thread': shared_semaphore}})
-                print("shared dict updated")
-            except Exception as e:
-                print("Issue with update {}".format(e))
+        if not clone_op:
+            # with lock:
+            #     try:
+            print("in lock")
+            # sample and store to the function key
+            trace_dict[fct_key] = sample(0, 1)
+            # old_val = shared_dict.pop(local_uuid, None)
+            # print("old val eradticated")
+            shared_dict.update({local_uuid: trace_dict})
+            print("val changed")
+            # shared_dict[local_uuid]['thread'] = shared_semaphore
+            # shared_dict.update({local_uuid: {'value': sample(0, 1), 'thread': shared_semaphore}})
+            print("shared dict updated")
+            # except Exception as e:
+            #     print("Issue with update {}".format(e))
         # print("Main coming through: {}".format(shared_dict[local_uuid]))
 
-        # print("Waiting on child")
-        print("Finished waitin, killing main")
-        # os.waitpid(pid, 0)
-        # print("Finished waitin, doing sumtin")
-        os._exit(0)
+            # print("Waiting on child")
+            print("Finished waitin, killing main")
+            # os.waitpid(pid, 0)
+            # print("Finished waitin, doing sumtin")
+            os._exit(0)
         # if main_exit:
         #     print("waiting then killing main {}".format(pid))
         #     os._exit(0)
 
     else:
         print("semaphore purgatory")
-        shared_semaphore = manager.Semaphore(0)
+        if not clone_op:
+            shared_semaphore = manager.Semaphore(0)
 
-        shared_dict.update({local_uuid + "_thread": {'thread': shared_semaphore}})
+            # thread_list = manager.list()
+            # thread_list.append(shared_semaphore)
 
-        # time.sleep(0)
-        shared_semaphore.acquire()
-        # print("Child fork, dying after release")
-        # os._exit(0)
-        print("Child fork, forking after release")
-        # # call in again plz -- why stop the fun?
-        foo(local_uuid, lock, shared_dict, manager, main_exit=True)
+            # want to continue from here?
+            thread_dict.update({local_uuid: shared_semaphore})
 
-        print("Post foo existance")
-        os._exit(0)
+            # time.sleep(0)
+            time.sleep(1)
+            shared_semaphore.acquire()
+            clone_count = thread_dict[local_uuid]
 
+            for i in range(clone_count):
+                print("Cloning {}_{}".format(local_uuid, i))
+                fork(fct_key, local_uuid, trace_dict, shared_dict, thread_dict, manager,
+                     clone_op=True, clone_thread=i)
+
+            print("Killing the clone loop")
+            os._exit(0)
+
+        else:
+            clone_id = "{}_{}".format(local_uuid, clone_thread)
+            print("In cloning op {}".format(clone_id))
+
+            # when you're the clone operation,
+            shared_semaphore = manager.Semaphore(0)
+            print("updating thread dict {} - {}".format(clone_id, len(shared_dict)))
+            thread_dict.update({clone_id: shared_semaphore})
+            # thread_dict[local_uuid]['threads'].append(shared_semaphore)
+            # thread_dict.update({clone_id: {'thread': shared_semaphore}})
+
+            print("HACK to finish the update across the wire-- this needs to be fixed")
+            time.sleep(1)
+
+            # while True:
+            # print("finished thread dict {} - {}".format(clone_id, len(shared_dict)))
+            print("finished 2thread dict {} - {}".format(clone_id, len(shared_dict)))
+            # gg = thread_dict[local_uuid]
+            # print("TH type {}".format(thread_list))
+            # with thread_list:
+            # gg['threads'].append(shared_semaphore)
+            # clone_thread.append(shared_semaphore)
+            # thread_dict.update({local_uuid: gg})
+            shared_semaphore.acquire()
+
+            print("HACK to finish the update post release")
+            time.sleep(1)
+            # print("Child fork, dying after release")
+            # os._exit(0)
+            print("Cloned accordingly, then wait for the fork")
+            # # call in again plz -- why stop the fun?
+            fork(fct_key, local_uuid, trace_dict, shared_dict, thread_dict, manager)
+
+            print("Post fork existance")
+            os._exit(0)
+
+
+def model(shared_traces, shared_threads, manager):
+
+    # go until you hit the first
+    # faux tracing
+    trace_dict = {}
+
+    uuid = get_uuid()
+
+    # call fork for our first "sample"
+    fork("first", uuid, trace_dict, shared_traces, shared_threads, manager)
 
 
 def main(*args, **kwargs):
     mp.set_start_method('fork')
     manager = Manager()
-    shared_obj = manager.dict()
+
+    shared_traces = manager.dict()
+    shared_threads = manager.dict()
+    # shared_obj = manager.dict()
     all_processes = []
-    lock = Lock()
 
 
     def run_and_kill():
-        p = mp.Process(target=foo, args=(get_uuid(), lock, shared_obj, manager,))
+        p = mp.Process(target=model, args=(shared_traces, shared_threads, manager,))
         p.start()
         all_processes.append(p)
         # p.join()
+
+
+    num_particles = 10
+    shared_traces.clear()
+    shared_threads.clear()
+
+    for i in range(num_particles):
+        run_and_kill()
+
+    while len(shared_traces.keys()) < num_particles or \
+            len(shared_threads.keys()) < num_particles:
+        # print("Waiting on traces")
+        pass
+
+    all_keys = list(shared_threads.keys())
+    all_threads = dict(**shared_threads)
+    num_clones = 20
+    clone_counts = defaultdict(lambda: 0)
+    for nn in range(num_clones):
+        sel = choice(all_keys)
+
+        # count the number of forks
+        clone_counts[sel] += 1
+
+    # got our previous
+    prev_shared = len(shared_threads)
+
+    # update with new info
+    shared_threads.update(dict(**clone_counts))
+
+    # now go through and call release for the previous threads
+    for key in clone_counts:
+        all_threads[key].release()
+
+    print("Waiting on copies {} - {}: clones {}".format(
+                                                len(shared_threads),
+                                                prev_shared,
+                                                num_clones))
+    # wait for the clone
+    while len(shared_threads) - prev_shared < num_clones:
+        pass
+
+
+
+    bb()
 
 
     reply = 'c'
@@ -124,7 +198,7 @@ def main(*args, **kwargs):
         if reply == 'f':
             print("running model fct")
             run_and_kill()
-            print(repr(shared_obj))
+            # print(repr(shared_obj))
 
         reply = input("f for function call / e for exit\n")
 
@@ -134,14 +208,14 @@ def main(*args, **kwargs):
     #     run_and_kill()
 
     # time.sleep(3)
-    for i in range(10):
-        obj_threads = [key for key in shared_obj.keys() if '_thread' in key]
-        o1 = obj_threads[0]
-        print('sh : {}'.format(shared_obj[o1.replace('_thread', '')]))
-        time.sleep(1)
-        shared_obj[o1]['thread'].release()
+    # for i in range(10):
+    #     obj_threads = [key for key in shared_obj.keys() if '_thread' in key]
+    #     o1 = obj_threads[0]
+    #     print('sh : {}'.format(shared_obj[o1.replace('_thread', '')]))
+    #     time.sleep(1)
+    #     shared_obj[o1]['thread'].release()
 
-    time.sleep(1)
+    # time.sleep(1)
     print("wait all")
     for mz in all_processes:
         mz.join()
