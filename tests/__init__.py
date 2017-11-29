960e75dabd2f03c6f24a823fa97884ecbcb5c47b
Neeraj Pradhan <npradhan@uber.com>
2017-11-28 22:52:33 -0800

    tweak format

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,7 +6,7 @@ logger = logging.getLogger(__name__)
 
 # create log handler for tests
 handler = logging.StreamHandler()
-formatter = logging.Formatter('%(levelname).1s %(name)s \n %(message)s')
+formatter = logging.Formatter('%(levelname).1s %(name)s \t %(message)s')
 handler.setFormatter(formatter)
 
 # set default logging level for tests
