27f057707dce0ad9ec05ff86ab5db747fa0e04a3
Adrian Dawid <adriandwd@gmail.com>
2017-12-17 00:58:42 +0100

    Implement batch_shape and improve log_pdf

--- example.py
+++ example.py
@@ -1,17 +1,23 @@
 import torch
 import pyro
 import matplotlib.pyplot as plt
+import numpy as np
 from torch.autograd import Variable
 
-Sigma = Variable(10 * torch.ones(1,1))
-mu = Variable(-2.4 * torch.ones(1))
+Sigma = Variable(1 * torch.ones(1,1))
+mu = Variable(0 * torch.ones(1))
 
 gaussian = pyro.distributions.MultivariateNormal(mu, Sigma)
-in_ = Variable(torch.arange(-10,10,0.01).unsqueeze(1))
+in_ = Variable(torch.arange(-10, 10, 0.01).unsqueeze(1))
 plt.plot(in_.data.numpy(), torch.exp(gaussian.batch_log_pdf(in_)).data.numpy())
+plt.plot(in_.data.numpy(), 1/np.sqrt(np.pi)*torch.exp(-in_**2/2).data.numpy())
 plt.show()
 
 Sigma = Variable(10 * torch.eye(2))
 mu = Variable(-2.4 * torch.ones(2))
 gaussian = pyro.distributions.MultivariateNormal(mu, Sigma)
 in_ = Variable(torch.rand(2000, 2) * 2 - 1)
+
+data = gaussian.sample(50000).data.numpy()
+plt.hist2d(data[:, 0], data[:, 1], bins=100)
+plt.show()
--- pyro/distributions/multivariate_normal.py
+++ pyro/distributions/multivariate_normal.py
@@ -19,7 +19,7 @@ class MultivariateNormal(Distribution):
     :param is_cholesky: Should be set to True if you want to directly pass a cholesky decomposition as sigma.
     """
 
-    def __init__(self, mu, sigma, batch_size=None, is_cholesky = False, use_inverse_for_batch_log=False, *args, **kwargs):
+    def __init__(self, mu, sigma, batch_size=None, is_cholesky=False, use_inverse_for_batch_log=False, *args, **kwargs):
         self.mu = mu
         self.output_shape = mu.shape
         self.use_inverse_for_batch_log = use_inverse_for_batch_log
@@ -28,10 +28,12 @@ class MultivariateNormal(Distribution):
             # potrf is the very sensible name for the Cholesky decomposition in PyTorch
             self.sigma_cholesky = torch.potrf(sigma)
         else:
-            self.sigma = sigma.transpose(0,1) @ sigma
+            self.sigma = sigma.transpose(0, 1) @ sigma
             self.sigma_cholesky = sigma
         if mu.dim() > 1:
             raise ValueError("The mean must be a vector, but got mu.size() = {}".format(mu.size()))
+        if not sigma.dim() == 2:
+            raise ValueError("The covariance matrix must be a matrix, but got sigma.size() = {}".format(mu.size()))
 
         super(MultivariateNormal, self).__init__(*args, **kwargs)
 
@@ -39,28 +41,44 @@ class MultivariateNormal(Distribution):
         """
         Ref: :py:meth:`pyro.distributions.distribution.Distribution.batch_shape`
         """
-        raise NotImplementedError()
+        mu = self.mu
+        if x is not None:
+            if x.size()[-1] != mu.size()[0]:
+                raise ValueError("The event size for the data and distribution parameters must match.\n"
+                                 "Expected x.size()[-1] == self.mu.size()[0], but got {} vs {}".format(
+                    x.size(-1), mu.size(-1)))
+            try:
+                mu = self.mu.expand_as(x)
+            except RuntimeError as e:
+                raise ValueError("Parameter `mu` with shape {} is not broadcastable to "
+                                 "the data shape {}. \nError: {}".format(mu.size(), x.size(), str(e)))
+        return mu.size()
 
     def event_shape(self):
         """
         Ref: :py:meth:`pyro.distributions.distribution.Distribution.event_shape`
         """
-        raise NotImplementedError()
+        return self.mu.size()
 
-    def sample(self):
+    def sample(self, n=1):
         """
         A classic multivariate normal sampler.
 
+        :param n: The number of samples to be drawn. Defaults to 1.
         Ref: :py:meth:`pyro.distributions.distribution.Distribution.sample`
         """
-        uncorrelated_standard_sample = Variable(torch.randn(self.mu.size()).type_as(self.mu.data))
+        uncorrelated_standard_sample = Variable(torch.randn(n, *self.mu.size()).type_as(self.mu.data))
         transformed_sample = self.mu + uncorrelated_standard_sample @ self.sigma_cholesky
-        return transformed_sample
+        return transformed_sample if not n == 1 else transformed_sample.squeeze(0)
 
     def batch_log_pdf(self, x):
         batch_size = x.size()[0]
-        normalization_factor = 0.5 * torch.log(self.sigma_cholesky.diag().prod()) + (self.mu.shape[0] / 2) * np.log(np.pi)
-        return -(normalization_factor + 0.5 * torch.sum((x-self.mu).unsqueeze(2) * torch.bmm(torch.potri(self.sigma_cholesky).expand(batch_size, *self.sigma_cholesky.size()), (x-self.mu).view(*x.size(), 1)), 1))
+        normalization_factor = 0.5 * torch.log(self.sigma_cholesky.diag()).sum() + (self.mu.shape[0] / 2) * np.log(
+            np.pi)
+        print(normalization_factor)
+        return -(normalization_factor + 0.5 * torch.sum((x - self.mu).unsqueeze(2) * torch.bmm(
+            torch.potri(self.sigma_cholesky).expand(batch_size, *self.sigma_cholesky.size()),
+            (x - self.mu).view(*x.size(), 1)), 1))
 
     def analytic_mean(self):
         """
