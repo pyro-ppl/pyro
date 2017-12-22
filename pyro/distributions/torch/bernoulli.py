Fix overflow issues with torch wrappers

pyro/distributions/torch/bernoulli.py
@@ -1,11 +1,10 @@
 from __future__ import absolute_import, division, print_function
 
 import torch
-import torch.nn.functional as F
 
 from pyro.distributions.bernoulli import Bernoulli as _Bernoulli
 from pyro.distributions.torch_wrapper import TorchDistribution
-from pyro.distributions.util import copy_docs_from, get_clamping_buffer
+from pyro.distributions.util import copy_docs_from, get_clamped_probs
 
 
 @copy_docs_from(_Bernoulli)
@@ -13,14 +12,11 @@ class Bernoulli(TorchDistribution):
     enumerable = True
 
     def __init__(self, ps=None, logits=None, *args, **kwargs):
-        if (ps is None) == (logits is None):
-            raise ValueError("Got ps={}, logits={}. Either `ps` or `logits` must be specified, "
-                             "but not both.".format(ps, logits))
-        if ps is None:
-            ps = F.sigmoid(logits)
-        eps = get_clamping_buffer(ps)
-        ps = ps.clamp(min=eps, max=1-eps)
+        ps = get_clamped_probs(ps, logits, is_multidimensional=False)
         torch_dist = torch.distributions.Bernoulli(ps)
         x_shape = ps.size()
         event_dim = 1
         super(Bernoulli, self).__init__(torch_dist, x_shape, event_dim, *args, **kwargs)
+
+    def enumerate_support(self):
+        return self.torch_dist.enumerate_support().float()

pyro/distributions/torch/categorical.py
@@ -4,7 +4,7 @@ import torch
 
 from pyro.distributions.categorical import Categorical as _Categorical
 from pyro.distributions.torch_wrapper import TorchDistribution
-from pyro.distributions.util import copy_docs_from, get_probs_and_logits
+from pyro.distributions.util import copy_docs_from, get_clamped_probs
 
 
 @copy_docs_from(_Categorical)
@@ -12,7 +12,7 @@ class Categorical(TorchDistribution):
     enumerable = True
 
     def __init__(self, ps=None, logits=None, *args, **kwargs):
-        ps, logits = get_probs_and_logits(ps, logits, is_multidimensional=True)
+        ps = get_clamped_probs(ps, logits, is_multidimensional=True)
         torch_dist = torch.distributions.Categorical(ps)
         x_shape = ps.shape[:-1] + (1,)
         event_dim = 1

pyro/distributions/torch/one_hot_categorical.py
@@ -5,7 +5,7 @@ from torch.autograd import Variable
 
 from pyro.distributions.one_hot_categorical import OneHotCategorical as _OneHotCategorical
 from pyro.distributions.torch_wrapper import TorchDistribution
-from pyro.distributions.util import copy_docs_from, get_probs_and_logits
+from pyro.distributions.util import copy_docs_from, get_clamped_probs
 
 
 @copy_docs_from(_OneHotCategorical)
@@ -13,7 +13,7 @@ class OneHotCategorical(TorchDistribution):
     enumerable = True
 
     def __init__(self, ps=None, logits=None, *args, **kwargs):
-        ps, logits = get_probs_and_logits(ps, logits, is_multidimensional=True)
+        ps = get_clamped_probs(ps, logits, is_multidimensional=True)
         torch_dist = torch.distributions.Categorical(ps)  # TODO switch to OneHotCategorical
         x_shape = ps.shape
         event_dim = 1