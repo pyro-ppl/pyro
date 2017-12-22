591628d8a595227d339eecaf1b932b2756b23fbe
Fritz Obermeyer <fritzo@uber.com>
2017-12-21 20:26:21 -0800

    Get torch Categorical working (except for overflow)

--- pyro/distributions/torch/categorical.py
+++ pyro/distributions/torch/categorical.py
@@ -4,7 +4,7 @@ import torch
 
 from pyro.distributions.categorical import Categorical as _Categorical
 from pyro.distributions.torch_wrapper import TorchDistribution
-from pyro.distributions.util import copy_docs_from
+from pyro.distributions.util import copy_docs_from, get_probs_and_logits
 
 
 @copy_docs_from(_Categorical)
@@ -12,28 +12,25 @@ class Categorical(TorchDistribution):
     enumerable = True
 
     def __init__(self, ps=None, logits=None, *args, **kwargs):
-        if logits is not None:
-            ps = torch.exp(logits - torch.max(logits))
-            ps /= ps.sum(-1, True)
+        ps, logits = get_probs_and_logits(ps, logits, is_multidimensional=True)
         torch_dist = torch.distributions.Categorical(ps)
         x_shape = ps.shape[:-1] + (1,)
         event_dim = 1
         super(Categorical, self).__init__(torch_dist, x_shape, event_dim, *args, **kwargs)
-        self._work_around_lack_of_scalar = (ps.dim() == 1 and ps.sum().dim() == 1)
 
     def sample(self):
         x = self.torch_dist.sample(self._sample_shape).float()
         return x.view(self._sample_shape + self._x_shape)
 
     def batch_log_pdf(self, x):
-        # if not self._work_around_lack_of_scalar:
-        #     x = x.squeeze(-1)
-        batch_log_pdf = self.torch_dist.log_prob(x.long())
+        log_pxs = self.torch_dist.log_prob(x.squeeze(-1)).unsqueeze(-1)
+        batch_log_pdf_shape = self.batch_shape(x) + (1,)
+        batch_log_pdf = torch.sum(log_pxs, -1).contiguous().view(batch_log_pdf_shape)
         if self.log_pdf_mask is not None:
             batch_log_pdf = batch_log_pdf * self.log_pdf_mask
         return batch_log_pdf
 
     def enumerate_support(self):
         values = self.torch_dist.enumerate_support().float()
-        return values.view((values.shape[0],) + self._x_shape)
-        # return self.torch_dist.enumerate_support().unsqueeze(-1).float()
+        sample_shape = (self.torch_dist.probs.shape[-1],)
+        return values.view(sample_shape + self._x_shape)
