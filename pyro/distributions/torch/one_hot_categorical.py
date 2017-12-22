19e265b09417b8ceae97cea4680a63aa11b72b44
Fritz Obermeyer <fritzo@uber.com>
2017-12-21 20:15:04 -0800

    Get torch OneHotCategorical working (except for overflow)

--- pyro/distributions/torch/one_hot_categorical.py
+++ pyro/distributions/torch/one_hot_categorical.py
@@ -5,7 +5,7 @@ from torch.autograd import Variable
 
 from pyro.distributions.one_hot_categorical import OneHotCategorical as _OneHotCategorical
 from pyro.distributions.torch_wrapper import TorchDistribution
-from pyro.distributions.util import copy_docs_from
+from pyro.distributions.util import copy_docs_from, get_probs_and_logits
 
 
 @copy_docs_from(_OneHotCategorical)
@@ -13,30 +13,32 @@ class OneHotCategorical(TorchDistribution):
     enumerable = True
 
     def __init__(self, ps=None, logits=None, *args, **kwargs):
-        if logits is not None:
-            ps = torch.exp(logits - torch.max(logits))
-            ps /= ps.sum(-1, True)
+        ps, logits = get_probs_and_logits(ps, logits, is_multidimensional=True)
         torch_dist = torch.distributions.Categorical(ps)  # TODO switch to OneHotCategorical
         x_shape = ps.shape
         event_dim = 1
         super(OneHotCategorical, self).__init__(torch_dist, x_shape, event_dim, *args, **kwargs)
-        self._work_around_lack_of_scalar = (ps.dim() == 1 and ps.sum().dim() == 1)
 
     def sample(self):
-        indices = self.torch_dist.sample(self._sample_shape).data
-        if not self._work_around_lack_of_scalar:
-            indices = indices.unsqueeze(-1)
         ps = self.torch_dist.probs.data
         zero = ps.new(self._sample_shape + ps.shape).zero_()
+        indices = super(OneHotCategorical, self).sample().data
+        if indices.dim() < zero.dim():
+            indices = indices.unsqueeze(-1)
         one_hot = zero.scatter_(-1, indices, 1)
         return Variable(one_hot)
 
     def batch_log_pdf(self, x):
-        return self.torch_dist.log_prob(x.max(-1)[1])
+        batch_log_pdf_shape = self.batch_shape(x) + (1,)
+        log_pxs = self.torch_dist.log_prob(x.max(-1)[1])
+        batch_log_pdf = log_pxs.view(batch_log_pdf_shape)
+        if self.log_pdf_mask is not None:
+            batch_log_pdf = batch_log_pdf * self.log_pdf_mask
+        return batch_log_pdf
 
     def enumerate_support(self):
         ps = self.torch_dist.probs.data
-        n = ps.Size(-1)
+        n = ps.shape[-1]
         values = torch.eye(n, out=ps.new(n, n))
         values = values.view((n,) + (1,) * (ps.dim() - 1) + (n,))
-        return values.expand((n,) + ps.shape)
+        return Variable(values.expand((n,) + ps.shape))
