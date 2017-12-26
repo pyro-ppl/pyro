69f20b2c23e8c4f26112c6512624c60d7d31ec8d
Adrian Dawid <adriandwd@gmail.com>
2017-12-26 21:44:32 +0100

    Update MultivariateNormal  interface

--- pyro/distributions/multivariate_normal.py
+++ pyro/distributions/multivariate_normal.py
@@ -4,6 +4,7 @@ import numpy as np
 import torch
 from torch.autograd import Variable
 
+from pyro.distributions.util import copy_docs_from
 from pyro.distributions.distribution import Distribution
 
 
@@ -11,6 +12,7 @@ def potri_compat(var):
     return Variable(torch.potri(var.data)) if torch.__version__ < '0.3.0' else torch.potri(var)
 
 
+@copy_docs_from(Distribution)
 class MultivariateNormal(Distribution):
     """
     Multivariate normal (Gaussian) distribution.
@@ -18,43 +20,52 @@ class MultivariateNormal(Distribution):
     A distribution over vectors in which all the elements have a joint
     Gaussian density.
 
-    :param torch.autograd.Variable mu: Mean. Must be a vector (Variable containing a 1d Tensor).
-    :param torch.autograd.Variable sigma: Covariance matrix. Must be symmetric and positive semidefinite.
-    :param is_cholesky: Should be set to True if you want to directly pass a Cholesky decomposition
-        of the covariance matrix as `sigma`.
+    :param torch.autograd.Variable loc: Mean. Must be a vector (Variable containing a 1d Tensor).
+    :param torch.autograd.Variable covariance_matrix: Covariance matrix. Must be symmetric and positive semidefinite.
+    :param torch.autograd.Variable scale_tril: The Cholesky decomposition of the covariance matrix. You can pass this
+            instead of `covariance_matrix`.
     :param use_inverse_for_batch_log: If this is set to true, the torch.inverse function will be used to compute
-        log_pdf. This means that the results of log_pdf can be differentiated with respect to sigma.
-        Since the gradient of torch.potri is currently not implemented, differentiation of log_pdf wrt. sigma is not
-        possible when using the Cholesky decomposition. Using the Cholesky decomposition is however much faster and
-        therefore enabled by default.
-    :raises: ValueError if the shape of mean or sigma is not supported.
+        log_pdf. This means that the results of log_pdf can be differentiated with respect to the covariance matrix.
+        Since the gradient of torch.potri is currently not implemented, differentiation of log_pdf wrt. covariance
+        matrix is not possible when using the Cholesky decomposition. Using the Cholesky decomposition is however much
+        faster and therefore enabled by default.
+    :param normalized: If set to `False` the normalization constant is omitted in the results of batch_log_pdf and
+            log_pdf. This might be preferable, as computing the determinant of the covariance matrix might not
+            always be numerically stable. Defaults to `True`.
+    :raises: ValueError if the shape of any parameter is not supported.
     """
 
     reparameterized = True
 
-    def __init__(self, mu, sigma, batch_size=None, is_cholesky=False, use_inverse_for_batch_log=False, *args, **kwargs):
-        self.mu = mu
-        self.output_shape = mu.size()
+    def __init__(self, loc, covariance_matrix, scale_tril=None, batch_size=None, use_inverse_for_batch_log=False,
+                 normalized=True, *args, **kwargs):
+        self.mu = loc
+        self.output_shape = loc.size()
         self.use_inverse_for_batch_log = use_inverse_for_batch_log
+        self.normalized = normalized
         self.batch_size = batch_size if batch_size is not None else 1
-        if not is_cholesky:
-            self.sigma = sigma
-            self.sigma_cholesky = Variable(torch.potrf(sigma.data)) if torch.__version__ < '0.3.0' else torch.potrf(
-                sigma)
+        if scale_tril is not None and covariance_matrix is not None:
+            raise ValueError("Only either 'covariance_matrix' or 'scale_tril' can be passed at once.")
+        if covariance_matrix is not None:
+            if not covariance_matrix.dim() == 2:
+                raise ValueError("The covariance matrix must be a matrix, but got covariance_matrix.size() = {}".format(
+                                    loc.size()))
+            self.sigma = covariance_matrix
+            self.sigma_cholesky = Variable(
+                torch.potrf(covariance_matrix.data)) if torch.__version__ < '0.3.0' else torch.potrf(covariance_matrix)
         else:
-            self.sigma = torch.mm(sigma.transpose(0, 1), sigma)
-            self.sigma_cholesky = sigma
-        if mu.dim() > 1:
-            raise ValueError("The mean must be a vector, but got mu.size() = {}".format(mu.size()))
-        if not sigma.dim() == 2:
-            raise ValueError("The covariance matrix must be a matrix, but got sigma.size() = {}".format(mu.size()))
+            if not scale_tril.dim() == 2:
+                raise ValueError("The Cholesky decomposition of the covariance matrix must be a matrix, "
+                                 "but got scale_tril.size() = {}".format(
+                                    loc.size()))
+            self.sigma = torch.mm(scale_tril.transpose(0, 1), scale_tril)
+            self.sigma_cholesky = scale_tril
+        if loc.dim() > 1:
+            raise ValueError("The mean must be a vector, but got loc.size() = {}".format(loc.size()))
 
         super(MultivariateNormal, self).__init__(*args, **kwargs)
 
     def batch_shape(self, x=None):
-        """
-        Ref: :py:meth:`pyro.distributions.distribution.Distribution.batch_shape`
-        """
         mu = self.mu.expand(self.batch_size, *self.mu.size()).squeeze(0)
         if x is not None:
             if x.size()[-1] != mu.size()[-1]:
@@ -70,15 +81,12 @@ class MultivariateNormal(Distribution):
         return mu.size()[:-1]
 
     def event_shape(self):
-        """
-        Ref: :py:meth:`pyro.distributions.distribution.Distribution.event_shape`
-        """
         return self.mu.size()[-1:]
 
     def sample(self):
         """
         Generate a sample with the specified covariance matrix and mean.
-        Differentiation wrt. to Sigma is only supported on PyTorch version 0.3.0 or higher.
+        Differentiation wrt. to the covariance matrix is only supported on PyTorch version 0.3.0 or higher.
         Ref: :py:meth:`pyro.distributions.distribution.Distribution.sample`
 
         """
@@ -87,23 +95,12 @@ class MultivariateNormal(Distribution):
         transformed_sample = self.mu + torch.mm(uncorrelated_standard_sample, self.sigma_cholesky)
         return transformed_sample if self.reparameterized else transformed_sample.detach()
 
-    def batch_log_pdf(self, x, normalized=True):
-        """
-        Return the logarithm of the probability density function evaluated at x.
-
-        :param x: The points for which the log_pdf should be evaluated batched along axis 0.
-        :param normalized: If set to `False` the normalization constant is omitted in the results.
-            This might be preferable, as computing the determinant of sigma might not always be numerically stable.
-            Defaults to `True`.
-        :return: A `torch.autograd.Variable` of size `self.batch_shape(x) + (1,)`
-
-        Ref: :py:meth:`pyro.distributions.distribution.Distribution.batch_log_pdf`
-        """
+    def batch_log_pdf(self, x):
         batch_size = x.size()[0] if len(x.size()) > len(self.mu.size()) else 1
         batch_log_pdf_shape = self.batch_shape(x) + (1,)
         x = x.view(batch_size, *self.mu.size())
         normalization_factor = torch.log(
-            self.sigma_cholesky.diag()).sum() + (self.mu.size()[0] / 2) * np.log(2 * np.pi) if normalized else 0
+            self.sigma_cholesky.diag()).sum() + (self.mu.size()[0] / 2) * np.log(2 * np.pi) if self.normalized else 0
         sigma_inverse = torch.inverse(self.sigma) if self.use_inverse_for_batch_log else \
             potri_compat(self.sigma_cholesky)
         return -(normalization_factor + 0.5 * torch.sum((x - self.mu).unsqueeze(2) * torch.bmm(
@@ -111,13 +108,7 @@ class MultivariateNormal(Distribution):
             (x - self.mu).unsqueeze(-1)), 1)).view(batch_log_pdf_shape)
 
     def analytic_mean(self):
-        """
-        Ref: :py:meth:`pyro.distributions.distribution.Distribution.analytic_mean`
-        """
         return self.mu
 
     def analytic_var(self):
-        """
-        Ref: :py:meth:`pyro.distributions.distribution.Distribution.analytic_var`
-        """
         return torch.diag(self.sigma)
