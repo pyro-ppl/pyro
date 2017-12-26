b2b5a8d69d45e95fea7e9687784f630d62619783
Adrian Dawid <adriandwd@gmail.com>
2017-12-26 21:45:02 +0100

    Add Cholesky example to test

--- tests/distributions/conftest.py
+++ tests/distributions/conftest.py
@@ -79,12 +79,15 @@ continuous_dists = [
     Fixture(pyro_dist=(dist.multivariate_normal, MultivariateNormal),
             scipy_dist=sp.multivariate_normal,
             examples=[
-               {
-                 'mu': [2.0, 1.0], 'sigma': [[1.0, 0.5], [0.5, 1.0]],
-                 'test_data': [[2.0, 1.0], [9.0, 3.4]]},
+               {'loc': [2.0, 1.0], 'covariance_matrix': [[1.0, 0.5], [0.5, 1.0]], 'scale_tril':None,
+                'test_data': [[2.0, 1.0], [9.0, 3.4]]},
+                {'loc': [2.0, 1.0], 'covariance_matrix': None, 'scale_tril': [[1.0, 0.5],
+                                                                              [0, 3900231685776981/4503599627370496]],
+                 'test_data': [[2.0, 1.0], [9.0, 3.4]]}
             ],
             # This hack seems to be the best option right now, as 'sigma' is not handled well by get_scipy_batch_logpdf
-            scipy_arg_fn=lambda mu, sigma: ((), {"mean": np.array(mu), "cov": np.array([[1.0, 0.5], [0.5, 1.0]])}),
+            scipy_arg_fn=lambda loc, covariance_matrix, scale_tril:
+                ((), {"mean": np.array(loc), "cov": np.array([[1.0, 0.5], [0.5, 1.0]])}),
             prec=0.01,
             min_samples=500000),
     Fixture(pyro_dist=(dist.lognormal, LogNormal),
