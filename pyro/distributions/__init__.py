5f5a39cbe298073e80ff73b264c0c76228996d14
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2017-12-29 21:00:16 -0800

    Wrap pytorch Uniform and Cauchy distributions (#657)

--- pyro/distributions/__init__.py
+++ pyro/distributions/__init__.py
@@ -18,7 +18,6 @@ import os
 
 # TODO move these implementations upstream to torch.distributions
 from pyro.distributions.binomial import Binomial
-from pyro.distributions.cauchy import Cauchy
 from pyro.distributions.delta import Delta
 from pyro.distributions.distribution import Distribution  # noqa: F401
 from pyro.distributions.half_cauchy import HalfCauchy
@@ -27,7 +26,6 @@ from pyro.distributions.multinomial import Multinomial
 from pyro.distributions.multivariate_normal import MultivariateNormal
 from pyro.distributions.poisson import Poisson
 from pyro.distributions.random_primitive import RandomPrimitive
-from pyro.distributions.uniform import Uniform
 
 # distribution classes with working torch versions in torch.distributions
 USE_TORCH_DISTRIBUTIONS = int(os.environ.get('PYRO_USE_TORCH_DISTRIBUTIONS', 0))
@@ -35,20 +33,24 @@ if USE_TORCH_DISTRIBUTIONS:
     from pyro.distributions.torch.bernoulli import Bernoulli
     from pyro.distributions.torch.beta import Beta
     from pyro.distributions.torch.categorical import Categorical
+    from pyro.distributions.torch.cauchy import Cauchy
     from pyro.distributions.torch.dirichlet import Dirichlet
     from pyro.distributions.torch.exponential import Exponential
     from pyro.distributions.torch.gamma import Gamma
     from pyro.distributions.torch.normal import Normal
     from pyro.distributions.torch.one_hot_categorical import OneHotCategorical
+    from pyro.distributions.torch.uniform import Uniform
 else:
     from pyro.distributions.bernoulli import Bernoulli
     from pyro.distributions.beta import Beta
     from pyro.distributions.categorical import Categorical
+    from pyro.distributions.cauchy import Cauchy
     from pyro.distributions.dirichlet import Dirichlet
     from pyro.distributions.exponential import Exponential
     from pyro.distributions.gamma import Gamma
     from pyro.distributions.normal import Normal
     from pyro.distributions.one_hot_categorical import OneHotCategorical
+    from pyro.distributions.uniform import Uniform
 
 # function aliases
 bernoulli = RandomPrimitive(Bernoulli)
