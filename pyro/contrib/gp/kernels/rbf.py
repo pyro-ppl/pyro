14e42c1d9757ddf1f3e10438be3dfff8aee074c7
fehiepsi <fehiepsi@gmail.com>
2018-01-15 20:14:14 +0900

    add rbf, gpr

--- pyro/contrib/gp/kernels/rbf.py
+++ pyro/contrib/gp/kernels/rbf.py
@@ -1,19 +1,32 @@
 from __future__ import absolute_import, division, print_function
 
 import torch
-from torch.nn.parameter import Parameter
+from torch.nn import Parameter
 
 from .kernel import Kernel
-from pyro.util import ng_ones, ng_zeros
 
 
 class RBF(Kernel):
-    def __init__(self, variance=torch.ones(1), lengthscale=torch.ones(1)):
+    """
+    Implementation of RBF kernel.
+    """
+
+    def __init__(self, variance=torch.ones(1), lengthscale=torch.ones(1), active_dims=None, name="RBF"):
+        super(RBF, self).__init__(active_dims=active_dims, name=name)
         self.variance = Parameter(variance)
         self.lengthscale = Parameter(lengthscale)
 
-    def forward(self, x, z=None):
-        if z is None:
-            z = x
-        r = torch.abs(x - z)
-        return self.variance * torch.exp(0.5 * r**2 / self.lengthscale)
+    def forward(self, X, Z=None):
+        if Z is None:
+            Z = X
+        if X.size() != Z.size():
+            raise ValueError("Inputs must have the same shapes.")
+        if X.dim() == 1:
+            X = X.unsqueeze(1)
+            Z = Z.unsqueeze(1)
+
+        XX = (X ** 2).sum(1)
+        ZZ = (Z ** 2).sum(1)
+        XZ = X.mm(Z.t())    
+        r2 = XX + ZZ - 2 * XZ
+        return self.variance * torch.exp(0.5 * r2 / self.lengthscale**2)
--- tests/contrib/gp/test_model.py
+++ tests/contrib/gp/test_model.py
@@ -1,1 +1,3 @@
 from __future__ import absolute_import, division, print_function
+
+# TODO: add tests
