58875e9e8d637542af20acff7eca76a8d4ede5fc
Fritz Obermeyer <fritzo@uber.com>
2018-01-16 22:25:42 -0800

    Fix _MVN_backward()

--- pyro/distributions/testing/symmetric_mvn.py
+++ pyro/distributions/testing/symmetric_mvn.py
@@ -6,22 +6,25 @@ from pyro.distributions.util import copy_docs_from
 from torch.autograd import Function, Variable
 
 
-def _MVN_backward(white, grad_output):
+def _MVN_backward(white, sigma_cholesky, grad_output):
     grad = (grad_output.unsqueeze(-2) * white.unsqueeze(-1)).squeeze(0)
-    grad = grad + grad.transpose(-1, -2)
-    grad *= 0.5
+    white_grad = torch.mm(sigma_cholesky.t(), grad)
+    white_grad = 0.5 * (white_grad + white_grad.t())
+    grad = torch.trtrs(white_grad, sigma_cholesky, transpose=True)[0].t()
     return grad_output, torch.triu(grad)
 
 
 class _SymmetricSample(Function):
     @staticmethod
     def forward(ctx, mu, sigma_cholesky):
+        ctx.save_for_backward(sigma_cholesky)
         ctx.white = mu.new(mu.size()).normal_()
         return mu + torch.mm(ctx.white, sigma_cholesky)
 
     @staticmethod
     def backward(ctx, grad_output):
-        return _MVN_backward(Variable(ctx.white), grad_output)
+        sigma_cholesky, = ctx.saved_variables
+        return _MVN_backward(Variable(ctx.white), sigma_cholesky, grad_output)
 
 
 @copy_docs_from(MultivariateNormal)
