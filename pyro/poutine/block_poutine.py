3d10bd1dd90c5183295c2261a0487d9e61171022
Adam Scibior <adscib@gmail.com>
2018-01-22 17:09:21 -0800

    Refactor BlockPoutine to BlockMessenger

--- pyro/poutine/block_poutine.py
+++ pyro/poutine/block_poutine.py
@@ -1,36 +1,10 @@
 from __future__ import absolute_import, division, print_function
 
-from .poutine import Poutine
+from .poutine import Messenger, Poutine
 
+class BlockMessenger(Messenger):
 
-class BlockPoutine(Poutine):
-    """
-    This Poutine selectively hides pyro primitive sites from the outside world.
-
-    For example, suppose the stochastic function fn has two sample sites "a" and "b".
-    Then any poutine outside of BlockPoutine(fn, hide=["a"])
-    will not be applied to site "a" and will only see site "b":
-
-    >>> fn_inner = TracePoutine(fn)
-    >>> fn_outer = TracePoutine(BlockPoutine(TracePoutine(fn), hide=["a"]))
-    >>> trace_inner = fn_inner.get_trace()
-    >>> trace_outer  = fn_outer.get_trace()
-    >>> "a" in trace_inner
-    True
-    >>> "a" in trace_outer
-    False
-    >>> "b" in trace_inner
-    True
-    >>> "b" in trace_outer
-    True
-
-    BlockPoutine has a flexible interface that allows users
-    to specify in several different ways
-    which sites should be hidden or exposed.
-    See the constructor for details.
-    """
-
-    def __init__(self, fn,
+    def __init__(self,
                  hide_all=True, expose_all=False,
                  hide=None, expose=None,
                  hide_types=None, expose_types=None):
@@ -42,7 +16,7 @@ class BlockPoutine(Poutine):
         :param list hide_types: list of site types to hide, rest will be exposed normally
         :param list expose_types: list of site types to expose normally, rest will be hidden
 
-        Constructor for blocking poutine
+        Constructor for blocking messenger
         Default behavior: block everything (hide_all == True)
 
         A site is hidden if at least one of the following holds:
@@ -51,7 +25,7 @@ class BlockPoutine(Poutine):
         3. msg["name"] not in expose and msg["type"] not in expose_types
         4. hide_all == True
         """
-        super(BlockPoutine, self).__init__(fn)
+        super(BlockMessenger, self).__init__()
         # first, some sanity checks:
         # hide_all and expose_all intersect?
         assert (hide_all is False and expose_all is False) or \
@@ -120,7 +94,7 @@ class BlockPoutine(Poutine):
         Default sample behavior with a side effect.
         Applies self._block_up to decide whether to hide the site.
         """
-        ret = super(BlockPoutine, self)._pyro_sample(msg)
+        ret = super(BlockMessenger, self)._pyro_sample(msg)
         msg["stop"] = self._block_up(msg)
         return ret
 
@@ -132,6 +106,78 @@ class BlockPoutine(Poutine):
         Default param behavior with a side effect.
         Applies self._block_up to decide whether to hide the site.
         """
-        ret = super(BlockPoutine, self)._pyro_param(msg)
+        ret = super(BlockMessenger, self)._pyro_param(msg)
         msg["stop"] = self._block_up(msg)
         return ret
+
+
+class BlockPoutine(Poutine):
+    """
+    This Poutine selectively hides pyro primitive sites from the outside world.
+
+    For example, suppose the stochastic function fn has two sample sites "a" and "b".
+    Then any poutine outside of BlockPoutine(fn, hide=["a"])
+    will not be applied to site "a" and will only see site "b":
+
+    >>> fn_inner = TracePoutine(fn)
+    >>> fn_outer = TracePoutine(BlockPoutine(TracePoutine(fn), hide=["a"]))
+    >>> trace_inner = fn_inner.get_trace()
+    >>> trace_outer  = fn_outer.get_trace()
+    >>> "a" in trace_inner
+    True
+    >>> "a" in trace_outer
+    False
+    >>> "b" in trace_inner
+    True
+    >>> "b" in trace_outer
+    True
+
+    BlockPoutine has a flexible interface that allows users
+    to specify in several different ways
+    which sites should be hidden or exposed.
+    See the constructor for details.
+    """
+
+    def __init__(self, fn,
+                 hide_all=True, expose_all=False,
+                 hide=None, expose=None,
+                 hide_types=None, expose_types=None):
+        """
+        :param bool hide_all: hide all sites
+        :param bool expose_all: expose all sites normally
+        :param list hide: list of site names to hide, rest will be exposed normally
+        :param list expose: list of site names to expose, rest will be hidden
+        :param list hide_types: list of site types to hide, rest will be exposed normally
+        :param list expose_types: list of site types to expose normally, rest will be hidden
+
+        Constructor for blocking poutine
+        Default behavior: block everything (hide_all == True)
+
+        A site is hidden if at least one of the following holds:
+        1. msg["name"] in hide
+        2. msg["type"] in hide_types
+        3. msg["name"] not in expose and msg["type"] not in expose_types
+        4. hide_all == True
+        """
+        super(BlockPoutine, self).__init__(fn)
+        self.msngr = BlockMessenger(hide_all, expose_all, hide, expose, hide_types, expose_types)
+
+    def _pyro_sample(self, msg):
+        """
+        :param msg:  current message at a trace site
+        :returns: a sample from the stochastic function at the site.
+
+        Default sample behavior with a side effect.
+        Applies self._block_up to decide whether to hide the site.
+        """
+        return self.msngr._pyro_sample(msg)
+
+    def _pyro_param(self, msg):
+        """
+        :param msg:  current message at a trace site
+        :returns: the result of querying the parameter store.
+
+        Default param behavior with a side effect.
+        Applies self._block_up to decide whether to hide the site.
+        """
+        return self.msngr._pyro_param(msg)
