9a497296b1b902812a6db43d39f5b48416831388
Adam Scibior <adscib@gmail.com>
2018-01-22 17:16:23 -0800

    Refactor EscapePoutine into EscapeMessenger

--- pyro/poutine/escape_poutine.py
+++ pyro/poutine/escape_poutine.py
@@ -2,23 +2,22 @@ from __future__ import absolute_import, division, print_function
 
 from pyro.util import NonlocalExit
 
-from .poutine import Poutine
+from .poutine import Messenger, Poutine
 
 
-class EscapePoutine(Poutine):
+class EscapeMessenger(Messenger):
     """
-    Poutine that does a nonlocal exit by raising a util.NonlocalExit exception
+    Messenger that does a nonlocal exit by raising a util.NonlocalExit exception
     """
-    def __init__(self, fn, escape_fn):
+    def __init__(self, escape_fn):
         """
-        :param fn: a stochastic function (callable containing pyro primitive calls)
         :param escape_fn: function that takes a msg as input and returns True
             if the poutine should perform a nonlocal exit at that site.
 
         Constructor.  Stores fn and escape_fn.
         """
+        super(EscapeMessenger, self).__init__()
         self.escape_fn = escape_fn
-        super(EscapePoutine, self).__init__(fn)
 
     def _pyro_sample(self, msg):
         """
@@ -34,4 +33,31 @@ class EscapePoutine(Poutine):
             msg["done"] = True
             raise NonlocalExit(msg)
         else:
-            return super(EscapePoutine, self)._pyro_sample(msg)
+            return super(EscapeMessenger, self)._pyro_sample(msg)
+
+class EscapePoutine(Poutine):
+    """
+    Poutine that does a nonlocal exit by raising a util.NonlocalExit exception
+    """
+    def __init__(self, fn, escape_fn):
+        """
+        :param fn: a stochastic function (callable containing pyro primitive calls)
+        :param escape_fn: function that takes a msg as input and returns True
+            if the poutine should perform a nonlocal exit at that site.
+
+        Constructor.  Stores fn and escape_fn.
+        """
+        super(EscapePoutine, self).__init__(fn)
+        self.msngr = EscapeMessenger(escape_fn)
+
+    def _pyro_sample(self, msg):
+        """
+        :param msg: current message at a trace site
+        :returns: a sample from the stochastic function at the site.
+
+        Evaluates self.escape_fn on the site (self.escape_fn(msg)).
+
+        If this returns True, raises an exception NonlocalExit(msg).
+        Else, implements default _pyro_sample behavior with no additional effects.
+        """
+        return self.msngr._pyro_sample(msg)
