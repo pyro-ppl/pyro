f2b7dfc92090f4078b643ab06d12625d2999cd13
Adam Scibior <adscib@gmail.com>
2018-01-22 18:48:46 -0800

    Switch to putting messengers instead of poutines on the stack

--- pyro/poutine/poutine.py
+++ pyro/poutine/poutine.py
@@ -157,10 +157,10 @@ class Poutine(object):
         Derived versions cannot be overridden to take arguments
         and must always return self.
         """
-        if not (self in _PYRO_STACK):
+        if not (self.msngr in _PYRO_STACK):
             # if this poutine is not already installed,
             # put it on the bottom of the stack.
-            _PYRO_STACK.insert(0, self)
+            _PYRO_STACK.insert(0, self.msngr)
 
             # necessary to return self because the return value of __enter__
             # is bound to VAR in with EXPR as VAR.
@@ -203,7 +203,7 @@ class Poutine(object):
             # this poutine should be on the bottom of the stack.
             # If so, remove it from the stack.
             # if not, raise a ValueError because something really weird happened.
-            if _PYRO_STACK[0] == self:
+            if _PYRO_STACK[0] == self.msngr:
                 _PYRO_STACK.pop(0)
             else:
                 # should never get here, but just in case...
@@ -213,8 +213,8 @@ class Poutine(object):
             # when the callee or enclosed block raises an exception,
             # find this poutine's position in the stack,
             # then remove it and everything below it in the stack.
-            if self in _PYRO_STACK:
-                loc = _PYRO_STACK.index(self)
+            if self.msngr in _PYRO_STACK:
+                loc = _PYRO_STACK.index(self.msngr)
                 for i in range(0, loc + 1):
                     _PYRO_STACK.pop(0)
 
