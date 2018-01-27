386246f12ee348ea797fc772e568335a25062375
Fritz Obermeyer <fritzo@uber.com>
2018-01-26 18:09:23 -0800

    Add clarifying comments to diag normal tests

--- tests/distributions/test_shapes.py
+++ tests/distributions/test_shapes.py
@@ -75,6 +75,7 @@ def test_diag_normal_batch_log_pdf_shape():
     mu1 = ng_zeros(2, 3)
     mu2 = ng_zeros(2, 4)
     sigma = ng_zeros(2, 1)
+    # TODO these will need extra_event_dims=1
     d1 = dist.Normal(mu1, sigma.expand_as(mu1))
     d2 = dist.Normal(mu2, sigma.expand_as(mu2))
     x1 = d1.sample()
@@ -88,6 +89,7 @@ def test_diag_normal_log_pdf_mask_shape():
     mu = ng_zeros(4, 3)
     sigma = ng_zeros(4, 3)
     mask = ng_ones(4, 1)  # note that 3 was summed out  TODO will become (2,)
+    # TODO this will need extra_event_dims=1
     d = dist.Normal(mu, sigma, log_pdf_mask=mask)
     x = d.sample()
     assert d.batch_log_pdf(x).size() == (4, 1)  # TODO will become (4,)
