0ff5d9ac5b74898b701feb75890df9639df660ef
Neeraj Pradhan <npradhan@uber.com>
2018-01-29 23:01:47 -0800

    add analytic mean/vars for log_normal

--- pyro/distributions/torch/log_normal.py
+++ pyro/distributions/torch/log_normal.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, division, print_function
 
 import torch
+from torch.autograd import Variable
 
 from pyro.distributions.torch_wrapper import TorchDistribution
 from pyro.distributions.util import copy_docs_from
@@ -24,5 +25,14 @@ class LogNormal(TorchDistribution):
     reparameterized = True
 
     def __init__(self, mu, sigma, *args, **kwargs):
+        self.mu = mu
+        self.sigma = sigma
         torch_dist = torch.distributions.LogNormal(mu, sigma)
         super(LogNormal, self).__init__(torch_dist, *args, **kwargs)
+
+    def analytic_mean(self):
+        return torch.exp(self.mu + 0.5 * torch.pow(self.sigma, 2.0))
+
+    def analytic_var(self):
+        return (torch.exp(torch.pow(self.sigma, 2.0)) - Variable(torch.ones(1))) * \
+               torch.pow(self.analytic_mean(), 2)
