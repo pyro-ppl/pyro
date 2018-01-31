0f0e7a1d4920da80d63a8910ca846fb521babfa9
Fritz Obermeyer <fritzo@uber.com>
2018-01-30 16:21:10 -0800

    Add extra_event_dims to additional model,guide pairs

--- examples/ss_vae_M2.py
+++ examples/ss_vae_M2.py
@@ -204,11 +204,11 @@ class SSVAE(nn.Module):
         # sample the handwriting style from the constant prior distribution
         prior_mu = Variable(torch.zeros([batch_size, self.z_dim]))
         prior_sigma = Variable(torch.ones([batch_size, self.z_dim]))
-        zs = pyro.sample("z", dist.normal, prior_mu, prior_sigma)
+        zs = pyro.sample("z", dist.normal, prior_mu, prior_sigma, extra_event_dims=1)
 
         # sample an image using the decoder
         mu = self.decoder.forward([zs, ys])
-        xs = pyro.sample("sample", dist.bernoulli, mu)
+        xs = pyro.sample("sample", dist.bernoulli, mu, extra_event_dims=1)
         return xs, mu
 
 
