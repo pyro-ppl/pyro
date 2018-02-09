d4cc1bfe23d79b58597687e8d9452496ee31a538
fehiepsi <fehiepsi@gmail.com>
2018-02-09 11:09:07 +0900

    fix test

--- tests/contrib/gp/test_models.py
+++ tests/contrib/gp/test_models.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, division, print_function
 import torch
 from torch.autograd import Variable
 
+
 from pyro.contrib.gp.kernels import RBF
 from pyro.contrib.gp.models import GPRegression, SparseGPRegression
 from pyro.contrib.gp import InducingPoints
@@ -10,7 +11,7 @@ from tests.common import assert_equal
 
 
 def test_forward_gpr():
-    kernel = RBF(input_dim=3, variance=torch.ones(1), lengthscale=torch.ones(3))
+    kernel = RBF(input_dim=3, variance=torch.Tensor([1]), lengthscale=torch.Tensor([3]), name="rbf1")
     X = Variable(torch.Tensor([[1, 2, 3], [4, 5, 6]]))
     y = Variable(torch.Tensor([0, 1]))
     # hacky: noise ~ 0, Xnew = X
@@ -30,11 +31,11 @@ def test_forward_gpr():
 
 
 def test_forward_sgpr():
-    kernel = RBF(input_dim=3, variance=torch.ones(2), lengthscale=torch.ones(2))
+    kernel = RBF(input_dim=3, variance=torch.Tensor([2]), lengthscale=torch.Tensor([2]), name="rbf2")
     X = Variable(torch.Tensor([[1, 5, 3], [4, 3, 7]]))
     y = Variable(torch.Tensor([0, 1]))
     # hacky: noise ~ 0, Xnew = Xu = X
-    Xu = InducingPoints(X.data.clone())
+    Xu = InducingPoints(X.data.clone(), name="Xu2")
     Xnew = X
     noise = torch.Tensor([1e-6])
 
@@ -51,10 +52,10 @@ def test_forward_sgpr():
 
 
 def test_forward_sgpr_vs_gpr():
-    kernel = RBF(input_dim=3, variance=torch.ones(2), lengthscale=torch.ones(2))
+    kernel = RBF(input_dim=3, variance=torch.Tensor([2]), lengthscale=torch.Tensor([2]), name="rbf3")
     X = Variable(torch.Tensor([[2, 5, 3], [4, 3, 7]]))
     y = Variable(torch.Tensor([0, 1]))
-    Xu = InducingPoints(X.data.clone())  # must be set to compare
+    Xu = InducingPoints(X.data.clone(), name="Xu3")  # must be set to compare
     Xnew = Variable(torch.Tensor([[3, 1, 4], [1, 3, 1]]))
     noise = torch.Tensor([1])
 
@@ -70,6 +71,6 @@ def test_forward_sgpr_vs_gpr():
     loc_sgpr, sd_sgpr = sgpr(Xnew, full_cov=False)
 
     assert_equal(loc_gpr, loc_sgpr)
-    assert_equal(cov_gpr, cov_sgpr, prec=1e-4)
-    assert_equal(cov_sgpr, cov_fitc, prec=1e-4)
-    assert_equal(sd_gpr, sd_sgpr, prec=1e-4)
+    assert_equal(cov_gpr, cov_sgpr)
+    assert_equal(cov_sgpr, cov_fitc)
+    assert_equal(sd_gpr, sd_sgpr)
