10b98b32667023c84cde0bb5afcb9a7447b9aa00
Eli <eli.bingham@uber.com>
2018-02-17 15:17:25 -0800

    removing complicated state logic from scopes

--- examples/scopes.py
+++ examples/scopes.py
@@ -1,7 +1,7 @@
 import pyro
 import pyro.util
 import pyro.poutine as poutine
-import pyro.distributions as dist
+import pyro.distributions.torch as dist
 
 import torch
 from torch.autograd import Variable
@@ -10,22 +10,26 @@ from pyro.poutine.scope_poutine import ScopeMessenger as scope
 
 
 @scope()
-def model1():
+def model1(r=True):
     model2()
+    with scope("inter"):
+        model2()
+        if r:
+            model1(r=False)
     model2()
 
 
 @scope(multi=True)
 def model2():
-    return pyro.sample("y", dist.normal,
-                       Variable(torch.zeros(1)),
-                       Variable(torch.ones(1)))
+    return pyro.sample("y", dist.Normal(Variable(torch.zeros(1)),
+                                        Variable(torch.ones(1))))
 
 
 @scope()
 def geometric(p):
-    x = pyro.sample("x", dist.bernoulli, p)
+    x = pyro.sample("x", dist.Bernoulli(p))
     if x.data[0] == 1.0:
+        model1()
         return x + geometric(p)
     else:
         return x
@@ -35,4 +39,4 @@ print(poutine.trace(model2).get_trace().nodes)
 
 print(poutine.trace(model1).get_trace().nodes)
 
-print(poutine.trace(geometric).get_trace(Variable(torch.Tensor([0.7]))).nodes)
+# print(poutine.trace(geometric).get_trace(Variable(torch.Tensor([0.7]))).nodes)
--- pyro/poutine/scope_poutine.py
+++ pyro/poutine/scope_poutine.py
@@ -4,13 +4,9 @@ from .poutine import Messenger, Poutine
 
 
 class ScopeMessenger(Messenger):
-    def __init__(self, prefix=None, multi=None):
+    def __init__(self, prefix=None, suffix=None):
         super(ScopeMessenger, self).__init__()
-        if multi is None:
-            multi = False
         self.prefix = prefix
-        self.multi = multi
-        self.n_calls = 1  # {}
 
     def __call__(self, fn):
         if self.prefix is None:
@@ -18,14 +14,7 @@ class ScopeMessenger(Messenger):
 
         @functools.wraps(fn)
         def _fn(*args, **kwargs):
-            # keep n_calls dict for each context
-            # each time called in same context, increment n_calls[ctx]
-            # reset all n_calls entries after returning to global context
-            # extra help: each context can only happen once per global context
-            with ScopeMessenger(prefix=self.prefix, multi=self.multi) as cm:
-                if self.multi:
-                    cm.n_calls = int(self.n_calls)
-                    self.n_calls += 1
+            with ScopeMessenger(prefix=self.prefix):
                 return fn(*args, **kwargs)
         return _fn
 
@@ -34,14 +23,7 @@ class ScopeMessenger(Messenger):
         return super(ScopeMessenger, self).__exit__(*args, **kwargs)
 
     def _pyro_sample(self, msg):
-        # check the context in the stack
-        # if ctx in n_calls, update n_calls[ctx] += 1
-        # if ctx not in n_calls, set n_calls[ctx] = 0
-        if self.multi:
-            msg["name"] = "{}_{}/{}".format(
-                self.prefix, self.n_calls, msg["name"])
-        else:
-            msg["name"] = "{}/{}".format(self.prefix, msg["name"])
+        msg["name"] = "{}_{}/{}".format(self.prefix, self.suffix, msg["name"])
         return None  # return super(ScopeMessenger, self)._pyro_sample(msg)
 
     def _pyro_param(self, msg):
