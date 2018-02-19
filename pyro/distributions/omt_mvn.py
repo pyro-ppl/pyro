143bc16592636433cecfa03644699f851d14c91c
Martin Jankowiak <jankowiak@uber.com>
2018-02-18 16:06:57 -0800

    cleanup and fix omtmvn

--- pyro/distributions/omt_mvn.py
+++ pyro/distributions/omt_mvn.py
@@ -44,7 +44,7 @@ class _OMTMVNSample(Function):
     @staticmethod
     @once_differentiable
     def backward(ctx, grad_output):
-        jitter = 1.0e-12  # do i really need this?
+        jitter = 1.0e-8  # do i really need this?
         L, = ctx.saved_tensors
         z = ctx.z
         epsilon = ctx.white
@@ -58,120 +58,25 @@ class _OMTMVNSample(Function):
 
         z_ja = z.unsqueeze(-1)
         g_R_inv = torch.matmul(g, R_inv).unsqueeze(-2)
-        diff_L_ab = 0.5 * sum_leftmost(g_R_inv * z_ja, -2)
-
         epsilon_jb = epsilon.unsqueeze(-2)
         g_ja = g.unsqueeze(-1)
-        diff_L_ab += 0.5 * sum_leftmost(g_ja * epsilon_jb, -2)
+        diff_L_ab = 0.5 * sum_leftmost(g_ja * epsilon_jb + g_R_inv * z_ja, -2)
 
         Sigma_inv = torch.mm(R_inv, R_inv.t())
-        V, D, Vt = torch.svd(Sigma_inv + jitter)
-        D_outer = D.unsqueeze(-1) + D.unsqueeze(0) + jitter
+        V, D, _ = torch.svd(Sigma_inv + jitter)
+        Vt = V.t()
+        D_outer = D.unsqueeze(-1) + D.unsqueeze(0)
 
-        # XXX can i do this expansion more cleanly?
-        jdim = z.dim() - 1
-        expand_tuple = tuple([-1]*jdim + [dim, dim])
+        expand_tuple = tuple([-1] * (z.dim() - 1) + [dim, dim])
         z_tilde = identity * torch.matmul(z, V).unsqueeze(-1).expand(*expand_tuple)
         g_tilde = identity * torch.matmul(g, V).unsqueeze(-1).expand(*expand_tuple)
 
-        Y = torch.matmul(z_tilde, torch.matmul(1.0 / D_outer, g_tilde))
-        Y_tilde = torch.matmul(V, torch.matmul(Y, Vt))
-        Y_tilde_symm = Y_tilde + torch.transpose(Y_tilde, -1, -2)
+        Y = sum_leftmost(torch.matmul(z_tilde, torch.matmul(1.0 / D_outer, g_tilde)), -2)
+        Y = torch.mm(V, torch.mm(Y, Vt))
+        Y = Y + Y.t()
 
-        Tr_xi_Y = torch.matmul(torch.matmul(Sigma_inv, Y_tilde_symm), R_inv)
-        Tr_xi_Y -= torch.matmul(Y_tilde_symm, torch.mm(Sigma_inv, R_inv))
-        diff_L_ab += 0.5 * sum_leftmost(Tr_xi_Y, -2)
+        Tr_xi_Y = torch.mm(torch.mm(Sigma_inv, Y), R_inv) - torch.mm(Y, torch.mm(Sigma_inv, R_inv))
+        diff_L_ab += 0.5 * Tr_xi_Y
         L_grad = torch.tril(diff_L_ab)
 
         return loc_grad, L_grad, None
-
-
-class OTCVMultivariateNormal(MultivariateNormal):
-    """Multivariate normal (Gaussian) distribution with optimal transport-inspired control variates.
-
-    A distribution over vectors in which all the elements have a joint Gaussian
-    density.
-
-    :param torch.autograd.Variable loc: Mean.
-    :param torch.autograd.Variable scale_tril: Cholesky of Covariance matrix.
-    :param torch.autograd.Variable B: tensor controlling the control variate
-    :param torch.autograd.Variable C: tensor controlling the control variate
-    :param torch.autograd.Variable D: tensor controlling the control variate
-    :param torch.autograd.Variable F: tensor controlling the control variate
-    """
-    params = {"loc": constraints.real, "scale_tril": constraints.lower_triangular}
-
-    def __init__(self, loc, scale_tril, B=None, C=None, D=None, F=None):
-        assert(loc.dim() == 1), "OMTMultivariateNormal loc must be 1-dimensional"
-        assert(scale_tril.dim() == 2), "OMTMultivariateNormal scale_tril must be 2-dimensional"
-        covariance_matrix = torch.mm(scale_tril, scale_tril.t())
-        super(OTCVMultivariateNormal, self).__init__(loc, covariance_matrix)
-        self.scale_tril = scale_tril
-        self.B = B
-        self.C = C
-        self.D = D
-        self.F = F
-        BC_mode = (B is not None) and (C is not None)
-        DF_mode = (D is not None) and (F is not None)
-        if BC_mode:
-            assert(scale_tril.size() == B.size() == C.size())
-        if DF_mode:
-            assert(scale_tril.size() == D.size() == F.size())
-        assert (BC_mode or DF_mode), "Must use at least one control variate parameterization"
-
-    def rsample(self, sample_shape=torch.Size()):
-        return _OTCVMVNSample.apply(self.loc, self.scale_tril, self.B, self.C, self.D, self.F,
-                                    sample_shape + self.loc.shape)
-
-
-class _OTCVMVNSample(Function):
-    @staticmethod
-    def forward(ctx, loc, scale_tril, B, C, D, F, shape):
-        ctx.save_for_backward(scale_tril)
-        ctx.white = loc.new(shape).normal_()
-        ctx.z = torch.matmul(ctx.white, scale_tril.t())
-        ctx.B, ctx.C = B, C
-        ctx.D, ctx.F = D, F
-        return loc + ctx.z
-
-    @staticmethod
-    @once_differentiable
-    def backward(ctx, grad_output):
-        L, = ctx.saved_tensors
-        z = ctx.z
-        epsilon = ctx.white
-        B, C = ctx.B, ctx.C
-        D, F = ctx.D, ctx.F
-        BC_mode = (B is not None) and (C is not None)
-        DF_mode = (D is not None) and (F is not None)
-
-        dim = L.size(0)
-        g = grad_output
-        loc_grad = sum_leftmost(grad_output, -1)
-
-        # compute the rep trick gradient
-        epsilon_jb = epsilon.unsqueeze(-2)
-        g_ja = g.unsqueeze(-1)
-        diff_L_ab = sum_leftmost(g_ja * epsilon_jb, -2)
-
-        # modulate the velocity field with an infinitessimal rotation
-        if BC_mode:
-            LB = torch.mm(L, B)
-            eps_C = torch.matmul(epsilon, C)
-            g_LB = torch.matmul(g, LB)
-            diff_L_ab += sum_leftmost(eps_C.unsqueeze(-2) * g_LB.unsqueeze(-1), -2)
-            LC = torch.mm(L, C)
-            eps_B = torch.matmul(epsilon, B)
-            g_LC = torch.matmul(g, LC)
-            diff_L_ab -= sum_leftmost(eps_B.unsqueeze(-1) * g_LC.unsqueeze(-2), -2)
-
-        # modulate the velocity field with an infinitessimal rotation
-        if DF_mode:
-            LD = torch.mm(L, D)
-            g_LD = torch.matmul(g, LD)
-            multiplier = (g_LD * epsilon).sum()
-            diff_L_ab += multiplier * F
-
-        L_grad = torch.tril(diff_L_ab)
-
-        return loc_grad, L_grad, None, None, None, None, None
--- tests/distributions/test_omt_mvn.py
+++ tests/distributions/test_omt_mvn.py
@@ -7,7 +7,7 @@ import numpy as np
 import torch
 from torch.autograd import Variable
 
-from pyro.distributions.omt_mvn import OMTMultivariateNormal, OTCVMultivariateNormal
+from pyro.distributions.omt_mvn import OMTMultivariateNormal
 from pyro.distributions import MultivariateNormal
 
 from tests.common import assert_equal
@@ -25,52 +25,25 @@ def analytic_grad(L11=1.0, L22=1.0, L21=1.0, omega1=1.0, omega2=1.0):
 @pytest.mark.parametrize('L11', [0.6, 0.95])
 @pytest.mark.parametrize('omega1', [0.5, 0.9])
 @pytest.mark.parametrize('sample_shape', [torch.Size([1000, 1000]), torch.Size([100000])])
-@pytest.mark.parametrize('mvn_dist', [OMTMultivariateNormal])
-#@pytest.mark.parametrize('mvn_dist', [OTCVMultivariateNormal])
-#@pytest.mark.parametrize('mvn_dist', [OMTMultivariateNormal, OTCVMultivariateNormal])
-def test_mean(mvn_dist, sample_shape, L21, omega1, L11, L22=0.8, L33=0.9, omega2=0.75):
-    dim = 3
+def test_mean(sample_shape, L21, omega1, L11, L22=0.8, L33=0.9, omega2=0.75):
     omega = Variable(torch.Tensor([omega1, omega2, 0.0]))
-    mu = Variable(torch.zeros(dim), requires_grad=True)
-    #off_diag = Variable(L21 * torch.ones(dim, dim), requires_grad=True)
+    mu = Variable(torch.zeros(3), requires_grad=True)
     zero_vec = [0.0, 0.0, 0.0]
     off_diag = Variable(torch.Tensor([zero_vec, [L21, 0.0, 0.0], zero_vec]), requires_grad=True)
-    #print("off_diag", off_diag)
-    #L = Variable(torch.diag(torch.Tensor([L11, L22]))) + torch.tril(off_diag, -1)
     L = Variable(torch.diag(torch.Tensor([L11, L22, L33]))) + off_diag
-    #print("L", L, torch.mm(L, L.t()))
 
-    if mvn_dist==OMTMultivariateNormal:
-        dist = mvn_dist(mu, L)
-    else:
-        noise = 0.3
-        B = Variable(noise * torch.randn(dim, dim))
-        C = Variable(noise * torch.randn(dim, dim))
-        D = noise * torch.randn(dim, dim)
-        D = Variable(D - D.t())
-        F = Variable(noise * torch.randn(dim, dim))
-        dist = mvn_dist(mu, L, B, C, D, F)
+    dist = OMTMultivariateNormal(mu, L)
     z = dist.rsample(sample_shape)
     torch.cos((omega*z).sum(-1)).mean().backward()
 
     computed_grad = off_diag.grad.data.numpy()[1, 0]
     analytic = analytic_grad(L11=L11, L22=L22, L21=L21, omega1=omega1, omega2=omega2)
-    return computed_grad
-    #print("an comp", analytic, computed_grad, L21, omega1, L11, sample_shape)
-    assert(mu.grad.size() == mu.size())
     assert(off_diag.grad.size() == off_diag.size())
-    #assert_equal(analytic, computed_grad, prec=0.015, msg='bad cholesky grad for %s (expected %.5f, got %.5f)' % (mvn_dist,
-    #             analytic, computed_grad))
+    assert(mu.grad.size() == mu.size())
+    assert(torch.triu(off_diag.grad, 1).sum() == 0.0)
+    assert_equal(analytic, computed_grad, prec=0.015,
+                 msg='bad cholesky grad for OMTMultivariateNormal (expected %.5f, got %.5f)' % (analytic, computed_grad))
 
-blah=[]
-for _ in range(20000):
-    blah.append(test_mean(OMTMultivariateNormal, torch.Size(), L21=0.9, omega1=1.3, L11=1.2))
-print("computed", np.mean(blah), np.std(blah))
-print("analytic", analytic_grad(L11=1.2, L22=0.8, L21=0.9, omega1=1.3, omega2=0.75))
-
-#test_mean(OTCVMultivariateNormal, torch.Size([2000, 2000]), 0.5, 1.0, 0.7)
-#test_mean(OMTMultivariateNormal, torch.Size([20000]), 0.9, 1.3, 1.2)
-#test_mean(OMTMultivariateNormal, torch.Size([2000, 10]), 0.5, 1.0, 0.7)
 
 def test_log_prob():
     loc = Variable(torch.Tensor([2, 1, 1, 2, 2]))
