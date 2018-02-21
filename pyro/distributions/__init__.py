6a55af873d7c44f98502e13d773cd92cbd6c5bd8
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2018-02-20 21:56:30 -0800

    Automatically import all standard torch distributions (#794)
    * Automatically import all standard torch distributions
    
    * Import all distributions into pyro.distributions

--- pyro/distributions/__init__.py
+++ pyro/distributions/__init__.py
@@ -2,14 +2,12 @@ from __future__ import absolute_import, division, print_function
 
 from pyro.distributions.delta import Delta
 from pyro.distributions.distribution import Distribution
+from pyro.distributions.omt_mvn import OMTMultivariateNormal
 from pyro.distributions.rejector import Rejector
-from pyro.distributions.torch import (Bernoulli, Beta, Binomial, Categorical, Cauchy, Dirichlet, Exponential, Gamma,
-                                      LogNormal, Multinomial, Normal, OneHotCategorical, Poisson,
-                                      TransformedDistribution, Uniform)
+from pyro.distributions.torch import *
 from pyro.distributions.torch.iaf import InverseAutoregressiveFlow
 from pyro.distributions.torch.multivariate_normal import MultivariateNormal
 from pyro.distributions.torch.sparse_multivariate_normal import SparseMultivariateNormal
 from pyro.distributions.torch_distribution import TorchDistribution
-from pyro.distributions.omt_mvn import OMTMultivariateNormal
 
 # flake8: noqa
