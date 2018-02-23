2f2018a8bded3974f4ff301bd58096d79a5c3694
jpchen <jonathanp.chen@gmail.com>
2018-02-23 12:21:37 -0800

    bump copyright, scrap version

--- tutorial/source/conf.py
+++ tutorial/source/conf.py
@@ -63,7 +63,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Pyro Tutorials'
-copyright = u'2017, Uber AI Labs'
+copyright = u'2018, Uber AI Labs'
 author = u'Uber AI Labs'
 
 # The version info for the project you're documenting, acts as replacement for
