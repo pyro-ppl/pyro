44ebd3a747f731facbdfa2517bfedea574ac5b7b
Martin Jankowiak <jankowiak@uber.com>
2018-02-26 10:42:14 -0800

    Fix test

--- tests/infer/test_util.py
+++ tests/infer/test_util.py
@@ -22,11 +22,11 @@ def test_multiview_tensor_add():
     assert_equal(x[torch.Size([2, 3, 4])].data, 5.5 * torch.ones(2, 3, 4))
 
 
-def test_multiview_tensor_sum_leftmost():
+def test_multiview_tensor_sum_leftmost_all_but():
     x = MultiViewTensor(Variable(1.5 * torch.ones(2, 3, 4)))
     x.add(Variable(2.5 * torch.ones(1, 3, 4)))
     x.add(Variable(3.5 * torch.ones(2, 3, 1)))
-    result = x.sum_leftmost(-2)
+    result = x.sum_leftmost_all_but(2)
     assert_equal(result[torch.Size([3, 4])].data, 5.5 * torch.ones(3, 4))
     assert_equal(result[torch.Size([3, 1])].data, 7.0 * torch.ones(3, 1))
 
@@ -44,8 +44,3 @@ def test_multiview_tensor_contract_to():
     assert_equal(result231, (3.5 + 10.0 + 6.0 + 4.5) * torch.ones(2, 3, 1))
     with pytest.raises(ValueError):
         x.contract_to(torch.ones(3, 1))
-
-
-test_multiview_tensor_add()
-test_multiview_tensor_sum_leftmost()
-test_multiview_tensor_contract_to()
