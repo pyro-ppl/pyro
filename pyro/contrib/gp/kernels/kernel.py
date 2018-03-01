91cfa39b8f05eee115d96b03b7045b0278610f15
fehiepsi <fehiepsi@gmail.com>
2018-03-01 20:00:21 +0900

    add deriving kernels

--- pyro/contrib/gp/kernels/kernel.py
+++ pyro/contrib/gp/kernels/kernel.py
@@ -103,7 +103,7 @@ class Kernel(Parameterized):
     def vertical_scale(self, vscaling_fn, name=None):
         """
         Creates a new kernel, which is an instance of VerticalScaling kernel, according to:
-        ``k_new(x, z) = f(x)exp(k(x, z))f(z)``,
+        ``k_new(x, z) = f(x)k(x, z)f(z)``,
         where ``f := vscaling_fn`` is a vertical scaling function.
         """
         return VerticalScaling(self, vscaling_fn, name=name)
@@ -128,6 +128,13 @@ class Kernel(Parameterized):
 
 
 class Combination(Kernel):
+    """
+    Base class for Sum and Product kernels.
+
+    :param pyro.contrib.gp.kernels.Kernel kernel0: First kernel to combine.
+    :param pyro.contrib.gp.kernels.Kernel or numbers.Number kernel1: Second kernel to combine.
+    """
+
     def __init__(self, kernel0, kernel1, name=None):
         if not (isinstance(kernel0, Kernel) and
                 (isinstance(kernel1, Kernel) or isinstance(kernel1, numbers.Number))):
@@ -170,34 +177,127 @@ class Combination(Kernel):
 
 
 class Sum(Combination):
+    """
+    Returns a new kernel which acts like a sum/direct sum of two kernels.
+    The second kernel can be a constant.
+    """
 
     def forward(self, X, Z=None, diag=False):
-        if isinstance(self.kernel1, Kernel):
+        if isinstance(self._kernel1, Kernel):
             return self.kernel0(X, Z, diag) + self.kernel1(X, Z, diag)
         else:  # constant
             return self.kernel0(X, Z, diag) + self.kernel1
 
 
 class Product(Combination):
+    """
+    Returns a new kernel which acts like a product/tensor product of two kernels.
+    The second kernel can be a constant.
+    """
 
     def forward(self, X, Z=None, diag=False):
-        if isinstance(self.kernel1, Kernel):
+        if isinstance(self._kernel1, Kernel):
             return self.kernel0(X, Z, diag) * self.kernel1(X, Z, diag)
         else:  # constant
             return self.kernel0(X, Z, diag) * self.kernel1
 
 
-class Deriving():
-    pass
+class Deriving(Kernel):
+    """
+    Base class for kernels derived from a kernel by some transforms such as: warping,
+    exponent, vertical scaling.
+
+    :param pyro.contrib.gp.kernels.Kernel kernel: The original kernel.
+    """
+
+    def __init__(self, kernel, name=None):
+        super(Deriving, self).__init__(kernel.input_dim, kernel.active_dims, name)
+
+        self.kernel = kernel
+
+        if kernel._subkernels:
+            self._subkernels.update(kernel._subkernels)
+        else:
+            self._subkernels[kernel.name] = kernel
+
+
+def _Horner_evaluate(x, coef):
+    """
+    Evaluates the value of a polynomial accoridng to Horner's method.
+    """
+    # https://en.wikipedia.org/wiki/Horner%27s_method
+    n = len(coef) - 1
+    b = coef[n]
+    for i in range(n-1, -1, -1):
+        b = coef[i] + b * x
+    return b
 
 
 class Warping(Deriving):
-    pass
+    """
+    Creates a new kernel according to ``k_new(x, z) = q(k(f(x), f(z)))``,
+    where ``f := iwarping_fn`` is a input warping function, and ``q`` is a polynomial
+    with non-negative coefficients ``owarping_coef``.
+
+    :param iwarping_fn: The input warping function, must be callable.
+    :param list owarping_coef: A list of coefficients of the output warping polynomial.
+        These coefficients must be non-negative.
+    """
+
+    def __init__(self, kernel, iwarping_fn, owarping_coef, name=None):
+        super(Deriving, self).__init__(kernel.input_dim, kernel.active_dims, name)
+
+        self.iwarping_fn = iwarping_fn
+
+        if owarping_coef is not None:
+            for coef in owarping_coef:
+                if not isinstance(coef, int) and coef < 0:
+                    raise ValueError("Coefficients of the polynomial must be a non-negative integer.")
+            if len(owarping_coef) < 2 and sum(owarping_coef) == 0:
+                raise ValueError("The ouput warping polynomial should have a degree of at least 1.")
+        self.owarping_coef = owarping_coef
+
+    def forward(self, X, Z=None, diag=False):
+        if self.iwarping_fn is None:
+            K_iwarp = self.kernel(X, Z, diag)
+        elif Z is None:
+            K_iwarp = self.kernel(self.iwarping_fn(X), None, diag)
+        else:
+            K_iwarp = self.kernel(self.iwarping_fn(X), self.iwarping_fn(Z), diag)
+
+        if self.owarping_coef is None:
+            return K_iwarp
+        else:
+            return _Horner_evaluate(K_iwarp, self.owarping_coef)
 
 
 class Exponent(Deriving):
-    pass
+    """
+    Creates a new kernel according to ``k_new(x, z) = exp(k(x, z))``.
+    """
+
+    def forward(self, X, Z=None, diag=False):
+        return self.kernel(X, Z, diag).exp()
 
 
 class VerticalScaling(Deriving):
-    pass
+    """
+    Creates a new kernel according to ``k_new(x, z) = f(x)k(x, z)f(z)``,
+    where ``f := vscaling_fn`` is a vertical scaling function.
+
+    :param vscaling_fn: The vertical scaling function, must be callable.
+    """
+
+    def __init__(self, kernel, vscaling_fn, name=None):
+        super(Deriving, self).__init__(kernel.input_dim, kernel.active_dims, name)
+
+        self.vscaling_fn = vscaling_fn
+
+    def forward(self, X, Z=None, diag=False):
+        if diag:
+            return self.vscaling_fn(X) * self.kernel(X, Z, diag) * self.vscaling_fn(X)
+        elif Z is None:
+            vscaled_X = self.vscaling_fn(X).unsqueeze(1)
+            return vscaled_X * self.kernel(X, Z, diag) * vscaled_X.t()
+        else:
+            return self.vscaling_fn(X).unsqueeze(1) * self.kernel(X, Z, diag) * self.vscaling_fn(Z).unsqueeze(0)
