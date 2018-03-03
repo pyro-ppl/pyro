994d2023d7d70980c2172c14e8f31637965f000c
Du Phan <fehiepsi@gmail.com>
2018-03-03 09:01:35 +0900

    Update __init__.py

--- pyro/contrib/gp/kernels/__init__.py
+++ pyro/contrib/gp/kernels/__init__.py
@@ -4,7 +4,8 @@ from .brownian import Brownian
 from .dot_product import DotProduct, Linear, Polynomial
 from .isotropic import (Exponential, Isotropy, Matern12, Matern32, Matern52,
                         RationalQuadratic, RBF, SquaredExponential)
-from .kernel import Kernel
+from .kernel import (Combination, Exponent, Kernel, Product, Sum,
+                     Transforming, VerticalScaling, Warping)
 from .periodic import Cosine, ExpSineSquared, Periodic
 from .static import Bias, Constant, WhiteNoise
 
