e51d38bb514197bb3681a27c2f20035f18afe194
Martin Jankowiak <jankowiak@uber.com>
2018-03-05 13:39:53 -0800

    attempt to fix pytest27 problem

--- tests/infer/test_conjugate_gradients.py
+++ tests/infer/test_conjugate_gradients.py
@@ -1,6 +1,3 @@
-from __future__ import absolute_import, division, print_function
-
-import pytest
 
 import pyro
 from pyro.infer.tracegraph_elbo import TraceGraph_ELBO
@@ -9,24 +6,26 @@ from tests.integration_tests.test_conjugate_gaussian_models import GaussianChain
 
 
 # TODO increase precision and number of particles once latter is parallelized properly
-@pytest.mark.parametrize("N", [3, 5])
-@pytest.mark.parametrize("reparameterized", [True, False])
-def test_conjugate_chain_gradient(N, reparameterized):
-    pyro.clear_param_store()
+class ConjugateChainGradientTests(GaussianChain):
 
-    gc = GaussianChain()
-    gc.setUp()
-    gc.setup_chain(N)
+    def test_gradients(self):
+        for N in [3, 5]:
+            for reparameterized in [True, False]:
+                self.do_test_gradients(N, reparameterized)
 
-    elbo = TraceGraph_ELBO(num_particles=1000)
-    elbo.loss_and_grads(gc.model, gc.guide, reparameterized=reparameterized)
+    def do_test_gradients(self, N, reparameterized):
+        pyro.clear_param_store()
+        self.setup_chain(N)
 
-    for i in range(1, N + 1):
-        for param_prefix in ["mu_q_%d", "log_sig_q_%d", "kappa_q_%d"]:
-            if i == N and param_prefix == 'kappa_q_%d':
-                continue
-            actual_grad = pyro.param(param_prefix % i).grad
-            assert_equal(actual_grad, 0.0 * actual_grad, prec=0.20, msg="".join([
-                         "parameter %s%d" % (param_prefix[:-2], i),
-                         "\nexpected = zero vector",
-                         "\n  actual = {}".format(actual_grad.detach().cpu().numpy())]))
+        elbo = TraceGraph_ELBO(num_particles=1000)
+        elbo.loss_and_grads(self.model, self.guide, reparameterized=reparameterized)
+
+        for i in range(1, N + 1):
+            for param_prefix in ["mu_q_%d", "log_sig_q_%d", "kappa_q_%d"]:
+                if i == N and param_prefix == 'kappa_q_%d':
+                    continue
+                actual_grad = pyro.param(param_prefix % i).grad
+                assert_equal(actual_grad, 0.0 * actual_grad, prec=0.20, msg="".join([
+                             "parameter %s%d" % (param_prefix[:-2], i),
+                             "\nexpected = zero vector",
+                             "\n  actual = {}".format(actual_grad.detach().cpu().numpy())]))
