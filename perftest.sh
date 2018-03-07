579a8d26a41996e7bb2b35268094e1136101ea4a
Neeraj Pradhan <npradhan@uber.com>
2018-03-07 12:39:09 -0800

    use default capture flags

--- perftest.sh
+++ perftest.sh
@@ -40,7 +40,7 @@ fi
 
 # Run benchmark comparison - fails if the min run time is 10% less than on the ref branch.
 if [ ${IS_BENCHMARK_FILE_IN_DEV} = 1 ]; then
-    pytest -vs tests/perf/test_benchmark.py --benchmark-compare --benchmark-compare-fail=min:10%
+    pytest -vx tests/perf/test_benchmark.py --benchmark-compare --benchmark-compare-fail=min:10%
 else
-    pytest -vs tests/perf/test_benchmark.py --benchmark-verbose
+    pytest -vx tests/perf/test_benchmark.py --benchmark-verbose
 fi
