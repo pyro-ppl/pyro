f3b8697dfffb7a840f3937c3d3c2176dda9bbfee
Fritz Obermeyer <fritzo@uber.com>
2018-03-08 15:50:38 -0800

    Integrate MultiFrameTensor into TraceGraph_ELBO

--- pyro/infer/util.py
+++ pyro/infer/util.py
@@ -133,48 +133,53 @@ class MultiViewTensor(dict):
         return '%s(%s)' % (type(self).__name__, ", ".join([str(k) for k in self.keys()]))
 
 
-class CondIndepTensor(dict):
+class MultiFrameTensor(dict):
     """
-    A container for sums of Tensors in different :class:`iarange` contexts.
+    A container for sums of Tensors among different :class:`iarange` contexts.
 
     Used in :class:`~pyro.infer.tracegraph_elbo.TraceGraph_ELBO` to simplify
     downstream cost computation logic.
 
     Example::
 
-        downstream_cost = CondIndepTensor()
+        downstream_cost = MultiFrameTensor()
         for site in downstream_nodes:
-            downstream_cost.add(site["cond_indep_stack"], site["batch_log_pdf"])
+            downstream_cost.add((site["cond_indep_stack"], site["batch_log_pdf"]))
+        downstream_cost.add(*other_costs.items())  # add in bulk
         summed = downstream_cost.sum_to(target_site["cond_indep_stack"])
     """
     def __init__(self, *items):
-        super(CondIndepTensor, self).__init__()
+        super(MultiFrameTensor, self).__init__()
         self.add(*items)
 
     def add(self, *items):
         """
-        Add a collection of (key, tensor) pairs. Keys are
+        Add a collection of (cond_indep_stack, tensor) pairs. Keys are
         ``cond_indep_stack``s, i.e. tuples of :class:`CondIndepStackFrame`s.
         Values are :class:`torch.Tensor`s.
         """
-        for key, value in items:
-            assert all(f.dim < 0 and -len(value.shape) <= f.dim for f in key)
-            if key in self:
-                self[key] = self[key] + value
+        for cond_indep_stack, value in items:
+            frames = frozenset(f for f in cond_indep_stack if f.vectorized)
+            assert all(f.dim < 0 and -len(value.shape) <= f.dim for f in frames)
+            if frames in self:
+                self[frames] = self[frames] + value
             else:
-                self[key] = value
+                self[frames] = value
 
-    def sum_to(self, target_key):
+    def sum_to(self, target_frames):
         total = None
-        for key, value in self:
-            for f in key:
-                if f not in target_key and value.shape[f.dim] != 1:
+        for frames, value in self.items():
+            for f in frames:
+                if f not in target_frames and value.shape[f.dim] != 1:
                     value = value.sum(f.dim, True)
+            while value.shape and value.shape[0] == 1:
+                value.squeeze_(0)
             total = value if total is None else total + value
         return total
 
     def __repr__(self):
-        return '%s(%s)' % (type(self).__name__, ",\n\t".join(['({}, ...)'.format(key) for key in self]))
+        return '%s(%s)' % (type(self).__name__, ",\n\t".join([
+            '({}, ...)'.format(frames) for frames in self]))
 
 
 class TreeSum(object):
