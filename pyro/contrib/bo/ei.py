6c07d7fee333f61c608d95403cd4ebe01bf7e6b0
Toby the potato <tobyclh@gmail.com>
2018-03-15 06:40:45 +0000

    Update ei.py
    make it a standalone class

--- pyro/contrib/bo/ei.py
+++ pyro/contrib/bo/ei.py
@@ -3,16 +3,16 @@ import numpy as np
 from .bo import BO
 from torch.distributions import Normal
 import torch
-class EI(BO):
+class EI:
     def __init__(self, X, y, kernel, noise=None):
         super().__init__(X, y, kernel, noise=noise)
         # loc = torch.zeros_like(X[0,:])
         # scale = torch.ones_like(X[0,:])
         # print(f'loc : {loc.shape}, scale : {scale.shape}')
         # self.norm = Normal(loc, scale)
-
+        self.xi=0.0
     
-    def acquire(self, x, xi=0.0):
+    def __call__(self, x ):
         """
         Expected Improvement acquisition function
 
@@ -22,7 +22,7 @@ class EI(BO):
         m, std = self.predict(x, return_std=True)
         norm = Normal(torch.zeros_like(m), torch.ones_like(m))
         # print(f'Acq m : {m.shape}')
-        z = (m - self.y.max() - xi)/std
+        z = (m - self.y.max() - self.xi)/std
         f = (z * norm.cdf(z) + torch.exp(norm.log_prob(z))) * std
         return f
 
