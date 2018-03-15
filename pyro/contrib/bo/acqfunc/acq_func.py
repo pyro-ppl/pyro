cc8505d2bdcbc22ddfd2be40481aded94a62cd3f
Toby the potato <tobyclh@gmail.com>
2018-03-16 08:51:58 +0900

    Add base acq func

---
+++ pyro/contrib/bo/acqfunc/acq_func.py
@@ -0,0 +1,19 @@
+import torch
+import pyro
+
+class AcquisitionFunction:
+    """
+    Base acquisition function.
+    
+    Keyword Arguments:
+    :param GPRegression gp: Gaussasin Process Regression Module.
+    """
+    def __init__(self, gp):
+        self.gp = gp
+
+    def update(self):
+        """
+        Called once by the Bayesian Optimization Module every time it get the next point to quire.
+        """
+        pass
+        
