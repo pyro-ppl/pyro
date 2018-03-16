405b7d80971cc134603142711eece4b91a605698
Neeraj Pradhan <npradhan@uber.com>
2018-03-16 14:32:40 -0700

    fix typo in script

--- scripts/install_pytorch.sh
+++ scripts/install_pytorch.sh
@@ -26,7 +26,7 @@ PYTORCH_MAC_PY_27_WHL="torch-${WHL_VERSION}-cp27-cp27m-macosx_10_6_x86_64"
 PYTORCH_MAC_PY_35_WHL="torch-${WHL_VERSION}-cp35-cp35m-macosx_10_6_x86_64"
 PYTORCH_MAC_PY_36_WHL="torch-${WHL_VERSION}-cp36-cp36m-macosx_10_6_x86_64"
 PYTORCH_LINUX_PY_27_WHL="torch-${WHL_VERSION}-cp27-cp27mu-linux_x86_64"
-PYTORCH_LINUX_PY_35_WHL="torch-${WHL_VERSION}-cp35-cp36m-linux_x86_64"
+PYTORCH_LINUX_PY_35_WHL="torch-${WHL_VERSION}-cp35-cp35m-linux_x86_64"
 PYTORCH_LINUX_PY_36_WHL="torch-${WHL_VERSION}-cp36-cp36m-linux_x86_64"
 
 # Cloudfront path for the builds
