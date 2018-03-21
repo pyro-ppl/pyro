96044b5eebf228eecb89b9de4bbf4491a150310b
Martin Jankowiak <jankowiak@uber.com>
2018-03-20 19:49:10 -0700

    add test

--- tests/integration_tests/test_advi.py
+++ tests/integration_tests/test_advi.py
@@ -1,3 +1,5 @@
+import logging
+
 import torch
 
 import pyro
@@ -8,6 +10,8 @@ import pytest
 from pyro.infer import SVI, ADVIDiagonalNormal, ADVIMultivariateNormal
 from tests.common import assert_equal
 from tests.integration_tests.test_conjugate_gaussian_models import GaussianChain
+
+logger = logging.getLogger(__name__)
 
 
 # simple test model to test ADVI guide construction
@@ -50,14 +54,14 @@ class ADVIGaussianChain(GaussianChain):
         self.do_test_advi(3, reparameterized=True, n_steps=10001)
 
     def do_test_advi(self, N, reparameterized, n_steps):
-        print("\nGoing to do ADVIGaussianChain test...")
+        logger.debug("\nGoing to do ADVIGaussianChain test...")
         pyro.clear_param_store()
         self.setUp()
         self.setup_chain(N)
         self.compute_target(N)
         self.advi = ADVIMultivariateNormal(self.model)
-        print("target advi_loc:", self.target_advi_mus[1:].detach().numpy())
-        print("target advi_diag_cov:", self.target_advi_diag_cov[1:].detach().numpy())
+        logger.debug("target advi_loc: {}".format(self.target_advi_mus[1:].detach().numpy()))
+        logger.debug("target advi_diag_cov: {}".format(self.target_advi_diag_cov[1:].detach().numpy()))
 
         # TODO speed up with parallel num_particles > 1
         adam = optim.Adam({"lr": .0005, "betas": (0.95, 0.999)})
@@ -67,12 +71,38 @@ class ADVIGaussianChain(GaussianChain):
             svi.step(reparameterized)
 
             if k % 1000 == 0 and k > 0 or k == n_steps - 1:
-                print("[step %d] advi mean parameter:" % k, pyro.param("advi_loc").detach().numpy())
+                logger.debug("[step {}] advi mean parameter: {}".format(k, pyro.param("advi_loc").detach().numpy()))
                 L = pyro.param("advi_lower_cholesky")
                 diag_cov = torch.mm(L, L.t()).diag()
-                print("[step %d] advi_diag_cov:" % k, diag_cov.detach().numpy())
+                logger.debug("[step {}] advi_diag_cov: {}".format(k, diag_cov.detach().numpy()))
 
         assert_equal(pyro.param("advi_loc"), self.target_advi_mus[1:], prec=0.05,
                      msg="advi mean off")
         assert_equal(diag_cov, self.target_advi_diag_cov[1:], prec=0.07,
                      msg="advi covariance off")
+
+
+@pytest.mark.parametrize('advi_class', [ADVIDiagonalNormal, ADVIMultivariateNormal])
+def test_diagonal_gaussians(advi_class):
+    n_steps = 3501 if advi_class == ADVIDiagonalNormal else 6001
+
+    def model():
+        pyro.sample("x", dist.Normal(-0.2, 1.2))
+        pyro.sample("y", dist.Normal(0.2, 0.7))
+
+    advi = advi_class(model)
+    adam = optim.Adam({"lr": .001, "betas": (0.95, 0.999)})
+    svi = SVI(advi.model, advi.guide, adam, loss="ELBO", trace_graph=False)
+
+    for k in range(n_steps):
+        svi.step()
+        if advi_class == ADVIMultivariateNormal:
+            L = pyro.param("advi_lower_cholesky")
+            diag_cov = torch.mm(L, L.t()).diag()
+        else:
+            diag_cov = torch.pow(pyro.param("advi_scale"), 2.0)
+
+    assert_equal(pyro.param("advi_loc"), torch.tensor([-0.2, 0.2]), prec=0.05,
+                 msg="advi mean off")
+    assert_equal(diag_cov, torch.tensor([1.44, 0.49]), prec=0.08,
+                 msg="advi covariance off")
