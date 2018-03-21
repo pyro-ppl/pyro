287770f77dc83526d9a015dc975eb8bfa7e8ab6b
Martin Jankowiak <jankowiak@uber.com>
2018-03-20 18:12:00 -0700

    integration test works fine

---
+++ pyro/infer/advi.py
@@ -0,0 +1,107 @@
+import torch
+import pyro
+import pyro.distributions as dist
+import pyro.poutine as poutine
+from pyro.poutine.util import prune_subsample_sites
+from torch.distributions import transform_to, constraints
+
+
+def _product(shape):
+    """
+    Computes the product of the dimensions of a given shape tensor
+    """
+    result = 1
+    for size in shape:
+        result *= size
+    return result
+
+
+class ADVI(object):
+    """
+    Base class for implementations of ADVI. Each derived class implements its own
+    :meth:`sample_latent` method. Assumes model structure and latent dimension are fixed.
+    """
+    def __init__(self, model):
+        self.prototype_trace = None
+        self.base_model = model
+
+    def setup_prototype(self, *args, **kwargs):
+        # run the model so we can inspect its structure
+        self.prototype_trace = poutine.block(poutine.trace(self.base_model).get_trace)(*args, **kwargs)
+        self.prototype_trace = prune_subsample_sites(self.prototype_trace)
+        self.latent_dim = sum(site["value"].view(-1).size(0)
+                              for site in self.prototype_trace.nodes.values()
+                              if site["type"] == "sample" and not site["is_observed"])
+
+    def sample_latent(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def guide(self, *args, **kwargs):
+        # if we've never run the model before, do so now so we can inspect the model structure
+        if self.prototype_trace is None:
+            self.setup_prototype(*args, **kwargs)
+
+        latent = self.sample_latent(*args, **kwargs)
+        pos = 0
+        for name, site in self.prototype_trace.nodes.items():
+            if site["type"] == "sample" and not site["is_observed"]:
+                shape = site["fn"].shape()
+                size = _product(shape)
+                unconstrained_value = latent[pos:pos + size].view(shape)
+                pos += size
+                value = transform_to(site["fn"].support)(unconstrained_value)
+                pyro.sample(name, dist.Delta(value))
+        assert pos == len(latent)
+
+    def model(self, *args, **kwargs):
+        # wrap sample statement with a 0.0 poutine.scale to zero out unwanted score
+        with poutine.scale("advi_scope", 0.0):
+            self.sample_latent(*args, **kwargs)
+        # actual model sample statements shouldn't be zeroed out
+        return self.base_model(*args, **kwargs)
+
+
+class ADVIMultivariateNormal(ADVI):
+    """
+    This implementation of ADVI uses a Cholesky factorization of a Multivariate
+    Normal distribution to construct a guide over the entire latent space.
+
+    By default the mean vector is initialized to zero and the Cholesky factor
+    is initialized to the identity.  To change this default behavior the user
+    should call :func:`pyro.param` before beginning inference, e.g.::
+
+        latent_dim = 10
+        pyro.param("advi_loc", torch.randn(latent_dim))
+        pyro.param("advi_cholesky_factor", torch.tril(torch.rand(latent_dim)),
+                   constraint=constraints.lower_cholesky)
+    """
+    # sample the (single) multivariate normal latent used in the advi guide
+    def sample_latent(self, *args, **kwargs):
+        loc = pyro.param("advi_loc", torch.zeros(self.latent_dim))
+        lower_cholesky = pyro.param("advi_lower_cholesky", torch.eye(self.latent_dim),
+                                    constraint=constraints.lower_cholesky)
+        cov = torch.mm(lower_cholesky, lower_cholesky.t())
+        # TODO use Multivariate normal that consumes L directly
+        return pyro.sample("_advi_latent", dist.MultivariateNormal(loc, cov))
+
+
+class ADVIDiagonalNormal(ADVI):
+    """
+    This implementation of ADVI uses a diagonal Normal distribution to
+    construct a guide over the entire latent space.
+
+    By default the mean vector is initialized to zero and the scale is
+    initialized to the identity.  To change this default behavior the user
+    should call :func:`pyro.param` before beginning inference, e.g.::
+
+        latent_dim = 10
+        pyro.param("advi_loc", torch.randn(latent_dim))
+        pyro.param("advi_scale", torch.ones(latent_dim),
+                   constraint=constraints.positive)
+    """
+    # sample the (single) multivariate normal latent used in the advi guide
+    def sample_latent(self, *args, **kwargs):
+        loc = pyro.param("advi_loc", torch.zeros(self.latent_dim))
+        scale = pyro.param("advi_scale", torch.ones(self.latent_dim),
+                           constraint=constraints.positive)
+        return pyro.sample("_advi_latent", dist.Normal(loc, scale).reshape(extra_event_dims=1))
---
+++ tests/integration_tests/test_advi.py
@@ -0,0 +1,78 @@
+import torch
+
+import pyro
+import pyro.distributions as dist
+import pyro.optim as optim
+import pyro.poutine as poutine
+import pytest
+from pyro.infer import SVI, ADVIDiagonalNormal, ADVIMultivariateNormal
+from tests.common import assert_equal
+from tests.integration_tests.test_conjugate_gaussian_models import GaussianChain
+
+
+# simple test model to test ADVI guide construction
+def test_model():
+    pyro.sample("z1", dist.Normal(0.0, 1.0))
+    pyro.sample("z2", dist.Normal(torch.zeros(3), 2.0 * torch.ones(3)))
+
+
+@pytest.mark.parametrize("advi_implementation", [ADVIMultivariateNormal, ADVIDiagonalNormal])
+def test_advi_scores(advi_implementation):
+    advi = advi_implementation(test_model)
+    guide_trace = poutine.trace(advi.guide).get_trace()
+    model_trace = poutine.trace(poutine.replay(advi.model, guide_trace)).get_trace()
+
+    guide_trace.compute_batch_log_pdf()
+    model_trace.compute_batch_log_pdf()
+
+    assert model_trace.nodes['_advi_latent']['log_pdf'].item() == 0.0
+    assert model_trace.nodes['z1']['log_pdf'].item() != 0.0
+    assert guide_trace.nodes['_advi_latent']['log_pdf'].item() != 0.0
+    assert guide_trace.nodes['z1']['log_pdf'].item() == 0.0
+
+
+# conjugate model to test ADVI logic from end-to-end (this has a non-mean-field posterior)
+class ADVIGaussianChain(GaussianChain):
+
+    # this is gross but we need to convert between different posterior factorizations
+    def compute_target(self, N):
+        self.target_advi_mus = torch.zeros(N + 1)
+        self.target_advi_diag_cov = torch.zeros(N + 1)
+        self.target_advi_mus[-1] = self.target_mus[N].item()
+        self.target_advi_diag_cov[-1] = 1.0 / self.lambda_posts[-1].item()
+        for n in range(N - 1, 0, -1):
+            self.target_advi_mus[n] += self.target_mus[n].item()
+            self.target_advi_mus[n] += self.target_kappas[n].item() * self.target_advi_mus[n + 1]
+            self.target_advi_diag_cov[n] += 1.0 / self.lambda_posts[n].item()
+            self.target_advi_diag_cov[n] += (self.target_kappas[n].item() ** 2) * self.target_advi_diag_cov[n + 1]
+
+    def test_multivariatate_normal_advi(self):
+        self.do_test_advi(3, reparameterized=True)
+
+    def do_test_advi(self, N, reparameterized, n_steps=10001):
+        print("\nGoing to do ADVIGaussianChain test...")
+        pyro.clear_param_store()
+        self.setUp()
+        self.setup_chain(N)
+        self.compute_target(N)
+        self.advi = ADVIMultivariateNormal(self.model)
+        print("target advi_loc:", self.target_advi_mus[1:].detach().numpy())
+        print("target advi_diag_cov:", self.target_advi_diag_cov[1:].detach().numpy())
+
+        # TODO speed up with parallel num_particles > 1
+        adam = optim.Adam({"lr": .0005, "betas": (0.95, 0.999)})
+        svi = SVI(self.advi.model, self.advi.guide, adam, loss="ELBO", trace_graph=False)
+
+        for k in range(n_steps):
+            svi.step(reparameterized)
+
+            if k % 1000 == 0 and k > 0 or k == n_steps - 1:
+                print("[step %d] advi mean parameter:" % k, pyro.param("advi_loc").detach().numpy())
+                L = pyro.param("advi_lower_cholesky")
+                diag_cov = torch.mm(L, L.t()).diag()
+                print("[step %d] advi_diag_cov:" % k, diag_cov.detach().numpy())
+
+        assert_equal(pyro.param("advi_loc"), self.target_advi_mus[1:], prec=0.05,
+                     msg="advi mean off")
+        assert_equal(diag_cov, self.target_advi_diag_cov[1:], prec=0.07,
+                     msg="advi covariance off")
