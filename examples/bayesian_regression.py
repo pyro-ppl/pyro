c90fcf5713f2447b1b6d15d50c697c3f27539b7e
JP <jonathanp.chen@gmail.com>
2018-03-24 18:14:55 -0700

    Update bayesian regression/tutorial example for 0.2 (#923)

--- examples/bayesian_regression.py
+++ examples/bayesian_regression.py
@@ -20,7 +20,8 @@ Learning a function of the form:
 def build_linear_dataset(N, p, noise_std=0.01):
     X = np.random.rand(N, p)
     # use random integer weights from [0, 7]
-    w = np.random.randint(8, size=p)
+    w = np.random.randint(4, size=p)
+    print('w = {}'.format(w))
     # set b = 1
     y = np.matmul(X, w) + np.repeat(1, N) + np.random.normal(0, noise_std, size=N)
     y = y.reshape(N, 1)
@@ -42,7 +43,7 @@ class RegressionModel(nn.Module):
 
 
 N = 100  # size of toy data
-p = 1  # number of features
+p = 2  # number of features
 
 softplus = nn.Softplus()
 regression_model = RegressionModel(p)
@@ -51,10 +52,11 @@ regression_model = RegressionModel(p)
 def model(data):
     # Create unit normal priors over the parameters
     mu = torch.zeros(1, p).type_as(data)
-    sigma = torch.ones(1, p).type_as(data)
+    sigma = 2 * torch.ones(1, p).type_as(data)
     bias_mu = torch.zeros(1).type_as(data)
-    bias_sigma = torch.ones(1).type_as(data)
-    w_prior, b_prior = Normal(mu, sigma), Normal(bias_mu, bias_sigma)
+    bias_sigma = 2 * torch.ones(1).type_as(data)
+    w_prior = Normal(mu, sigma).reshape(extra_event_dims=1)
+    b_prior = Normal(bias_mu, bias_sigma).reshape(extra_event_dims=1)
     priors = {'linear.weight': w_prior, 'linear.bias': b_prior}
     # lift module parameters to random variables sampled from the priors
     lifted_module = pyro.random_module("module", regression_model, priors)
@@ -82,8 +84,8 @@ def guide(data):
     mb_param = pyro.param("guide_mean_bias", b_mu)
     sb_param = softplus(pyro.param("guide_log_sigma_bias", b_log_sig))
     # gaussian guide distributions for w and b
-    w_dist = Normal(mw_param, sw_param)
-    b_dist = Normal(mb_param, sb_param)
+    w_dist = Normal(mw_param, sw_param).reshape(extra_event_dims=1)
+    b_dist = Normal(mb_param, sb_param).reshape(extra_event_dims=1)
     dists = {'linear.weight': w_dist, 'linear.bias': b_dist}
     # overloading the parameters in the module with random samples from the guide distributions
     lifted_module = pyro.random_module("module", regression_model, dists)
@@ -92,7 +94,7 @@ def guide(data):
 
 
 # instantiate optim and inference objects
-optim = Adam({"lr": 0.001})
+optim = Adam({"lr": 0.05})
 svi = SVI(model, guide, optim, loss="ELBO")
 
 
@@ -105,6 +107,7 @@ def get_batch_indices(N, batch_size):
 
 
 def main(args):
+    pyro.clear_param_store()
     data = build_linear_dataset(N, p)
     if args.cuda:
         # make tensors and modules CUDA
