bf35a0c5594030228b807ef53fc0f390d7915953
Neeraj Pradhan <npradhan@uber.com>
2018-03-29 16:41:10 -0700

    use default tensor type

--- examples/dmm/polyphonic_data_loader.py
+++ examples/dmm/polyphonic_data_loader.py
@@ -117,10 +117,10 @@ def get_mini_batch(mini_batch_indices, sequences, seq_lengths, cuda=False):
     # get mask for mini-batch
     mini_batch_mask = get_mini_batch_mask(mini_batch, sorted_seq_lengths)
 
-    # wrap in PyTorch Tensors
-    mini_batch = torch.tensor(mini_batch, dtype=torch.float32)
-    mini_batch_reversed = torch.tensor(mini_batch_reversed, dtype=torch.float32)
-    mini_batch_mask = torch.tensor(mini_batch_mask, dtype=torch.float32)
+    # wrap in PyTorch Tensors, using default tensor type
+    mini_batch = torch.tensor(mini_batch).type(torch.empty(1).type())
+    mini_batch_reversed = torch.tensor(mini_batch_reversed).type(torch.empty(1).type())
+    mini_batch_mask = torch.tensor(mini_batch_mask).type(torch.empty(1).type())
 
     # cuda() here because need to cuda() before packing
     if cuda:
