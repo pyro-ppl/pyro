e6ff8036517db37ee1a7582b4c175e592954bec0
fehiepsi <fehiepsi@gmail.com>
2018-03-30 18:04:00 +0900

    temp save

--- pyro/contrib/gp/models/model.py
+++ pyro/contrib/gp/models/model.py
@@ -7,16 +7,46 @@ from pyro.optim import Adam, PyroOptim
 
 class GPModel(Parameterized):
     """
-    Base class for models used in Gaussian Process.
+    Base class for Gaussian Process (GP) models.
 
-    :param torch.Tensor X: A 1D or 2D tensor of input data for training.
-    :param torch.Tensor y: A tensor of output data for training with
+    GP models are :class:`Parameterized` subclasses. So its parameters can be learned,
+    set priors, or fixed by using corresponding methods from :class:`Parameterized`.
+
+    + You can use MCMC algorithms in :mod:`pyro.infer.mcmc` on :meth:`model` to get posterior samples for your test :func:`torch.tensor` 
+      GP's parameters. For example:
+    
+        >>> xx
+        >>> yy
+        >>> zz
+        
+    + Using SVI on 
+    
+        >>> dd
+        >>> dd
+        >>> dd
+    
+    Or you can train with SVI using the pair :meth:`model`, :meth:`guide`
+    as in `SVI tutorial <http://pyro.ai/examples/svi_part_i.html>`_. The source of
+    :meth:`optimize` shows an example for how to use SVI for GP models.
+
+    :param torch.Tensor X: A 1D or 2D input data for training. Its first dimension is
+        the number of data points.
+    :param torch.Tensor y: An output data for training. Its last dimension is the
         ``y.shape[-1]`` equals to number of data points.
     :param pyro.contrib.gp.kernels.Kernel kernel: A Pyro kernel object.
-    :param torch.Size latent_shape: Shape for latent processes. By default, it equals
-        to output batch shape ``y.shape[:-1]``. For the multi-class classification
-        problems, ``latent_shape[-1]`` should corresponse to the number of classes.
     :param float jitter: An additional jitter to help stablize Cholesky decomposition.
+    :param str name: Name of this model.
+    
+    Example::
+
+        >>> X = torch.tensor([[1, 5, 3], [4, 3, 7]])  # 2 data points
+        >>> y = torch.tensor([2, 1])
+        >>> kernel = gp.kernels.RBF(input_dim=3)
+        >>> gpr = gp.models.GPRegression(X, y, kernel)
+        >>> gpr.optimize()
+        >>> Xnew = torch.tensor([[2, 3, 1]])  # 1 test data
+        >>> f_loc, f_cov = gpr(Xnew, full_cov=True)
+
     """
     def __init__(self, X, y, kernel, jitter=1e-6, name=None):
         super(GPModel, self).__init__(name)
@@ -36,8 +66,9 @@ class GPModel(Parameterized):
             raise ValueError("Expected input tensor of 1 or 2 dimensions, "
                              "actual dim = {}".format(X.dim()))
         if y is not None and X.shape[0] != y.shape[-1]:
-            raise ValueError("Expected the number of data inputs equal to the number of data "
-                             "outputs, but got {} and {}.".format(X.shape[0], y.shape[-1]))
+            raise ValueError("Expected the number of data inputs equal to the number "
+                             "of data outputs, but got {} and {}."
+                             .format(X.shape[0], y.shape[-1]))
         self.X = X
         self.y = y
 
@@ -58,13 +89,14 @@ class GPModel(Parameterized):
         A convenient method to optimize parameters for the Gaussian Process model
         using SVI.
 
-        :param pyro.optim.PyroOptim optimizer: Optimizer.
+        :param PyroOptim optimizer: Optimizer.
         :param int num_steps: Number of steps to run SVI.
         :returns: losses of the training procedure
         :rtype: list
         """
         if not isinstance(optimizer, PyroOptim):
-            raise ValueError("Optimizer should be an instance of pyro.optim.PyroOptim class.")
+            raise ValueError("Optimizer should be an instance of "
+                             "pyro.optim.PyroOptim class.")
         svi = SVI(self.model, self.guide, optimizer, loss="ELBO")
         losses = []
         for i in range(num_steps):
