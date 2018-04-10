0afbb5db5d7ddb3ad6637ad53211795380d3a4f7
Eli <eli.bingham@uber.com>
2018-04-10 15:39:40 -0700

    add all aliases to decorators

--- pyro/poutine/decorators.py
+++ pyro/poutine/decorators.py
@@ -20,3 +20,77 @@ from .trace_poutine import TraceMessenger
 
 
 block = BlockMessenger
+condition = ConditionMessenger
+enum = EnumerateMessenger
+escape = EscapeMessenger
+indep = IndepMessenger
+infer_config = InferConfigMessenger
+lift = LiftMessenger
+replay = ReplayMessenger
+scale = ScaleMessenger
+trace = TraceMessenger
+
+
+def do(data):
+
+    # @do(data)
+    # def fn(...):
+    #     ...
+
+    def _fn(fn):
+        return block(hide=list(data.keys()))(
+            condition(data=data)(fn))
+
+    return _fn
+
+
+def queue(queue, max_tries=None,
+          extend_fn=None, escape_fn=None, num_samples=None):
+    """
+    :param queue: a queue data structure like multiprocessing.Queue to hold partial traces
+    :param max_tries: maximum number of attempts to compute a single complete trace
+    :param extend_fn: function (possibly stochastic) that takes a partial trace and a site
+    and returns a list of extended traces
+    :param escape_fn: function (possibly stochastic) that takes a partial trace and a site
+    and returns a boolean value to decide whether to exit
+    :param num_samples: optional number of extended traces for extend_fn to return
+    :returns: stochastic function wrapped in poutine logic
+
+    Given a queue,
+    return wrapper that will get a return value from a complete trace in the queue
+    """
+
+    if max_tries is None:
+        max_tries = int(1e6)
+
+    if extend_fn is None:
+        extend_fn = util.enum_extend
+
+    if escape_fn is None:
+        escape_fn = util.discrete_escape
+
+    if num_samples is None:
+        num_samples = -1
+
+    def queue_decorator(fn):
+        def _fn(*args, **kwargs):
+
+            for i in xrange(max_tries):
+                assert not queue.empty(), \
+                    "trying to get() from an empty queue will deadlock"
+
+                next_trace = queue.get()
+                try:
+                    ftr = trace(escape(replay(fn, next_trace),
+                                       functools.partial(escape_fn, next_trace)))
+                    return ftr(*args, **kwargs)
+                except util.NonlocalExit as site_container:
+                    site_container.reset_stack()
+                    for tr in extend_fn(ftr.trace.copy(), site_container.site,
+                                        num_samples=num_samples):
+                        queue.put(tr)
+
+            raise ValueError("max tries ({}) exceeded".format(str(max_tries)))
+        return _fn
+
+    return queue_decorator
