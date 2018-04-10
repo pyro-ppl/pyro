2a5bac11dd7385d9b84812c49df4103c0e5d1d14
jpchen <jonathanp.chen@gmail.com>
2018-04-09 22:09:14 -0700

    fixes

--- examples/gmm_advi.py
+++ examples/gmm_advi.py
@@ -7,10 +7,11 @@ from pyro.infer import SVI, ADVIDiagonalNormal
 import pyro.optim as optim
 from pdb import set_trace as bb
 
+
 def model(K, N, D, y, alpha0, alpha0_vec):
     theta = pyro.sample("theta", dist.Dirichlet(alpha0_vec))
-    mu = pyro.sample("mu", dist.Normal(torch.zeros(K,D), torch.ones(K,D)*10.))
-    sigma = pyro.sample("sigma", dist.LogNormal(torch.ones(K,D), torch.ones(K,D))) #.abs()
+    mu = pyro.sample("mu", dist.Normal(torch.zeros(K, D), torch.ones(K, D)*10.))
+    sigma = pyro.sample("sigma", dist.LogNormal(torch.ones(K, D), torch.ones(K, D)))
     # sigma = transform_to(dist.Normal.arg_constraints['scale'])(sigma)
 
     ps = torch.zeros(K)
@@ -19,7 +20,6 @@ def model(K, N, D, y, alpha0, alpha0_vec):
         for k in range(K):
             ps[k] = torch.log(theta[k]) + torch.sum(dist.Normal(mu[k], sigma[k]).log_prob(y[n]))
         pyro.sample("ps[%d]" % (n), dist.Bernoulli(log_sum_exp(ps)), obs=(1))
-
 
 
 def get_data(fname, varnames):
@@ -36,35 +36,27 @@ def get_data(fname, varnames):
         d[var_name] = val
     return tuple([d[k] for k in varnames]), d
 
+
 def transformed_data(K, N, D, y, alpha0):
     alpha0_vec = torch.ones(K)*alpha0
     return alpha0_vec
 
+
 def main(K, N, D, y, alpha0, alpha0_vec):
     advi = ADVIDiagonalNormal(model)
-    adam = optim.Adam({'lr': 1e-3,})
+    adam = optim.Adam({'lr': 1e-3})
     svi = SVI(advi.model, advi.guide, adam, loss="ELBO")
     for i in range(100):
         loss = svi.step(K, N, D, y, alpha0, alpha0_vec)
-        print('loss=',loss)
-        if i%5 == 0:
+        print('loss=', loss)
+        if i % 5 == 0:
             d = advi.median()
             print({k: d[k] for k in ["mu", "theta", "sigma"]})
 
-import pystan
 
-def run_stan(data):
-    with open("gmm_diag.stan","r") as f:
-        code=f.read()
-
-    sm = pystan.StanModel(model_code=code)
-    fit = sm.sampling(data=data)
-    site_values = fit.extract(permuted=True)
-    bb()
-if __name__== "__main__":
+if __name__ == "__main__":
     varnames = ["K", "N", "D", "y", "alpha0"]
-    (K, N, D, y, alpha0), data = get_data("training.data.json", varnames)
+    (K, N, D, y, alpha0), data = get_data("data/training.data.json", varnames)
     alpha0_vec = transformed_data(K, N, D, y, alpha0)
     args = (K, N, D, y, alpha0, alpha0_vec)
-    run_stan(data)
     main(*args)
