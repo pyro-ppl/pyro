dd4a7de00f080a951ef7fbfc02718bf4e894cd42
Fritz Obermeyer <fritzo@uber.com>
2018-04-09 21:12:32 -0700

    Update contrib.named examples

--- examples/contrib/named/mixture.py
+++ examples/contrib/named/mixture.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, division, print_function
 import argparse
 
 import torch
-import torch.nn.functional as F
+from torch.distributions import constraints
 
 import pyro
 import pyro.distributions as dist
@@ -21,9 +21,9 @@ def model(data, k):
     latent = named.Object("latent")
 
     # Create parameters for a Gaussian mixture model.
-    latent.probs.param_(torch.tensor(torch.ones(k) / k, requires_grad=True))
-    latent.locs.param_(torch.zeros(k, requires_grad=True))
-    latent.scales.param_(torch.ones(k, requires_grad=True))
+    latent.probs.param_(torch.ones(k) / k, constraint=constraints.simplex)
+    latent.locs.param_(torch.zeros(k))
+    latent.scales.param_(torch.ones(k), constraint=constraints.positive)
 
     # Observe all the data. We pass a local latent in to the local_model.
     latent.local = named.List()
@@ -32,7 +32,7 @@ def model(data, k):
 
 
 def local_model(latent, ps, locs, scales, obs=None):
-    i = latent.id.sample_(dist.Categorical(F.softmax(ps, dim=-1)))
+    i = latent.id.sample_(dist.Categorical(ps))
     return latent.x.sample_(dist.Normal(locs[i], scales[i]), obs=obs)
 
 
@@ -46,21 +46,26 @@ def guide(data, k):
 
 def local_guide(latent, k):
     # The local guide simply guesses category assignments.
-    latent.probs.param_(torch.tensor(torch.ones(k) / k, requires_grad=True))
-    latent.id.sample_(dist.Categorical(F.softmax(latent.probs, dim=-1)))
+    latent.probs.param_(torch.ones(k) / k, constraint=constraints.positive)
+    latent.id.sample_(dist.Categorical(latent.probs))
 
 
 def main(args):
+    pyro.set_rng_seed(0)
+    pyro.enable_validation()
+
     optim = Adam({"lr": 0.1})
     inference = SVI(model, guide, optim, loss="ELBO")
     data = torch.tensor([0.0, 1.0, 2.0, 20.0, 30.0, 40.0])
     k = 2
 
     print('Step\tLoss')
+    loss = 0.0
     for step in range(args.num_epochs):
-        if step % 1000 == 0:
-            loss = inference.step(data, k)
+        if step and step % 10 == 0:
             print('{}\t{:0.5g}'.format(step, loss))
+            loss = 0.0
+        loss += inference.step(data, k)
 
     print('Parameters:')
     for name in sorted(pyro.get_param_store().get_all_param_names()):
@@ -69,6 +74,6 @@ def main(args):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="parse args")
-    parser.add_argument('-n', '--num-epochs', default=10000, type=int)
+    parser.add_argument('-n', '--num-epochs', default=200, type=int)
     args = parser.parse_args()
     main(args)
