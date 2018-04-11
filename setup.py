dc3346a75bcc47eaa1ebf6b0fd23786cfee907a5
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2018-04-11 12:19:45 -0700

    Fix setup.py packages list (#1008)
    * Fix setup.py packages list
    
    * Avoid editable install in travis

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import subprocess
 import sys
 
 import os
-from setuptools import setup
+from setuptools import find_packages, setup
 
 PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 VERSION = """
@@ -65,7 +65,7 @@ setup(
     version=version,
     description='A Python library for probabilistic modeling and inference',
     long_description=long_description,
-    packages=['pyro'],
+    packages=find_packages(include=['pyro', 'pyro.*']),
     url='http://pyro.ai',
     author='Uber AI Labs',
     author_email='pyro@uber.com',
