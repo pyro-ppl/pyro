f9b649fde13c63f24cba6774ee299e33dc60c726
Eli <eli.bingham@uber.com>
2018-04-10 17:16:00 -0700

    fix import in pyro.infer.importance

--- pyro/infer/importance.py
+++ pyro/infer/importance.py
@@ -2,7 +2,8 @@ from __future__ import absolute_import, division, print_function
 
 import logging
 
-import pyro.poutine as poutine
+# import pyro.poutine as poutine
+import pyro.poutine.decorators as poutine
 
 from .abstract_infer import TracePosterior
 
