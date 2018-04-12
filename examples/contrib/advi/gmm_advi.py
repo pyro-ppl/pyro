058fbf7b5b64b81460c66b06b23ad4b9c1b1b05c
jpchen <jonathanp.chen@gmail.com>
2018-04-11 17:41:21 -0700

    debug

--- examples/contrib/advi/gmm_advi.py
+++ examples/contrib/advi/gmm_advi.py
@@ -1,26 +1,25 @@
 import torch
 import pyro
 import pyro.distributions as dist
-from pyro.distributions.util import log_sum_exp
-from torch.distributions import constraints, transform_to
 from pyro.infer import SVI
 from pyro import poutine
-from pyro.contrib.autoguide import (ADVIDiagonalNormal, ADVIDiscreteParallel,
+from pyro.contrib.autoguide import (ADVIDiagonalNormal, ADVIDiscreteParallel,  # noqa: F401
                                     ADVIMaster, ADVIMultivariateNormal)
 import pyro.optim as optim
 from utils import get_data
-from pdb import set_trace as bb
 
 
 def model(K, alpha0, y):
     theta = pyro.sample("theta", dist.Dirichlet(alpha0 * torch.ones(K)))
-    mu = pyro.sample("mu", dist.Normal(torch.zeros(K, y.shape[-1]), 10. * torch.ones(K, y.shape[-1])))
-    sigma = pyro.sample("sigma", dist.LogNormal(torch.ones(K, y.shape[-1]), torch.ones(K, y.shape[-1])))
-    # sigma = transform_to(dist.Normal.arg_constraints['scale'])(sigma)
+    mu = pyro.sample("mu", dist.Normal(torch.zeros(K, y.shape[-1]),
+                                       10. * torch.ones(K, y.shape[-1])).reshape(extra_event_dims=1))
+    sigma = pyro.sample("sigma", dist.LogNormal(torch.ones(K, y.shape[-1]),
+                                                torch.ones(K, y.shape[-1])).reshape(extra_event_dims=1))
 
     with pyro.iarange('data', len(y)):
-        assign = pyro.sample('mixture', dist.Categorical(theta))
-        pyro.sample('obs', dist.Normal(mu[assign], sigma[assign]), obs=y[assign])
+        assign = pyro.sample('mixture', dist.Categorical(theta.unsqueeze(0).expand(len(y), K)))
+        obs_dist = dist.Normal(mu[assign], sigma[assign]).reshape(extra_event_dims=1)
+        pyro.sample('obs', obs_dist, obs=y[assign])
 
 
 def main(args):
@@ -29,13 +28,13 @@ def main(args):
     advi.add(ADVIDiscreteParallel(poutine.block(model, expose=["mixture"])))
 
     adam = optim.Adam({'lr': 1e-3})
-    svi = SVI(advi.model, advi.guide, adam, loss="ELBO")
+    svi = SVI(advi.model, advi.guide, adam, loss="ELBO", enum_discrete=True, max_iarange_nesting=1)
     for i in range(100):
         loss = svi.step(*args)
         print('loss=', loss)
-        if i % 5 == 0:
-            d = advi.median()
-#             print({k: d[k] for k in ["mu", "theta", "sigma"]})
+        if i % 10 == 9:
+            d = advi.parts[0].median()
+            print({k: d[k].sum() / 200 for k in ["mu", "theta", "sigma"]})
 
 
 if __name__ == "__main__":
