5be7cd16924381879e33d016659df51b7b607236
Neeraj Pradhan <npradhan@uber.com>
2018-04-22 15:38:08 -0700

    fix mcmc test

--- tests/infer/mcmc/test_mcmc.py
+++ tests/infer/mcmc/test_mcmc.py
@@ -47,7 +47,7 @@ def normal_normal_model(data):
 def test_mcmc_interface():
     data = torch.tensor([1.0])
     kernel = PriorKernel(normal_normal_model)
-    mcmc = MCMC(kernel=kernel, num_samples=800, warmup_steps=100)(data)
+    mcmc = MCMC(kernel=kernel, num_samples=800, warmup_steps=100).run(data)
     marginal = EmpiricalMarginal(mcmc)
     assert_equal(marginal.sample_size, 800)
     sample_mean = marginal.mean
