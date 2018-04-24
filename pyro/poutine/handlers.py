229cd5c58e0b40d6b5e30624a6405ae39e288033
Eli <eli.bingham@uber.com>
2018-04-24 14:48:49 -0700

    moving docstrings around in handlers

--- pyro/poutine/handlers.py
+++ pyro/poutine/handlers.py
@@ -25,19 +25,18 @@ from .trace_messenger import TraceMessenger
 
 def trace(fn=None, graph_type=None, param_only=None):
     """
+    Return a handler that records the inputs and outputs of primitive calls
+    and their dependencies.
+
+    Adds trace data structure site constructors to primitive stacks
+
+    Alias for :class:`TraceMessenger` constructor.
+
     :param fn: a stochastic function (callable containing pyro primitive calls)
     :param graph_type: string that specifies the kind of graph to construct
     :param param_only: if true, only records params and not samples
     :returns: stochastic function wrapped in a TraceHandler
     :rtype: pyro.poutine.TraceHandler
-
-    Alias for TraceHandler constructor.
-
-    Given a callable that contains Pyro primitive calls, return a TraceHandler callable
-    that records the inputs and outputs to those primitive calls
-    and their dependencies.
-
-    Adds trace data structure site constructors to primitive stacks
     """
     msngr = TraceMessenger(graph_type=graph_type, param_only=param_only)
     return msngr(fn) if fn is not None else msngr
@@ -45,18 +44,17 @@ def trace(fn=None, graph_type=None, param_only=None):
 
 def replay(fn=None, trace=None, sites=None):
     """
+    Given a callable that contains Pyro primitive calls,
+    return a callable that runs the original, reusing the values at sites in trace
+    at those sites in the new trace
+
     :param fn: a stochastic function (callable containing pyro primitive calls)
     :param trace: a Trace data structure to replay against
     :param sites: list or dict of names of sample sites in fn to replay against,
         defaulting to all sites
-    :returns: stochastic function wrapped in a ReplayHandler
-    :rtype: pyro.poutine.ReplayHandler
+    :returns: a replay messenger
+    :rtype: pyro.poutine.ReplayMessenger
 
-    Alias for ReplayHandler constructor.
-
-    Given a callable that contains Pyro primitive calls,
-    return a callable that runs the original, reusing the values at sites in trace
-    at those sites in the new trace
     """
     msngr = ReplayMessenger(trace=trace, sites=sites)
     return msngr(fn) if fn is not None else msngr
@@ -64,13 +62,13 @@ def replay(fn=None, trace=None, sites=None):
 
 def lift(fn=None, prior=None):
     """
-    :param fn: function whose parameters will be lifted to random values
-    :param prior: prior function in the form of a Distribution or a dict of stochastic fns
-    :returns: stochastic function wrapped in LiftHandler
-
     Given a stochastic function with param calls and a prior distribution,
     create a stochastic function where all param calls are replaced by sampling from prior.
     Prior should be a callable or a dict of names to callables.
+
+    :param fn: function whose parameters will be lifted to random values
+    :param prior: prior function in the form of a Distribution or a dict of stochastic fns
+    :returns: stochastic function wrapped in LiftHandler
     """
     msngr = LiftMessenger(prior=prior)
     return msngr(fn) if fn is not None else msngr
@@ -78,6 +76,9 @@ def lift(fn=None, prior=None):
 
 def block(fn=None, hide=None, expose=None, hide_types=None, expose_types=None):
     """
+    Given a callable that contains Pyro primitive calls,
+    selectively hide some of those calls from poutines higher up the stack
+
     :param fn: a stochastic function (callable containing pyro primitive calls)
     :param hide: list of site names to hide
     :param expose: list of site names to be exposed while all others hidden
@@ -85,11 +86,6 @@ def block(fn=None, hide=None, expose=None, hide_types=None, expose_types=None):
     :param expose_types: list of site types to be exposed while all others hidden
     :returns: stochastic function wrapped in a BlockHandler
     :rtype: pyro.poutine.BlockHandler
-
-    Alias for BlockHandler constructor.
-
-    Given a callable that contains Pyro primitive calls,
-    selectively hide some of those calls from poutines higher up the stack
     """
     msngr = BlockMessenger(hide=hide, expose=expose,
                            hide_types=hide_types, expose_types=expose_types)
@@ -98,17 +94,15 @@ def block(fn=None, hide=None, expose=None, hide_types=None, expose_types=None):
 
 def escape(fn=None, escape_fn=None):
     """
-    :param fn: a stochastic function (callable containing pyro primitive calls)
-    :param escape_fn: function that takes a partial trace and a site,
-        and returns a boolean value to decide whether to exit at that site
-    :returns: stochastic function wrapped in EscapeHandler
-
-    Alias for EscapeHandler constructor.
-
     Given a callable that contains Pyro primitive calls,
     evaluate escape_fn on each site, and if the result is True,
     raise a NonlocalExit exception that stops execution
     and returns the offending site.
+
+    :param fn: a stochastic function (callable containing pyro primitive calls)
+    :param escape_fn: function that takes a partial trace and a site,
+        and returns a boolean value to decide whether to exit at that site
+    :returns: stochastic function wrapped in EscapeHandler
     """
     msngr = EscapeMessenger(escape_fn)
     return msngr(fn) if fn is not None else msngr
@@ -116,17 +110,16 @@ def escape(fn=None, escape_fn=None):
 
 def condition(fn=None, data=None):
     """
+    Given a stochastic function with some sample statements
+    and a dictionary of observations at names,
+    change the sample statements at those names into observes
+    with those values
+
     :param fn: a stochastic function (callable containing pyro primitive calls)
     :param data: a dict or a Trace
     :returns: stochastic function wrapped in a ConditionHandler
     :rtype: pyro.poutine.ConditionHandler
 
-    Alias for ConditionHandler constructor.
-
-    Given a stochastic function with some sample statements
-    and a dictionary of observations at names,
-    change the sample statements at those names into observes
-    with those values
     """
     msngr = ConditionMessenger(data=data)
     return msngr(fn) if fn is not None else msngr
@@ -134,14 +127,13 @@ def condition(fn=None, data=None):
 
 def infer_config(fn=None, config_fn=None):
     """
-    :param fn: a stochastic function (callable containing pyro primitive calls)
-    :param config_fn: a callable taking a site and returning an infer dict
-
-    Alias for :class:`~pyro.poutine.infer_config_messenger.InferConfigHandler` constructor.
-
     Given a callable that contains Pyro primitive calls
     and a callable taking a trace site and returning a dictionary,
     updates the value of the infer kwarg at a sample site to config_fn(site)
+
+    :param fn: a stochastic function (callable containing pyro primitive calls)
+    :param config_fn: a callable taking a site and returning an infer dict
+    :returns: stochastic function wrapped in InferConfigHandler
     """
     msngr = InferConfigMessenger(config_fn)
     return msngr(fn) if fn is not None else msngr
@@ -149,14 +141,14 @@ def infer_config(fn=None, config_fn=None):
 
 def scale(fn=None, scale=None):
     """
-    :param scale: a positive scaling factor
-    :rtype: pyro.poutine.ScaleMessenger
-
-    Alias for ScaleMessenger constructor.
-
     Given a stochastic function with some sample statements and a positive
     scale factor, scale the score of all sample and observe sites in the
     function.
+
+    :param fn: a stochastic function (callable containing Pyro primitive calls)
+    :param scale: a positive scaling factor
+    :returns: stochastic function wrapped in a ScaleHandler
+    :rtype: pyro.poutine.ScaleMessenger
     """
     msngr = ScaleMessenger(scale=scale)
     # XXX temporary compatibility fix
@@ -165,7 +157,7 @@ def scale(fn=None, scale=None):
 
 def indep(fn=None, name=None, size=None, dim=None):
     """
-    Alias for IndepMessenger constructor.
+    Note: Low-level; use `pyro.iarange` instead.
 
     This messenger keeps track of stack of independence information declared by
     nested ``irange`` and ``iarange`` contexts. This information is stored in
@@ -178,14 +170,12 @@ def indep(fn=None, name=None, size=None, dim=None):
 
 def enum(fn=None, first_available_dim=None):
     """
+    Enumerates in parallel over discrete sample sites marked
+    ``infer={"enumerate": "parallel"}``.
+
     :param int first_available_dim: The first tensor dimension (counting
         from the right) that is available for parallel enumeration. This
         dimension and all dimensions left may be used internally by Pyro.
-
-    Alias for EnumerateMessenger constructor.
-
-    Enumerates in parallel over discrete sample sites marked
-    ``infer={"enumerate": "parallel"}``.
     """
     msngr = EnumerateMessenger(first_available_dim=first_available_dim)
     return msngr(fn) if fn is not None else msngr
@@ -197,17 +187,16 @@ def enum(fn=None, first_available_dim=None):
 
 def do(fn=None, data=None):
     """
-    :param fn: a stochastic function (callable containing pyro primitive calls)
-    :param data: a dict or a Trace
-    :returns: stochastic function wrapped in a BlockHandler and ConditionHandler
-    :rtype: pyro.poutine.BlockHandler
-
     Given a stochastic function with some sample statements
     and a dictionary of values at names,
     set the return values of those sites equal to the values
     and hide them from the rest of the stack
     as if they were hard-coded to those values
     by using BlockHandler
+
+    :param fn: a stochastic function (callable containing pyro primitive calls)
+    :param data: a dict or a Trace
+    :returns: stochastic function wrapped in a BlockHandler and ConditionHandler
     """
     def wrapper(wrapped):
         return block(condition(wrapped, data=data), hide=list(data.keys()))
@@ -217,6 +206,9 @@ def do(fn=None, data=None):
 def queue(fn=None, queue=None, max_tries=None,
           extend_fn=None, escape_fn=None, num_samples=None):
     """
+    Given a stochastic function and a queue,
+    return a return value from a complete trace in the queue
+
     :param fn: a stochastic function (callable containing pyro primitive calls)
     :param queue: a queue data structure like multiprocessing.Queue to hold partial traces
     :param max_tries: maximum number of attempts to compute a single complete trace
@@ -226,9 +218,6 @@ def queue(fn=None, queue=None, max_tries=None,
         and returns a boolean value to decide whether to exit
     :param num_samples: optional number of extended traces for extend_fn to return
     :returns: stochastic function wrapped in poutine logic
-
-    Given a stochastic function and a queue,
-    return a return value from a complete trace in the queue
     """
 
     if max_tries is None:
