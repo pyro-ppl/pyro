65a9e9b85467498a494f8c6b85aee1e5c06e8df8
Fritz Obermeyer <fritzo@uber.com>
2018-05-17 21:55:10 -0700

    Error if init_tensor shape disagrees with existing tensor shape

--- pyro/params/param_store.py
+++ pyro/params/param_store.py
@@ -106,6 +106,11 @@ class ParamStoreDict(object):
             # keep track of each tensor and it's name
             self._param_to_name[unconstrained_param] = name
 
+        elif init_tensor is not None and not callable(init_tensor):
+            if self._params[name].shape != init_tensor.shape:
+                raise ValueError("param {} init tensor shape does not match existing value: {} vs {}".format(
+                    name, init_tensor.shape, self._params[name].shape))
+
         # get the guaranteed to exist param
         unconstrained_param = self._params[name]
 
