2b27e5a3c6ab66aa8773d78941b1d8d2815d2489
JP <jonathanp.chen@gmail.com>
2018-05-21 12:18:13 -0700

    autoguide error on model with no latents (#1143)

--- pyro/contrib/autoguide/__init__.py
+++ pyro/contrib/autoguide/__init__.py
@@ -267,6 +267,8 @@ class AutoContinuous(AutoGuide):
             self._cond_indep_stacks[name] = site["cond_indep_stack"]
 
         self.latent_dim = sum(_product(shape) for shape in self._unconstrained_shapes.values())
+        if self.latent_dim == 0:
+            raise RuntimeError('{} found no latent variables; Use an empty guide instead'.format(type(self).__name__))
 
     def sample_latent(self, *args, **kwargs):
         """
