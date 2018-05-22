21b8cd510d9142f5b6cee24ff2b6f28b66c5b6eb
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2018-05-22 11:14:52 -0700

    Bump to version 0.2.1 (#1156)

--- pyro/__init__.py
+++ pyro/__init__.py
@@ -8,7 +8,7 @@ from pyro.primitives import (clear_param_store, enable_validation, get_param_sto
                              random_module, sample, validation_enabled)
 from pyro.util import set_rng_seed
 
-version_prefix = '0.2.0'
+version_prefix = '0.2.1'
 
 # Get the __version__ string from the auto-generated _version.py file, if exists.
 try:
