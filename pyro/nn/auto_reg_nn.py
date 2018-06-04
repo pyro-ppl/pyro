fb8f3e17304bc91eafc25186fa7e2e06ace8cbad
jpchen <jonathanp.chen@gmail.com>
2018-06-04 16:52:21 -0700

    fix another bug in auto reg nn

--- pyro/nn/auto_reg_nn.py
+++ pyro/nn/auto_reg_nn.py
@@ -83,8 +83,11 @@ class AutoRegressiveNN(nn.Module):
         self.mask2 = torch.zeros(input_dim * self.output_dim_multiplier, hidden_dim)
 
         for k in range(hidden_dim):
-            # fill in mask1
-            m_k = self.mask_encoding[k].item()
+            if self.mask_encoding.dim():
+                # fill in mask1
+                m_k = self.mask_encoding[k].item()
+            else:
+                m_k = self.mask_encoding.item()
             slice_k = torch.cat([torch.ones(m_k), torch.zeros(input_dim - m_k)])
             for j in range(input_dim):
                 self.mask1[k, self.permutation[j]] = slice_k[j]
