4be53531e9d5fd1e939464514e49e0200914c8ab
jpchen <jonathanp.chen@gmail.com>
2018-06-26 21:32:07 -0600

    calc eigenvalues

--- pyro/ops/newton.py
+++ pyro/ops/newton.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import, division, print_function
 
+import numpy as np
+
 import torch
 from torch.autograd import grad
 
@@ -28,20 +30,17 @@ def _eig_3d(H):
         q = (H[..., 0, 0] + H[..., 1, 1] + H[..., 2, 2]) / 3
         p2 = (H[..., 0, 0] - q).pow(2) + (H[..., 1, 1] - q).pow(2) + (H[..., 2, 2] - q).pow(2) + 2 * p1
         p = torch.sqrt(p2 / 6)
-        B = (1 / p) * (H - q * I)
+        B = (1 / p) * (H - q * torch.eye(3).expand_as(H))
         r = _determinant_3d(B) / 2
+        phi = H.new_ones(H.size())
+        phi[r <= -1] = np.pi / 3
+        phi[r >= 1] = 0.
+        phi[phi == 1] = torch.acos(r[-1 < r < 1]) / 3
 
-    if r <= -1:
-       phi = pi / 3
-    elif r >= 1:
-       phi = 0
-    else:
-       phi = acos(r) / 3
-
-       # eig3 <= eig2 <= eig1
-       eig1 = q + 2 * p * cos(phi)
-       eig3 = q + 2 * p * cos(phi + (2*pi/3))
-       eig2 = 3 * q - eig1 - eig3
+        # eig3 <= eig2 <= eig1
+        eig1 = q + 2 * p * torch.cos(phi)
+        eig3 = q + 2 * p * torch.cos(phi + (2 * np.pi/3))
+        eig2 = 3 * q - eig1 - eig3
     return eig1, eig2, eig3
 
 
