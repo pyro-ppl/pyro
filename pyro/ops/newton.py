fix `1d solver, fix tests, fix 3d_eig masking

pyro/ops/newton.py
@@ -35,10 +35,9 @@ def _eig_3d(H):
     else:
         B = (1 / p).unsqueeze(-1).unsqueeze(-1) * (H - q.unsqueeze(-1).unsqueeze(-1) * torch.eye(3))
     r = _determinant_3d(B) / 2
-    phi = H.new_ones(H.size())
-    phi[r <= -1] = np.pi / 3
-    phi[r >= 1] = 0.
-    phi[phi == 1] = torch.acos(r[(r < 1) & (r > -1)]).unsqueeze(-1).expand(-1, 9).reshape(-1) / 3
+    phi = r.acos().unsqueeze(-1).unsqueeze(-1).expand(r.shape + (3, 3)) / 3
+    phi[r < -1 + 1e-6] = np.pi / 3
+    phi[r > 1 - 1e-6] = 0.
 
     eig1 = q + 2 * p * torch.cos(phi[..., 0, 0])
     eig2 = q + 2 * p * torch.cos(phi[..., 0, 0] + (2 * np.pi/3))
@@ -156,18 +155,14 @@ def newton_step_1d(loss, x, trust_radius=None):
     H = grad(g.sum(), [x], create_graph=True)[0]
     warn_if_nan(g, 'g')
     warn_if_nan(H, 'H')
-
+    Hinv = H.clamp(min=1e-8).reciprocal()
+    dx = -g * Hinv
+    dx[~(dx == dx)] = 0
     if trust_radius is not None:
-        # regularize to keep update within ball of given trust_radius
-        regularizer = (torch.abs(g) / trust_radius - H).clamp_(min=1e-8)
-        warn_if_nan(regularizer, 'regularizer')
-        H = H + regularizer
-
-    # compute newton update
-    Hinv = H.reciprocal()
+        dx.clamp_(min=-trust_radius, max=trust_radius)
 
     # apply update
-    x_new = x.detach() - g * Hinv
+    x_new = x.detach() + dx
     assert x_new.shape == x.shape
     return x_new, Hinv.unsqueeze(-1)
 

tests/ops/test_newton.py
@@ -22,7 +22,7 @@ def random_inside_unit_circle(shape, requires_grad=False):
 
 @pytest.mark.parametrize('batch_shape', [(), (1,), (2,), (10,), (3, 2), (2, 3)])
 @pytest.mark.parametrize('trust_radius', [None, 2.0, 100.0])
-@pytest.mark.parametrize('dims', [2, 3])
+@pytest.mark.parametrize('dims', [1, 2, 3])
 def test_newton_step(batch_shape, trust_radius, dims):
     batch_shape = torch.Size(batch_shape)
     mode = 0.5 * random_inside_unit_circle(batch_shape + (dims,), requires_grad=True)
@@ -70,12 +70,12 @@ def test_newton_step(batch_shape, trust_radius, dims):
 
 
 @pytest.mark.parametrize('trust_radius', [None, 0.1, 1.0, 10.0])
-@pytest.mark.parametrize('dims', [2, 3])
+@pytest.mark.parametrize('dims', [1, 2, 3])
 def test_newton_step_trust(trust_radius, dims):
     batch_size = 100
     batch_shape = torch.Size((batch_size,))
-    mode = random_inside_unit_circle(batch_shape + (dims,), requires_grad=True) - 1
-    x = random_inside_unit_circle(batch_shape + (dims,), requires_grad=True) + 1
+    mode = random_inside_unit_circle(batch_shape + (dims,), requires_grad=True) + 1
+    x = random_inside_unit_circle(batch_shape + (dims,), requires_grad=True) - 1
 
     # create a quadratic loss function
     noise = torch.randn(batch_size, dims, dims)
@@ -94,11 +94,11 @@ def test_newton_step_trust(trust_radius, dims):
     if trust_radius is None:
         assert ((x - x_updated).pow(2).sum(-1) > 1.0).any(), 'test is too weak'
     else:
-        assert ((x - x_updated).pow(2).sum(-1) <= trust_radius**2).all(), 'trust region violated'
+        assert ((x - x_updated).pow(2).sum(-1) <= 1e-8 + trust_radius**2).all(), 'trust region violated'
 
 
 @pytest.mark.parametrize('trust_radius', [None, 0.1, 1.0, 10.0])
-@pytest.mark.parametrize('dims', [2, 3])
+@pytest.mark.parametrize('dims', [1, 2, 3])
 def test_newton_step_converges(trust_radius, dims):
     batch_size = 100
     batch_shape = torch.Size((batch_size,))