5c44ac755caf09b0bbf764b6c02971c32ce38ee5
Eli <eli.bingham@uber.com>
2018-07-17 19:00:26 -0700

    update autoname.scope

--- pyro/contrib/autoname/scoping.py
+++ pyro/contrib/autoname/scoping.py
@@ -5,63 +5,12 @@ a semantically meaningful prefix to names of sample sites.
 """
 import functools
 
+from pyro.poutine.handlers import Handler
 from pyro.poutine.messenger import Messenger
 from pyro.poutine.runtime import apply_stack
 
 
 class ScopeMessenger(Messenger):
-    """
-    ``ScopeMessenger`` is the implementation of :func:`pyro.contrib.autoname.scope`
-    """
-    def __init__(self, prefix=None, inner=None):
-        super(ScopeMessenger, self).__init__()
-        self.prefix = prefix
-        self.inner = inner
-
-    def __enter__(self):
-        if self.prefix is None:
-            raise ValueError("no prefix was provided")
-        if not self.inner:
-            # to accomplish adding a counter to duplicate scopes,
-            # we will treat ScopeMessenger.__enter__ like a sample statement
-            # so that the same mechanism that adds counters to sample names
-            # can be used to add a counter to a scope name
-            msg = {
-                "type": "sample",
-                "name": self.prefix,
-                "fn": lambda: True,
-                "is_observed": False,
-                "args": (),
-                "kwargs": {},
-                "value": None,
-                "infer": {},
-                "scale": 1.0,
-                "cond_indep_stack": (),
-                "done": False,
-                "stop": False,
-                "continuation": None,
-                "PRUNE": True  # this keeps the dummy node from appearing in the trace
-            }
-            apply_stack(msg)
-            self.prefix = msg["name"].split("/")[-1]
-        return super(ScopeMessenger, self).__enter__()
-
-    def __call__(self, fn):
-        if self.prefix is None:
-            self.prefix = fn.__code__.co_name  # fn.__name__
-
-        @functools.wraps(fn)
-        def _fn(*args, **kwargs):
-            with type(self)(prefix=self.prefix, inner=self.inner):
-                return fn(*args, **kwargs)
-        return _fn
-
-    def _pyro_sample(self, msg):
-        msg["name"] = "{}/{}".format(self.prefix, msg["name"])
-        return None
-
-
-def scope(fn=None, prefix=None, inner=None):
     """
     :param fn: a stochastic function (callable containing Pyro primitive calls)
     :param prefix: a string to prepend to sample names (optional if ``fn`` is provided)
@@ -112,5 +61,52 @@ def scope(fn=None, prefix=None, inner=None):
         ...
         >>> assert "model/x" in poutine.trace(model).get_trace()
     """
-    msngr = ScopeMessenger(prefix=prefix, inner=inner)
-    return msngr(fn) if fn is not None else msngr
+    def __init__(self, prefix=None, inner=None):
+        super(ScopeMessenger, self).__init__()
+        self.prefix = prefix
+        self.inner = inner
+
+    def __enter__(self):
+        if self.prefix is None:
+            raise ValueError("no prefix was provided")
+        if not self.inner:
+            # to accomplish adding a counter to duplicate scopes,
+            # we will treat ScopeMessenger.__enter__ like a sample statement
+            # so that the same mechanism that adds counters to sample names
+            # can be used to add a counter to a scope name
+            msg = {
+                "type": "sample",
+                "name": self.prefix,
+                "fn": lambda: True,
+                "is_observed": False,
+                "args": (),
+                "kwargs": {},
+                "value": None,
+                "infer": {},
+                "scale": 1.0,
+                "cond_indep_stack": (),
+                "done": False,
+                "stop": False,
+                "continuation": None,
+                "PRUNE": True  # this keeps the dummy node from appearing in the trace
+            }
+            apply_stack(msg)
+            self.prefix = msg["name"].split("/")[-1]
+        return super(ScopeMessenger, self).__enter__()
+
+    def __call__(self, fn):
+        if self.prefix is None:
+            self.prefix = fn.__code__.co_name  # fn.__name__
+
+        @functools.wraps(fn)
+        def _fn(*args, **kwargs):
+            with type(self)(prefix=self.prefix, inner=self.inner):
+                return fn(*args, **kwargs)
+        return _fn
+
+    def _pyro_sample(self, msg):
+        msg["name"] = "{}/{}".format(self.prefix, msg["name"])
+        return None
+
+
+scope = Handler(messenger=ScopeMessenger)
