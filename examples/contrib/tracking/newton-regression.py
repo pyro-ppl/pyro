34a0b76d85b489063234530a068ef8a2eb4d5df5
Alican Bozkurt <alicanb@uber.com>
2018-08-02 11:34:23 -0700

    2 objects wo spurious working

--- examples/contrib/tracking/newton-regression.py
+++ examples/contrib/tracking/newton-regression.py
@@ -28,7 +28,7 @@ smoke_test = ('CI' in os.environ)
 def model(args, observations):
     emission_noise_scale = pyro.param("emission_noise_scale")
     states_loc = pyro.param("states_loc")
-    max_num_objects = 1
+    max_num_objects = 2
     with pyro.iarange("objects", max_num_objects):
         states_loc = pyro.sample("states", dist.Normal(0., 1.).expand([2]).independent(1))
         positions = get_positions(states_loc, args.num_frames)
@@ -36,17 +36,38 @@ def model(args, observations):
     with pyro.iarange("detections", observations.shape[1]):
         with pyro.iarange("time", args.num_frames):
             # The remaining continuous part is exact.
+            assign = pyro.sample("assign",
+                                 dist.Categorical(torch.ones(max_num_objects + 1)))
             observed_positions = observations[..., 0]
-            pyro.sample('observations',
-                        dist.Normal(positions, emission_noise_scale),
+            bogus_position = positions.new_zeros(args.num_frames, 1)
+            augmented_positions = torch.cat([positions, bogus_position], -1)
+            # weird tricks because index and input must be same dimension in gather
+            assign = torch.cat((assign, assign[..., :1]), -1)
+            predicted_positions = torch.gather(augmented_positions, -1, assign)
+            predicted_positions = predicted_positions[..., :-1]
+            #pdb.set_trace()
+            pyro.sample('observations', dist.Normal(predicted_positions, emission_noise_scale),
                         obs=observed_positions)
 
 
 @poutine.broadcast
 def guide(args, observations):
     states_loc = pyro.param("states_loc")
+    # emission_noise_scale = pyro.param("emission_noise_scale")
+    assign_probs = torch.zeros(args.num_frames, observations.shape[1], states_loc.shape[0] + 1)
+    assign_probs[:, 0, 0] = 0.99
+    assign_probs[:, 0, 1] = 0.01
+    assign_probs[:, 1, 1] = 0.99
+    assign_probs[:, 1, 0] = 0.01
+    assign_dist = dist.Categorical(probs=assign_probs)
     with pyro.iarange("objects", states_loc.shape[0]):
+            #  states_var = states_cov.reshape(states_cov.shape[:-2] + (-1,))[..., ::n+1]
+            #  pyro.sample("states", dist.Normal(states_loc, states_var).independent(1))
         pyro.sample("states", dist.Delta(states_loc, event_dim=1))
+
+    with pyro.iarange("detections", observations.shape[1]):
+        with pyro.iarange("time", args.num_frames):
+            pyro.sample("assign", assign_dist, infer={"enumerate": "sequential"})
 
 
 def init_params():
@@ -62,7 +83,7 @@ def main(args):
 
     # initialization
     viz = init_visdom(args.visdom)
-    pyro.set_rng_seed(0)
+    pyro.set_rng_seed(args.seed)
     true_states, true_positions, observations = generate_observations(args)
     true_num_objects = len(true_states)
     max_num_detections = observations.shape[1]
@@ -73,7 +94,7 @@ def main(args):
         (observations[..., -1] > 0).long().sum(), true_num_objects))
     print('true_states = {}'.format(true_states))
 
-    pyro.set_rng_seed(1)  # Use a different seed from data generation
+    pyro.set_rng_seed(args.seed + 1)  # Use a different seed from data generation
     pyro.clear_param_store()
     init_params()
 
@@ -89,7 +110,7 @@ def main(args):
             plot_exists_prob(p_exists, viz)
 
     # Optimization
-    pyro.set_rng_seed(1)  # Use a different seed from data generation
+    pyro.set_rng_seed(args.seed + 1)  # Use a different seed from data generation
     losses = []
     ens = []
 
@@ -98,17 +119,20 @@ def main(args):
     adam = ClippedAdam({'lr': 0.1})
     optim = MixedMultiOptimizer([(['emission_noise_scale'], adam),
                                  (['states_loc'], newton)])
-    for svi_step in range(args.svi_iters):
-        with poutine.trace(param_only=True) as param_capture:
-            loss = elbo.differentiable_loss(model, guide, args, observations)
-        params = {name: pyro.param(name).unconstrained()
-                  for name in param_capture.trace.nodes.keys()}
-        optim.step(loss, params)
+    try:
+        for svi_step in range(args.svi_iters):
+            with poutine.trace(param_only=True) as param_capture:
+                loss = elbo.differentiable_loss(model, guide, args, observations)
+            params = {name: pyro.param(name).unconstrained()
+                      for name in param_capture.trace.nodes.keys()}
+            optim.step(loss, params)
 
-        ens.append(pyro.param("emission_noise_scale").item())
-        losses.append(loss.item() if isinstance(loss, torch.Tensor) else loss)
-        print('epoch {: >3d} loss = {}, emission_noise_scale = {}'.format(
-            svi_step, loss, ens[-1]))
+            ens.append(pyro.param("emission_noise_scale").item())
+            losses.append(loss.item() if isinstance(loss, torch.Tensor) else loss)
+            print('epoch {: >3d} loss = {}, emission_noise_scale = {}'.format(
+                svi_step, loss, ens[-1]))
+    except KeyboardInterrupt:
+        print('Interrupted')
 
     # run visualizations
     if viz is not None:
@@ -129,9 +153,10 @@ def main(args):
 def parse_args(*args):
     from shlex import split
     parser = argparse.ArgumentParser()
-    parser.add_argument('--num-frames', default=2, type=int, help='number of frames')
-    parser.add_argument('--max-num-objects', default=1, type=int, help='maximum number of objects')
-    parser.add_argument('--expected-num-objects', default=1.0, type=float, help='expected number of objects')
+    parser.add_argument('--num-frames', default=10, type=int, help='number of frames')
+    parser.add_argument('--seed', default=1, type=int, help='seed')
+    parser.add_argument('--max-num-objects', default=2, type=int, help='maximum number of objects')
+    parser.add_argument('--expected-num-objects', default=2.0, type=float, help='expected number of objects')
     parser.add_argument('--expected-num-spurious', default=1e-5, type=float,
                         help='expected number of false positives, if this is too small, BP will be unstable.')
     parser.add_argument('--emission-prob', default=.9999, type=float,
@@ -139,65 +164,11 @@ def parse_args(*args):
     parser.add_argument('--emission-noise-scale', default=0.1, type=float,
                         help='emission noise scale, if this is too small, SVI will see flat gradients.')
     parser.add_argument('--svi-iters', default=20, type=int, help='number of SVI iterations')
-    parser.add_argument('--em-iters', default=10, type=int, help='number of EM iterations')
-    parser.add_argument('--use-multi-opt', action="store_true", dest='use_multi_opt', default=False,
-                        help='Whether use MixedMultiOptimizer')
     parser.add_argument('--no-visdom', action="store_false", dest='visdom', default=True,
                         help='Whether plotting in visdom is desired')
     if len(args):
         return parser.parse_args(split(args[0]))
     return parser.parse_args()
-
-
-@pytest.mark.parametrize("args", ['--no-visdom'])
-def test_data_generation(args):
-    if isinstance(args, str):
-        args = parse_args(args)
-    pyro.set_rng_seed(0)
-    true_states, true_positions, observations = generate_observations(args)
-    true_num_objects = len(true_states)
-    max_num_detections = observations.shape[1]
-    assert true_states.shape == (true_num_objects, 2), \
-        "true_states.shape: {}".format(true_states.shape)
-    assert true_positions.shape == (args.num_frames, true_num_objects), \
-        "true_positions.shape: {}".format(true_positions.shape)
-    assert observations.shape == (args.num_frames, max_num_detections, 2), \
-        "observations.shape: {}".format(observations.shape)
-
-
-@pytest.mark.parametrize("args", ['--no-visdom'])
-def test_guide(args):
-    if isinstance(args, str):
-        args = parse_args(args)
-    pyro.set_rng_seed(0)
-    true_states, true_positions, observations = generate_observations(args)
-    pyro.set_rng_seed(1)  # Use a different seed from data generation
-    pyro.clear_param_store()
-    assignment, states_loc = guide(args, observations)
-    p_exists = assignment.exists_dist.probs
-    positions = get_positions(states_loc, args.num_frames)
-    assert p_exists.dim() == 1
-    assert positions.shape[0] == args.num_frames
-
-
-@pytest.mark.parametrize("args", ['--no-visdom --svi-iters 2'])
-def test_svi(args):
-    if isinstance(args, str):
-        args = parse_args(args)
-    pyro.set_rng_seed(0)
-    true_states, true_positions, observations = generate_observations(args)
-    pyro.set_rng_seed(1)  # Use a different seed from data generation
-    pyro.clear_param_store()
-
-    elbo = TraceEnum_ELBO(max_iarange_nesting=2)
-    optim = ClippedAdam({'lr': 0.1})
-    svi = SVI(model, guide, optim, elbo)
-    losses = []
-    for epoch in range(args.svi_iters):
-        loss = svi.step(args, observations)
-        losses.append(loss)
-        print('epoch {: >3d} loss = {}, emission_noise_scale = {}'.format(
-            epoch, loss, pyro.param("emission_noise_scale").item()))
 
 
 if __name__ == '__main__':
