1944ae12805f0345cfe144e7dad04a79b26f1c96
jpchen <jonathanp.chen@gmail.com>
2018-08-15 16:24:14 -0700

    add some tests

--- tests/contrib/tracking/test_dynamic_models.py
+++ tests/contrib/tracking/test_dynamic_models.py
@@ -109,3 +109,76 @@ def test_NcvContinuous():
 
     dx = ncv.process_noise_dist(dt).sample()
     assert dx.shape == (ncv.dimension,)
+
+
+def test_NcpDiscrete():
+    framerate = 100  # Hz
+    dt = 1.0/framerate
+    d = 3
+    ncp = NcpDiscrete(dimension=d, sv2=2.0)
+    assert ncp.dimension == d
+    assert ncp.dimension_pv == 2*d
+    assert ncp.num_process_noise_parameters == 1
+
+    x = torch.rand(d)
+    y = ncp(x, dt)
+    assert_equal(y, x)
+
+    dx = ncp.geodesic_difference(x, y)
+    assert_equal(dx, torch.zeros(d))
+
+    x_pv = ncp.mean2pv(x)
+    assert len(x_pv) == 6
+    assert_equal(x, x_pv[:d])
+    assert_equal(torch.zeros(d), x_pv[d:])
+
+    P = torch.eye(d)
+    P_pv = ncp.cov2pv(P)
+    assert P_pv.shape == (2*d, 2*d)
+    P_pv_ref = torch.zeros((2*d, 2*d))
+    P_pv_ref[:d, :d] = P
+    assert_equal(P_pv_ref, P_pv)
+
+    Q = ncp.process_noise_cov(dt)
+    Q1 = ncp.process_noise_cov(dt)  # Test caching.
+    assert_equal(Q, Q1)
+    assert Q1.shape == (d, d)
+    stm.assert_cov_validity(Q1)
+
+    dx = ncp.process_noise_dist(dt).sample()
+    assert dx.shape == (ncp.dimension,)
+
+
+def test_NcvDiscrete():
+    framerate = 100  # Hz
+    dt = 1.0/framerate
+    dt = 100
+    d = 6
+    ncv = NcvDiscrete(dimension=d, sa2=2.0)
+    assert ncv.dimension == d
+    assert ncv.dimension_pv == d
+    assert ncv.num_process_noise_parameters == 1
+
+    x = torch.rand(d)
+    y = ncv(x, dt)
+    assert_equal(y[0], x[0] + dt*x[d//2])
+
+    dx = ncv.geodesic_difference(x, y)
+    assert_not_equal(dx, torch.zeros(d))
+
+    x_pv = ncv.mean2pv(x)
+    assert len(x_pv) == d
+    assert_equal(x, x_pv)
+
+    P = torch.eye(d)
+    P_pv = ncv.cov2pv(P)
+    assert P_pv.shape == (d, d)
+    assert_equal(P, P_pv)
+
+    Q = ncv.process_noise_cov(dt)
+    Q1 = ncv.process_noise_cov(dt)  # Test caching.
+    assert_equal(Q, Q1)
+    assert Q1.shape == (d, d)
+
+    dx = ncv.process_noise_dist(dt).sample()
+    assert dx.shape == (ncv.dimension,)
