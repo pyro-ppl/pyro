24bc57c1b424c0cd0b0e1c04727da2c3f9c3dc44
JP <jonathanp.chen@gmail.com>
2018-08-15 16:03:00 -0700

    Add EKF NCV/NCP continuous implementation  (#1281)
    * inital commit, reformat all docstrings
    
    * test ekf continuous
    
    * make tests pass
    
    * wow had to update fl8 which now catches bad var names
    
    * I means identity
    
    * add docs
    
    * fix tests
    
    * add dynamic model tests
    
    * fix travis sphinx version
    
    * try this
    
    * try bumping travis sphinx version
    
    * address comments
    
    * comment out cache stuff
    
    * use abcmeta for py2 compatability
    
    * clear cache
    
    * add discrete frames
    
    * measurement tests
    
    * py2 compatibility
    
    * cleanup
    
    * return ll of update
    
    * remove all mutable states, remove disc and cont times from ekf, remove
    copy methods
    
    * raise notimplementederrors
    
    * Revert "remove all mutable states, remove disc and cont times from ekf, remove"
    
    This reverts commit 1a258f9c5b06758da0e5448a41fd5549336f67fb.
    
    * predict, update return new objects, states immutable
    
    * remove copy methods
    
    * make all dynamic models nn modules
    
    * use eye_like from pytorch-0.4.1

---
+++ tests/contrib/tracking/test_measurements.py
@@ -0,0 +1,25 @@
+
+from __future__ import absolute_import, division, print_function
+
+import torch
+from pyro.contrib.tracking.measurements import PositionMeasurement
+
+
+def test_PositionMeasurement():
+    dimension = 3
+    time = 0.232
+    frame_num = 5
+    measurement = PositionMeasurement(
+        mean=torch.rand(dimension),
+        cov=torch.eye(dimension), time=time, frame_num=frame_num)
+    assert measurement.dimension == dimension
+    x = torch.rand(2*dimension)
+    assert measurement(x).shape == (dimension,)
+    assert measurement.mean.shape == (dimension,)
+    assert measurement.cov.shape == (dimension, dimension)
+    assert measurement.time == time
+    assert measurement.frame_num == frame_num
+    assert measurement.geodesic_difference(
+        torch.rand(dimension), torch.rand(dimension)).shape \
+        == (dimension,)
+    assert measurement.jacobian().shape == (dimension, 2*dimension)
