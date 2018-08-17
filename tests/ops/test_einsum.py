6126a43421bd4be422b2f7d3ce7d6c9d2edccb88
Fritz Obermeyer <fritzo@uber.com>
2018-08-16 22:25:51 -0700

    Add non-color-coded test

--- tests/ops/test_einsum.py
+++ tests/ops/test_einsum.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, division, print_function
 
 import opt_einsum
+import pytest
 import torch
 
 from pyro.ops._einsum import Deferred, deferred_tensor, shared_intermediates
@@ -90,6 +91,44 @@ def test_partial_sharing():
         opt_einsum.contract('ab,bc,cd->', x_, y_, z1_, backend='pyro.ops._einsum')
         opt_einsum.contract('ab,bc,cd->', x_, y_, z2_, backend='pyro.ops._einsum')
         num_exprs_sharing = len(cache) - 4  # ignore deferred_tensor
+
+    print('-' * 40)
+    print('Without sharing: {} expressions'.format(num_exprs_nosharing))
+    print('With sharing: {} expressions'.format(num_exprs_sharing))
+    assert num_exprs_nosharing > num_exprs_sharing
+
+
+def compute_cost(cache):
+    return sum(1 for v in cache.values()
+               if type(v).__name__ in ('Einsum', 'Tensordot'))
+
+
+@pytest.mark.parametrize('size', [3, 4, 5])
+def test_chain_sharing(size):
+    xs = [torch.randn(2, 2) for _ in range(size)]
+    alphabet = ''.join(opt_einsum.get_symbol(i) for i in range(size + 1))
+    names = [alphabet[i:i+2] for i in range(size)]
+    inputs = ','.join(names)
+
+    num_exprs_nosharing = 0
+    for i in range(size + 1):
+        with shared_intermediates() as cache:
+            target = alphabet[i]
+            equation = '{}->{}'.format(inputs, target)
+            xs_ = [deferred_tensor(x) for x in xs]
+            opt_einsum.contract(equation, *xs_, backend='pyro.ops._einsum')
+            num_exprs_nosharing += compute_cost(cache)
+
+    with shared_intermediates() as cache:
+        print(inputs)
+        for i in range(size + 1):
+            target = alphabet[i]
+            equation = '{}->{}'.format(inputs, target)
+            xs_ = [deferred_tensor(x) for x in xs]
+            path_info = opt_einsum.contract_path(equation, *xs_)
+            print(path_info[1])
+            opt_einsum.contract(equation, *xs_, backend='pyro.ops._einsum')
+        num_exprs_sharing = compute_cost(cache)
 
     print('-' * 40)
     print('Without sharing: {} expressions'.format(num_exprs_nosharing))
