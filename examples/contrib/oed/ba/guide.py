97d4cfe738b46bf80d7dd5a5d78f9466a589b46f
Adam Foster <adamf@uber.com>
2018-08-18 12:56:03 -0700

    Fix analytic answer

--- examples/contrib/oed/ba/guide.py
+++ examples/contrib/oed/ba/guide.py
@@ -7,20 +7,24 @@ import pyro.distributions as dist
 
 class Ba_lm_guide(nn.Module):
 
-    def __init__(self, shape):
+    def __init__(self, regu_shape, scale_tril_shape, w_sizes):
         super(Ba_lm_guide, self).__init__()
-        self.regu = nn.Parameter(-2.*torch.ones(shape[-1] - 1))
-        self.scale_tril = nn.Parameter(10.*torch.ones(shape))
+        self.regu = nn.Parameter(-2.*torch.ones(regu_shape))
+        self.scale_tril = nn.Parameter(10.*torch.ones(scale_tril_shape))
+        self.w_sizes = w_sizes
         self.softplus = nn.Softplus()
 
-    def forward(self, y, design):
+    def forward(self, y, design, target_label):
+
+        # TODO fix this
+        design = design[..., :self.w_sizes[target_label]]
 
         anneal = torch.diag(self.softplus(self.regu))
         xtx = torch.matmul(design.transpose(-1, -2), design) + anneal
-        xtxi = tensorized_2_by_2_matrix_inverse(xtx)
+        xtxi = tensorized_matrix_inverse(xtx)
         mu = torch.matmul(xtxi, torch.matmul(design.transpose(-1, -2), y.unsqueeze(-1))).squeeze(-1)
 
-        scale_tril = tensorized_2_by_2_tril(self.scale_tril)
+        scale_tril = tensorized_tril(self.scale_tril)
 
         return mu, scale_tril
 
@@ -30,27 +34,37 @@ class Ba_lm_guide(nn.Module):
         pyro.module("ba_guide", self)
 
         y = y_dict["y"]
-        mu, scale_tril = self.forward(y, design)
-        
+        mu, scale_tril = self.forward(y, design, target_label)
+
         # guide distributions for w
         w_dist = dist.MultivariateNormal(mu, scale_tril=scale_tril)
-        pyro.sample(target_label, w_dist)
+        w = pyro.sample(target_label, w_dist)
 
 
-def tensorized_2_by_2_matrix_inverse(M):
-    det = M[..., 0, 0]*M[..., 1, 1] - M[..., 1, 0]*M[..., 0, 1]
-    inv = torch.zeros(M.shape)
-    inv[..., 0, 0] = M[..., 1, 1]
-    inv[..., 1, 1] = M[..., 0, 0]
-    inv[..., 0, 1] = -M[..., 0, 1]
-    inv[..., 1, 0] = -M[..., 1, 0]
-    inv = inv/det.unsqueeze(-1).unsqueeze(-1)
-    return inv
+def tensorized_matrix_inverse(M):
+    if M.shape[-1] == 1:
+        return 1./M
+    elif M.shape[-1] == 2:
+        det = M[..., 0, 0]*M[..., 1, 1] - M[..., 1, 0]*M[..., 0, 1]
+        inv = torch.zeros(M.shape)
+        inv[..., 0, 0] = M[..., 1, 1]
+        inv[..., 1, 1] = M[..., 0, 0]
+        inv[..., 0, 1] = -M[..., 0, 1]
+        inv[..., 1, 0] = -M[..., 1, 0]
+        inv = inv/det.unsqueeze(-1).unsqueeze(-1)
+        return inv
+    else:
+        raise NotImplemented()
 
 
-def tensorized_2_by_2_tril(M):
-    tril = torch.zeros(M.shape[:-1] + (2, 2))
-    tril[..., 0, 0] = M[..., 0]
-    tril[..., 1, 0] = M[..., 1]
-    tril[..., 1, 1] = M[..., 2]
-    return tril
+def tensorized_tril(M):
+    if M.shape[-1] == 1:
+        return M.unsqueeze(-1)
+    if M.shape[-1] == 3:
+        tril = torch.zeros(M.shape[:-1] + (2, 2))
+        tril[..., 0, 0] = M[..., 0]
+        tril[..., 1, 0] = M[..., 1]
+        tril[..., 1, 1] = M[..., 2]
+        return tril
+    else:
+        raise NotImplemented()
