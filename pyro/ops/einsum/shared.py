65bde20c2a50f61d8acd70b386a0c42c699364da
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2018-08-18 12:21:53 -0700

    Memoize path optimization in einsum.contract (#1318)

--- pyro/ops/einsum/shared.py
+++ pyro/ops/einsum/shared.py
@@ -9,6 +9,7 @@ from opt_einsum.backends.dispatch import get_func
 
 _SHARING_STACK = []
 _CURRENT_BACKEND = []
+_PATH_CACHE = {}
 LAST_CACHE_SIZE = [0]  # for profiling
 
 
@@ -33,9 +34,14 @@ def shared_intermediates(cache=None):
 
 def contract(equation, *operands, **kwargs):
     """
-    Like :func:`opt_einsum.contract` but works with shared_intermediates contexts.
+    Like :func:`opt_einsum.contract` but works with
+    :func:`~pyro.ops.einsum.shared_intermediates` contexts.
+
+    :param bool cache_path: whether to cache the contraction path.
+        Defaults to True.
     """
     backend = kwargs.pop('backend', 'numpy')
+    cache_path = kwargs.pop('cache_path', True)
 
     # special handling under shared_intermediates()
     if _SHARING_STACK and not _CURRENT_BACKEND:
@@ -51,7 +57,20 @@ def contract(equation, *operands, **kwargs):
     if backend == 'pyro.ops.einsum.shared' and not _CURRENT_BACKEND:
         raise ValueError('shared backend is available only via shared_intermediates')
 
-    return opt_einsum.contract(equation, *operands, backend=backend, **kwargs)
+    if not cache_path:
+        return opt_einsum.contract(equation, *operands, backend=backend, **kwargs)
+
+    # memoize the contraction path
+    out = kwargs.pop('out', None)
+    kwargs_key = tuple(kwargs.items())
+    shapes = tuple(tuple(t.shape) for t in operands)
+    key = equation, shapes, kwargs_key
+    if key in _PATH_CACHE:
+        expr = _PATH_CACHE[key]
+    else:
+        expr = opt_einsum.contract_expression(equation, *shapes, **kwargs)
+        _PATH_CACHE[key] = expr
+    return expr(*operands, backend=backend, out=out)
 
 
 def _alpha_canonicalize(equation):
