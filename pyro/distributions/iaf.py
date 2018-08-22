c1122684fc9e1c9f7ce2631a9165a1133662e86c
Stefan Webb <stefan.webb@wadh.ox.ac.uk>
2018-08-22 17:16:33 +0100

    New versions of IAF

--- pyro/distributions/iaf.py
+++ pyro/distributions/iaf.py
@@ -7,6 +7,8 @@ from torch.distributions import constraints
 
 from pyro.distributions.util import copy_docs_from
 
+def clamp_preserve_gradients(x, min, max):
+  return x + (x.clamp(min, max) - x).detach()
 
 @copy_docs_from(Transform)
 class InverseAutoregressiveFlow(Transform):
@@ -52,12 +54,141 @@ class InverseAutoregressiveFlow(Transform):
 
     codomain = constraints.real
 
-    def __init__(self, autoregressive_nn, sigmoid_bias=2.0):
+    def __init__(self, autoregressive_nn, log_scale_min_clip=-5., log_scale_max_clip=3.):
         super(InverseAutoregressiveFlow, self).__init__()
         self.module = nn.Module()
         self.module.arn = autoregressive_nn
-        self.module.sigmoid = nn.Sigmoid()
-        self.module.sigmoid_bias = torch.tensor(sigmoid_bias)
+        self._intermediates_cache = {}
+        self.add_inverse_to_cache = True
+        self.log_scale_min_clip = log_scale_min_clip
+        self.log_scale_max_clip = log_scale_max_clip
+
+    @property
+    def arn(self):
+        """
+        :rtype: pyro.nn.AutoRegressiveNN
+
+        Return the AutoRegressiveNN associated with the InverseAutoregressiveFlow
+        """
+        return self.module.arn
+
+    def _call(self, x):
+        """
+        :param x: the input into the bijection
+        :type x: torch.Tensor
+
+        Invokes the bijection x=>y; in the prototypical context of a TransformedDistribution `x` is a
+        sample from the base distribution (or the output of a previous flow)
+        """
+        mean, log_scale = self.module.arn(x)
+        log_scale = clamp_preserve_gradients(log_scale, self.log_scale_min_clip, self.log_scale_max_clip)
+        scale = torch.exp(log_scale)
+
+        y = scale * x + mean
+        self._add_intermediate_to_cache(x, y, 'x')
+        self._add_intermediate_to_cache(log_scale, y, 'log_scale')
+        return y
+
+    def _inverse(self, y):
+        """
+        :param y: the output of the bijection
+        :type y: torch.Tensor
+
+        Inverts y => x. Uses a previously cached inverse if available, otherwise performs the inversion afresh.
+        """
+        if (y, 'x') in self._intermediates_cache:
+            x = self._intermediates_cache.pop((y, 'x'))
+            return x
+        else:
+            x_size = y.size()[:-1]
+            perm = self.module.arn.permutation
+            input_dim = y.size(-1)
+            x = [torch.zeros(x_size, device=y.device)]*input_dim
+
+            # NOTE: Inversion is an expensive operation that scales in the dimension of the input
+            for idx in perm:
+                mean, log_scale = self.module.arn(torch.stack(x, dim=-1))
+                inverse_scale = torch.exp(-clamp_preserve_gradients(log_scale[..., idx], min=-5., max=3))
+                mean = mean[..., idx]
+                x[idx] = (y[..., idx] - mean) * inverse_scale
+
+            x = torch.stack(x, dim=-1)
+
+            #_, log_scale = self.module.arn(x)
+            #scale = torch.exp(clamp_preserve_gradients(log_scale, min=-5., max=3))
+            #self._add_intermediate_to_cache(scale, y, 'scale')
+            return x
+
+    def _add_intermediate_to_cache(self, intermediate, y, name):
+        """
+        Internal function used to cache intermediate results computed during the forward call
+        """
+        assert((y, name) not in self._intermediates_cache),\
+            "key collision in _add_intermediate_to_cache"
+        self._intermediates_cache[(y, name)] = intermediate
+
+    def log_abs_det_jacobian(self, x, y):
+        """
+        Calculates the elementwise determinant of the log jacobian
+        """
+        if (y, 'scale') in self._intermediates_cache:
+            log_scale = self._intermediates_cache.pop((y, 'log_scale'))
+        else:
+            _, log_scale = self.module.arn(x)
+            log_scale = clamp_preserve_gradients(log_scale, min=-5., max=3)
+        return log_scale
+
+@copy_docs_from(Transform)
+class InverseAutoregressiveFlowStable(Transform):
+    """
+    An implementation of an Inverse Autoregressive Flow. Together with the `TransformedDistribution` this
+    provides a way to create richer variational approximations.
+
+    Example usage:
+
+    >>> from pyro.nn import AutoRegressiveNN
+    >>> base_dist = dist.Normal(torch.zeros(10), torch.ones(10))
+    >>> iaf = InverseAutoregressiveFlowStable(AutoRegressiveNN(10, [40]))
+    >>> iaf_module = pyro.module("my_iaf", iaf.module)
+    >>> iaf_dist = dist.TransformedDistribution(base_dist, [iaf])
+    >>> iaf_dist.sample()  # doctest: +SKIP
+        tensor([-0.4071, -0.5030,  0.7924, -0.2366, -0.2387, -0.1417,  0.0868,
+                0.1389, -0.4629,  0.0986])
+
+    Note that this implementation is only meant to be used in settings where the inverse of the Bijector
+    is never explicitly computed (rather the result is cached from the forward call). In the context of
+    variational inference, this means that the InverseAutoregressiveFlow should only be used in the guide,
+    i.e. in the variational distribution. In other contexts the inverse could in principle be computed but
+    this would be a (potentially) costly computation that scales with the dimension of the input (and in
+    any case support for this is not included in this implementation).
+
+    :param autoregressive_nn: an autoregressive neural network whose forward call returns a real-valued
+        mean and logit-scale as a tuple
+    :type autoregressive_nn: nn.Module
+    :param sigmoid_bias: bias on the hidden units fed into the sigmoid; default=`2.0`
+    :type sigmoid_bias: float
+
+    References:
+
+    1. Improving Variational Inference with Inverse Autoregressive Flow [arXiv:1606.04934]
+    Diederik P. Kingma, Tim Salimans, Rafal Jozefowicz, Xi Chen, Ilya Sutskever, Max Welling
+
+    2. Variational Inference with Normalizing Flows [arXiv:1505.05770]
+    Danilo Jimenez Rezende, Shakir Mohamed
+
+    3. MADE: Masked Autoencoder for Distribution Estimation [arXiv:1502.03509]
+    Mathieu Germain, Karol Gregor, Iain Murray, Hugo Larochelle
+    """
+
+    codomain = constraints.real
+
+    def __init__(self, autoregressive_nn, sigmoid_bias=2.0):
+        super(InverseAutoregressiveFlowStable, self).__init__()
+        self.module = nn.Module()
+        self.module.arn = autoregressive_nn
+        self.sigmoid = nn.Sigmoid()
+        self.logsigmoid = nn.LogSigmoid()
+        self.sigmoid_bias = sigmoid_bias
         self._intermediates_cache = {}
         self.add_inverse_to_cache = True
 
@@ -78,12 +209,14 @@ class InverseAutoregressiveFlow(Transform):
         Invokes the bijection x=>y; in the prototypical context of a TransformedDistribution `x` is a
         sample from the base distribution (or the output of a previous flow)
         """
-        mean, scale = self.module.arn(x)
-        scale = self.module.sigmoid(scale + scale.new_tensor(self.module.sigmoid_bias))
+        mean, logit_scale = self.module.arn(x)
+        logit_scale = logit_scale + self.sigmoid_bias
+        scale = self.sigmoid(logit_scale)
+        log_scale = self.logsigmoid(logit_scale)
 
         y = scale * x + (1 - scale) * mean
         self._add_intermediate_to_cache(x, y, 'x')
-        self._add_intermediate_to_cache(scale, y, 'scale')
+        self._add_intermediate_to_cache(log_scale, y, 'log_scale')
         return y
 
     def _inverse(self, y):
@@ -91,16 +224,29 @@ class InverseAutoregressiveFlow(Transform):
         :param y: the output of the bijection
         :type y: torch.Tensor
 
-        Inverts y => x. As noted above, this implementation is incapable of inverting arbitrary values
-        `y`; rather it assumes `y` is the result of a previously computed application of the bijector
-        to some `x` (which was cached on the forward call)
+        Inverts y => x. Uses a previously cached inverse if available, otherwise performs the inversion afresh.
         """
         if (y, 'x') in self._intermediates_cache:
             x = self._intermediates_cache.pop((y, 'x'))
             return x
         else:
-            raise KeyError("InverseAutoregressiveFlow expected to find "
-                           "key in intermediates cache but didn't")
+            x_size = y.size()[:-1]
+            perm = self.module.arn.permutation
+            input_dim = y.size(-1)
+            x = [torch.zeros(x_size, device=y.device)]*input_dim
+
+            # NOTE: Inversion is an expensive operation that scales in the dimension of the input
+            for idx in perm:
+                mean, logit_scale = self.module.arn(torch.stack(x, dim=-1))
+                inverse_scale = 1 + torch.exp(-logit_scale[..., idx] - self.sigmoid_bias)
+                x[idx] = inverse_scale * y[..., idx]  + (1 - inverse_scale) * mean[..., idx]
+
+            x = torch.stack(x, dim=-1)
+
+            #_, log_scale = self.module.arn(x)
+            #scale = torch.exp(clamp_preserve_gradients(log_scale, min=-5., max=3))
+            #self._add_intermediate_to_cache(scale, y, 'scale')
+            return x
 
     def _add_intermediate_to_cache(self, intermediate, y, name):
         """
@@ -115,8 +261,8 @@ class InverseAutoregressiveFlow(Transform):
         Calculates the elementwise determinant of the log jacobian
         """
         if (y, 'scale') in self._intermediates_cache:
-            scale = self._intermediates_cache.pop((y, 'scale'))
+            log_scale = self._intermediates_cache.pop((y, 'log_scale'))
         else:
-            raise KeyError("Bijector InverseAutoregressiveFlow expected to find" +
-                           "key in intermediates cache but didn't")
-        return scale.log()
+            _, logit_scale = self.module.arn(x)
+            log_scale = self.logsigmoid(logit_scale + self.sigmoid_bias)
+        return log_scale
