1269776273ad75c5de0e888de3b61a718165dd07
Fritz Obermeyer <fritzo@uber.com>
2018-08-25 19:09:31 -0700

    Exclude prior terms from likelihood evaluation

--- examples/hmm.py
+++ examples/hmm.py
@@ -17,17 +17,18 @@ from pyro.optim import Adam
 logging.basicConfig(format='%(relativeCreated) 9d %(message)s', level=logging.INFO)
 
 
-def model(sequences, lengths, args, batch_size=None):
-    assert sequences.shape[0] == lengths.shape[0]
-    assert sequences.shape[1] >= lengths.max()
-    data_dim = sequences.shape[-1]
-    trans = pyro.sample("p_trans",
-                        dist.Dirichlet(0.9 * torch.eye(args.hidden_dim) + 0.1)
-                            .independent(1))
-    emit = pyro.sample("p_emit",
-                       dist.Beta(0.1, 0.9)
-                           .expand([args.hidden_dim, data_dim]).independent(2))
-    tones_iarange = pyro.iarange("tones", sequences.shape[-1], dim=-1)
+def model(sequences, lengths, args, batch_size=None, include_prior=True):
+    num_sequences, max_length, data_dim = sequences.shape
+    assert lengths.shape == (num_sequences,)
+    assert lengths.max() <= max_length
+    with poutine.mask(mask=torch.tensor(include_prior)):
+        trans = pyro.sample("p_trans",
+                            dist.Dirichlet(0.9 * torch.eye(args.hidden_dim) + 0.1)
+                                .independent(1))
+        emit = pyro.sample("p_emit",
+                           dist.Beta(0.1, 0.9)
+                               .expand([args.hidden_dim, data_dim]).independent(2))
+    tones_iarange = pyro.iarange("tones", data_dim, dim=-1)
     with pyro.iarange("sequences", len(sequences), batch_size, dim=-2) as batch:
         lengths = lengths[batch]
         x = 0
@@ -54,34 +55,35 @@ def main(args):
         pyro.clear_param_store()
         pyro.enable_validation(True)
         guide = AutoDelta(poutine.block(model, expose_fn=lambda msg: msg["name"].startswith("p_")))
-        optim = Adam({'lr': 0.1})
+        optim = Adam({'lr': args.learning_rate})
         Elbo = JitTraceEnum_ELBO if args.jit else TraceEnum_ELBO
         elbo = Elbo(max_iarange_nesting=2)
         svi = SVI(model, guide, optim, elbo)
-        logging.info('Epoch\tLoss')
+        logging.info('Step\tLoss')
         try:
-            for epoch in range(args.num_epochs):
+            for step in range(args.num_steps):
                 loss = svi.step(sequences, lengths, args, batch_size=args.batch_size)
-                logging.info('{: >5d}\t{}'.format(epoch, loss / num_observations))
+                logging.info('{: >5d}\t{}'.format(step, loss / num_observations))
         except KeyboardInterrupt:
             pass
-        train_loss = elbo.loss(model, guide, sequences, lengths, args) / num_observations
-        logging.info('training loss = {}'.format(train_loss))
+        train_loss = elbo.loss(model, guide, sequences, lengths, args, include_prior=False)
+        logging.info('training loss = {}'.format(train_loss / num_observations))
 
         logging.info('-' * 40)
         logging.info('Evaluating on {} test sequences'.format(len(data['test']['sequences'])))
         sequences = torch.tensor(data['test']['sequences'], dtype=torch.float32)
         lengths = torch.tensor(data['test']['sequence_lengths'], dtype=torch.long)
         num_observations = float(lengths.sum())
-        test_loss = elbo.loss(model, guide, sequences, lengths, args) / num_observations
-        logging.info('test loss = {}'.format(test_loss))
+        test_loss = elbo.loss(model, guide, sequences, lengths, args, include_prior=False)
+        logging.info('test loss = {}'.format(test_loss / num_observations))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Bayesian Baum-Welch learning Bach Chorales")
-    parser.add_argument("-n", "--num-epochs", default=100, type=int)
+    parser.add_argument("-n", "--num-steps", default=50, type=int)
     parser.add_argument("-b", "--batch-size", default=8, type=int)
-    parser.add_argument("-d", "--hidden-dim", default=16, type=int)
+    parser.add_argument("-d", "--hidden-dim", default=32, type=int)
+    parser.add_argument("-lr", "--learning-rate", default=0.1, type=float)
     parser.add_argument('--jit', action='store_true')
     args = parser.parse_args()
     main(args)
