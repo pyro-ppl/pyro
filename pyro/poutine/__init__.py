71b7d3da0c4028ca3c27fdf9e677f74c0c512eb0
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2018-08-26 10:51:42 -0700

    Separate poutine.mask() from poutine.scale() (#1334)
    * Separate poutine.mask from poutine.scale
    
    * Fix some tests
    
    * Fix failing tests
    
    * Fix typo
    
    * Switch to cheaper .masked_fill_()

--- pyro/poutine/__init__.py
+++ pyro/poutine/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, division, print_function
 
 from .handlers import block, broadcast, condition, do, enum, escape, indep, infer_config, lift, \
-    replay, queue, scale, trace
+    mask, replay, queue, scale, trace
 from .runtime import NonlocalExit
 from .trace_struct import Trace
 from .util import enable_validation, is_validation_enabled
@@ -19,6 +19,7 @@ __all__ = [
     "infer_config",
     "is_validation_enabled",
     "lift",
+    "mask",
     "NonlocalExit",
     "replay",
     "queue",
