b583470bfcbeb4bc0c32e6fd9d7305f0e3afa50b
Adam Foster <adamf@uber.com>
2018-08-29 17:43:58 -0700

    Add missing tests, fix exposed bug :)

--- pyro/contrib/oed/util.py
+++ pyro/contrib/oed/util.py
@@ -4,12 +4,12 @@ import torch
 def get_indices(labels, sizes=None, tensors=None):
     indices = []
     start = 0
-    for label in labels:
-        if sizes is not None:
-            end = start+sizes[label]
-        else:
-            end = start+tensors[label].shape[0]
-        indices.extend(range(start, end))
+    if sizes is None:
+        sizes = {l: t.shape[0] for l, t in tensors.items()}
+    for label in sizes:
+        end = start+sizes[label]
+        if label in labels:
+            indices.extend(range(start, end))
         start = end
     return torch.tensor(indices)
 
@@ -50,41 +50,6 @@ def lexpand(A, *dimensions):
 def rexpand(A, *dimensions):
     """Expand tensor, adding new dimensions on right."""
     return A.view(A.shape + (1,)*len(dimensions)).expand(A.shape + tuple(dimensions))
-
-
-def rinverse(M):
-    """Matrix inversion of rightmost dimensions (batched).
-    Accelerated for 1x1 and 2x2 matrices.
-    """
-    assert M.shape[-1] == M.shape[-2]
-    if M.shape[-1] == 1:
-        return 1./M
-    # diagonal matrix
-    elif (torch.abs(M*(1. - torch.eye(M.shape[-1]))) < 1e-25).all():
-        print("using good inverse")
-        print(M)
-        return 1./M
-    elif M.shape[-1] == 2:
-        print("call to 2x2 inverse")
-        det = M[..., 0, 0]*M[..., 1, 1] - M[..., 1, 0]*M[..., 0, 1]
-        inv = torch.zeros(M.shape)
-        inv[..., 0, 0] = M[..., 1, 1]
-        inv[..., 1, 1] = M[..., 0, 0]
-        inv[..., 0, 1] = -M[..., 0, 1]
-        inv[..., 1, 0] = -M[..., 1, 0]
-        return inv/det.unsqueeze(-1).unsqueeze(-1)
-    else:
-        # Use blockwise inversion
-        d = M.shape[-1]//2
-        A, B, C, D = M[..., :d, :d], M[..., :d, d:], M[..., d:, :d], M[..., d:, d:]
-        Ainv = rinverse(A)
-        schur = rinverse(D - C.matmul(Ainv).matmul(B))
-        inv = torch.zeros(M.shape)
-        inv[..., :d, :d] = Ainv + Ainv.matmul(B).matmul(schur).matmul(C).matmul(Ainv)
-        inv[..., :d, d:] = -Ainv.matmul(B).matmul(schur)
-        inv[..., d:, :d] = -schur.matmul(C).matmul(Ainv)
-        inv[..., d:, d:] = schur
-        return inv
 
 
 def rdiag(v):
