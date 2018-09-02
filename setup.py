26df57251dbf3efecaf5492546369911b1e73bae
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2018-09-02 10:04:27 -0700

    flake8

--- setup.py
+++ setup.py
@@ -123,6 +123,6 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-   ],
+    ],
     # yapf
 )
