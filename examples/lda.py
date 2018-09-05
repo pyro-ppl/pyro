788dda9bc249310cd2b5aa7f2066e8f98ff15414
Fritz Obermeyer <fritzo@uber.com>
2018-09-05 00:06:15 -0700

    Refactor to make jit work

--- examples/lda.py
+++ examples/lda.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, division, print_function
 
 import argparse
+import functools
 import logging
 
 import torch
@@ -21,7 +22,7 @@ logging.basicConfig(format='%(relativeCreated) 9d %(message)s', level=logging.IN
 # (specifically it is not a histogram). We assume in this simple example
 # that all documents have the same number of words.
 @poutine.broadcast
-def model(args, data=None):
+def model(data=None, args=None, batch_size=None):
     # Globals.
     with pyro.iarange("topics", args.num_topics):
         topic_weights = pyro.sample("topic_weights", dist.Gamma(1. / args.num_topics, 1.))
@@ -49,68 +50,68 @@ def model(args, data=None):
 
 # We will use amortized inference of the local topic variables, achieved by a
 # multi-layer perceptron. We'll wrap the guide in an nn.Module.
-class Guide(nn.Module):
-    def __init__(self, args):
-        super(Guide, self).__init__()
-        layer_sizes = ([args.num_words] +
-                       [int(s) for s in args.layer_sizes.split('-')] +
-                       [args.num_topics])
-        logging.info('Creating MLP with sizes {}'.format(layer_sizes))
-        layers = []
-        for in_size, out_size in zip(layer_sizes, layer_sizes[1:]):
-            layer = nn.Linear(in_size, out_size)
-            layer.weight.data.normal_(0, 0.001)
-            layer.bias.data.normal_(0, 0.001)
-            layers.append(layer)
-            layers.append(nn.Sigmoid())
-        self.local_guide = nn.Sequential(*layers)
+def make_predictor(args):
+    layer_sizes = ([args.num_words] +
+                   [int(s) for s in args.layer_sizes.split('-')] +
+                   [args.num_topics])
+    logging.info('Creating MLP with sizes {}'.format(layer_sizes))
+    layers = []
+    for in_size, out_size in zip(layer_sizes, layer_sizes[1:]):
+        layer = nn.Linear(in_size, out_size)
+        layer.weight.data.normal_(0, 0.001)
+        layer.bias.data.normal_(0, 0.001)
+        layers.append(layer)
+        layers.append(nn.Sigmoid())
+    return nn.Sequential(*layers)
 
-    @poutine.broadcast
-    def __call__(self, args, data):
-        # Use a conjugate guide for global variables.
-        topic_weights_posterior = pyro.param(
-                "topic_weights_posterior",
-                lambda: torch.ones(args.num_topics) / args.num_topics,
-                constraint=constraints.positive)
-        topic_words_posterior = pyro.param(
-                "topic_words_posterior",
-                lambda: torch.ones(args.num_topics, args.num_words) / args.num_words,
-                constraint=constraints.positive)
-        with pyro.iarange("topics", args.num_topics):
-            pyro.sample("topic_weights", dist.Gamma(topic_weights_posterior, 1.))
-            pyro.sample("topic_words", dist.Dirichlet(topic_words_posterior))
 
-        # Use an amortized guide for local variables.
-        pyro.module("local_guide", self.local_guide)
-        with pyro.iarange("documents", args.num_docs, args.batch_size) as ind:
-            # The neural network will operate on histograms rather than word
-            # index vectors, so we'll convert the raw data to a histogram.
-            counts = torch.zeros(args.num_words, args.batch_size)
-            counts.scatter_add_(0, data[:, ind], torch.tensor(1.).expand(counts.shape))
-            doc_topics = self.local_guide(counts.transpose(0, 1))
-            pyro.sample("doc_topics", dist.Delta(doc_topics, event_dim=1))
+@poutine.broadcast
+def parametrized_guide(predictor, data, args, batch_size=None):
+    # Use a conjugate guide for global variables.
+    topic_weights_posterior = pyro.param(
+            "topic_weights_posterior",
+            lambda: torch.ones(args.num_topics) / args.num_topics,
+            constraint=constraints.positive)
+    topic_words_posterior = pyro.param(
+            "topic_words_posterior",
+            lambda: torch.ones(args.num_topics, args.num_words) / args.num_words,
+            constraint=constraints.positive)
+    with pyro.iarange("topics", args.num_topics):
+        pyro.sample("topic_weights", dist.Gamma(topic_weights_posterior, 1.))
+        pyro.sample("topic_words", dist.Dirichlet(topic_words_posterior))
+
+    # Use an amortized guide for local variables.
+    pyro.module("predictor", predictor)
+    with pyro.iarange("documents", args.num_docs, batch_size) as ind:
+        # The neural network will operate on histograms rather than word
+        # index vectors, so we'll convert the raw data to a histogram.
+        counts = torch.zeros(args.num_words, len(ind))
+        counts.scatter_add_(0, data[:, ind], torch.tensor(1.).expand(counts.shape))
+        doc_topics = predictor(counts.transpose(0, 1))
+        pyro.sample("doc_topics", dist.Delta(doc_topics, event_dim=1))
 
 
 def main(args):
     logging.info('Generating data')
     pyro.set_rng_seed(0)
     # We can generate synthetic data directly by calling the model.
-    true_topic_weights, true_topic_words, data = model(args)
+    true_topic_weights, true_topic_words, data = model(args=args)
 
     # We'll train using SVI.
     logging.info('-' * 40)
     logging.info('Training on {} documents'.format(args.num_docs))
-    guide = Guide(args)
+    predictor = make_predictor(args)
+    guide = functools.partial(parametrized_guide, predictor)
     Elbo = JitTraceEnum_ELBO if args.jit else TraceEnum_ELBO
     elbo = Elbo(max_iarange_nesting=2)
     optim = Adam({'lr': args.learning_rate})
     svi = SVI(model, guide, optim, elbo)
     logging.info('Step\tLoss')
     for step in range(args.num_steps):
-        loss = svi.step(args, data)
+        loss = svi.step(data, args=args, batch_size=args.batch_size)
         if step % 10 == 0:
             logging.info('{: >5d}\t{}'.format(step, loss))
-    loss = elbo.loss(model, guide, args, data)
+    loss = elbo.loss(model, guide, data, args=args)
     logging.info('final loss = {}'.format(loss))
 
 
