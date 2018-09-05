8e53f75a013b710954d3f2070f447bbd2dcdc214
Fritz Obermeyer <fritzo@uber.com>
2018-09-04 22:26:09 -0700

    Add comments

--- examples/lda.py
+++ examples/lda.py
@@ -16,6 +16,10 @@ from pyro.optim import Adam
 logging.basicConfig(format='%(relativeCreated) 9d %(message)s', level=logging.INFO)
 
 
+# This is a fully generative model of a batch of documents.
+# data is a [num_words_per_doc, num_documents] shaped array of word ids
+# (specifically it is not a histogram). We assume in this simple example
+# that all documents have the same number of words.
 @poutine.broadcast
 def model(args, data=None):
     # Globals.
@@ -31,15 +35,20 @@ def model(args, data=None):
             data = data[:, ind]
         doc_topics = pyro.sample("doc_topics", dist.Dirichlet(topic_weights))
         with pyro.iarange("words", args.num_words_per_doc):
+            # The word_topics variable is marginalized out during inference,
+            # achieved by specifying infer={"enumerate": "parallel"} and using
+            # TraceEnum_ELBO for inference. Thus we can ignore this variable in
+            # the guide.
             word_topics = pyro.sample("word_topics", dist.Categorical(doc_topics),
                                       infer={"enumerate": "parallel"})
-            # TODO use poutine.mask if docs have different lengths
             data = pyro.sample("doc_words", dist.Categorical(topic_words[word_topics]),
                                obs=data)
 
     return topic_weights, topic_words, data
 
 
+# We will use amortized inference of the local topic variables, achieved by a
+# multi-layer perceptron. We'll wrap the guide in an nn.Module.
 class Guide(nn.Module):
     def __init__(self, args):
         super(Guide, self).__init__()
@@ -74,6 +83,8 @@ class Guide(nn.Module):
         # Use an amortized guide for local variables.
         pyro.module("local_guide", self.local_guide)
         with pyro.iarange("documents", args.num_docs, args.batch_size) as ind:
+            # The neural network will operate on histograms rather than word
+            # index vectors, so we'll convert the raw data to a histogram.
             counts = torch.zeros(args.num_words, args.batch_size)
             counts.scatter_add_(0, data[:, ind], torch.tensor(1.).expand(counts.shape))
             doc_topics = self.local_guide(counts.transpose(0, 1))
@@ -83,8 +94,10 @@ class Guide(nn.Module):
 def main(args):
     logging.info('Generating data')
     pyro.set_rng_seed(0)
+    # We can generate synthetic data directly by calling the model.
     true_topic_weights, true_topic_words, data = model(args)
 
+    # We'll train using SVI.
     logging.info('-' * 40)
     logging.info('Training on {} documents'.format(args.num_docs))
     guide = Guide(args)
@@ -92,7 +105,6 @@ def main(args):
     elbo = Elbo(max_iarange_nesting=2)
     optim = Adam({'lr': args.learning_rate})
     svi = SVI(model, guide, optim, elbo)
-
     logging.info('Step\tLoss')
     for step in range(args.num_steps):
         loss = svi.step(args, data)
