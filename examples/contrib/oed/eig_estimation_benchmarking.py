1608cea81d0de8822789aa4f40d8f01742e37f44
Adam Foster <adamf@uber.com>
2018-09-05 18:00:40 -0700

    Move models and guides to new contrib.glmm

--- examples/contrib/oed/eig_estimation_benchmarking.py
+++ examples/contrib/oed/eig_estimation_benchmarking.py
@@ -12,12 +12,12 @@ from pyro.contrib.oed.eig import (
     vi_ape, naive_rainforth_eig, donsker_varadhan_eig, barber_agakov_ape
 )
 from pyro.contrib.oed.util import get_indices, lexpand
-
-from models.bayes_linear import (
+from pyro.contrib.glmm import (
     zero_mean_unit_obs_sd_lm, group_assignment_matrix, analytic_posterior_cov,
     normal_inverse_gamma_linear_model, normal_inverse_gamma_guide, group_linear_model,
     group_normal_guide, sigmoid_model, rf_group_assignments
 )
+
 from guides.amort import LinearModelGuide, NormalInverseGammaGuide, SigmoidGuide, GuideDV
 
 PLOT = True
--- tests/contrib/oed/test_eig.py
+++ tests/contrib/oed/test_eig.py
@@ -13,14 +13,11 @@ from pyro.contrib.oed.eig import (
     vi_ape, naive_rainforth_eig, donsker_varadhan_eig, barber_agakov_ape
 )
 from pyro.contrib.oed.util import get_indices
-# TODO move the following code out of examples, e.g. to contrib.lmer
-from examples.contrib.oed.models.bayes_linear import (
+from pyro.contrib.glmm import (
     zero_mean_unit_obs_sd_lm, group_assignment_matrix, analytic_posterior_cov,
     group_linear_model, group_normal_guide
 )
-from examples.contrib.oed.guides.amort import (
-    LinearModelGuide, GuideDV
-)
+from pyro.contrib.glmm.guides import LinearModelGuide, GuideDV
 
 
 #########################################################################################
