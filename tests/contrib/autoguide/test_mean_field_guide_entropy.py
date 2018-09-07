bd168d2f1254047013e679bc72f24d22e4f0f207
ae-foster <ae-foster@users.noreply.github.com>
2018-09-07 14:16:55 -0700

    Barber-Agakov, and OED refactoring (#1361)
    * Merge changes from oed-master excluding sequential sigmoid example
    
    * Some flake8 and Python 2 changes
    
    * Indentation and noqa for lambdas
    
    * Blank __init__ file
    
    * Better docstrings, fix some tests
    
    * Revert Makefile
    
    * Some cosmetic tune-ups
    
    * Correct ewma formula in doc
    
    * A better docstring
    
    * Add from __future__ import... to all files
    
    * Better treatment of intial values
    
    * Rename variables
    
    * Rename shuffled properly
    
    * Better docstring

--- tests/contrib/autoguide/test_mean_field_guide_entropy.py
+++ tests/contrib/autoguide/test_mean_field_guide_entropy.py
@@ -25,4 +25,4 @@ def h(p):
     (mean_field_guide, (torch.eye(2), None), (h(0.2) + h(0.5))*torch.ones(2, 2))
 ])
 def test_guide_entropy(guide, args, expected_entropy):
-    assert_equal(mean_field_guide_entropy(guide, *args), expected_entropy)
+    assert_equal(mean_field_guide_entropy(guide, args), expected_entropy)
