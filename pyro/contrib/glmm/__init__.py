1df6d8b74a93df11c0bcbbf2d0ac9f1259ec5404
Adam Foster <adamf@uber.com>
2018-09-06 17:48:40 -0700

    Use right tensor shapes in glmm __init__

--- pyro/contrib/glmm/__init__.py
+++ pyro/contrib/glmm/__init__.py
@@ -7,12 +7,15 @@ To create a classical Bayesian linear model, use::
 
     from pyro.contrib.glmm import known_covariance_linear_model
 
-    model = known_covariance_linear_model(coef_mean=torch.tensor(0.),
-                                          coef_sd=torch.tensor(10.),
+    # Note: coef is a p-vector, observation_sd is a scalar
+    # Here, p=1 (one feature)
+    model = known_covariance_linear_model(coef_mean=torch.tensor([0.]),
+                                          coef_sd=torch.tensor([10.]),
                                           observation_sd=torch.tensor(2.))
 
     # An n x p design tensor
-    design = torch.tensor(torch.tensor([[1.]]))
+    # Here, n=2 (two observations)
+    design = torch.tensor(torch.tensor([[1.], [-1.]]))
 
     model(design)
 
@@ -20,8 +23,9 @@ use a non-linear link function, such as::
 
     from pyro.contrib.glmm import logistic_regression_model
 
-    model = logistic_regression_model(coef_mean=torch.tensor(0.),
-                                      coef_sd=torch.tensor(10.))
+    # No observation_sd is needed for logistic models
+    model = logistic_regression_model(coef_mean=torch.tensor([0.]),
+                                      coef_sd=torch.tensor([10.]))
 
 Random effects may be incorporated as regular Bayesian regression coefficients.
 For random effects with a shared covariance matrix, see `pyro.contrib.glmm.lmer_model`.
