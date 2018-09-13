df0e6f54d162cdfda8c498a1d5de50e5d6226c72
Neeraj Pradhan <npradhan@uber.com>
2018-09-13 16:45:01 -0700

    Disable documentation inheritance in sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,6 +46,8 @@ extensions = [
     'sphinx.ext.doctest',
 ]
 
+autodoc_inherit_docstrings = False
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
