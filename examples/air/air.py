pyro-ppl/pyro

commit 01e7e69b8f65dd953c2a15af45ecd9ede79b1d71

bugfix for AIR example (#1351)
* bugfix --no-baseline

* added breking test

* var renaming

examples/air/air.py
@@ -228,12 +228,12 @@ class AIR(nn.Module):
         z_pres_p, z_where_loc, z_where_scale = self.predict(h)
 
         # Compute baseline estimates for discrete choice z_pres.
-        bl_value, bl_h, bl_c = self.baseline_step(prev, inputs)
+        infer_dict, bl_h, bl_c = self.baseline_step(prev, inputs)
 
         # Sample presence.
         z_pres = pyro.sample('z_pres_{}'.format(t),
                              dist.Bernoulli(z_pres_p * prev.z_pres).independent(1),
-                             infer=dict(baseline=dict(baseline_value=bl_value.squeeze(-1))))
+                             infer=infer_dict)
 
         sample_mask = z_pres if self.use_masking else torch.tensor(1.0)
 
@@ -259,7 +259,7 @@ class AIR(nn.Module):
 
     def baseline_step(self, prev, inputs):
         if not self.use_baselines:
-            return None, None, None
+            return dict(), None, None
 
         # Prevent gradients flowing back from baseline loss to
         # inference net by detaching from graph here.
@@ -282,7 +282,8 @@ class AIR(nn.Module):
         if self.baseline_scalar is not None:
             bl_value = bl_value * self.baseline_scalar
 
-        return bl_value, bl_h, bl_c
+        infer_dict = dict(baseline_value=bl_value.squeeze(-1))
+        return infer_dict, bl_h, bl_c
 
 
 # Spatial transformer helpers.

tests/test_examples.py
@@ -15,6 +15,7 @@ pytestmark = pytest.mark.stage('test_examples')
 
 CPU_EXAMPLES = [
     'air/main.py --num-steps=1',
+    'air/main.py --num-steps=1 --no-baseline',
     'baseball.py --num-samples=200 --warmup-steps=100',
     'bayesian_regression.py --num-epochs=1',
     'contrib/autoname/scoping_mixture.py --num-epochs=1',

commit b03be07f31aeaf11f3526453874b396b69c7e854

Fix AIR example bugs. (#1388)
* AIR: Use correct learning rate for all baseline parameters.

Fixes #1374.

* AIR: Fix regression introduced in #1351.

examples/air/air.py
@@ -282,7 +282,7 @@ class AIR(nn.Module):
         if self.baseline_scalar is not None:
             bl_value = bl_value * self.baseline_scalar
 
-        infer_dict = dict(baseline_value=bl_value.squeeze(-1))
+        infer_dict = dict(baseline=dict(baseline_value=bl_value.squeeze(-1)))
         return infer_dict, bl_h, bl_c
 
 

examples/air/main.py
@@ -194,8 +194,11 @@ def main(**kwargs):
         z, x = air.prior(5, z_pres_prior_p=partial(z_pres_prior_p, 0))
         vis.images(draw_many(x, tensor_to_objs(latents_to_tensor(z))))
 
+    def isBaselineParam(module_name, param_name):
+        return 'bl_' in module_name or 'bl_' in param_name
+
     def per_param_optim_args(module_name, param_name):
-        lr = args.baseline_learning_rate if 'bl_' in param_name else args.learning_rate
+        lr = args.baseline_learning_rate if isBaselineParam(module_name, param_name) else args.learning_rate
         return {'lr': lr}
 
     adam = optim.Adam(per_param_optim_args)