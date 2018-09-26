385404b9e67a71af185904f3c97c3e4c5ca74150
Neeraj Pradhan <npradhan@uber.com>
2018-09-25 18:51:44 -0700

    fix comment

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,8 +2,8 @@ from __future__ import absolute_import, division, print_function
 
 import logging
 
-# create log handler for tests
 import os
 
+# create log handler for tests
 level = logging.INFO if "CI" in os.environ else logging.DEBUG
 logging.basicConfig(format='%(levelname).1s \t %(message)s', level=level)
