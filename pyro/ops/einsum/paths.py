2d413a667539472694c329aa736e13b9bb91d649
Fritz Obermeyer <fritzo@uber.com>
2018-10-04 01:38:45 -0400

    Fix docstrings

--- pyro/ops/einsum/paths.py
+++ pyro/ops/einsum/paths.py
@@ -12,13 +12,13 @@ def ssa_to_linear(ssa_path):
     Convert a path with static single assignment ids to a path with recycled
     linear ids. For example::
 
-        >>> ssa_to_linear([(0, 3), (1, 4), (2, 5)])
+        >>> ssa_to_linear([(0, 3), (2, 4), (1, 5)])
         [(0, 3), (1, 2), (0, 1)]
     """
-    ids = np.arange(sum(map(len, ssa_path)), dtype=np.int64)
+    ids = np.arange(1 + max(map(max, ssa_path)), dtype=np.int32)
     path = []
-    for i, ssa_ids in enumerate(ssa_path):
-        path.append(tuple(ids[ssa_id] for ssa_id in ssa_ids))
+    for ssa_ids in ssa_path:
+        path.append(tuple(int(ids[ssa_id]) for ssa_id in ssa_ids))
         for ssa_id in ssa_ids:
             ids[ssa_id:] -= 1
     return path
@@ -30,7 +30,7 @@ def linear_to_ssa(path):
     assignment ids. For example::
 
         >>> linear_to_ssa([(0, 3), (1, 2), (0, 1)])
-        [(0, 3), (1, 4), (2, 5)]
+        [(0, 3), (2, 4), (1, 5)]
     """
     num_inputs = sum(map(len, path)) - len(path) + 1
     linear_to_ssa = list(range(num_inputs))
