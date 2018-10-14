f672f17d6d67a7e7363c87d9a1958c5271bcc40d
Rob Salomone <37037906+robsalomone@users.noreply.github.com>
2018-10-14 12:49:43 +1000

    updated importance.py after review
    Implemented changes to address code review by jochen.

--- pyro/infer/importance.py
+++ pyro/infer/importance.py
@@ -1,12 +1,11 @@
 from __future__ import absolute_import, division, print_function
 import torch
-import logging
+import warnings
 
+from pyro.distributions.util import logsumexp
 import pyro.poutine as poutine
 
 from .abstract_infer import TracePosterior
-
-logger = logging.getLogger(__name__)
 
 
 class Importance(TracePosterior):
@@ -19,6 +18,7 @@ class Importance(TracePosterior):
     using the guide as the proposal distribution.
     If no guide is provided, it defaults to proposing from the model's prior.
     """
+
     def __init__(self, model, guide=None, num_samples=None):
         """
         Constructor. default to num_samples = 10, guide = model
@@ -26,7 +26,7 @@ class Importance(TracePosterior):
         super(Importance, self).__init__()
         if num_samples is None:
             num_samples = 10
-            logger.warn("num_samples not provided, defaulting to {}".format(num_samples))
+            warnings.warn("num_samples not provided, defaulting to {}".format(num_samples))
         if guide is None:
             # propose from the prior by making a guide from the model by hiding observes
             guide = poutine.block(model, hide_types=["observe"])
@@ -50,27 +50,33 @@ class Importance(TracePosterior):
         Estimator of the normalizing constant of the target distribution.
         (mean of the unnormalized weights)
         """
-        log_w = torch.tensor(self.log_weights)
-        log_num_samples = torch.log(torch.tensor(self.num_samples, dtype=torch.float))
-        return torch.logsumexp(log_w - log_num_samples, 0)
+        # ensure list is not empty
+        if self.log_weights:
+            log_w = torch.tensor(self.log_weights)
+            log_num_samples = torch.log(torch.tensor(self.num_samples * 1.))
+            return logsumexp(log_w - log_num_samples, 0)
+        else:
+            warnings.warn("The log_weights list is empty, can not compute normalizing constant estimate.")
 
     def get_normalized_weights(self, log_scale=False):
         """
         Compute the normalized importance weights.
         """
-        log_w = torch.tensor(self.log_weights)
-        log_w_norm = log_w - torch.logsumexp(log_w, 0)
-        return log_w_norm if log_scale else torch.exp(log_w_norm)
+        if self.log_weights:
+            log_w = torch.tensor(self.log_weights)
+            log_w_norm = log_w - logsumexp(log_w, 0)
+            return log_w_norm if log_scale else torch.exp(log_w_norm)
+        else:
+            warnings.warn("The log_weights list is empty. There is nothing to normalize.")
 
     def get_ESS(self):
         """
         Compute (Importance Sampling) Effective Sample Size (ESS).
         """
-        # check to make sure list is not empty
         if self.log_weights:
             log_w_norm = self.get_normalized_weights(log_scale=True)
-            ess = torch.exp(-torch.logsumexp(2*log_w_norm, 0))
+            ess = torch.exp(-logsumexp(2*log_w_norm, 0))
         else:
-            logger.warn("The log_weights list is empty, effective sample size is zero.")
+            warnings.warn("The log_weights list is empty, effective sample size is zero.")
             ess = 0
         return ess
