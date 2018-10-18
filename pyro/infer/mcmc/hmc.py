5c3465f89b7eacd5df3c17b92479107408d35b5b
Neeraj Pradhan <npradhan@uber.com>
2018-10-18 14:00:50 -0700

    Fix non determinism in HMC / NUTS

--- pyro/infer/mcmc/hmc.py
+++ pyro/infer/mcmc/hmc.py
@@ -156,9 +156,9 @@ class HMC(TraceKernel):
     def _kinetic_energy(self, r):
         r_flat = torch.cat([r[site_name].reshape(-1) for site_name in sorted(r)])
         if self.full_mass:
-            return 0.5 * r_flat.dot(self._inverse_mass_matrix.matmul(r_flat))
+            return 0.5 * (r_flat * (self._inverse_mass_matrix.matmul(r_flat))).sum()
         else:
-            return 0.5 * self._inverse_mass_matrix.dot(r_flat ** 2)
+            return 0.5 * (self._inverse_mass_matrix * (r_flat ** 2)).sum()
 
     def _potential_energy(self, z):
         # Since the model is specified in the constrained space, transform the
