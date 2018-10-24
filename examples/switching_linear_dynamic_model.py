ea2d89ea18706e11fcca550cc2f82dec65aa4e97
Li <ruijili@ebay.com>
2018-10-24 10:56:05 +0800

    - rename segment with state - make MAP and VB working - MAP   - computing marginal is very slow - VB   - seems that VB is working, but VB is so slow..   - don't know if there is a easier way to add some auto guide on some continuous variables, see line 171

--- examples/switching_linear_dynamic_model.py
+++ examples/switching_linear_dynamic_model.py
@@ -8,51 +8,53 @@ import logging
 import torch
 from torch.distributions import constraints
 
+import re
 import pyro
 import pyro.distributions as dist
 from pyro import poutine
-from pyro.contrib.autoguide import AutoDelta, AutoGuide, AutoGuideList
+from pyro.contrib.autoguide import AutoDelta, AutoGuide, AutoGuideList, AutoDiagonalNormal
 from pyro.infer import SVI, JitTraceEnum_ELBO, TraceEnum_ELBO, Trace_ELBO, JitTrace_ELBO, config_enumerate
 from pyro.optim import Adam
+
+
 logging.basicConfig(format='%(relativeCreated) 9d %(message)s', level=logging.INFO)
 
-@poutine.broadcast
-def model_slds(sequences, lengths, args, batch_size=None, include_prior=True):
+def model_slds(sequences, lengths, args, include_prior=True):
     num_sequences, max_length, data_dim = sequences.shape
     assert lengths.shape == (num_sequences,)
     assert lengths.max() <= max_length
 
-    n_seg = args.num_segment # K, number of segments
+    n_state = args.num_state # K, number of states
     n_lat = args.num_latent
     n_out = data_dim
     normal_std = 1e-1
     with poutine.mask(mask=torch.tensor(include_prior)):
         # transition matrix for HMM, K*K,
         hmm_dyn = pyro.sample("hmm_dyn",
-                              dist.Dirichlet(0.9 * torch.eye(n_seg) + 0.1)
+                              dist.Dirichlet(0.9 * torch.eye(n_state) + 0.1)
                                   .independent(1))
         ## LDS
         ssm_dyn = pyro.sample("ssm_dyn",
-                              dist.Normal(0,normal_std).expand_by([n_seg, n_lat, n_lat])
+                              dist.Normal(0,normal_std).expand_by([n_state, n_lat, n_lat])
                                   .independent(3))
         ssm_bias = pyro.sample("ssm_bias",
-                              dist.Normal(0,1.).expand_by([n_seg, n_lat, 1])
+                              dist.Normal(0,1.).expand_by([n_state, n_lat, 1])
                                   .independent(3))
         ssm_noise = pyro.sample("ssm_noise",
-                              dist.Gamma(1e0,1e0).expand_by([n_seg, n_lat, 1])
+                              dist.Gamma(1e0,1e0).expand_by([n_state, n_lat, 1])
                                   .independent(3))
         ## observation
         obs_weight = pyro.sample("obs_weight",
-                              dist.Normal(0,1).expand_by([n_seg, n_out, n_lat])
+                              dist.Normal(0,1).expand_by([n_state, n_out, n_lat])
                                   .independent(3))
         obs_bias = pyro.sample("obs_bias",
-                              dist.Normal(0,1).expand_by([n_seg, n_out, 1])
+                              dist.Normal(0,1).expand_by([n_state, n_out, 1])
                                   .independent(3))
         obs_noise = pyro.sample("obs_noise",
-                              dist.Gamma(1e0,1e0).expand_by([n_seg, n_out, 1])
+                              dist.Gamma(1e0,1e0).expand_by([n_state, n_out, 1])
                                   .independent(3))
 
-    with pyro.iarange("sequences", len(sequences), batch_size, dim=-1) as batch:
+    with pyro.plate("sequences", len(sequences), dim=args.plate_dim) as batch:
         ## ssm init state
         # use a (n_lat,1) matrix (instead of vector) to ease batch operation,
         # as for high dim tensor , matmul would do batch matrix-matrix product, instead of matrix-vector product.
@@ -63,13 +65,13 @@ def model_slds(sequences, lengths, args, batch_size=None, include_prior=True):
         z = 0
         x = ssm_init
         for t in range(lengths.max()):
-            with poutine.mask(mask=(t < lengths).unsqueeze(-1)):
-                z = pyro.sample("z_{}".format(t), dist.Categorical(hmm_dyn[z]),
-                                infer={"enumerate": "parallel"})
-                x = pyro.sample("x_{}".format(t), dist.Normal(torch.matmul(ssm_dyn[z], x) + ssm_bias[z], torch.sqrt(1./ssm_noise[z])).independent(2))
-                obs_lat = torch.matmul(obs_weight[z], x) + obs_bias[z]
-                pyro.sample("y_{}".format(t), dist.Normal(obs_lat, torch.sqrt(1./obs_noise[z])).independent(2),
-                            obs=sequences[batch, t].unsqueeze(-1))
+            # with poutine.mask(mask=(t < lengths).unsqueeze(-1)):
+            z = pyro.sample("z_{}".format(t), dist.Categorical(hmm_dyn[z]),
+                            infer={"enumerate": "parallel"})
+            x = pyro.sample("x_{}".format(t), dist.Normal(torch.matmul(ssm_dyn[z], x) + ssm_bias[z], torch.sqrt(1./ssm_noise[z])).independent(2))
+            obs_lat = torch.matmul(obs_weight[z], x) + obs_bias[z]
+            pyro.sample("y_{}".format(t), dist.Normal(obs_lat, torch.sqrt(1./obs_noise[z])).independent(2),
+                        obs=sequences[batch, t].unsqueeze(-1))
 
 
                 # print('===={}===='.format(t))
@@ -93,6 +95,23 @@ def model_slds(sequences, lengths, args, batch_size=None, include_prior=True):
                 # print('obs_final.shape', sequences[batch, t].unsqueeze(-1).shape)
 
 
+def load_sequences(datafile, N=None):
+
+    npz = np.load(datafile)
+    data, labels, A1, A2, A3 = [npz['arr_%d' % i] for i in range(5)]
+
+    # just repeat
+    sequences = torch.tensor(np.array([data]*N), dtype=torch.float32) if N is not None else data
+    return sequences
+
+def gen_random_sequences(N,T,D):
+    sequences = torch.tensor(np.random.randn(N,T,D), dtype=torch.float32)
+    return sequences
+
+
+def gen_nseg_ar_sequences(N,T,D,n_seg):
+    # TODO
+    return 0
 
 def main(args):
     if args.cuda:
@@ -101,48 +120,73 @@ def main(args):
     logging.info('Generating data')
     logging.info(args)
 
+    retfile = '/Users/ruijili/Documents/3_correlation/tsa/pao/sssm/ret.npz'
+
+
+
     ########################################################################
     # just generate a random dataset for debug purpose
     ########################################################################
     N = 3  # number of sequences
     T = 10 # number of timestamps
     D = 2
-    sequences = torch.tensor(np.random.randn(N,T,D), dtype=torch.float32)
-    logging.info('-' * 40)
+
+    # change me
+    if args.data_path != '':
+        sequences = load_sequences(args.data_path, N)
+    else:
+        sequences = gen_random_sequences(N, T, D)
+
+
+    N, T, D = sequences.shape
+    lengths = torch.ones(N, dtype=torch.long)*T
+
+
     model = model_slds
-    lengths = torch.ones(sequences.shape[0], dtype=torch.long)*T
 
 
     ###################
     # guide for MAP
     ###################
-    myguide_map = AutoDelta(poutine.block(model, hide_fn=lambda m: m['name'].startswith('z_')))
+    guide_map = AutoGuideList(model)
+    guide_map.add(AutoDelta(poutine.block(model, hide_fn=lambda m: re.match('[z]_', m['name']))))
+
+
+    # guide_map.add(AutoDelta(poutine.block(model, hide_fn=lambda m: re.match('[zx]_', m['name']))))
+    # guide_map.add(AutoDiagonalNormal(poutine.block(model, expose_fn=lambda m: re.match('x_', m['name']))))
 
 
     #################################################################
     # guide for VB on z, for simplicity of debug, focusing on z, ignoring the rest
     #################################################################
-    @poutine.broadcast
-    @config_enumerate(default="parallel")
-    def guide_vb(sequences, lengths, args, batch_size=None, include_prior=True):
+    guide_vb_0 = AutoGuideList(model)
+    guide_vb_0.add(AutoDelta(poutine.block(model, hide_fn=lambda m: re.match('[zx]_', m['name']))))
+    guide_vb_0.add(AutoDiagonalNormal(poutine.block(model, expose_fn=lambda m: re.match('x_', m['name']))))
+
+    def guide_vb(sequences, lengths, args, include_prior=True):
         num_sequences, max_length, data_dim = sequences.shape
         # print('in guide, ', num_sequences, max_length, data_dim)
-        n_seg = args.num_segment
+        n_state = args.num_state
 
-        # myguide_map(sequences, lengths, args, batch_size, include_prior)
-        with pyro.iarange("sequences", len(sequences), batch_size, dim=-2) as batch:
+        ####################
+        ## if enabled there will be error claiming that 'sequence' is used twice.
+        ########################
+        # guide_vb_0(sequences, lengths, args, include_prior=True)
+
+        with pyro.plate("sequences", len(sequences), dim=args.plate_dim) as batch:
             lengths = lengths[batch]
             for t in range(lengths.max()):
-                ret_prob = pyro.param('assignment_probs_{}'.format(t), torch.ones(len(lengths), 1, n_seg) / n_seg,
-                                      constraint=constraints.unit_interval)
+                ret_prob = pyro.param('assignment_probs_{}'.format(t), torch.ones(len(lengths), n_state) / n_state,
+                                      constraint=constraints.simplex)
+                z = pyro.sample("z_{}".format(t), dist.Categorical(ret_prob), infer={"enumerate": "parallel"})
 
-                with poutine.mask(mask=(t < lengths).unsqueeze(-1)):
-                    z = pyro.sample("z_{}".format(t), dist.Categorical(ret_prob))
-        # print('in guide, ', num_sequences, max_length, data_dim)
-        # print('in guide, z.shape', z.shape)
-        # print('in guide, max_lengths', max(lengths))
+        print('in guide, ', num_sequences, max_length, data_dim)
+        print('in guide, z.shape', z.shape)
+        print('in guide, max_lengths', max(lengths))
 
 
+
+    guide = guide_map if args.map else guide_vb
 
 
     logging.info('Training {} on {} sequences'.format('slds', 1))
@@ -158,74 +202,69 @@ def main(args):
     # out the hidden state x. This is accomplished via an automatic guide that
     # learns point estimates of all of our conditional probability tables,
     # named probs_*.
-    if args.map:
-        guide = AutoDelta(poutine.block(model, hide_fn=lambda m: m['name'].startswith('z_')))
-    else:
-        guide = guide_vb
-
-
 
 
     ######## debug purpose only, to check the shapes in mode and guide #####
-    trace_model = poutine.trace(model).get_trace(sequences, lengths, args, batch_size=args.batch_size)
-    trace_guide = poutine.trace(guide).get_trace(sequences, lengths, args, batch_size=args.batch_size)
+    trace_model = poutine.trace(model).get_trace(sequences, lengths, args)
+    trace_guide = poutine.trace(guide).get_trace(sequences, lengths, args)
     #####################
 
 
 
-    # Enumeration requires a TraceEnum elbo and declaring the max_iarange_nesting.
-    # All of our models have two iaranges: "data" and "tones".
+
+
     Elbo = JitTraceEnum_ELBO if args.jit else TraceEnum_ELBO
-    elbo = Elbo(max_iarange_nesting=2)
+    elbo = Elbo(max_plate_nesting=5)
 
 
     optim = Adam({'lr': args.learning_rate})
     svi = SVI(model, guide, optim, elbo)
     logging.info('Step\tLoss')
     for step in range(args.num_steps):
-        loss = svi.step(sequences, lengths, args, batch_size=args.batch_size)
+        loss = svi.step(sequences, lengths, args)
         logging.info('{: >5d}\t{}'.format(step, loss / num_observations))
 
     # We evaluate on the entire training dataset,
     # excluding the prior term so our results are comparable across models.
-    train_loss = elbo.loss(model, guide, sequences, lengths, args, include_prior=False)
-    logging.info('training loss = {}'.format(train_loss / num_observations))
+    # train_loss = elbo.loss(model, guide, sequences, lengths, args, include_prior=False)
+    # logging.info('training loss = {}'.format(train_loss / num_observations))
 
 
 
     ###############################
     ## exam the enumeration of z
     ###############################
-    r1 = elbo.compute_marginals(model, guide, sequences, lengths, args, batch_size=N)
-
     # save result for further investigation
     dict_ret = {}
     for name in pyro.get_param_store().get_all_param_names():
         dict_ret[name] = pyro.param(name).data.numpy()
 
-    if not args.map:
+    if 1:
+        logging.info("computing marginals")
+        r1 = elbo.compute_marginals(model, guide, sequences, lengths, args)
         for k,v in r1.items():
             dict_ret['marg_%s' % k] = v.logits.data.numpy()
 
-    np.savez('./slds_out.npz', **dict_ret)
+    np.savez(retfile, **dict_ret)
 
 
-    print('done')
+    logging.info('done')
 
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="MAP Baum-Welch learning Bach Chorales")
-    parser.add_argument("-n", "--num-steps", default=1, type=int)
-    parser.add_argument("-b", "--batch-size", default=3, type=int)
+    parser.add_argument("-n", "--num-steps", default=50, type=int)
     parser.add_argument("-l", "--num-latent", default=2, type=int)
-    parser.add_argument("-s", "--num-segment", default=5, type=int)
-
+    parser.add_argument("-s", "--num-state", default=5, type=int)
     parser.add_argument("-lr", "--learning-rate", default=0.1, type=float)
     parser.add_argument("-t", "--truncate", type=int)
     parser.add_argument('--cuda', action='store_true')
-    parser.add_argument('--jit', action='store_true')
-    parser.add_argument("--map", action='store_true', default=True) # MAP estimation or VB
+    parser.add_argument('--jit', action='store_true', default=False)
+    parser.add_argument("--map", action='store_true', default=False) # MAP estimation or VB, Use MAP for the moment as VB is not working
+    parser.add_argument("-pd", "--plate-dim", default=-1, type=int)
+    parser.add_argument("-dp", "--data-path", default='', type=str)
+    # parser.add_argument("-dp", "--data-path", default='/Users/ruijili/Documents/3_correlation/tsa/pao/sssm/hmmar.npz', type=str)
 
     args = parser.parse_args()
     main(args)
