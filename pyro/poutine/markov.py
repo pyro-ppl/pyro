da3f322474c5ab756bc8fdd990f8692757370089
Fritz Obermeyer <fritzo@uber.com>
2018-11-06 08:00:26 -0800

    WIP break all the things

--- pyro/poutine/markov.py
+++ pyro/poutine/markov.py
@@ -20,7 +20,7 @@ class ReentrantMessenger(Messenger):
 
     def __enter__(self):
         self._ref_count += 1
-        if self.ref_count == 1:
+        if self._ref_count == 1:
             super(ReentrantMessenger, self).__enter__()
         return self
 
@@ -32,7 +32,7 @@ class ReentrantMessenger(Messenger):
 
 class MarkovMessenger(ReentrantMessenger):
     """
-    Statistical equivalent of an memory management arena.
+    Statistical equivalent of a memory management arena.
 
     .. warning:: This assumes markov contexts can be reentrant but cannot be
         interleaved. I.e. the following is invalid::
@@ -42,12 +42,18 @@ class MarkovMessenger(ReentrantMessenger):
                 with x_axis:  # <--- error here
                     ...
     """
-    def __init__(self, window=1):
-        assert window > 0  # otherwise we need more precise slicing below
-        self.window = window
-        self._stack = []  # list of sets of dimensions
-        self._set = set()  # set of active dimensions
+    def __init__(self, history=1, keep=False):
+        assert history > 0  # otherwise we need more precise slicing below
+        self.history = history
+        self.keep = keep
         self.iterator = None
+
+        self._pos = -1
+        self._stack = []
+        self._set = set()  # set of active dimensions
+
+        # _ref_counts is shared among all MarkovMessengers, with lifetime of a model call.
+        self._ref_counts = None
 
     def generator(self, iterable):
         self.iterable = list(iterable)
@@ -60,29 +66,36 @@ class MarkovMessenger(ReentrantMessenger):
                 yield value
 
     def __enter__(self):
-        self._stack.push(set())
-        self._set = set().union(*self._stack[-self.window:])
+        self._pos += 1
+        if len(self._stack) <= self._pos:
+            self._stack.push(set())
+        self._set = set().union(*self._stack[min(0, self._pos - self.history):1 + self._pos])
         return super(MarkovMessenger, self).__enter__()
 
     def __exit__(self, *args, **kwargs):
-        self._stack.pop()
-        self._set = set().union(*self._stack[-self.window:])
+        if not self.keep:
+            self._stack.pop()
+        self._pos -= 1
+        self._set = set().union(*self._stack[min(0, self._pos - self.history):1 + self._pos])
+        if self._pos == -1:
+            self._ref_counts = None
         # FIXME handle exceptions correctly
         return super(MarkovMessenger, self).__exit__(*args, **kwargs)
 
     def _pyro_sample(self, msg):
-        cond_dep_set = msg.setdefault("cond_dep_set", self._set)
-        if cond_dep_set is not self._set:
-            msg["cond_dep_set"] = cond_dep_set & self._set
-        if "_markov" in msg["infer"]:
-            msg["infer"]["_markov"] += 1
+        if "cond_dep_set" in msg:
+            msg["cond_dep_set"] &= self._set
         else:
-            msg["infer"]["_markov"] = 0
+            msg["cond_dep_set"] = self._set.copy()
+
+        self._markov_depth = msg["infer"].get("_markov_depth", defaultdict(int))
+        self._markov_depth[msg["name"]] += 1
 
     def _pyro_post_sample(self, msg):
-        msg["infer"]["_markov"] -= 1
-        if msg["infer"]["_markov"] == 0:
+        if self._markov_depths[msg["name"]]
+        msg["infer"]["_markov_depth"] -= 1
+        if msg["infer"]["_markov_depth"] == 0:
             dim = msg["infer"].get("_enumerate_dim")
             if dim is not None:
-                self._stack[dim].add(dim)
+                self._stack[self._pos].add(dim)
                 self._set.add(dim)
