90def62a537db4aef3eb45c660fff073470758bd
Neeraj Pradhan <npradhan@uber.com>
2018-11-08 14:34:24 -0800

    remove gaussian hmm test

--- tests/infer/mcmc/test_nuts.py
+++ tests/infer/mcmc/test_nuts.py
@@ -268,34 +268,3 @@ def test_gaussian_hmm_enum_shape(num_steps, use_einsum):
     nuts_kernel = NUTS(model, max_plate_nesting=0, experimental_use_einsum=use_einsum)
     MCMC(nuts_kernel, num_samples=5, warmup_steps=5).run(data)
 
-
-@pytest.mark.parametrize("num_steps", [10, 20])
-def test_gaussian_hmm(num_steps):
-    dim = 4
-
-    def model(data):
-        initialize = pyro.sample("initialize", dist.Dirichlet(torch.ones(dim)))
-        transition = pyro.sample("transition", dist.Dirichlet(torch.ones(dim, dim)))
-        emission_loc = pyro.sample("emission_loc", dist.Normal(torch.zeros(dim), torch.ones(dim)))
-        emission_scale = pyro.sample("emission_scale", dist.LogNormal(torch.zeros(dim), torch.ones(dim)))
-        x = None
-        for t, y in enumerate(data):
-            x = pyro.sample("x_{}".format(t), dist.Categorical(initialize if x is None else transition[x]))
-            pyro.sample("y_{}".format(t), dist.Normal(emission_loc[x], emission_scale[x]), obs=y)
-            # check shape
-            effective_dim = sum(1 for size in x.shape if size > 1)
-            assert effective_dim == 1
-
-    transition_probs = torch.tensor([[0.3, 0.2, 0.1, 0.4],
-                                     [0.2, 0.3, 0.1, 0.1],
-                                     [0.3, 0.2, 0.2, 0.3],
-                                     [0.6, 0.2, 0.1, 0.1]])
-    fixed_emissions = torch.tensor([10., 20., 30., 40.])
-    state = torch.tensor(1)
-    data = [fixed_emissions[state]]
-    for _ in range(num_steps):
-        state = dist.Categorical(transition_probs[state]).sample()
-        data.append(fixed_emissions[state])
-    data = torch.stack(data)
-    nuts_kernel = NUTS(model, max_plate_nesting=0, experimental_use_einsum=True, adapt_mass_matrix=True)
-    MCMC(nuts_kernel, num_samples=200, warmup_steps=100).run(data)
