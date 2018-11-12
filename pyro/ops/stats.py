799cf19a3839632e73c48d33fcf560a6eb997bcc
fehiepsi <fehiepsi@gmail.com>
2018-11-12 13:47:21 -0500

    implement effective sample size

--- pyro/ops/stats.py
+++ pyro/ops/stats.py
@@ -38,7 +38,7 @@ def resample(input, num_samples, dim=-1, replacement=False):
     """
     Draws `num_samples` samples from `input` at dimension `dim`.
     """
-    weights = input.new_ones(size(dim))
+    weights = input.new_ones(input.size(dim))
     indices = torch.multinomial(weights, num_samples, replacement)
     return input.index_select(dim, indices)
 
@@ -53,6 +53,7 @@ def quantile(input, probs, dim=-1):
     sorted_input = input.sort(dim)[0]
     max_index = input.size(dim) - 1
     indices = probs * max_index
+
     # because indices is float, we interpolate the quantiles linearly from nearby points
     indices_below = indices.long()
     indices_above = (indices_below + 1).clamp(max=max_index)
@@ -86,12 +87,14 @@ def hpdi(input, prob, dim=-1):
     intervals_lower = sorted_input.index_select(dim, torch.arange(mass - index_length))
     intervals_upper = sorted_input.index_select(dim, torch.arange(index_length, mass))
     intervals_length = intervals_upper - intervals_lower
+
     index_start = intervals_length.argmin(dim)
     indices = torch.stack([index_start, index_start + index_length], dim)
     return torch.gather(sorted_input, dim, indices)
 
 
 WAICinfo = namedtuple("WAICinfo", ["waic", "p_waic", "se", "waic_vec"])
+
 
 def _waic_from_batch_log_prob(log_probs):
     n = log_probs.size(0)
@@ -117,7 +120,8 @@ def waic(traces):
         obs_nodes = trace.observation_nodes
         trace.compute_log_prob(site_filter=lambda name, site: name in obs_nodes)
         for node in obs_nodes:
-            trace_log_pdf[node].append(trace.nodes[node]["log_prob"].detach())
+            trace_log_prob[node].append(trace.nodes[node]["log_prob"].detach())
+
     obs_waic_dict = {}
     for node, batch_log_prob in trace_log_prob.items():
         obs_waic_dict[node] = _waic_from_batch_log_prob(torch.stack(batch_log_prob, dim=0))
@@ -127,22 +131,26 @@ def waic(traces):
 ModelInfo = namedtuple("ModelInfo", ["name", "waic", "p_waic", "se",
                                      "waic_diff", "diff_se", "weight"])
 
+
 def compare(model_traces_dict):
     model_waic_dict = {}
     obs = None
     for model, traces in model_traces_dict.items():
         model_waic = waic(traces)
         obs = list(model_waic.keys())[0] if obs is None else obs
-        if len(model_waic) > 1 or obs is not in model_waic:
+        if len(model_waic) > 1 or obs not in model_waic:
             raise ValueError("We can only compare models with the same observation node.")
         model_waic_dict[model] = model_waic[obs]
+
     model_names = sorted(model_waic_dict)
     waics = torch.stack([model_waic_dict[model].waic for model in model_names])
     _, indices = waics.sort(dim=0)
+
     waic_diff = waics - waics[indices[0]]
     waic_vecs = torch.stack([model_waic_dict[model].waic_vec for model in model_names])
-    diff_se = math.sqrt(n) * (waic_vecs - waic_vecs[indices[0]]).std()
+    diff_se = math.sqrt(waic_vecs.size(1)) * (waic_vecs - waic_vecs[indices[0]]).std()
     weights = torch.nn.functional.softmax(-0.5 * waics)
+
     model_table = []
     for i in indices:
         name = model_names[i]
@@ -160,12 +168,14 @@ def gelman_rubin(input):
     """
     C, N = input.size(0), input.size(1)
     chain_mean = input.mean(dim=1)
+    var_between = chain_mean.var(dim=0) * C / (C - 1)
+
     chain_biased_var = input.var(dim=1)
-    chain_mean_var = chain_mean.var(dim=0) * C / (C - 1)
-    chain_biased_var_mean = chain_var.mean(dim=0)
-    chain_var_mean = chain_biased_var_mean * N / (N - 1)
-    variance_estimator = chain_biased_var_mean + chain_mean_var
-    return (variance_estimator / chain_var_mean).sqrt(), chain_var_mean, variance_estimator
+    biased_var_within = chain_biased_var.mean(dim=0)
+    var_within = biased_var_within * N / (N - 1)
+
+    var_estimator = biased_var_within + var_between
+    return (var_estimator / var_within).sqrt(), var_within, var_estimator
 
 
 def split_gelman_rubin(input):
@@ -198,31 +208,90 @@ def _fft_next_good_size(N):
 
 def autocorrelation(input):
     """
-    Computes the autocorrelation of a sample.
+    Computes the autocorrelation of samples. The last dimension is the number of
+    samples. Other dimensions are batch dimensions.
 
     Reference: https://en.wikipedia.org/wiki/Autocorrelation#Efficient_computation
     """
     # Adapted from Stan implementation
     # https://github.com/stan-dev/math/blob/develop/stan/math/prim/mat/fun/autocorrelation.hpp
-    N = input.size(0)
+    N = input.size(-1)
     M = _fft_next_good_size(N)
     M2 = 2 * M
     # centering and padding x
-    centered_signal = input.new_zeros(M2)
-    centered_signal[:N] = input - input.mean()
+    centered_signal = input - input.mean()
+    pad = input.new_zeros(input.shape[:-1] + (M2 - N,))
+    centered_signal = torch.cat([centered_signal, pad], dim=-1)
     # Fourier transform
     freqvec = torch.rfft(centered_signal, signal_ndim=1, onesided=False)
     # take square of magnitude of freqvec (or freqvec x freqvec*)
-    freqvec_gram = input.new_zeros(M2, 2)
-    freqvec_gram[:, 0] = freqvec.pow(2).sum(-1)
+    freqvec_gram = freqvec.pow(2).sum(-1, keepdim=True)
+    freqvec_gram = torch.cat([freqvec_gram, input.new_zeros(freqvec_gram.shape)], dim=-1)
     # inverse Fourier transform
     ac = torch.irfft(freqvec_gram, signal_ndim=1, onesided=False)
-    ac = ac[:N]
+    ac = ac[..., :N]
     ac = ac / input.new_tensor(range(N, 0, -1))
-    ac = ac / ac[0]
+    ac = ac / ac[..., :1]
     return ac
 
 
-def effective_number(input, dim=0):
-    """Compute effective number of input."""
-    pass
+def autocovariance(input):
+    """
+    Computes the autocovariance of samples. The last dimension is the number of
+    samples. Other dimensions are batch dimensions.
+    """
+    return autocorrelation(input) * input.var(-1)
+
+
+def _cummin(input):
+    """
+    Computes cummulative minimum of input at dimension `dim=0`.
+    """
+    # FIXME: is there a better trick to find accumulate min of a sequence?
+    N = input.size(0)
+    input_tril = input.unsqueeze(0).expand((N,) + input.shape)
+    mask_triu = input.new_ones(N, N).triu(diagonal=1).reshape((N, N) + (1,) * (input.dim() - 1))
+    mask_triu = mask_triu.expand((N, N) + input.shape[1:])
+    input_tril[mask_triu > 0.5] = 0
+    return input_tril.mean(0)
+
+
+def effective_sample_size(input):
+    """
+    Computes effective sample size of input. The first two dimensions of `input`
+    is `C x N` where `C` is the number of chains and `N` is the number of samples.
+
+    Reference:
+
+    [1] `Introduction to Markov Chain Monte Carlo`,
+        Charles J. Geyer
+    [2] `Stan Reference Manual version 2.18`,
+        Stan Development Team
+    """
+    C, N = input.size(0), input.size(1)
+    # C x N x sample_shape -> C x sample_shape x N
+    reshaped_input = input.unsqueeze(-1).transpose(1, -1).reshape((C,) + input.shape[2:] + (N,))
+    # find autocovariance for each chain at lag k
+    gamma_k_c = autocovariance(reshaped_input)  # C x sample_shape x N
+    gamma_k_c = gamma_k_c.unsqueeze(1).transpose(1, -1).reshape((C, N) + input.shape[2:])
+
+    # find autocorrelation at lag k (from Stan reference)
+    _, var_within, var_estimator = gelman_rubin(input)  # C x N x sample_shape -> sample_shape
+    rho_k = (var_estimator - var_within + gamma_k_c.mean(0)) / var_estimator  # N x sample_shape
+    rho_k[0] = 1  # correlation at lag 0 is always 1
+
+    # initial positive sequence (formula 1.18 in [1]) applied for autocorrelation
+    Rho_k = rho_k if N % 2 == 0 else rho_k[:-1]
+    Rho_k = rho_k.reshape((N // 2, 2) + rho_k.shape[2:]).sum(dim=1)
+    # separate the first index
+    Rho_init = Rho_k[0]
+    # Theoretical, Rho_k is positive, but due to noise of correlation computation,
+    # Rho_k might not be positive at some point. So we need to truncate (ignore first index).
+    Rho_positive = Rho_k[1:].clamp(min=0)
+
+    # Now we make the initial monotone (decreasing) sequence.
+    Rho_monotone = _cummin(Rho_positive)
+
+    # Formula 1.19 in [1]
+    tau = -1 + 2 * Rho_init + 2 * Rho_monotone.sum(dim=0)
+    return C * N / tau
