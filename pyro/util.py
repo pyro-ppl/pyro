dc5c0fd10734fceca0cc52bad0ae743fe62a86d9
Neeraj Pradhan <npradhan@uber.com>
2018-11-14 12:10:19 -0800

    add prog bar

--- pyro/util.py
+++ pyro/util.py
@@ -1,8 +1,11 @@
 from __future__ import absolute_import, division, print_function
 
 import functools
+import math
 import numbers
 import random
+import subprocess
+import sys
 import warnings
 from collections import defaultdict
 from contextlib import contextmanager
@@ -342,3 +345,92 @@ def deep_getattr(obj, name):
     Throws an AttributeError if bad attribute
     """
     return functools.reduce(getattr, name.split("."), obj)
+
+
+class ProgressBar(object):
+    def __init__(self, total, desc, postfix):
+        self._width = 40
+        self._fill = "="
+        self._t = 0
+        self._update_interval = total // self._width
+        self.total = total
+        self.desc = desc
+        self.postfix = postfix
+        self.progbar = ""
+
+    @staticmethod
+    def _get_screen_width():
+        cols = None
+        try:
+            cols = int(subprocess.check_call(["tput", "cols"]))
+        except Exception:
+            pass
+        return cols
+
+    @property
+    def num_lines(self):
+        screen_width = self._get_screen_width()
+        return int(math.ceil(len(str(self)) / screen_width)) if screen_width not in (None, 0) else 1
+
+    def update(self):
+        sys.stdout.flush()
+        sys.stdout.write("\r" + str(self))
+
+    def increment(self):
+        self._t += 1
+        if self._t % self._update_interval == 0:
+            self.progbar += self._fill
+
+    def __str__(self):
+        postfix = str(self.postfix)
+        return self.desc + " [" + self.progbar.ljust(self._width) + "] " + "({})".format(postfix)
+
+
+class MultiProgressBars(object):
+    def __init__(self):
+        self._width = 40
+        self._fill = "="
+        self._t = 0
+        self.progbars = []
+
+    @staticmethod
+    def _get_screen_width():
+        cols = None
+        try:
+            cols = int(subprocess.check_call(["tput", "cols"]))
+        except Exception:
+            pass
+        return cols
+
+    @property
+    def num_lines(self):
+        return sum(p.num_lines for p in self.progbars)
+
+    def update(self):
+        sys.stdout.flush()
+        num_lines = self.num_lines
+        if num_lines > 1:
+            sys.stdout.write("\033[F" * (num_lines - 1))
+        else:
+            sys.stdout.write("\r")
+        sys.stdout.write(str(self))
+
+    def add(self, total, desc, postfix):
+        self.progbars.append(ProgressBar(total, desc, postfix))
+
+    def increment(self, pos=0):
+        self.progbars[pos].increment()
+
+    def __str__(self):
+        return "\n".join([str(p) for p in self.progbars])
+
+from time import sleep
+
+p = MultiProgressBars()
+p.add(total=100, desc="hello", postfix="bye")
+p.add(total=100, desc="second", postfix="bye")
+for i in range(100):
+    sleep(0.2)
+    p.increment(pos=0)
+    p.increment(pos=1)
+    p.update()
