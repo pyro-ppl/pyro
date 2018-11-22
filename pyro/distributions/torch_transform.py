3a2ac0f55f77c786c9478eae6a05b7e534216cca
Stefan Webb <stefan.webb@wadh.ox.ac.uk>
2018-11-22 01:14:54 +0000

    Docs for TransformModule

--- pyro/distributions/torch_transform.py
+++ pyro/distributions/torch_transform.py
@@ -4,10 +4,14 @@ import torch
 
 
 class TransformModule(torch.distributions.Transform, torch.nn.Module):
+    """
+    Transforms with learnable parameters such as normalizing flows should inherit from this class rather 
+    than `Transform` so they are also a subclass of `nn.Module` and inherit all the useful methods of that class.
+
+    """
+
     def __init__(self):
         super(TransformModule, self).__init__()
-        # torch.distributions.Transform.__init__(self)
-        # torch.nn.Module.__init__(self)
 
     def __hash__(self):
         return super(torch.nn.Module, self).__hash__()
