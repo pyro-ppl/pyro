c2fa8927234e3582b575a386ec19c0f6ff905218
fehiepsi <fehiepsi@gmail.com>
2018-11-23 17:07:33 -0500

    add lbfgs test

--- tests/infer/test_static_svi.py
+++ tests/infer/test_static_svi.py
@@ -44,6 +44,7 @@ def test_inference():
     assert_equal(pyro.param("beta_q_log"), log_beta_n, prec=0.04)
 
 
+@pytest.mark.init(rng_seed=3)
 def test_lbfgs():
     x = 1 + torch.randn(10)
     x_mean = x.mean()
@@ -52,17 +53,18 @@ def test_lbfgs():
     def model():
         mu = pyro.param("mu", torch.tensor(0.))
         sigma = pyro.param("sigma", torch.tensor(1.), constraint=constraints.positive)
-        return pyro.sample("x", dist.Normal(mu, sigma), obs=x)
+        with pyro.plate("plate"):
+            return pyro.sample("x", dist.Normal(mu, sigma), obs=x)
 
     def guide():
         pass
 
-    adam = optim.LBFGS({"lr": 0.1, "max_iter": 100})
+    adam = optim.LBFGS({})
     svi = StaticSVI(model, guide, adam, loss=Trace_ELBO())
     svi.step()
 
-    assert_equal(pyro.param("mu"), x_mean)
-    assert_equal(pyro.param("sigma"), x_std)
+    assert_equal(pyro.param("mu"), x_mean, prec=0.01)
+    assert_equal(pyro.param("sigma"), x_std, prec=0.05)
 
 
 def test_params_not_match():
