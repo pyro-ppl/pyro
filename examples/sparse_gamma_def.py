e0e41b70283018b650e179351b157162946f03dc
Martin Jankowiak <jankowiak@uber.com>
2018-11-27 15:08:28 -0800

    make sparse_gamma_def work with autoguide and tracemeanfield_elbo

--- examples/sparse_gamma_def.py
+++ examples/sparse_gamma_def.py
@@ -1,11 +1,13 @@
-# this is an implementation of the sparse gamma deep exponential family experiment presented in
+# This is an implementation of the sparse gamma deep exponential family model described in
 # Ranganath, Rajesh, Tang, Linpeng, Charlin, Laurent, and Blei, David. Deep exponential families.
 #
-# the Olivetti faces dataset is originally from http://www.cl.cam.ac.uk/research/dtg/attarchive/facedatabase.html
+# To do inference we use either a custom guide (i.e. a hand-designed variational family) or
+# a guide that is automatically constructed using pyro.contrib.autoguide.
 #
-# compare to Christian Naesseth's implementation here:
+# The Olivetti faces dataset is originally from http://www.cl.cam.ac.uk/research/dtg/attarchive/facedatabase.html
+#
+# Compare to Christian Naesseth's implementation here:
 # https://github.com/blei-lab/ars-reparameterization/tree/master/sparse%20gamma%20def
-
 
 from __future__ import absolute_import, division, print_function
 
@@ -19,7 +21,7 @@ import pyro
 import pyro.optim as optim
 import wget
 from pyro.distributions import Gamma, Poisson
-from pyro.infer import SVI, Trace_ELBO
+from pyro.infer import SVI, TraceMeanField_ELBO
 from pyro.contrib.autoguide import AutoDiagonalNormal
 
 torch.set_default_tensor_type('torch.FloatTensor')
@@ -52,7 +54,6 @@ class SparseGammaDEF(object):
         # sample the global weights
         with pyro.plate("w_top_plate", self.top_width * self.mid_width):
             w_top = pyro.sample("w_top", Gamma(self.alpha_w, self.beta_w))
-            print("\nw_top", w_top.shape)
         with pyro.plate("w_mid_plate", self.mid_width * self.bottom_width):
             w_mid = pyro.sample("w_mid", Gamma(self.alpha_w, self.beta_w))
         with pyro.plate("w_bottom_plate", self.bottom_width * self.image_size):
@@ -62,21 +63,26 @@ class SparseGammaDEF(object):
         # (the plate encodes the fact that the z's for different datapoints are conditionally independent)
         with pyro.plate("data", x_size):
             z_top = pyro.sample("z_top", Gamma(self.alpha_z, self.beta_z).expand([self.top_width]).independent(1))
-            print("z_top", z_top.shape)
-            print("w_top_reshape",  w_top.reshape(-1, self.top_width, self.mid_width).shape)
-            mean_mid = torch.matmul(z_top, w_top.reshape(-1, self.top_width, self.mid_width))
-            print("mean_mid", mean_mid.shape)
-            #print("mean_mid", mean_mid.shape)
+            # note that we need to use matmul (batch matrix multiplication) as well as appropriate reshaping
+            # to make sure our code is fully vectorized
+            w_top = w_top.reshape(self.top_width, self.mid_width) if w_top.dim() == 1 else \
+                w_top.reshape(-1, self.top_width, self.mid_width)
+            mean_mid = torch.matmul(z_top, w_top)
             z_mid = pyro.sample("z_mid", Gamma(self.alpha_z, self.beta_z / mean_mid).independent(1))
-            mean_bottom = torch.matmul(z_mid, w_mid.view(-1, self.mid_width, self.bottom_width))
+
+            w_mid = w_mid.reshape(self.mid_width, self.bottom_width) if w_mid.dim() == 1 else \
+                w_mid.reshape(-1, self.mid_width, self.bottom_width)
+            mean_bottom = torch.matmul(z_mid, w_mid)
             z_bottom = pyro.sample("z_bottom", Gamma(self.alpha_z, self.beta_z / mean_bottom).independent(1))
-            print("z_bottom", z_bottom.shape)
-            mean_obs = torch.matmul(z_bottom, w_bottom.view(-1, self.bottom_width, self.image_size))
+
+            w_bottom = w_bottom.reshape(self.bottom_width, self.image_size) if w_bottom.dim() == 1 else \
+                w_bottom.reshape(-1, self.bottom_width, self.image_size)
+            mean_obs = torch.matmul(z_bottom, w_bottom)
 
             # observe the data using a poisson likelihood
             pyro.sample('obs', Poisson(mean_obs).independent(1), obs=x)
 
-    # define the guide a.k.a. variational distribution.
+    # define our custom guide a.k.a. variational distribution.
     # (note the guide is mean field)
     def guide(self, x):
         x_size = x.size(0)
@@ -117,7 +123,7 @@ class SparseGammaDEF(object):
             sample_zs("mid", self.mid_width)
             sample_zs("bottom", self.bottom_width)
 
-    # define a helper function to clip parameters defining the variational family.
+    # define a helper function to clip parameters defining the custom guide.
     # (this is to avoid regions of the gamma distributions with extremely small means)
     def clip_params(self):
         for param, clip in zip(("log_alpha", "log_mean"), (-2.5, -4.5)):
@@ -134,25 +140,37 @@ def main(args):
     data = torch.tensor(np.loadtxt('faces_training.csv', delimiter=',')).float()
 
     sparse_gamma_def = SparseGammaDEF()
-    opt = optim.AdagradRMSProp({"eta": args.learning_rate, "t": 0.1})
 
-    if args.auto_guide:
-        auto_guide = AutoDiagonalNormal(sparse_gamma_def.model)
-        svi = SVI(sparse_gamma_def.model, auto_guide, opt, loss=Trace_ELBO())
-        svi_eval = SVI(sparse_gamma_def.model, auto_guide, opt, loss=Trace_ELBO(num_particles=args.eval_particles, vectorize_particles=True))
-    else:
-        svi = SVI(sparse_gamma_def.model, sparse_gamma_def.guide, opt, loss=Trace_ELBO())
-        svi_eval = SVI(sparse_gamma_def.model, sparse_gamma_def.guide, opt, loss=Trace_ELBO(num_particles=args.eval_particles, vectorize_particles=True))
+    # due to the special logic in the custom guide (e.g. parameter clipping), the custom guide
+    # is more numerically stable and enables us to use a larger learning rate (and consequently
+    # achieves better results)
+    learning_rate = 0.2 if args.auto_guide else 4.5
+    momentum = 0.05 if args.auto_guide else 0.1
+    opt = optim.AdagradRMSProp({"eta": learning_rate, "t": momentum})
 
-    print('\nbeginning training...')
+    # either use an automatically constructed guide (see pyro.contrib.autoguide for details) or our custom guide
+    guide = AutoDiagonalNormal(sparse_gamma_def.model) if args.auto_guide else sparse_gamma_def.guide
+
+    # this is the svi object we use during training; we use TraceMeanField_ELBO to
+    # get analytic KL divergences
+    svi = SVI(sparse_gamma_def.model, guide, opt, loss=TraceMeanField_ELBO())
+
+    # we use svi_eval during evaluation; since we took care to write down our model in
+    # a fully vectorized way, this computation can be done efficiently with large tensor ops
+    svi_eval = SVI(sparse_gamma_def.model, guide, opt,
+                   loss=TraceMeanField_ELBO(num_particles=args.eval_particles, vectorize_particles=True))
+
+    guide_description = 'automatically constructed' if args.auto_guide else 'custom'
+    print('\nbeginning training with %s guide...' % guide_description)
 
     # the training loop
     for k in range(args.num_epochs):
         loss = svi.step(data)
         if not args.auto_guide:
-            sparse_gamma_def.clip_params()  # we clip params after each gradient step
+            # for the custom guide we clip parameters after each gradient step
+            sparse_gamma_def.clip_params()
 
-        if k % 1 == 0:
+        if k % args.eval_frequency == 0 and k > 0 or k == args.num_epochs - 1:
             loss = svi_eval.evaluate_loss(data)
             print("[epoch %04d] training elbo: %.4g" % (k, -loss))
 
@@ -160,9 +178,11 @@ def main(args):
 if __name__ == '__main__':
     # parse command line arguments
     parser = argparse.ArgumentParser(description="parse args")
-    parser.add_argument('-n', '--num-epochs', default=1, type=int, help='number of training epochs')
-    parser.add_argument('-ep', '--eval-particles', default=1, type=int, help='number of samples to use during evaluation')
-    parser.add_argument('-lr', '--learning-rate', default=4.5, type=float, help='learning rate')
-    parser.add_argument('--auto-guide', action='store_true')
+    parser.add_argument('-n', '--num-epochs', default=1000, type=int, help='number of training epochs')
+    parser.add_argument('-ef', '--eval-frequency', default=25, type=int,
+                        help='how often to evaluate elbo (number of epochs)')
+    parser.add_argument('-ep', '--eval-particles', default=20, type=int,
+                        help='number of samples/particles to use during evaluation')
+    parser.add_argument('--auto-guide', action='store_true', help='whether to use an automatically constructed guide')
     args = parser.parse_args()
     main(args)
