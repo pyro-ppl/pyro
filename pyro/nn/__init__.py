dc357af05d33ab72345ca998a528c728acd36728
fehiepsi <fehiepsi@gmail.com>
2018-12-02 09:48:25 +0900

    add ..nparameterized

--- pyro/nn/__init__.py
+++ pyro/nn/__init__.py
@@ -1,8 +1,10 @@
 from __future__ import absolute_import, division, print_function
 
 from pyro.nn.auto_reg_nn import AutoRegressiveNN, MaskedLinear
+from pyro.nn.parameterized import Parameterized
 
 __all__ = [
     "AutoRegressiveNN",
     "MaskedLinear",
+    "Parameterized",
 ]
