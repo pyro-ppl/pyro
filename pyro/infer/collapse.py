f5321cf62f8dea5cbfb876e657fe437a047eb1ad
Eli <eli.bingham@uber.com>
2018-12-04 19:04:53 -0800

    break collapse into sample_posterior and collapse, fix a smoke test, and fix plate_to_symbol bug in sample_posterior

--- pyro/infer/collapse.py
+++ pyro/infer/collapse.py
@@ -7,7 +7,13 @@ import pyro.poutine as poutine
 
 from pyro.ops.contract import contract_tensor_tree
 from pyro.ops.einsum.adjoint import require_backward
-from pyro.ops.rings import SampleRing
+from pyro.ops.rings import MapRing, SampleRing
+
+
+_RINGS = {
+    0: MapRing,
+    1: SampleRing,
+}
 
 
 class CollapseEnumMessenger(poutine.enumerate_messenger.EnumerateMessenger):
@@ -20,17 +26,16 @@ class CollapseEnumMessenger(poutine.enumerate_messenger.EnumerateMessenger):
         super(CollapseEnumMessenger, self)._pyro_sample(msg)
 
 
-class CollapseReplayMessenger(poutine.replay_messenger.ReplayMessenger):
+class SamplePosteriorMessenger(poutine.replay_messenger.ReplayMessenger):
 
     def _pyro_sample(self, msg):
         if msg["infer"].get("collapse"):
-            super(CollapseReplayMessenger, self)._pyro_sample(msg)
-            msg["stop"] = True
+            super(SamplePosteriorMessenger, self)._pyro_sample(msg)
         if msg["name"] in self.trace:
             msg["cond_indep_stack"] = self.trace.nodes[msg["name"]]["cond_indep_stack"]
 
 
-def collapse(model, first_available_dim):
+def sample_posterior(model, first_available_dim, temperature=1):
     """
     Use `ubersum` to collapse sample sites marked with `site["infer"]["collapse"] = True`
 
@@ -54,14 +59,16 @@ def collapse(model, first_available_dim):
         for node in enum_trace.nodes.values():
             if node["type"] == "sample":
 
+
+                ordinal = frozenset(enum_trace.plate_to_symbol[f.name] for f in node["cond_indep_stack"] if f.vectorized)
+
                 log_prob = node["packed"]["log_prob"]
-                ordinal = frozenset(log_prob._pyro_dims[f.dim] for f in node["cond_indep_stack"] if f.vectorized)
                 log_probs.setdefault(ordinal, []).append(log_prob)
                 sum_dims.update(set(log_prob._pyro_dims))
 
                 for frame in node["cond_indep_stack"]:
                     if frame.vectorized:
-                        frame_dim = log_prob._pyro_dims[frame.dim]
+                        frame_dim = enum_trace.plate_to_symbol[frame.name]
                         frame_to_dim[frame] = frame_dim
                         sum_dims.remove(frame_dim)
 
@@ -69,16 +76,23 @@ def collapse(model, first_available_dim):
                 if not node["is_observed"]:
                     queries.append(log_prob)
                     require_backward(log_prob)
+                    log_prob._pyro_backward_result = False
 
-        ring = SampleRing()
+        try:
+            ring = _RINGS[temperature]()
+        except KeyError:
+            raise ValueError("temperature must be 0 (map) or 1 (sample) for now")
+
+        import pdb; pdb.set_trace()
         log_probs = contract_tensor_tree(log_probs, sum_dims, ring=ring)
         query_ordinal = {}
         for ordinal, terms in log_probs.items():
             for term in terms:
-                term._pyro_backward()
+                if hasattr(term, "_pyro_backward"):
+                    term._pyro_backward()
             # Note: makes collapse quadratic in number of ordinals
             for query in queries:
-                if query not in query_ordinal and query._pyro_backward_result is not None:
+                if query not in query_ordinal and query._pyro_backward_result is not False:
                     query_ordinal[query] = ordinal
 
         collapsed_trace = poutine.Trace()
@@ -87,31 +101,43 @@ def collapse(model, first_available_dim):
                 # TODO move this into a Leaf implementation somehow
                 new_node = {"type": "sample", "name": node["name"], "is_observed": False}
                 log_prob = node["packed"]["log_prob"]
-                ordinal = query_ordinal[log_prob]
-                new_node["cond_indep_stack"] = tuple(
-                    f for f in node["cond_indep_stack"]
-                    if not f.vectorized or frame_to_dim[f] in ordinal)
-
                 new_node["infer"] = node["infer"].copy()
 
-                # TODO move this into a custom SampleRing Leaf implementation
-                sample = log_prob._pyro_backward_result
-                new_value = packed.pack(node["value"], node["infer"]["_dim_to_symbol"])
-                for index, dim in zip(sample, sample._pyro_sample_dims):
-                    if dim in new_value._pyro_dims:
-                        index._pyro_dims = sample._pyro_dims[1:]
-                        new_value = packed.gather(new_value, index, dim)
+                if hasattr(log_prob, "_pyro_backward"):
+                    ordinal = query_ordinal[log_prob]
+                    new_node["cond_indep_stack"] = tuple(
+                        f for f in node["cond_indep_stack"]
+                        if not f.vectorized or frame_to_dim[f] in ordinal)
 
-                new_node["value"] = packed.unpack(new_value, enum_trace.symbol_to_dim)
+
+                    # TODO move this into a custom SampleRing Leaf implementation
+                    sample = log_prob._pyro_backward_result
+                    if sample is None:
+                        # node did not depend on an enumerated variable, so no sampling necessary
+                        new_node["value"] = node["value"]
+                    else:
+                        new_value = packed.pack(node["value"], node["infer"]["_dim_to_symbol"])
+                        for index, dim in zip(sample, sample._pyro_sample_dims):
+                            if dim in new_value._pyro_dims:
+                                index._pyro_dims = sample._pyro_dims[1:]
+                                new_value = packed.gather(new_value, index, dim)
+                        new_node["value"] = packed.unpack(new_value, enum_trace.symbol_to_dim)
+                else:
+                    new_node["cond_indep_stack"] = node["cond_indep_stack"]
+                    new_node["value"] = node["value"]
 
                 collapsed_trace.add_node(node["name"], **new_node)
 
-        # Replay model correctly against collapsed_trace (get correct cond_indep_stack)
-        collapsed_sites = set(node["name"] for node in enum_trace.nodes.values()
-                              if node["type"] == "sample" and node["infer"].get("collapse"))
-
-        with poutine.block(hide_fn=lambda msg: msg["name"] in collapsed_sites):
-            with CollapseReplayMessenger(trace=collapsed_trace):
-                return model(*args, **kwargs)
+        with SamplePosteriorMessenger(trace=collapsed_trace):
+            return model(*args, **kwargs)
 
     return _collapsed_model
+
+
+def _is_collapsed(node):
+    return node["type"] == "sample" and node["infer"].get("collapse")
+
+
+def collapse(model, first_available_dim):
+    collapsed_model = sample_posterior(model, first_available_dim, temperature=1)
+    return poutine.block(collapsed_model, hide_fn=_is_collapsed)
--- tests/infer/test_collapse.py
+++ tests/infer/test_collapse.py
@@ -5,6 +5,7 @@ import pytest
 
 import torch
 from torch.distributions import constraints
+from torch.autograd import grad
 
 import pyro
 import pyro.distributions as dist
@@ -15,6 +16,38 @@ from pyro.infer.collapse import collapse
 from tests.common import assert_equal
 
 logger = logging.getLogger(__name__)
+
+
+def test_collapse_exact_inference():
+    data = torch.tensor([1., 2., 3.])
+    num_particles = 10000
+
+    @config_enumerate(default="parallel")
+    def model():
+        p1 = pyro.param("p1", torch.tensor(0.25))
+        with pyro.plate("num_particles", num_particles, dim=-2):
+            z = pyro.sample("z", dist.Bernoulli(p1), infer={"collapse": True})
+            with pyro.plate("data", 3):
+                pyro.sample("x", dist.Normal(z, 1.), obs=data)
+
+    def hand_model():
+        p1 = pyro.param("p1", torch.tensor(0.25))
+        with pyro.plate("data", 3):
+            with poutine.scale(scale=1.-p1):
+                pyro.sample("x0", dist.Normal(0., 1.), obs=data)
+            with poutine.scale(scale=p1):
+                pyro.sample("x1", dist.Normal(1., 1.), obs=data)
+
+    expected_logprob = poutine.trace(hand_model).get_trace().log_prob_sum()
+    actual_logprob = poutine.trace(collapse(model, -3)).get_trace().log_prob_sum() / num_particles
+
+    assert_equal(expected_logprob, actual_logprob, prec=1e-3)
+
+    p1 = pyro.param("p1")
+    expected_grad = grad(expected_logprob, [p1,])[0]
+    actual_grad = grad(actual_logprob, [p1,])[0]
+
+    assert_equal(expected_grad, actual_grad, prec=1e-3)
 
 
 def test_collapse_guide_smoke():
@@ -43,7 +76,7 @@ def test_collapse_guide_smoke():
     uncollapsed_tr = poutine.trace(guide).get_trace()
     collapsed_tr = poutine.trace(collapsed_guide).get_trace()
 
-    assert set(collapsed_tr.nodes.keys()) == set(uncollapsed_tr.nodes.keys()) - {"e"} | {"_collapse_0"}
+    assert set(collapsed_tr.nodes.keys()) == set(uncollapsed_tr.nodes.keys()) - {"e"}
 
 
 def test_collapse_traceenumelbo_smoke():
@@ -70,7 +103,7 @@ def test_collapse_traceenumelbo_smoke():
     elbo.differentiable_loss(model, collapsed_guide)
 
 
-# @pytest.mark.xfail(reason="EnumerateMessenger and collapse produce bad shapes")
+@pytest.mark.xfail(reason="We don't handle this enumeration scenario with collapse yet")
 def test_collapse_elbo_categorical():
 
     @config_enumerate(default="parallel")
@@ -104,7 +137,7 @@ def test_collapse_elbo_categorical():
     pyro.infer.enable_validation(False)
 
     elbo = pyro.infer.TraceEnum_ELBO(
-        max_plate_nesting=0,  # XXX what should this be?
+        max_plate_nesting=1,  # XXX what should this be?
         strict_enumeration_warning=False)
 
     expected = elbo.differentiable_loss(model, guide, True)
@@ -113,7 +146,6 @@ def test_collapse_elbo_categorical():
     assert_equal(expected, actual)
 
 
-# @pytest.mark.xfail(reason="EnumerateMessenger and collapse produce bad shapes")
 def test_collapse_enum_interaction_smoke():
 
     # @config_enumerate(default="parallel")
