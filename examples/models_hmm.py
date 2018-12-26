8f4421e0bcd78e517f1f300424c443cd2e02548d
Martin Jankowiak <jankowiak@uber.com>
2018-12-26 06:51:21 -0800

    add/renumber models

--- examples/models_hmm.py
+++ examples/models_hmm.py
@@ -24,10 +24,11 @@ class model1(nn.Module):
         num_sequences, max_length, data_dim = sequences.shape
         assert lengths.shape == (num_sequences,)
         assert lengths.max() <= max_length
-        probs_x = pyro.param("probs_x", lambda: 0.9 * torch.eye(args['hidden_dim']) + \
-                                                0.1 * torch.ones(args['hidden_dim'], args['hidden_dim']),
+        hidden_dim = args['hidden_dim']
+        probs_x = pyro.param("probs_x", lambda: 0.9 * torch.eye(hidden_dim) + \
+                                                0.1 * torch.ones(hidden_dim, hidden_dim),
                              constraint=constraints.simplex)
-        probs_y = pyro.param("probs_y", lambda: torch.rand(args['hidden_dim'], data_dim),
+        probs_y = pyro.param("probs_y", lambda: torch.rand(hidden_dim, data_dim),
                              constraint=constraints.unit_interval)
         tones_plate = pyro.plate("tones", data_dim, dim=-1)
         with pyro.plate("sequences", mb.size(0), subsample=mb, dim=-2) as batch:
@@ -43,6 +44,7 @@ class model1(nn.Module):
                                         obs=sequences[batch, t])
 
 
+# was model3
 class model2(nn.Module):
     def __init__(self, args, data_dim):
         super(model2, self).__init__()
@@ -51,35 +53,7 @@ class model2(nn.Module):
         num_sequences, max_length, data_dim = sequences.shape
         assert lengths.shape == (num_sequences,)
         assert lengths.max() <= max_length
-        probs_x = pyro.param("probs_x", lambda: 0.9 * torch.eye(args['hidden_dim']) + \
-                                                0.1 * torch.ones(args['hidden_dim'], args['hidden_dim']),
-                             constraint=constraints.simplex)
-        probs_y = pyro.param("probs_y", lambda: torch.rand(args['hidden_dim'], 2, data_dim),
-                             constraint=constraints.unit_interval)
-        tones_plate = pyro.plate("tones", data_dim, dim=-1)
-        with pyro.plate("sequences", mb.size(0), subsample=mb, dim=-2) as batch:
-            lengths = lengths[batch]
-            with pyro.poutine.scale(scale=scale):
-                x, y = 0, 0
-                for t in pyro.markov(range(lengths.max())):
-                    with poutine.mask(mask=(t < lengths).unsqueeze(-1)):
-                        x = pyro.sample("x_{}".format(t), dist.Categorical(probs_x[x]),
-                                        infer={"enumerate": "parallel"})
-                        with tones_plate as tones:
-                            y = pyro.sample("y_{}".format(t), dist.Bernoulli(probs_y[x, y, tones]),
-                                            obs=sequences[batch, t]).long()
-
-
-class model3(nn.Module):
-    def __init__(self, args, data_dim):
-        super(model3, self).__init__()
-
-    def model(self, sequences, lengths, args, mb=None, scale=1.0):
-        num_sequences, max_length, data_dim = sequences.shape
-        assert lengths.shape == (num_sequences,)
-        assert lengths.max() <= max_length
-        hidden_dim = int(args['hidden_dim'] / 2)
-        #hidden_dim = int(args['hidden_dim'] ** 0.5)  # split between w and x
+        hidden_dim = args['hidden_dim']
         probs_w = pyro.param("probs_w", lambda: 0.9 * torch.eye(hidden_dim) + \
                                                 0.1 * torch.ones(hidden_dim, hidden_dim),
                              constraint=constraints.simplex)
@@ -103,7 +77,43 @@ class model3(nn.Module):
                             pyro.sample("y_{}".format(t), dist.Bernoulli(probs_y[w, x, tones]),
                                         obs=sequences[batch, t])
 
+# was model4
+class model3(nn.Module):
+    def __init__(self, args, data_dim):
+        super(model3, self).__init__()
 
+    def model(self, sequences, lengths, args, mb=None, scale=1.0):
+        num_sequences, max_length, data_dim = sequences.shape
+        assert lengths.shape == (num_sequences,)
+        assert lengths.max() <= max_length
+        hidden_dim = args['hidden_dim']
+        hidden = torch.arange(hidden_dim, dtype=torch.long)
+        probs_w = pyro.param("probs_w", lambda: 0.9 * torch.eye(hidden_dim) + \
+                                                0.1 * torch.ones(hidden_dim, hidden_dim),
+                             constraint=constraints.simplex)
+        probs_x = pyro.param("probs_x", lambda: (0.9 * torch.eye(hidden_dim) + \
+                                                 0.1 * torch.ones(hidden_dim, hidden_dim)).
+                                                 unsqueeze(-1).expand(hidden_dim, hidden_dim, hidden_dim),
+                             constraint=constraints.simplex)
+        probs_y = pyro.param("probs_y", lambda: torch.rand(hidden_dim, hidden_dim, data_dim),
+                             constraint=constraints.unit_interval)
+        tones_plate = pyro.plate("tones", data_dim, dim=-1)
+        with pyro.plate("sequences", mb.size(0), subsample=mb, dim=-2) as batch:
+            lengths = lengths[batch]
+            with pyro.poutine.scale(scale=scale):
+                w = x = torch.tensor(0, dtype=torch.long)
+                for t in pyro.markov(range(lengths.max())):
+                    with poutine.mask(mask=(t < lengths).unsqueeze(-1)):
+                        w = pyro.sample("w_{}".format(t), dist.Categorical(probs_w[w]),
+                                        infer={"enumerate": "parallel"})
+                        x = pyro.sample("x_{}".format(t),
+                                        dist.Categorical(probs_x[w.unsqueeze(-1), x.unsqueeze(-1), hidden]),
+                                        infer={"enumerate": "parallel"})
+                        with tones_plate as tones:
+                            pyro.sample("y_{}".format(t), dist.Bernoulli(probs_y[w, x, tones]),
+                                        obs=sequences[batch, t])
+
+# was model2
 class model4(nn.Module):
     def __init__(self, args, data_dim):
         super(model4, self).__init__()
@@ -112,8 +122,67 @@ class model4(nn.Module):
         num_sequences, max_length, data_dim = sequences.shape
         assert lengths.shape == (num_sequences,)
         assert lengths.max() <= max_length
-        hidden_dim = int(args['hidden_dim'] / 2)
-        #hidden_dim = int(args['hidden_dim'] ** 0.5)  # split between w and x
+        hidden_dim = args['hidden_dim']
+        probs_x = pyro.param("probs_x", lambda: 0.9 * torch.eye(hidden_dim) + \
+                                                0.1 * torch.ones(hidden_dim, hidden_dim),
+                             constraint=constraints.simplex)
+        probs_y = pyro.param("probs_y", lambda: torch.rand(hidden_dim, 2, data_dim),
+                             constraint=constraints.unit_interval)
+        tones_plate = pyro.plate("tones", data_dim, dim=-1)
+        with pyro.plate("sequences", mb.size(0), subsample=mb, dim=-2) as batch:
+            lengths = lengths[batch]
+            with pyro.poutine.scale(scale=scale):
+                x, y = 0, 0
+                for t in pyro.markov(range(lengths.max())):
+                    with poutine.mask(mask=(t < lengths).unsqueeze(-1)):
+                        x = pyro.sample("x_{}".format(t), dist.Categorical(probs_x[x]),
+                                        infer={"enumerate": "parallel"})
+                        with tones_plate as tones:
+                            y = pyro.sample("y_{}".format(t), dist.Bernoulli(probs_y[x, y, tones]),
+                                            obs=sequences[batch, t]).long()
+
+
+class model5(nn.Module):
+    def __init__(self, args, data_dim):
+        super(model5, self).__init__()
+
+    def model(self, sequences, lengths, args, mb=None, scale=1.0):
+        num_sequences, max_length, data_dim = sequences.shape
+        assert lengths.shape == (num_sequences,)
+        assert lengths.max() <= max_length
+        hidden_dim = args['hidden_dim']
+        probs_w = pyro.param("probs_w", lambda: 0.9 * torch.eye(hidden_dim) + \
+                                                0.1 * torch.ones(hidden_dim, hidden_dim),
+                             constraint=constraints.simplex)
+        probs_x = pyro.param("probs_x", lambda: 0.9 * torch.eye(hidden_dim) + \
+                                                0.1 * torch.ones(hidden_dim, hidden_dim),
+                             constraint=constraints.simplex)
+        probs_y = pyro.param("probs_y", lambda: torch.rand(hidden_dim, hidden_dim, 2, data_dim),
+                             constraint=constraints.unit_interval)
+        tones_plate = pyro.plate("tones", data_dim, dim=-1)
+        with pyro.plate("sequences", mb.size(0), subsample=mb, dim=-2) as batch:
+            lengths = lengths[batch]
+            with pyro.poutine.scale(scale=scale):
+                w, x, y = 0, 0
+                for t in pyro.markov(range(lengths.max())):
+                    with poutine.mask(mask=(t < lengths).unsqueeze(-1)):
+                        w = pyro.sample("w_{}".format(t), dist.Categorical(probs_w[w]),
+                                        infer={"enumerate": "parallel"})
+                        x = pyro.sample("x_{}".format(t), dist.Categorical(probs_x[x]),
+                                        infer={"enumerate": "parallel"})
+                        with tones_plate as tones:
+                            y = pyro.sample("y_{}".format(t), dist.Bernoulli(probs_y[w, x, y, tones]),
+                                            obs=sequences[batch, t]).long()
+
+class model6(nn.Module):
+    def __init__(self, args, data_dim):
+        super(model6, self).__init__()
+
+    def model(self, sequences, lengths, args, mb=None, scale=1.0):
+        num_sequences, max_length, data_dim = sequences.shape
+        assert lengths.shape == (num_sequences,)
+        assert lengths.max() <= max_length
+        hidden_dim = args['hidden_dim']
         hidden = torch.arange(hidden_dim, dtype=torch.long)
         probs_w = pyro.param("probs_w", lambda: 0.9 * torch.eye(hidden_dim) + \
                                                 0.1 * torch.ones(hidden_dim, hidden_dim),
@@ -146,7 +215,7 @@ class TonesGenerator(nn.Module):
         self.args = args
         self.data_dim = data_dim
         super(TonesGenerator, self).__init__()
-        self.x_to_hidden = nn.Linear(args['hidden_dim'], args['nn_dim'])
+        self.x_to_hidden = nn.Linear(hidden_dim, args['nn_dim'])
         self.y_to_hidden = nn.Linear(args['nn_channels'] * data_dim, args['nn_dim'])
         self.conv = nn.Conv1d(1, args['nn_channels'], 3, padding=1)
         self.hidden_to_logits = nn.Linear(args['nn_dim'], data_dim)
@@ -162,19 +231,20 @@ class TonesGenerator(nn.Module):
 
 
 
-# The neural HMM model now uses tones_generator at each time step.
-class model5(nn.Module):
+# was model5
+class model7(nn.Module):
     def __init__(self, args, data_dim):
-        super(model5, self).__init__()
+        super(model7, self).__init__()
         self.tones_generator = TonesGenerator(args, data_dim)
 
     def model(self, sequences, lengths, args, mb=None, scale=1.0):
         num_sequences, max_length, data_dim = sequences.shape
         assert lengths.shape == (num_sequences,)
         assert lengths.max() <= max_length
+        hidden_dim = args['hidden_dim']
         pyro.module("tones_generator", self.tones_generator)
-        probs_x = pyro.param("probs_x", lambda: 0.9 * torch.eye(args['hidden_dim']) + \
-                                                0.1 * torch.ones(args['hidden_dim'], args['hidden_dim']),
+        probs_x = pyro.param("probs_x", lambda: 0.9 * torch.eye(hidden_dim) + \
+                                                0.1 * torch.ones(hidden_dim, hidden_dim),
                              constraint=constraints.simplex)
         with pyro.plate("sequences", mb.size(0), subsample=mb, dim=-2) as batch:
             lengths = lengths[batch]
