3b3baaef12756f3cf197b360eb2569820329d8dd
fehiepsi <fehiepsi@gmail.com>
2019-01-05 07:13:50 +0900

    temp save

--- pyro/infer/mcmc/__init__.py
+++ pyro/infer/mcmc/__init__.py
@@ -1,10 +1,12 @@
 from pyro.infer.mcmc.hmc import HMC
 from pyro.infer.mcmc.mcmc import MCMC, MCMCMarginals
+from pyro.infer.mcmc.mcmc1 import MCMC1
 from pyro.infer.mcmc.nuts import NUTS
 
 __all__ = [
     "HMC",
     "MCMC",
+    "MCMC1",
     "MCMCMarginals",
     "NUTS",
 ]
