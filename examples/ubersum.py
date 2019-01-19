e49a5f15238b15a73542dc608ff5909a049a4512
Neeraj Pradhan <npradhan@uber.com>
2019-01-18 23:38:20 -0800

    add results and plotting script

--- examples/ubersum.py
+++ examples/ubersum.py
@@ -1,6 +1,8 @@
 from __future__ import absolute_import, division, print_function
 
 import argparse
+import csv
+import os
 import timeit
 
 import torch
@@ -69,7 +71,11 @@ def jit_serve(equation, *operands, **kwargs):
                 result._pyro_backward()
 
             # Retrieve results.
-            return tuple([x._pyro_backward_result for x in operands])
+            results = []
+            for x in operands:
+                results.append(x._pyro_backward_result)
+                x._pyro_backward_result = None
+            return tuple(results)
 
         fn = torch.jit.trace(_sample, operands, check_trace=False)
         _CACHE[key] = fn
@@ -113,16 +119,21 @@ def main(args):
 
     dim_size = args.dim_size
     times = {}
-    for plate_size in reversed(range(1, 1 + args.max_plate_size)):
-        operands = []
-        for dims in inputs:
-            shape = torch.Size([plate_size if d in batch_dims else dim_size
-                                for d in dims])
-            operands.append(torch.randn(shape, requires_grad=True))
+    file = os.path.join(args.outdir, args.method + ".csv")
+    with open(file, "w", newline="") as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(["plate_size", "time"])
+        for plate_size in range(8, 1 + args.max_plate_size, 8):
+            operands = []
+            for dims in inputs:
+                shape = torch.Size([plate_size if d in batch_dims else dim_size
+                                    for d in dims])
+                operands.append(torch.randn(shape, requires_grad=True))
 
-        time = time_fn(fn, equation, *operands, batch_dims=batch_dims, iters=args.iters)
-        times[plate_size] = time
-        print('{}\t{}'.format(plate_size, time))
+            time = time_fn(fn, equation, *operands, batch_dims=batch_dims, iters=args.iters)
+            times[plate_size] = time
+            print('{}\t{}'.format(plate_size, time))
+            writer.writerow([plate_size, time])
 
 
 if __name__ == '__main__':
@@ -131,9 +142,10 @@ if __name__ == '__main__':
     parser.add_argument("-b", "--batch-dims", default="ij")
     parser.add_argument("-d", "--dim-size", default=32, type=int)
     parser.add_argument("-p", "--max-plate-size", default=32, type=int)
-    parser.add_argument("-n", "--iters", default=100, type=int)
+    parser.add_argument("-n", "--iters", default=10000, type=int)
     parser.add_argument('--cuda', action='store_true')
-    parser.add_argument('--jit', action='store_true')
+    parser.add_argument('--jit', action='store_true', default=True)
+    parser.add_argument('--outdir', type=str)
     parser.add_argument("-m", "--method", default="ubersum",
                         help="one of: ubersum, train, marginal, map, sample")
     args = parser.parse_args()
