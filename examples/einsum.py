0c49858ab8c5f263d1ece7f212180c8ccd8da370
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2019-01-24 22:43:06 -0800

    Rename ubersum -> einsum (#1720)

--- examples/einsum.py
+++ examples/einsum.py
@@ -1,12 +1,12 @@
 """
-This example demonstrates how to use ``ubersum`` with different backends to
-compute logprob, gradients, MAP estimates, posterior samples, and marginals.
+This example demonstrates how to use plated ``einsum`` with different backends
+to compute logprob, gradients, MAP estimates, posterior samples, and marginals.
 
 The interface for adjoint algorithms requires four steps:
 
 1. Call ``require_backward()`` on all inputs.
-2. Call ``x, = ubersum(..., backend=...)`` with a nonstandard backend.
-3. Call ``x._pyro_backward()` on the ubersum output.
+2. Call ``x, = einsum(..., backend=...)`` with a nonstandard backend.
+3. Call ``x._pyro_backward()` on the einsum output.
 4. Retrieve results from ``._pyro_backward_result`` attributes of the inputs.
 
 The results of these computations are returned, but this script does not
@@ -14,8 +14,8 @@ make use of them; instead we simply time the operations for profiling.
 All profiling is done on jit-compiled functions. We exclude jit compilation
 time from profiling results, assuming this can be done once.
 
-You can measure complexity of different ubersum problems by specifying
-``--equation`` and ``--batch-dims``.
+You can measure complexity of different einsum problems by specifying
+``--equation`` and ``--plates``.
 """
 
 from __future__ import absolute_import, division, print_function
@@ -26,7 +26,7 @@ import timeit
 import torch
 from torch.autograd import grad
 
-from pyro.ops.contract import ubersum
+from pyro.ops.contract import einsum
 from pyro.ops.einsum.adjoint import require_backward
 from pyro.util import ignore_jit_warnings
 
@@ -34,38 +34,56 @@ from pyro.util import ignore_jit_warnings
 _CACHE = {}
 
 
+def jit_prob(equation, *operands, **kwargs):
+    """
+    Runs einsum to compute the partition function.
+
+    This is cheap but less numerically stable than using the torch_log backend.
+    """
+    key = 'prob', equation, kwargs['plates']
+    if key not in _CACHE:
+
+        # This simply wraps einsum for jit compilation.
+        def _einsum(*operands):
+            return einsum(equation, *operands, **kwargs)
+
+        _CACHE[key] = torch.jit.trace(_einsum, operands, check_trace=False)
+
+    return _CACHE[key](*operands)
+
+
 def jit_logprob(equation, *operands, **kwargs):
     """
-    Runs ubersum to compute the partition function.
+    Runs einsum to compute the log partition function.
 
     This simulates evaluating an undirected graphical model.
     """
-    key = 'logprob', equation, kwargs['batch_dims']
+    key = 'logprob', equation, kwargs['plates']
     if key not in _CACHE:
 
-        # This simply wraps ubersum for jit compilation.
-        def _ubersum(*operands):
-            return ubersum(equation, *operands, **kwargs)
+        # This simply wraps einsum for jit compilation.
+        def _einsum(*operands):
+            return einsum(equation, *operands, backend='pyro.ops.einsum.torch_log', **kwargs)
 
-        _CACHE[key] = torch.jit.trace(_ubersum, operands, check_trace=False)
+        _CACHE[key] = torch.jit.trace(_einsum, operands, check_trace=False)
 
     return _CACHE[key](*operands)
 
 
 def jit_gradient(equation, *operands, **kwargs):
     """
-    Runs ubersum and calls backward on the partition function.
+    Runs einsum and calls backward on the partition function.
 
     This is simulates training an undirected graphical model.
     """
-    key = 'gradient', equation, kwargs['batch_dims']
+    key = 'gradient', equation, kwargs['plates']
     if key not in _CACHE:
 
-        # This wraps ubersum for jit compilation, but we will call backward on the result.
-        def _ubersum(*operands):
-            return ubersum(equation, *operands, **kwargs)
+        # This wraps einsum for jit compilation, but we will call backward on the result.
+        def _einsum(*operands):
+            return einsum(equation, *operands, backend='pyro.ops.einsum.torch_log', **kwargs)
 
-        _CACHE[key] = torch.jit.trace(_ubersum, operands, check_trace=False)
+        _CACHE[key] = torch.jit.trace(_einsum, operands, check_trace=False)
 
     # Run forward pass.
     losses = _CACHE[key](*operands)
@@ -83,24 +101,24 @@ def jit_gradient(equation, *operands, **kwargs):
 
 def _jit_adjoint(equation, *operands, **kwargs):
     """
-    Runs ubersum in forward-backward mode using ``pyro.ops.adjoint``.
+    Runs einsum in forward-backward mode using ``pyro.ops.adjoint``.
 
     This simulates serving predictions from an undirected graphical model.
     """
     backend = kwargs.pop('backend', 'pyro.ops.einsum.torch_sample')
-    key = backend, equation, tuple(x.shape for x in operands), kwargs['batch_dims']
+    key = backend, equation, tuple(x.shape for x in operands), kwargs['plates']
     if key not in _CACHE:
 
         # This wraps a complete adjoint algorithm call.
         @ignore_jit_warnings()
         def _forward_backward(*operands):
             # First we request backward results on each input operand.
-            # This is the pyro.ops.adjoint equivalent of torch's .require_grad_().
+            # This is the pyro.ops.adjoint equivalent of torch's .requires_grad_().
             for operand in operands:
                 require_backward(operand)
 
             # Next we run the forward pass.
-            results = ubersum(equation, *operands, backend=backend, **kwargs)
+            results = einsum(equation, *operands, backend=backend, **kwargs)
 
             # The we run a backward pass.
             for result in results:
@@ -153,7 +171,7 @@ def main(args):
         torch.set_default_tensor_type('torch.FloatTensor')
 
     if args.method == 'all':
-        for method in ['logprob', 'gradient', 'marginal', 'map', 'sample']:
+        for method in ['prob', 'logprob', 'gradient', 'marginal', 'map', 'sample']:
             args.method = method
             main(args)
         return
@@ -161,7 +179,7 @@ def main(args):
     print('Plate size  Time per iteration of {} (ms)'.format(args.method))
     fn = globals()['jit_{}'.format(args.method)]
     equation = args.equation
-    batch_dims = args.batch_dims
+    plates = args.plates
     inputs, outputs = equation.split('->')
     inputs = inputs.split(',')
 
@@ -169,24 +187,24 @@ def main(args):
     for plate_size in range(8, 1 + args.max_plate_size, 8):
         operands = []
         for dims in inputs:
-            shape = torch.Size([plate_size if d in batch_dims else args.dim_size
+            shape = torch.Size([plate_size if d in plates else args.dim_size
                                 for d in dims])
-            operands.append(torch.randn(shape, requires_grad=True))
+            operands.append((torch.empty(shape).uniform_() + 0.5).requires_grad_())
 
-        time = time_fn(fn, equation, *operands, batch_dims=batch_dims, modulo_total=True,
+        time = time_fn(fn, equation, *operands, plates=plates, modulo_total=True,
                        iters=args.iters)
-        print('{: <11s} {:0.4g}'.format('{} ** {}'.format(plate_size, len(args.batch_dims)), time * 1e3))
+        print('{: <11s} {:0.4g}'.format('{} ** {}'.format(plate_size, len(args.plates)), time * 1e3))
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='ubersum profiler')
+    parser = argparse.ArgumentParser(description='plated einsum profiler')
     parser.add_argument('-e', '--equation', default='a,abi,bcij,adj,deij->')
-    parser.add_argument('-b', '--batch-dims', default='ij')
+    parser.add_argument('-p', '--plates', default='ij')
     parser.add_argument('-d', '--dim-size', default=32, type=int)
-    parser.add_argument('-p', '--max-plate-size', default=32, type=int)
+    parser.add_argument('-s', '--max-plate-size', default=32, type=int)
     parser.add_argument('-n', '--iters', default=10, type=int)
     parser.add_argument('--cuda', action='store_true')
     parser.add_argument('-m', '--method', default='all',
-                        help='one of: logprob, gradient, marginal, map, sample')
+                        help='one of: prob, logprob, gradient, marginal, map, sample')
     args = parser.parse_args()
     main(args)
