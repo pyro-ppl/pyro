7110256a0afb9f36d5a3c05496fa6fe9872731f6
Amos Elb <amos.elberg@me.com>
2019-01-29 17:21:25 -0800

    code based on stan code; following discussion on the github issue; untested

--- pyro/distributions/lkj.py
+++ pyro/distributions/lkj.py
@@ -1,66 +1,142 @@
 from torch.distributions import Transform
 from torch.distributions import constraints
+from pyro.distributions import TorchDistribution
+from pyro.distributions import Beta
 import math
 
-class LKJCholeskyTransform(Transform):
+class _LowerCholeskyCorr(Constraint):
+    def check(self, value):
+        # check if `value` diagonal is positive, and squared norm of each row is 1
+
+lower_cholesky_corr = _LowerCholeskyCorr()
+
+class LowerCholeskyCorrTransform(Transform):
+    """
+    Transforms a vector of canonical partial correlations into the cholesky factor of
+    a covariance matrix.
+
+    Note that this transformation assumes that the vector of cpc's is already on the interval
+    [-1, 1].
+    """
     domain = constraints.interval(-1, 1)
-    codomain = constraints.lower_cholesky
+    codomain = constraints.lower_cholesky_corr
     bijective = True
     sign = +1
 
     def __eq__(self, other):
-        return isinstance(other, LKJCholeskyTransform)
+        return isinstance(other, LowerCholeskyCorrTransform)
 
-    def _call(self, y):
-        D = (1.0 + math.sqrt(1.0 + 8.0 * y.shape[0]))/2.0
+    def _call(self, z):
+        D = (1.0 + math.sqrt(1.0 + 8.0 * z.shape[0]))/2.0
         if D % 1 != 0:
-            raise ValueError("LKJ Priors require d choose 2 inputs")
+            raise ValueError("Correlation matrix transformation requires d choose 2 inputs")
         D = int(D)
-        # Start with the identity matrix, to simplify things
-        z = y.tanh()
-        w = torch.eye(D, device=y.device)
 
-        # compute y from the vector x as in Stan reference:
-        # https://mc-stan.org/docs/2_18/reference-manual/correlation-matrix-transform-section.html
-        last_z = z[0:(D-1)]
-        w[1:D, 0] = last_z
+        x = torch.zeros((D,D), device=z.device)
+
+        x[0,0] = 1
+        x[1:,0] = current_x = z[:(D-1)]
         i = D - 1
-        for d in range(1, D-1):
-            distance_to_copy = D - 1 - d
+        last_squared_x = None
+        for j in range(1, D):
+            distance_to_copy = D - 1 - j
             new_z = z[i:(i + distance_to_copy)]
-            w[(d+1):D, d] = new_z * w[(d+1):, d - 1] * (1.0 - last_z[1:].pow(2)).sqrt()
+            if last_squared_x is None:
+                last_squared_x = current_x**2
+            else:
+                last_squared_x = last_squared_x[1:] + current_x**2
+            x[j, j] = (1 - last_squared_x[0]).sqrt()
+            current_x = new_z * (1 - last_squared_x[1:]).sqrt()
+            x[(j+1):, j] = current_x
             i += distance_to_copy
-            last_z = new_z
 
-        return w
+        return x
 
-    def _inverse(self, w):
-        if (w.shape[0] != w.shape[1]):
+    def _inverse(self, x):
+        if (x.shape[0] != x.shape[1]):
             raise ValueError("A matrix that isn't square can't be a Cholesky factor of a correlation matrix")
-        D = w.shape[0]
-
-        def z_inverse(x):
-            return (1 - x.pow(2)).sqrt()
+        D = x.shape[0]
 
         z_stack = [
-            w[1:, 0]
+            x[1:, 0]
         ]
-        z_inv_stack = [
-            z_inverse(z_stack[0])
-        ]
+        current_x = z_stack[0]
+        last_squared_x = None
+        for j in range(1, D):
+            if last_squared_x is None:
+                last_squared_x = current_x**2
+            else:
+                last_squared_x += current_x[1:]**2
+        current_x = x[j:, j]
+        z_stack.append(current_x / (1 - last_squared_x).sqrt())
+        z = torch.cat(z_stack)
+        return z
 
-        for d in range(1, D-1):
-            z = w[(d+1):, d]
-            for dd in range(0, d):
-                old_z = z_inv_stack[dd][(d - dd):]
-                old_w = w[(d+1):, dd]
-                z /= (old_z * old_w)
-            z_stack.append(z)
-            z_inv_stack.append(z_inverse(z))
+    def log_abs_det_jacobian(self, x, z):
+        return (1 - x.tril(-1).pow(2).sum(1)).log().sum() * .5
 
-        x = torch.cat(z_stack)
-        return torch.log((x + 1) / (1 - x))/2
+class UnconstrainedLowerCholeskyCorrTransform(LowerCholeskyCorrTransform):
+    """
+    Transforms a vector of reals into the cholesky factor of
+    a covariance matrix.
+
+    Note that this transformation does not assume that the vector of cpc's is already on the interval
+    [-1, 1].
+    """
+    domain = constraints.real
+    codomain = constraints.lower_cholesky_corr
+    bijective = True
+    sign = +1
+
+    def __eq__(self, other):
+        return isinstance(other, UnconstrainedLowerCholeskyCorrTransform)
+
+    def _call(self, y):
+        return super(UnconstrainedLowerCholeskyCorrTransform, self)(y.tanh())
+
+    def _inverse(self, x):
+        z = super(UnconstrainedLowerCholeskyCorrTransform, self)(x)
+        return torch.log((z + 1) / (1 - z))/2
 
     def log_abs_det_jacobian(self, x, y):
-        # see the above reference
+        transformation_part = super(UnconstrainedLowerCholeskyCorrTransform, self)(x, y.tanh())
+        tanh_jacobian = y.cosh().log().sum() * -2
+        log_abs_det = transformation_part + tanh_jacobian
         return log_abs_det
+
+class LKJCholeskyFactor(TorchDistribution):
+    """
+    Generates cholesky factors of correlation matrices using an LKJ prior.
+
+    The expected use is to combine it with a vector of variances and pass it
+    to the scale_tril parameter of a multivariate distribution such as MultivariateNormal.
+
+    E.g., if \theta is a (positive) vector of covariances with the same dimensionality
+    as this distribution, and \Omega is sampled from this distribution, scale_tril=diag(sqrt(\theta))*\Omega
+
+    :param int d: Dimensionality of the matrix
+    :param torch.Tensor eta: A single positive number parameterizing the distribution.
+    """
+    def __init__(self, d, eta):
+        if ! is.tensor(eta):
+            eta = torch.FloatTensor([eta])
+        if any(eta <= 0):
+            raise ValueException("eta must be > 0")
+        vector_size = (d * (d - 1)) // 2
+        alpha = eta + 0.5 * (d  - 1.0)
+
+        concentrations = eta.new().expand(vector_size)
+        i = 0
+        for k in range(d-1):
+            alpha -= .5
+            for j in range(k+1, d):
+                concentrations[i] = alpha
+                i += 1
+        self._generating_distribution = Beta(concentrations, concentrations)
+        self._transformation = LowerCholeskyCorrTransform()
+
+    def sample(self):
+        return self._transformation(self._generating_distribution.sample().mul(2) - 1.0)
+
+    def log_prob(self, x):
+        return ???
