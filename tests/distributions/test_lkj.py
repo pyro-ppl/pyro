6e38f28144be322595c47c570e95a42cbad0e73e
fehiepsi <fehiepsi@gmail.com>
2019-01-31 16:48:41 -0500

    refactor transforms class, add test for tanh transform

--- tests/distributions/test_lkj.py
+++ tests/distributions/test_lkj.py
@@ -4,7 +4,7 @@ import pytest
 import torch
 
 import pyro.distributions as dist
-from pyro.distributions.lkj import corr_cholesky_constraint
+from pyro.distributions.lkj import corr_cholesky_constraint, _TanhTransform
 from tests.common import assert_tensors_equal
 
 
@@ -14,4 +14,40 @@ def test_constraint(value_shape):
     value.diagonal(dim1=-2, dim2=-1).exp_()
     value = value / value.norm(2, dim=-1, keepdim=True)
 
+    # this also tests for shape
     assert_tensors_equal(corr_cholesky_constraint.check(value), torch.ones(value_shape[:-2]))
+
+
+@pytest.mark.parametrize("x_shape", [(), (1,), (3, 1), (3,), (1, 3), (5, 3)])
+def test_tanh_transform(x_shape):
+    transform = _TanhTransform()
+    x = torch.randn(x_shape, requires_grad=True)
+    y = transform(x)
+
+    # test codomain
+    assert transform.codomain.check(y).all()
+
+    # test inv
+    z = transform.inv(y)
+    assert_tensors_equal(x, z)
+
+    # test log_abs_det_jacobian
+    log_det = transform.log_abs_det_jacobian(x, y)
+    assert log_det.shape == x.shape
+    if x_shape == ():
+        assert_tensors_equal(torch.autograd.grad(y, (x,), retain_graph=True)[0].abs().log(),
+                             log_det, prec=0.0002)
+        assert_tensors_equal(torch.autograd.grad(z, (y,), retain_graph=True)[0].abs().log(),
+                             -log_det, prec=0.0002)
+
+
+def test_partial_corr_to_corr_cholesky_transform():
+    pass
+
+
+def test_biject_to():
+    pass
+
+
+def test_transform_to():
+    pass
