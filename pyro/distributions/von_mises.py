672635ecce569d8256d7eb30e14b5d8676dbdfaa
Ahmad Salim Al-Sibahi <asal@itu.dk>
2019-02-02 16:19:39 +0100

    Provide function that estimates von Mises parameters from samples
    This allows easier testing of correctness of sampling

--- pyro/distributions/von_mises.py
+++ pyro/distributions/von_mises.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, division, print_function
 import math
 
 import torch
+from torch import optim
 from torch.distributions import constraints
 from torch.distributions.utils import broadcast_all
 
@@ -24,6 +25,9 @@ _I1_COEF_SMALL = [0.5, 0.87890594, 0.51498869, 0.15084934, 0.2658733e-1, 0.30153
 _I1_COEF_LARGE = [0.39894228, -0.3988024e-1, -0.362018e-2, 0.163801e-2, -0.1031555e-1,
                   0.2282967e-1, -0.2895312e-1, 0.1787654e-1, -0.420059e-2]
 
+_COEF_SMALL = [_I0_COEF_SMALL, _I1_COEF_SMALL]
+_COEF_LARGE = [_I0_COEF_LARGE, _I1_COEF_LARGE]
+
 
 def _log_modified_bessel_fn(x, order=0):
     """
@@ -32,22 +36,49 @@ def _log_modified_bessel_fn(x, order=0):
     """
     assert order == 0 or order == 1
 
-    small_coeff = [_I0_COEF_SMALL, _I1_COEF_SMALL]
-    large_coeff = [_I0_COEF_LARGE, _I1_COEF_LARGE]
-
     # compute small solution
     y = (x / 3.75).pow(2)
-    small = _eval_poly(y, small_coeff[order]).log()
+    small = _eval_poly(y, _COEF_SMALL[order]).log()
 
     # compute large solution
     y = 3.75 / x
-    large = x - 0.5 * x.log() + _eval_poly(y, large_coeff[order]).log()
+    large = x - 0.5 * x.log() + _eval_poly(y, _COEF_LARGE[order]).log()
 
     mask = (x < 3.75)
     result = large
     if mask.any():
         result[mask] = small[mask]
     return result
+
+
+def _fit_params_from_samples(samples, n_iter=50):
+    assert samples.dim() == 1
+    samples_count = samples.size(0)
+    samples_cs = samples.cos().sum()
+    samples_ss = samples.sin().sum()
+    mu = torch.atan2(samples_ss / samples_count, samples_cs / samples_count)
+    samples_r = (samples_cs ** 2 + samples_ss ** 2).sqrt() / samples_count
+    # From Banerjee, Arindam, et al.
+    # "Clustering on the unit hypersphere using von Mises-Fisher distributions."
+    # Journal of Machine Learning Research 6.Sep (2005): 1345-1382.
+    # By mic (https://stats.stackexchange.com/users/67168/mic),
+    # Estimating kappa of von Mises distribution, URL (version: 2015-06-12):
+    # https://stats.stackexchange.com/q/156692
+    kappa = (samples_r * 2 - samples_r ** 3) / (1 - samples_r ** 2)
+    kappa.requires_grad = True
+    bfgs = optim.LBFGS([kappa])
+
+    def bfgs_closure():
+        bfgs.zero_grad()
+        obj = (_log_modified_bessel_fn(kappa, order=1) 
+               - _log_modified_bessel_fn(kappa, order=0)).exp()
+        obj = (obj - samples_r).abs()
+        obj.backward()
+        return obj
+
+    for i in range(n_iter):
+        bfgs.step(bfgs_closure)
+    return mu, kappa.detach()
 
 
 class VonMises(TorchDistribution):
