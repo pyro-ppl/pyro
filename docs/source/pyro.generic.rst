98109217da7ea7b939cdff74c96732961a057c2a
Fritz Obermeyer <fritzo@uber.com>
2019-03-25 13:46:37 -0700

    Add a pyro.generic module to enable backend-agnostic Pyro models

---
+++ docs/source/pyro.generic.rst
@@ -0,0 +1,10 @@
+Generic Interface
+-----------------
+
+The ``pyro.generic`` module provides an interface to dynamically dispatch Pyro code
+to custom backends.
+
+.. module:: pyro.generic
+    :members:
+    :undoc-members:
+    :show-inheritance:
--- examples/minipyro.py
+++ examples/minipyro.py
@@ -10,63 +10,62 @@ import argparse
 
 import torch
 
-import pyro.distributions as dist
+# We use the pyro.generic interface to support dynamic choice of backend.
+from pyro.generic import backend
+from pyro.generic import distributions as dist
+from pyro.generic import infer, optim, pyro
 
 
 def main(args):
     # Because the API in minipyro matches that of Pyro proper, this
     # example code works with both implementations.
-    if args.full_pyro:
-        import pyro
-        from pyro.infer import SVI, Trace_ELBO
-        from pyro.optim import Adam
-        elbo = Trace_ELBO()
-    else:
-        import pyro.contrib.minipyro as pyro
-        from pyro.contrib.minipyro import SVI, Adam, elbo
+    with backend('pyro' if args.full_pyro else 'minipyro'):
+        if args.full_pyro:
+            elbo = infer.Trace_ELBO()
+        else:
+            elbo = infer.elbo
 
-    # Define a basic model with a single Normal latent random variable `loc`
-    # and a batch of Normally distributed observations.
-    def model(data):
-        loc = pyro.sample("loc", dist.Normal(0., 1.))
-        with pyro.plate("data", len(data), dim=-1):
-            pyro.sample("obs", dist.Normal(loc, 1.), obs=data)
+        # Define a basic model with a single Normal latent random variable `loc`
+        # and a batch of Normally distributed observations.
+        def model(data):
+            loc = pyro.sample("loc", dist.Normal(0., 1.))
+            with pyro.plate("data", len(data), dim=-1):
+                pyro.sample("obs", dist.Normal(loc, 1.), obs=data)
 
-    # Define a guide (i.e. variational distribution) with a Normal
-    # distribution over the latent random variable `loc`.
-    def guide(data):
-        guide_loc = pyro.param("guide_loc", torch.tensor(0.))
-        guide_scale = pyro.param("guide_scale_log", torch.tensor(0.)).exp()
-        pyro.sample("loc", dist.Normal(guide_loc, guide_scale))
+        # Define a guide (i.e. variational distribution) with a Normal
+        # distribution over the latent random variable `loc`.
+        def guide(data):
+            guide_loc = pyro.param("guide_loc", torch.tensor(0.))
+            guide_scale = pyro.param("guide_scale_log", torch.tensor(0.)).exp()
+            pyro.sample("loc", dist.Normal(guide_loc, guide_scale))
 
-    # Generate some data.
-    torch.manual_seed(0)
-    data = torch.randn(100) + 3.0
-    pyro.get_param_store().clear()
+        # Generate some data.
+        torch.manual_seed(0)
+        data = torch.randn(100) + 3.0
+        pyro.get_param_store().clear()
 
-    # Construct an SVI object so we can do variational inference on our
-    # model/guide pair.
-    svi = SVI(model, guide, Adam({"lr": args.learning_rate}), elbo)
+        # Construct an SVI object so we can do variational inference on our
+        # model/guide pair.
+        svi = infer.SVI(model, guide, optim.Adam({"lr": args.learning_rate}), elbo)
 
-    # Basic training loop
-    for step in range(args.num_steps):
-        loss = svi.step(data)
-        if step % 100 == 0:
-            print("step {} loss = {}".format(step, loss))
+        # Basic training loop
+        for step in range(args.num_steps):
+            loss = svi.step(data)
+            if step % 100 == 0:
+                print("step {} loss = {}".format(step, loss))
 
-    # Report the final values of the variational parameters
-    # in the guide after training.
-    for name, value in pyro.get_param_store().items():
-        print("{} = {}".format(name, value.detach().cpu().numpy()))
+        # Report the final values of the variational parameters
+        # in the guide after training.
+        for name, value in pyro.get_param_store().items():
+            print("{} = {}".format(name, value.detach().cpu().numpy()))
 
-    # For this simple (conjugate) model we know the exact posterior. In
-    # particular we know that the variational distribution should be
-    # centered near 3.0. So let's check this explicitly.
-    assert (pyro.param("guide_loc") - 3.0).abs() < 0.1
+        # For this simple (conjugate) model we know the exact posterior. In
+        # particular we know that the variational distribution should be
+        # centered near 3.0. So let's check this explicitly.
+        assert (pyro.param("guide_loc") - 3.0).abs() < 0.1
 
 
 if __name__ == "__main__":
-    import pyro
     assert pyro.__version__.startswith('0.3.1')
     parser = argparse.ArgumentParser(description="Mini Pyro demo")
     parser.add_argument("-f", "--full-pyro", action="store_true", default=False)
