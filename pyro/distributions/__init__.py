fbf5bdf4ef078d49dd0c9fb1498ed2ce2e30290c
Stefan Webb <stefanwebb@users.noreply.github.com>
2019-03-29 08:34:43 -0700

    DeepSigmoidalFlow variant of Neural Autoregressive Flow (#1789)

--- pyro/distributions/__init__.py
+++ pyro/distributions/__init__.py
@@ -11,6 +11,7 @@ from pyro.distributions.empirical import Empirical
 from pyro.distributions.gaussian_scale_mixture import GaussianScaleMixture
 from pyro.distributions.iaf import InverseAutoregressiveFlow, InverseAutoregressiveFlowStable
 from pyro.distributions.mixture import MaskedMixture
+from pyro.distributions.naf import DeepSigmoidalFlow
 from pyro.distributions.omt_mvn import OMTMultivariateNormal
 from pyro.distributions.permute import PermuteTransform
 from pyro.distributions.planar import PlanarFlow
@@ -30,6 +31,7 @@ from pyro.distributions.lkj import (LKJCorrCholesky, CorrLCholeskyTransform, cor
 __all__ = [
     "AVFMultivariateNormal",
     "BetaBinomial",
+    "DeepSigmoidalFlow",
     "Delta",
     "DirichletMultinomial",
     "Distribution",
