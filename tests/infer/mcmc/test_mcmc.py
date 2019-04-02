5ffaa5ac42d4ac017d8117789cfa94735485eb4b
Neeraj Pradhan <npradhan@uber.com>
2019-04-02 16:34:50 -0700

    skipif multiple chains on CI

--- tests/infer/mcmc/test_mcmc.py
+++ tests/infer/mcmc/test_mcmc.py
@@ -12,7 +12,7 @@ from pyro.infer.mcmc import HMC, NUTS
 from pyro.infer.mcmc.mcmc import MCMC, _SingleSampler, _ParallelSampler
 from pyro.infer.mcmc.trace_kernel import TraceKernel
 from pyro.util import optional
-from tests.common import assert_equal
+from tests.common import assert_equal, skipif_param
 
 
 class PriorKernel(TraceKernel):
@@ -58,7 +58,7 @@ def test_mcmc_interface():
 
 @pytest.mark.parametrize("num_chains", [
     1,
-    pytest.param(2, marks=[pytest.mark.skipif("CI" in os.environ, reason="CI only provides 1 CPU")])
+    skipif_param(2, condition="CI" in os.environ, reason="CI only provides 1 CPU"),
 ])
 def test_mcmc_diagnostics(num_chains):
     data = torch.tensor([2.0]).repeat(3)
@@ -98,7 +98,10 @@ def _empty_model():
     (NUTS, _empty_model),
 ])
 @pytest.mark.parametrize("jit", [False, True])
-@pytest.mark.parametrize("num_chains", [1, 2])
+@pytest.mark.parametrize("num_chains", [
+    1,
+    skipif_param(2, condition="CI" in os.environ, reason="CI only provides 1 CPU")
+])
 def test_empty_sample_sites(kernel, kernel_args, jit, num_chains):
     num_warmup, num_samples = 10, 10
     kern = kernel(kernel_args, jit_compile=jit)
