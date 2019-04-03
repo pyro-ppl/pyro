152fe65b5034ccc362f50b0a483f117623792ade
Stefan Webb <stefan.webb@wadham.ox.ac.uk>
2019-04-02 18:23:36 -0700

    Fix to docstring

--- pyro/distributions/radial.py
+++ pyro/distributions/radial.py
@@ -40,7 +40,7 @@ class RadialFlow(TransformModule):
     radial flow can be scored.
 
     :param input_dim: the dimension of the input (and output) variable.
-    :type autoregressive_nn: int
+    :type input_dim: int
 
     References:
 
