c9d8a2df08e8d8f37b26cc57c8cdf4039804e381
Fritz Obermeyer <fritzo@uber.com>
2019-04-04 10:05:32 -0700

    Fix use of infer_discrete

--- pyro/contrib/tabular/serving.py
+++ pyro/contrib/tabular/serving.py
@@ -9,11 +9,21 @@ class TreeCatServer(object):
     def __init__(self, model):
         self._model = model
 
-    def impute(self, data=None, num_particles=None):
+    def impute(self, data=None, num_particles=None, temperature=1):
         if data is None:
             data = [None] * len(self._model.features)
+
+        # Sample global parameters from guide.
         guide_trace = poutine.trace(self._model.guide).get_trace(data)
         model = poutine.replay(self._model.model, guide_trace)
+
+        # Sample local latent variables using variable elimination.
+        first_available_dim = -2
         if num_particles is not None:
-            model = pyro.plate("num_particles_vectorized", num_particles, dim=-2)(model)
-        return infer_discrete(model)(data, impute=True)
+            model = pyro.plate("num_particles_vectorized", num_particles,
+                               dim=first_available_dim)(model)
+            first_available_dim -= 1
+        model = infer_discrete(model, first_available_dim=-1, temperature=temperature)
+
+        # Run the model.
+        return model(data, impute=True)
