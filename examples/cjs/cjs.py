13be1adb071b5454dcb0fdd3266341ead1097ef8
Martin Jankowiak <jankowiak@uber.com>
2019-04-06 15:46:00 -0700

    add model 5

--- examples/cjs/cjs.py
+++ examples/cjs/cjs.py
@@ -9,16 +9,16 @@ This is Norway's national bird.
 Compare to the Stan implementations in [6].
 
 References
-[1] Kéry, M., & Schaub, M. (2011). Bayesian population analysis using
+[1] Kery, M., & Schaub, M. (2011). Bayesian population analysis using
     WinBUGS: a hierarchical perspective. Academic Press.
 [2] Lebreton, J.D., Burnham, K.P., Clobert, J., & Anderson, D.R. (1992).
     Modeling survival and testing biological hypotheses using marked animals:
     a unified approach with case studies. Ecological monographs, 62(1), 67-118.
 [3] Cormack, R.M., 1964. Estimates of survival from the sighting of marked animals.
-    Biometrika 51, 429–438.
+    Biometrika 51, 429-438.
 [4] Jolly, G.M., 1965. Explicit estimates from capture-recapture data with both death
-    and immigration-stochastic model. Biometrika 52, 225–247.
-[5] Seber, G.A.F., 1965. A note on the multiple recapture census. Biometrika 52, 249–259.
+    and immigration-stochastic model. Biometrika 52, 225-247.
+[5] Seber, G.A.F., 1965. A note on the multiple recapture census. Biometrika 52, 249-259.
 [6] https://github.com/stan-dev/example-models/tree/master/BPA/Ch.07
 """
 
@@ -170,6 +170,48 @@ def model_4(capture_history, sex):
                 pyro.sample("y_{}".format(t), dist.Bernoulli(mu_y_t),
                             obs=capture_history[:, t])
             first_capture_mask |= capture_history[:, t].byte()
+
+
+"""
+In our final model variant we include both fixed group effects and fixed
+time effects for the survival probability phi:
+
+logit(phi_t) = beta_group + gamma_t
+
+We need to take care that the model is not overparameterized; to do this
+we effectively let a single scalar beta encode the difference in male
+and female survival probabilities.
+"""
+
+
+def model_5(capture_history, sex):
+    N, T = capture_history.shape
+
+    # mean survival probabilities for males/females in logit space
+    phi_beta = pyro.sample("phi_beta", dist.Normal(0.0, 10.0))
+    phi_beta = sex * phi_beta
+    rho = pyro.sample("rho", dist.Uniform(0.0, 1.0))  # recapture probability
+
+    z = torch.ones(N)
+    first_capture_mask = torch.zeros(N).byte()
+    # we create the plate once, outside of the loop over t
+    dippers_plate = pyro.plate("dippers", N, dim=-1)
+    for t in pyro.markov(range(T)):
+        # note that phi_t needs to be outside the plate, since
+        # phi_t is shared across all N birds
+        phi_gamma_t = pyro.sample("phi_gamma_{}".format(t), dist.Normal(0.0, 10.0)) if t > 0 \
+                      else 0.0
+        phi_t = torch.sigmoid(phi_beta + phi_gamma_t)
+        with dippers_plate, poutine.mask(mask=first_capture_mask):
+            mu_z_t = first_capture_mask.float() * phi_t * z + (1 - first_capture_mask.float())
+            # we use parallel enumeration to exactly sum out
+            # the discrete states z_t.
+            z = pyro.sample("z_{}".format(t), dist.Bernoulli(mu_z_t),
+                            infer={"enumerate": "parallel"})
+            mu_y_t = rho * z
+            pyro.sample("y_{}".format(t), dist.Bernoulli(mu_y_t),
+                        obs=capture_history[:, t])
+        first_capture_mask |= capture_history[:, t].byte()
 
 
 models = {name[len('model_'):]: model
