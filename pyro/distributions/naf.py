Numerical fixes for NAF

pyro/distributions/naf.py
@@ -64,7 +64,7 @@ class DeepSigmoidalFlow(TransformModule):
 
         # Not entirely sure this is necessary, but copying from NAF paper's implementation
         self.safe_log = lambda x: torch.log(x * 1e2) - math.log(1e2)
-        self.safe_logit = lambda x: self.safe_log(x) - self.safe_log(1 - x)
+        self.safe_logit = lambda x: self.safe_log(x + eps) - self.safe_log(1 - x + eps)
 
         # The output of safe_sigmoid is between [0.5*eps, 1-0.5*eps], which stops logit from returning +-Inf (overflow)
         self.sigmoid = nn.Sigmoid()
@@ -112,12 +112,12 @@ class DeepSigmoidalFlow(TransformModule):
 
         # See C.1 of Huang Et Al. for a derivation of this
         # NOTE: Since safe_logit is mathematically the same as logit, this line doesn't need any modification
-        log_dydD = -torch.log(D + 1e-8) - torch.log(1 - D)
+        log_dydD = -torch.log(D + eps) - torch.log(1 - D + eps)
 
         # NOTE: However, safe_sigmoid differs from torch.sigmoid, which we need to take into account in the derivative!
         # Hence the log(1 - eps) term
         log_dDdx = torch.logsumexp(self.logsoftmax(W_pre) + F.logsigmoid(C) + F.logsigmoid(-C) +
-                                   torch.log1p(torch.tensor(-eps)) + torch.log(A), dim=-2)
+                                   torch.log1p(torch.tensor(-eps)) + torch.log(A+eps), dim=-2)
         log_det = log_dydD + log_dDdx
 
         return log_det.sum(-1)