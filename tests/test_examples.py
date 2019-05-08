78cebeadf84669937247345ed3d44aac80fcbab5
Du Phan <fehiepsi@gmail.com>
2019-05-08 09:49:32 -0400

    Enable svdkl test in travis (#1857)

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -7,7 +7,7 @@ from subprocess import check_call
 
 import pytest
 
-from tests.common import EXAMPLES_DIR, requires_cuda, skipif_param
+from tests.common import EXAMPLES_DIR, requires_cuda
 
 logger = logging.getLogger(__name__)
 pytestmark = pytest.mark.stage('test_examples')
@@ -27,12 +27,8 @@ CPU_EXAMPLES = [
     'contrib/autoname/scoping_mixture.py --num-epochs=1',
     'contrib/autoname/mixture.py --num-epochs=1',
     'contrib/autoname/tree_data.py --num-epochs=1',
-    skipif_param('contrib/gp/sv-dkl.py --epochs=1 --num-inducing=4 --batch-size=1000',
-                 condition='CI' in os.environ,
-                 reason='https://github.com/uber/pyro/issues/1540'),
-    skipif_param('contrib/gp/sv-dkl.py --binary --epochs=1 --num-inducing=4 --batch-size=1000',
-                 condition='CI' in os.environ,
-                 reason='https://github.com/uber/pyro/issues/1540'),
+    'contrib/gp/sv-dkl.py --epochs=1 --num-inducing=4 --batch-size=1000',
+    'contrib/gp/sv-dkl.py --binary --epochs=1 --num-inducing=4 --batch-size=1000',
     'contrib/oed/ab_test.py --num-vi-steps=10 --num-bo-steps=2',
     'contrib/oed/item_response.py -N=1000 -M=1000',
     'contrib/oed/sequential_oed_sigmoid_lm.py --num-experiments=2 --num-runs=2 --no-plot',
