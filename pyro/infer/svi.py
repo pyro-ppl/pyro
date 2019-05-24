150805fa8101f1c77db3b29cc4310ec32e727886
Matt Wescott <mattwescott@gmail.com>
2019-05-24 13:48:44 -0400

    Add missing import (#1884)

--- pyro/infer/svi.py
+++ pyro/infer/svi.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, division, print_function
 import torch
 
 import pyro
+import pyro.optim
 import pyro.poutine as poutine
 from pyro.infer.abstract_infer import TracePosterior
 from pyro.infer.elbo import ELBO
