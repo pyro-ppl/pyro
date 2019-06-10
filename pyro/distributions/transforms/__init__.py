7c9f97bbb01aa68cd3fbc7fac67ef16ea807036c
Stefan Webb <stefandwebb@gmail.com>
2019-06-10 13:23:03 +0100

    Absolute imports

--- pyro/distributions/transforms/__init__.py
+++ pyro/distributions/transforms/__init__.py
@@ -1,11 +1,13 @@
-from .batch_norm import BatchNormTransform
-from .householder import HouseholderFlow
-from .iaf import InverseAutoregressiveFlow, InverseAutoregressiveFlowStable
-from .naf import DeepELUFlow, DeepLeakyReLUFlow, DeepSigmoidalFlow
-from .permute import PermuteTransform
-from .planar import PlanarFlow
-from .radial import RadialFlow
-from .sylvester import SylvesterFlow
+from __future__ import absolute_import
+
+from pyro.distributions.transforms.batch_norm import BatchNormTransform
+from pyro.distributions.transforms.householder import HouseholderFlow
+from pyro.distributions.transforms.iaf import InverseAutoregressiveFlow, InverseAutoregressiveFlowStable
+from pyro.distributions.transforms.naf import DeepELUFlow, DeepLeakyReLUFlow, DeepSigmoidalFlow
+from pyro.distributions.transforms.permute import PermuteTransform
+from pyro.distributions.transforms.planar import PlanarFlow
+from pyro.distributions.transforms.radial import RadialFlow
+from pyro.distributions.transforms.sylvester import SylvesterFlow
 
 __all__ = [
     'BatchNormTransform',
