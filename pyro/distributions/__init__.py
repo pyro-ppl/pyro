b382baad162bc35dd6aac187104a8cfb607f7a30
Stefan Webb <stefanwebb@users.noreply.github.com>
2019-06-10 16:31:36 +0100

    Reorganization of transforms (#1908)

--- pyro/distributions/__init__.py
+++ pyro/distributions/__init__.py
@@ -2,7 +2,6 @@ from __future__ import absolute_import, division, print_function
 
 import pyro.distributions.torch_patch  # noqa F403
 from pyro.distributions.avf_mvn import AVFMultivariateNormal
-from pyro.distributions.batch_norm import BatchNormTransform
 from pyro.distributions.conjugate import BetaBinomial, DirichletMultinomial, GammaPoisson
 from pyro.distributions.delta import Delta
 from pyro.distributions.diag_normal_mixture import MixtureOfDiagNormals
@@ -10,20 +9,13 @@ from pyro.distributions.diag_normal_mixture_shared_cov import MixtureOfDiagNorma
 from pyro.distributions.distribution import Distribution
 from pyro.distributions.empirical import Empirical
 from pyro.distributions.gaussian_scale_mixture import GaussianScaleMixture
-from pyro.distributions.householder import HouseholderFlow
 from pyro.distributions.inverse_gamma import InverseGamma
-from pyro.distributions.iaf import InverseAutoregressiveFlow, InverseAutoregressiveFlowStable
 from pyro.distributions.mixture import MaskedMixture
-from pyro.distributions.naf import DeepELUFlow, DeepLeakyReLUFlow, DeepSigmoidalFlow
 from pyro.distributions.omt_mvn import OMTMultivariateNormal
-from pyro.distributions.permute import PermuteTransform
-from pyro.distributions.planar import PlanarFlow
-from pyro.distributions.radial import RadialFlow
 from pyro.distributions.rejector import Rejector
 from pyro.distributions.relaxed_straight_through import (RelaxedBernoulliStraightThrough,
                                                          RelaxedOneHotCategoricalStraightThrough)
 from pyro.distributions.spanning_tree import SpanningTree
-from pyro.distributions.sylvester import SylvesterFlow
 from pyro.distributions.torch import *  # noqa F403
 from pyro.distributions.torch import __all__ as torch_dists
 from pyro.distributions.torch_distribution import TorchDistribution
@@ -33,24 +25,18 @@ from pyro.distributions.von_mises import VonMises
 from pyro.distributions.von_mises_3d import VonMises3D
 from pyro.distributions.zero_inflated_poisson import ZeroInflatedPoisson
 from pyro.distributions.lkj import (LKJCorrCholesky, CorrLCholeskyTransform, corr_cholesky_constraint)
+from pyro.distributions.transforms import * # noqa F403
 
 __all__ = [
     "AVFMultivariateNormal",
-    "BatchNormTransform",
     "BetaBinomial",
-    "DeepELUFlow",
-    "DeepLeakyReLUFlow",
-    "DeepSigmoidalFlow",
     "Delta",
     "DirichletMultinomial",
     "Distribution",
     "Empirical",
     "GammaPoisson",
     "GaussianScaleMixture",
-    "HouseholderFlow",
-    "InverseAutoregressiveFlow",
     "InverseGamma",
-    "InverseAutoregressiveFlowStable",
     "LKJCorrCholesky",
     "CorrLCholeskyTransform",
     "corr_cholesky_constraint",
@@ -58,14 +44,10 @@ __all__ = [
     "MixtureOfDiagNormals",
     "MixtureOfDiagNormalsSharedCovariance",
     "OMTMultivariateNormal",
-    "PermuteTransform",
-    "PlanarFlow",
-    "RadialFlow",
     "Rejector",
     "RelaxedBernoulliStraightThrough",
     "RelaxedOneHotCategoricalStraightThrough",
     "SpanningTree",
-    "SylvesterFlow",
     "TorchDistribution",
     "TransformModule",
     "VonMises",
@@ -79,3 +61,6 @@ __all__ = [
 # Import all torch distributions from `pyro.distributions.torch_distribution`
 __all__.extend(torch_dists)
 del torch_dists
+
+# For backwards compatibility, import all transforms into distributions module
+__all__.extend(transforms.__all__) # noqa
---
+++ pyro/distributions/transforms/__init__.py
@@ -0,0 +1,24 @@
+from __future__ import absolute_import
+
+from pyro.distributions.transforms.batch_norm import BatchNormTransform
+from pyro.distributions.transforms.householder import HouseholderFlow
+from pyro.distributions.transforms.iaf import InverseAutoregressiveFlow, InverseAutoregressiveFlowStable
+from pyro.distributions.transforms.naf import DeepELUFlow, DeepLeakyReLUFlow, DeepSigmoidalFlow
+from pyro.distributions.transforms.permute import PermuteTransform
+from pyro.distributions.transforms.planar import PlanarFlow
+from pyro.distributions.transforms.radial import RadialFlow
+from pyro.distributions.transforms.sylvester import SylvesterFlow
+
+__all__ = [
+    'BatchNormTransform',
+    'DeepELUFlow',
+    'DeepLeakyReLUFlow',
+    'DeepSigmoidalFlow',
+    'HouseholderFlow',
+    'InverseAutoregressiveFlow',
+    'InverseAutoregressiveFlowStable',
+    'PermuteTransform',
+    'PlanarFlow',
+    'RadialFlow',
+    'SylvesterFlow',
+]
