13271c01f59782d14be4e41f0d880c014355c923
Stefan Webb <stefandwebb@gmail.com>
2019-06-10 17:51:25 +0100

    Refactotred tests for Block-NAF

--- tests/distributions/test_flows.py
+++ tests/distributions/test_flows.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import, division, print_function
 
+from functools import partial
 from unittest import TestCase
 
 import pytest
@@ -94,14 +95,8 @@ class FlowTests(TestCase):
         bn.eval()
         return bn
 
-    def _make_blocknaf(self, input_dim):
-        return dist.BlockNAFFlow(input_dim)
-
-    def _make_blocknaf_normal(self, input_dim):
-        return dist.BlockNAFFlow(input_dim, residual='normal')
-
-    def _make_blocknaf_gated(self, input_dim):
-        return dist.BlockNAFFlow(input_dim, residual='gated')
+    def _make_blocknaf(self, input_dim, activation, residual):
+        return dist.BlockNAFFlow(input_dim, activation=activation, residual=residual)
 
     def _make_iaf(self, input_dim):
         arn = AutoRegressiveNN(input_dim, [3 * input_dim + 1])
@@ -112,11 +107,11 @@ class FlowTests(TestCase):
         return dist.InverseAutoregressiveFlowStable(arn, sigmoid_bias=0.5)
 
     def _make_def(self, input_dim):
-        arn = AutoRegressiveNN(input_dim, [3 * input_dim + 1], param_dims=[16]*3)
+        arn = AutoRegressiveNN(input_dim, [3 * input_dim + 1], param_dims=[16] * 3)
         return dist.DeepELUFlow(arn, hidden_units=16)
 
     def _make_dlrf(self, input_dim):
-        arn = AutoRegressiveNN(input_dim, [3 * input_dim + 1], param_dims=[16]*3)
+        arn = AutoRegressiveNN(input_dim, [3 * input_dim + 1], param_dims=[16] * 3)
         return dist.DeepLeakyReLUFlow(arn, hidden_units=16)
 
     def _make_dsf(self, input_dim):
@@ -135,22 +130,21 @@ class FlowTests(TestCase):
             self._test_jacobian(input_dim, self._make_batchnorm)
 
     def test_blocknaf_jacobians(self):
-        for input_dim in [2, 3, 5, 7, 9, 11]:
-            self._test_jacobian(input_dim, self._make_blocknaf)
-
-    def test_blocknaf_normal_jacobians(self):
-        for input_dim in [2, 3, 5, 7, 9, 11]:
-            self._test_jacobian(input_dim, self._make_blocknaf_normal)
-
-    def test_blocknaf_gated_jacobians(self):
-        for input_dim in [2, 3, 5, 7, 9, 11]:
-            self._test_jacobian(input_dim, self._make_blocknaf_gated)
+        for activation in ['ELU', 'LeakyReLU', 'sigmoid', 'tanh']:
+            for residual in [None, 'normal', 'gated']:
+                for input_dim in [2, 3, 5, 7, 9, 11]:
+                    self._test_jacobian(
+                        input_dim,
+                        partial(
+                            self._make_blocknaf,
+                            activation=activation,
+                            residual=residual))
 
     def _make_radial(self, input_dim):
         return dist.RadialFlow(input_dim)
 
     def _make_sylvester(self, input_dim):
-        return dist.SylvesterFlow(input_dim, count_transforms=input_dim//2 + 1)
+        return dist.SylvesterFlow(input_dim, count_transforms=input_dim // 2 + 1)
 
     def test_iaf_jacobians(self):
         for input_dim in [2, 3, 5, 7, 9, 11]:
@@ -213,16 +207,10 @@ class FlowTests(TestCase):
             self._test_shape(shape, self._make_batchnorm)
 
     def test_blocknaf_shapes(self):
-        for shape in [(3,), (3, 4), (3, 4, 2)]:
-            self._test_shape(shape, self._make_blocknaf)
-
-    def test_blocknaf_normal_shapes(self):
-        for shape in [(3,), (3, 4), (3, 4, 2)]:
-            self._test_shape(shape, self._make_blocknaf_normal)
-
-    def test_blocknaf_gated_shapes(self):
-        for shape in [(3,), (3, 4), (3, 4, 2)]:
-            self._test_shape(shape, self._make_blocknaf_gated)
+        for activation in ['ELU', 'LeakyReLU', 'sigmoid', 'tanh']:
+            for residual in [None, 'normal', 'gated']:
+                for shape in [(3,), (3, 4), (3, 4, 2)]:
+                    self._test_shape(shape, partial(self._make_blocknaf, activation=activation, residual=residual))
 
     def test_iaf_shapes(self):
         for shape in [(3,), (3, 4), (3, 4, 2)]:
