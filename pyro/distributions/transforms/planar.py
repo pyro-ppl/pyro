b9920569807eb3457072b1747c809dc3f6d5deeb
Stefan Webb <stefandwebb@gmail.com>
2019-06-14 17:24:20 -0700

    `ConditionalPlanarFlow`

--- pyro/distributions/transforms/planar.py
+++ pyro/distributions/transforms/planar.py
@@ -7,7 +7,7 @@ import torch.nn as nn
 from torch.distributions import constraints
 import torch.nn.functional as F
 
-from pyro.distributions.torch_transform import TransformModule
+from pyro.distributions.torch_transform import ConditionalTransformModule, TransformModule
 from pyro.distributions.util import copy_docs_from
 
 
@@ -107,3 +107,107 @@ class PlanarFlow(TransformModule):
 
         return (torch.log(torch.abs(1 + torch.matmul(psi_z, self.u_hat())).unsqueeze(-1)) *
                 torch.ones_like(x) / x.size(-1)).sum(-1)
+
+
+@copy_docs_from(ConditionalTransformModule)
+class ConditionalPlanarFlow(ConditionalTransformModule):
+    """
+    A conditional 'planar' normalizing flow that uses the transformation
+
+        :math:`\\mathbf{y} = \\mathbf{x} + \\mathbf{u}\\tanh(\\mathbf{w}^T\\mathbf{z}+b)`
+
+    where :math:`\\mathbf{x}` are the inputs with dimension :math:`D`, :math:`\\mathbf{y}` are the outputs,
+    and the pseudo-parameters :math:`b\\in\\mathbb{R}`, :math:`\\mathbf{u}\\in\\mathbb{R}^D`, and
+    :math:`\\mathbf{w}\\in\\mathbb{R}^D` are the output of a function, e.g. a NN, with input
+    :math:`z\\in\\mathbb{R}^{M}` representing the observed variable to condition on. For this to be an
+    invertible transformation, the condition :math:`\\mathbf{w}^T\\mathbf{u}>-1` is enforced.
+
+    Together with `ConditionalTransformedDistribution` this provides a way to create richer variational
+    approximations.
+
+    Example usage:
+
+    *** FINISH THIS EXAMPLE ONCE HAVE BASIC NN ***
+    >>> base_dist = dist.Normal(torch.zeros(10), torch.ones(10))
+    >>> plf = ConditionalPlanarFlow(10, 5)
+    >>> pyro.module("my_plf", plf)  # doctest: +SKIP
+    >>> plf_dist = dist.TransformedDistribution(base_dist, [plf])
+    >>> z =
+    >>> plf_dist.sample(obs=z)  # doctest: +SKIP
+        tensor([-0.4071, -0.5030,  0.7924, -0.2366, -0.2387, -0.1417,  0.0868,
+                0.1389, -0.4629,  0.0986])
+
+    The inverse of this transform does not possess an analytical solution and is left unimplemented. However,
+    the inverse is cached when the forward operation is called during sampling, and so samples drawn using
+    planar flow can be scored.
+
+    :param input_dim: the dimension of the input (and output) variable.
+    :type input_dim: int
+    :param observed_dim: the dimension of the observed variable.
+    :type input_dim: int
+    :param nn: a function inputting the observed variable and outputting a triplet of real-valued parameters
+        of dimensions :math:`(1, D, D)`.
+    :type nn: callable
+
+    References:
+
+    Variational Inference with Normalizing Flows [arXiv:1505.05770]
+    Danilo Jimenez Rezende, Shakir Mohamed
+
+    """
+
+    domain = constraints.real
+    codomain = constraints.real
+    bijective = True
+    event_dim = 1
+
+    def __init__(self, nn):
+        super(ConditionalPlanarFlow, self).__init__(cache_size=1)
+
+        self.nn = nn
+        self._cached_logDetJ = None
+
+    # This method ensures that torch(u_hat, w) > -1, required for invertibility
+    def u_hat(self, u, w):
+        alpha = torch.dot(u, w)
+        a_prime = -1 + F.softplus(alpha)
+        return u + (a_prime - alpha) * w.div(w.norm())
+
+    def _call(self, x, obs):
+        """
+        :param x: the input into the bijection
+        :type x: torch.Tensor
+
+        Invokes the bijection x | Z=obs => y; in the prototypical context of a TransformedDistribution `x` is a
+        sample from the base distribution (or the output of a previous flow)
+        """
+        bias, u, w = self.nn(obs)
+        act = torch.tanh(F.linear(x, w, bias))
+        u_hat = self.u_hat(u, w)
+
+        y = x + u_hat * act
+
+        # TODO: Need to unsqueeze w?
+        psi_z = (1. - act.pow(2)) * w
+        self._cached_logDetJ = (torch.log(torch.abs(1 + torch.matmul(psi_z, u_hat)).unsqueeze(-1)) *
+                                torch.ones_like(x) / x.size(-1)).sum(-1)
+
+        return y
+
+    def _inverse(self, y, obs):
+        """
+        :param y: the output of the bijection
+        :type y: torch.Tensor
+
+        Inverts y | Z=obs => x. As noted above, this implementation is incapable of inverting arbitrary values
+        `y`; rather it assumes `y` is the result of a previously computed application of the bijector
+        to some `x` (which was cached on the forward call)
+        """
+
+        raise KeyError("ConditionalPlanarFlow expected to find key in intermediates cache but didn't")
+
+    def log_abs_det_jacobian(self, x, y, obs):
+        """
+        Calculates the elementwise determinant of the log jacobian
+        """
+        return self._cached_logdetJ
