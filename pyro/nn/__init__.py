0241d2701501033a17d5708d323594a5c14b7123
Stefan Webb <stefandwebb@gmail.com>
2019-06-21 19:24:52 -0700

    Added to `__init__.py` import

--- pyro/nn/__init__.py
+++ pyro/nn/__init__.py
@@ -1,8 +1,9 @@
 from __future__ import absolute_import, division, print_function
 
-from pyro.nn.auto_reg_nn import AutoRegressiveNN, MaskedLinear
+from pyro.nn.auto_reg_nn import AutoRegressiveNN, ConditionalAutoRegressiveNN, MaskedLinear
 
 __all__ = [
     "AutoRegressiveNN",
+    "ConditionalAutoRegressiveNN",
     "MaskedLinear",
 ]
