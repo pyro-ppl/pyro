6ecd7d39f012d130576921ad1959c0b50a74630e
Adam Foster <a.evan.foster@gmail.com>
2019-06-28 13:20:34 +0100

    use reshape in place of contiguous().view

--- pyro/contrib/oed/util.py
+++ pyro/contrib/oed/util.py
@@ -17,7 +17,7 @@ def linear_model_ground_truth(model, design, observation_labels, target_labels, 
     prior_cov = torch.diag(w_sd**2)
     design_shape = design.shape
     posterior_covs = [analytic_posterior_cov(prior_cov, x, model.obs_sd) for x in
-                      torch.unbind(design.contiguous().view(-1, design_shape[-2], design_shape[-1]))]
+                      torch.unbind(design.reshape(-1, design_shape[-2], design_shape[-1]))]
     target_indices = get_indices(target_labels, tensors=model.w_sds)
     target_posterior_covs = [S[target_indices, :][:, target_indices] for S in posterior_covs]
     output = torch.tensor([0.5 * torch.logdet(2 * math.pi * math.e * C)
