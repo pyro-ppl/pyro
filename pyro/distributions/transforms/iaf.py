73f7a80babdfc32f4f00ba088ec57cb768ef197f
Stefan Webb <stefandwebb@gmail.com>
2019-06-30 00:41:33 +0100

    Utils

--- pyro/distributions/transforms/iaf.py
+++ pyro/distributions/transforms/iaf.py
@@ -6,11 +6,7 @@ from torch.distributions import constraints
 
 from pyro.distributions.torch_transform import TransformModule
 from pyro.distributions.util import copy_docs_from
-
-
-def clamp_preserve_gradients(x, min, max):
-    # This helper function clamps gradients but still passes through the gradient in clamped regions
-    return x + (x.clamp(min, max) - x).detach()
+from pyro.distributions.transforms.utils import clamp_preserve_gradients
 
 
 @copy_docs_from(TransformModule)
