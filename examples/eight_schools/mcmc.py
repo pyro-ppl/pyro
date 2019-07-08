e5bf71e5a8116e8f936c3e8a63db297c8b84514f
fehiepsi <fehiepsi@gmail.com>
2019-07-08 16:28:20 -0400

    nit

--- examples/eight_schools/mcmc.py
+++ examples/eight_schools/mcmc.py
@@ -46,7 +46,7 @@ def main(args):
 
 
 if __name__ == '__main__':
-    #assert pyro.__version__.startswith('0.3.3')
+    assert pyro.__version__.startswith('0.3.3')
     parser = argparse.ArgumentParser(description='Eight Schools MCMC')
     parser.add_argument('--num-samples', type=int, default=1000,
                         help='number of MCMC samples (default: 1000)')
