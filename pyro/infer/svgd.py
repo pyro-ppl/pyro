df182a27801c99c10660ddde4c50aa82f9d3aef6
Martin Jankowiak <jankowiak@uber.com>
2019-07-31 13:26:51 -0700

    wip

--- pyro/infer/svgd.py
+++ pyro/infer/svgd.py
@@ -1,3 +1,5 @@
+import math
+
 import torch
 
 import pyro
@@ -23,20 +25,23 @@ class RBFKernel(object):
     """
     def _bandwidth(self, norm_sq):
         """
-        Compute the bandwidth along each dimension using the median pairwise distance between particles.
+        Compute the bandwidth along each dimension using the median pairwise squared distance between particles.
         """
         num_particles = norm_sq.size(0)
         index = torch.arange(num_particles)
         norm_sq = norm_sq[index > index.unsqueeze(-1), ...]
         median = norm_sq.median(dim=0)[0]
-        return median / (num_particles + 1).log()
+        return median / math.log(num_particles + 1)
 
     def _log_kernel_and_grad(self, param):
+        """
+        Compute the kernel and (parts of) its gradient
+        """
         num_particles = param.size(0)
-        delta_x = param.unsqueeze(0) - param.unsqueeze(1)
+        delta_x = param.unsqueeze(1) - param.unsqueeze(0)
         norm_sq = delta_x.reshape(num_particles, num_particles, -1).pow(2.0)
         h = self._bandwidth(norm_sq)
-        log_kernel = -norm_sq / h
+        log_kernel = -(norm_sq / h).sum(-1)
         grad_term = (-2.0) * delta_x / h.reshape(param.shape[1:])
         return log_kernel, grad_term
 
@@ -52,7 +57,9 @@ class RBFKernel(object):
 
     def apply(self, kernel, grads):
         assert isinstance(grads, dict)
-        return {name: torch.einsum("ab,b...->a...", kernel, grad) for name, grad in grads.items()}
+        for name, grad in grads.items():
+            print("apply grads[%s]" % name, grad.shape, "kernel", kernel.shape, "result", torch.einsum("ab,b...->a...", kernel, grad).shape)
+        return {name: torch.einsum("ji,ji...->i...", kernel, grad) for name, grad in grads.items()}
 
 
 class SVGD(object):
@@ -72,11 +79,15 @@ class SVGD(object):
         self.model = vectorize(model, num_particles, max_plate_nesting)
         # TODO: fix circular import hack
         from pyro.contrib.autoguide import AutoDelta
-        self.guide = AutoDelta(model)
+        self.guide = AutoDelta(self.model, prefix="svgd")
         self.kernel = kernel
         self.num_particles = num_particles
         self.max_plate_nesting = max_plate_nesting
         self.loss = Trace_ELBO().differentiable_loss
+
+    def get_particles(self):
+        params = {name: pyro.param('svgd_{}'.format(name)) for name, site in self.guide.prototype_trace.iter_stochastic_nodes()}
+        return params
 
     def compute_grad(self, *args, **kwargs):
         """
@@ -85,8 +96,17 @@ class SVGD(object):
         loss = self.loss(self.model, self.guide, *args, **kwargs)
         loss.backward()
 
-        params = {name: pyro.param('auto_{}'.format(name)) for name, site in self.guide.prototype_trace.iter_stochastic_nodes()}
-        for p, v in params.items():
-            print(p, v.shape)
+        params = self.get_particles()
+        kernel, kernel_grads = self.kernel.kernel_and_grads(params)
 
-        kernel, grads = self.kernel.kernel_and_grads(params)
+        param_grads = {name: param.grad for name, param in params.items()}
+        param_grads = self.kernel.apply(kernel, param_grads)
+
+        for name, param in params.items():
+            print("param_grads[name]",param_grads[name].shape)
+            print("kernel_grads[name]",kernel_grads[name].shape)
+            param.grad.data = (param_grads[name] + kernel_grads[name]) / self.num_particles
+
+
+
+
