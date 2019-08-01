b5f9743feb1c1de5560208d2c54407aca080d808
Martin Jankowiak <jankowiak@uber.com>
2019-07-31 18:02:26 -0700

    more asserts in test

--- tests/infer/test_svgd.py
+++ tests/infer/test_svgd.py
@@ -13,7 +13,7 @@ from torch.distributions.transforms import AffineTransform
 @pytest.mark.parametrize("latent_dist", [#dist.TransformedDistribution(dist.Normal(torch.zeros(1), torch.ones(1)),
                                          #     AffineTransform(loc=-1., scale=2.0))])
     #dist.Normal(torch.zeros(1) + 0.5, torch.ones(1)).to_event(1)])#,
-                                         dist.LogNormal(torch.tensor([0.0]), torch.tensor([1.0])).to_event(1)])
+                                         dist.LogNormal(torch.tensor([-1.0]), torch.tensor([0.5])).to_event(1)])
                                          #dist.Gamma(torch.tensor([2.0]), torch.tensor([2.0])).to_event(1)])
                                          #dist.Beta(torch.tensor([0.5]), torch.tensor([0.5])).to_event(1)])
 def test_mean_variance(latent_dist):
@@ -28,8 +28,8 @@ def test_mean_variance(latent_dist):
     svgd = SVGD(model, kernel, adam, 1000, 0)
 
     bandwidth_start = 1.0
-    bandwidth_end = 10.0
-    n_steps = 201
+    bandwidth_end = 1.0
+    n_steps = 301
 
     for step in range(n_steps):
         bandwidth = bandwidth_start + (step / n_steps) * (bandwidth_end - bandwidth_start)
@@ -49,14 +49,21 @@ def test_mean_variance(latent_dist):
     unc = latent_dist.sample(sample_shape=(1000,)).log()
     print("unconstrained z mean, variance = ", unc.mean(0).data.cpu().numpy(), unc.var(0).data.cpu().numpy())
 
+
 @pytest.mark.parametrize("shape", [(1, 1), (2, 1, 3), (4, 2), (1, 2, 1, 3)])
 def test_shapes(shape):
     pyro.clear_param_store()
 
     def model():
-        pyro.sample("z", dist.Normal(torch.zeros(shape), 1.0).to_event(len(shape)))
+        pyro.sample("z1", dist.Normal(torch.zeros(shape), 1.0).to_event(len(shape)))
+        pyro.sample("z2", dist.Normal(torch.zeros(shape + (5,)), 1.0).to_event(1 + len(shape)))
 
-    svgd = SVGD(model, SVGDRBFKernel(), Adam({"lr": 0.001}), 7, 0)
+    num_particles = 7
+    svgd = SVGD(model, SVGDRBFKernel(), Adam({"lr": 0.001}), num_particles, 0)
 
     for step in range(2):
         svgd.step()
+
+    particles = svgd.get_named_particles()
+    assert particles['z1'].shape == (num_particles,) + shape
+    assert particles['z2'].shape == (num_particles,) + shape + (5,)
