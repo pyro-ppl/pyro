e62392ab82fe190c23ccaaf778c3b135c7a6d90f
Martin Jankowiak <jankowiak@uber.com>
2019-08-06 12:20:19 -0700

    refactor until tests pass

--- pyro/infer/svgd.py
+++ pyro/infer/svgd.py
@@ -1,3 +1,4 @@
+from abc import ABCMeta, abstractmethod
 import math
 from contextlib import ExitStack  # python 3
 
@@ -7,7 +8,7 @@ from torch.distributions import biject_to
 import pyro
 from pyro.distributions import Delta
 from pyro.infer.trace_elbo import Trace_ELBO
-from pyro.infer.autoguide.guides import AutoDelta
+from pyro.infer.autoguide.guides import AutoContinuous
 from pyro.infer.autoguide.initialization import init_to_sample
 from pyro.distributions.util import sum_rightmost
 
@@ -19,48 +20,27 @@ def vectorize(fn, num_particles, max_plate_nesting):
     return _fn
 
 
-class _SVGDGuide(AutoDelta):
+class _SVGDGuide(AutoContinuous):
     """
-    This modification of :class:`AutoDelta` is used internally in the
+    This modification of :class:`AutoContinuous` is used internally in the
     :class:`SVGD` inference algorithm.
     """
     def __init__(self, model):
         super(_SVGDGuide, self).__init__(model, prefix="svgd", init_loc_fn=init_to_sample)
 
-    def __call__(self, *args, **kwargs):
+    def get_posterior(self, *args, **kwargs):
+        svgd_particles = pyro.param("svgd_particles", self._init_loc)
+        return Delta(svgd_particles, event_dim=1)
+
+
+class SteinKernel(object, metaclass=ABCMeta):
+
+    @abstractmethod
+    def log_kernel_and_grad(self, particles):
         """
-        An automatic guide with the same ``*args, **kwargs`` as the base ``model``.
-
-        :return: A dict mapping sample site name to sampled value.
-        :rtype: dict
+        Compute the component kernels and (parts of) their gradients.
         """
-        # if we've never run the model before, do so now so we can inspect the model structure
-        if self.prototype_trace is None:
-            self._setup_prototype(*args, **kwargs)
-
-        plates = self._create_plates()
-        result = {}
-        for name, site in self.prototype_trace.iter_stochastic_nodes():
-            with ExitStack() as stack:
-                for frame in site["cond_indep_stack"]:
-                    if frame.vectorized:
-                        stack.enter_context(plates[frame.name])
-                value = pyro.param("{}_{}".format(self.prefix, name),
-                                   site["value"].detach(),
-                                   constraint=site["fn"].support)
-                # The following lines are where this method differs from the AutoDelta version.
-                # For SVGD (but not MAP) we need the log det jacobian terms for correctness.
-                transform = biject_to(site["fn"].support)
-                unconstrained_value = pyro.param("{}_{}".format(self.prefix, name)).unconstrained()
-                log_density = transform.inv.log_abs_det_jacobian(value, unconstrained_value)
-                log_density = sum_rightmost(log_density, log_density.dim() - value.dim() + site["fn"].event_dim)
-                result[name] = pyro.sample(name, Delta(value, log_density=log_density,
-                                           event_dim=site["fn"].event_dim))
-        return result
-
-
-class SteinKernel(object):
-    pass
+        raise NotImplementedError
 
 
 class RBFSteinKernel(SteinKernel):
@@ -68,21 +48,12 @@ class RBFSteinKernel(SteinKernel):
     A RBF kernel for use in the SVGD inference algorithm. The bandwidth of the kernel is chosen from the
     particles using a simple heuristic as in reference [1].
 
-    :param str mode: whether to use a Kernelized Stein Discrepancy that makes use of multivariate
-        test functions (as in [1]) or univariate test functions (as in [2]). defaults to 'univariate'.
-
     References
 
     [1] "Stein Variational Gradient Descent: A General Purpose Bayesian Inference Algorithm,"
         Qiang Liu, Dilin Wang
-    [2] "Kernelized Complete Conditional Stein Discrepancy,"
-        Raghav Singhal, Saad Lahlou, Rajesh Ranganath
     """
-    def __init__(self, mode='univariate'):
-        assert mode in ['univariate', 'multivariate'], "mode must be one of (univariate, multivariate)"
-        self.mode = mode
-
-    def _bandwidth(self, norm_sq):
+    def _bandwidth(self, norm_sq, bandwidth_factor=None):
         """
         Compute the bandwidth along each dimension using the median pairwise squared distance between particles.
         """
@@ -90,43 +61,24 @@ class RBFSteinKernel(SteinKernel):
         index = torch.arange(num_particles)
         norm_sq = norm_sq[index > index.unsqueeze(-1), ...]
         median = norm_sq.median(dim=0)[0]
+        if bandwidth_factor is not None:
+            median = bandwidth_factor * median
+        assert median.shape == norm_sq.shape[-1:]
         return median / math.log(num_particles + 1)
 
-    def _log_kernel_and_grad(self, param, bandwidth_factor=None):
+    def log_kernel_and_grad(self, particles, bandwidth_factor=None):
         """
         Compute the component kernels and (parts of) their gradients.
-
-        :param float bandwidth_factor: optional factor by which to scale the bandwidth
         """
-        num_particles = param.size(0)
-        delta_x = param.unsqueeze(0) - param.unsqueeze(1)
-        norm_sq = delta_x.reshape(num_particles, num_particles, -1).pow(2.0)
-        h = self._bandwidth(norm_sq)
-        if bandwidth_factor:
-            h *= bandwidth_factor
-        log_kernel = -(norm_sq / h)
-        grad_term = (-2.0) * delta_x / h.reshape(param.shape[1:])
+        num_particles = particles.size(0)
+        delta_x = particles.unsqueeze(0) - particles.unsqueeze(1)  # N N D
+        assert delta_x.dim() == 3
+        norm_sq = delta_x.pow(2.0)  # N N D
+        h = self._bandwidth(norm_sq, bandwidth_factor=bandwidth_factor)  # D
+        log_kernel = -(norm_sq / h)  # N N D
+        grad_term = 2.0 * delta_x / h  # N N D
+        assert log_kernel.shape == grad_term.shape
         return log_kernel, grad_term
-
-    def kernel_and_grads(self, params, bandwidth_factor=None):
-        """
-        Compute the full kernel and its gradient
-
-        :param dict params: dictionary of (name, parameter) pairs
-        :param float bandwidth_factor: optional factor by which to scale the bandwidth
-        """
-        log_kernels, grads = {}, {}
-        for name, param in params.items():
-            log_kernel, grad = self._log_kernel_and_grad(param, bandwidth_factor=bandwidth_factor)
-            log_kernels[name] = log_kernel
-            grads[name] = grad
-        if self.mode == "multivariate":
-            kernel = torch.exp(sum([lk.sum(-1) for lk in log_kernels.values()]))
-            grads = {name: torch.einsum("ji,ji...->i...", kernel, grad) for name, grad in grads.items()}
-        elif self.mode == "univariate":
-            kernel = {name: lk.exp() for name, lk in log_kernels.items()}
-            grads = {name: torch.einsum("j...,j...->...", kernel[name], grad) for name, grad in grads.items()}
-        return kernel, grads
 
 
 class SVGD(object):
@@ -139,6 +91,8 @@ class SVGD(object):
     :type optim: pyro.optim.PyroOptim
     :param int num_particles: the number of particles used in SVGD
     :param int max_plate_nesting: the max number of nested :func:`pyro.plate` contexts in the model.
+    :param str mode: whether to use a Kernelized Stein Discrepancy that makes use of `multivariate`
+        test functions (as in [1]) or `univariate` test functions (as in [2]). defaults to `univariate`.
 
     Example usage:
 
@@ -165,29 +119,31 @@ class SVGD(object):
     [2] "Kernelized Complete Conditional Stein Discrepancy,"
         Raghav Singhal, Saad Lahlou, Rajesh Ranganath
     """
-    def __init__(self, model, kernel, optim, num_particles, max_plate_nesting):
+    def __init__(self, model, kernel, optim, num_particles, max_plate_nesting, mode="univariate"):
         assert callable(model)
         assert isinstance(kernel, SteinKernel), "Must provide a valid SteinKernel"
         assert isinstance(optim, pyro.optim.PyroOptim), "Must provide a valid Pyro optimizer"
         assert num_particles > 1, "Must use at least two particles"
         assert max_plate_nesting >= 0
+        assert mode in ['univariate', 'multivariate'], "mode must be one of (univariate, multivariate)"
 
         self.model = vectorize(model, num_particles, max_plate_nesting)
         self.kernel = kernel
         self.optim = optim
         self.num_particles = num_particles
         self.max_plate_nesting = max_plate_nesting
+        self.mode = mode
 
         self.loss = Trace_ELBO().differentiable_loss
         self.guide = _SVGDGuide(self.model)
 
     def get_named_particles(self):
         """
-        Get a dictionary of named particles of the form {name: particle}
+        Get a dictionary of named particles of the form {name: particle}. The leading dimension
+        corresponds to particles.
         """
-        params = {name: pyro.param('svgd_{}'.format(name))
-                  for name, site in self.guide.prototype_trace.iter_stochastic_nodes()}
-        return params
+        return {site["name"]: biject_to(site["fn"].support)(unconstrained_value)
+                for site, unconstrained_value in self.guide._unpack_latent(pyro.param("svgd_particles"))}
 
     def step(self, *args, **kwargs):
         """
@@ -209,28 +165,38 @@ class SVGD(object):
         loss = self.loss(self.model, self.guide, *args, **kwargs)
         loss.backward()
 
-        # compute the kernel ingredients needed for SVGD
-        params = {name: param.unconstrained() for name, param in self.get_named_particles().items()}
-        kernel, kernel_grads = self.kernel.kernel_and_grads(params, bandwidth_factor=bandwidth_factor)
+        # get particles used in the _SVGDGuide and reshape to have num_particles leading dimension
+        particles = pyro.param("svgd_particles").unconstrained()
+        reshaped_particles = particles.reshape(self.num_particles, -1)
+        reshaped_particles_grad = particles.grad.reshape(self.num_particles, -1)
 
-        if self.kernel.mode == "multivariate":
-            param_grads = {name: torch.einsum("ij,j...->i...", kernel, param.grad) for name, param in params.items()}
-        elif self.kernel.mode == "univariate":
-            param_grads = {name: torch.einsum("ji...,i...->j...", kernel[name], param.grad)
-                           for name, param in params.items()}
+        # compute kernel ingredients
+        log_kernel, kernel_grad = self.kernel.log_kernel_and_grad(reshaped_particles, bandwidth_factor=bandwidth_factor)
+
+        if self.mode == "multivariate":
+            kernel = log_kernel.sum(-1).exp()
+            assert kernel.shape == (self.num_particles, self.num_particles)
+            attractive_grad = torch.mm(kernel, reshaped_particles_grad)
+            repulsive_grad = torch.einsum("nm,nm...->n...", kernel, kernel_grad)
+        elif self.mode == "univariate":
+            kernel = log_kernel.exp()
+            assert kernel.shape == (self.num_particles, self.num_particles, reshaped_particles.size(-1))
+            attractive_grad = torch.einsum("nmd,md->nd", kernel, reshaped_particles_grad)
+            repulsive_grad = torch.einsum("nmd,nmd->nd", kernel, kernel_grad)
 
         # combine the attractive and repulsive terms in the SVGD gradient
-        for name, param in params.items():
-            assert param_grads[name].shape == kernel_grads[name].shape
-            param.grad.data = (param_grads[name] + kernel_grads[name]) / self.num_particles
+        assert attractive_grad.shape == repulsive_grad.shape
+        particles.grad = (attractive_grad + repulsive_grad).reshape(particles.shape) / self.num_particles
 
         # optionally report per-parameter mean squared gradients to user
         if gradient_callback is not None:
-            squared_gradients = {name: param.grad.pow(2.0).mean().item() for name, param in params.items()}
+            squared_gradients = {site["name"]: value.mean().item()
+                                 for site, value in self.guide._unpack_latent(particles.grad.pow(2.0))}
             gradient_callback(squared_gradients=squared_gradients)
 
+        # what about other params???
         # torch.optim objects gets instantiated for any params that haven't been seen yet
-        self.optim(params.values())
+        self.optim([particles])
 
         # zero gradients
-        pyro.infer.util.zero_grads(params.values())
+        pyro.infer.util.zero_grads([particles])
--- tests/infer/test_svgd.py
+++ tests/infer/test_svgd.py
@@ -12,11 +12,14 @@ from pyro.optim import Adam
 from tests.common import assert_equal
 
 
-@pytest.mark.parametrize("latent_dist", [dist.Normal(torch.zeros(2), torch.ones(2)).to_event(1),
+@pytest.mark.parametrize("latent_dist", [
+                                         dist.Normal(torch.zeros(2), torch.ones(2)).to_event(1),
                                          dist.LogNormal(torch.tensor([-1.0]), torch.tensor([0.7])).to_event(1),
-                                         dist.Beta(torch.tensor([0.4]), torch.tensor([0.6])).to_event(1)])
+                                         dist.LogNormal(torch.tensor(-1.0), torch.tensor(0.7)),
+                                         dist.Beta(torch.tensor([0.4]), torch.tensor([0.6])).to_event(1)
+                                         ])
 @pytest.mark.parametrize("mode", ["univariate", "multivariate"])
-def test_mean_variance(latent_dist, mode, verbose=False):
+def test_mean_variance(latent_dist, mode, verbose=True):
     pyro.clear_param_store()
 
     def model():
@@ -26,33 +29,35 @@ def test_mean_variance(latent_dist, mode, verbose=False):
     def gradient_callback(squared_gradients, step):
         print("[step %03d] mean squared gradients:" % step, squared_gradients)
 
-    kernel = RBFSteinKernel(mode=mode)
+    kernel = RBFSteinKernel()
     adam = Adam({"lr": 0.05})
-    svgd = SVGD(model, kernel, adam, 200, 0)
+    svgd = SVGD(model, kernel, adam, 200, 0, mode=mode)
 
     bandwidth_start = 1.0
     bandwidth_end = 5.0
-    n_steps = 251
+    n_steps = 301
 
     # scramble initial particles
     svgd.step()
-    pyro.param('svgd_z').unconstrained().data *= 1.3
-    pyro.param('svgd_z').unconstrained().data += 0.7
+    pyro.param('svgd_particles').unconstrained().data *= 1.3
+    pyro.param('svgd_particles').unconstrained().data += 0.7
 
     for step in range(n_steps):
         bandwidth = bandwidth_start + (step / n_steps) * (bandwidth_end - bandwidth_start)
         callback = partial(gradient_callback, step=step) if step % 125 == 0 else None
         svgd.step(bandwidth_factor=bandwidth, gradient_callback=callback)
 
+        final_particles = svgd.get_named_particles()['z']
+
     if verbose:
-        print("[mean]: actual, expected = ", pyro.param('svgd_z').mean(0).data.numpy(),
+        print("[mean]: actual, expected = ", final_particles.mean(0).data.numpy(),
               latent_dist.mean.data.numpy())
-        print("[var]: actual, expected = ", pyro.param('svgd_z').var(0).data.numpy(),
+        print("[var]: actual, expected = ", final_particles.var(0).data.numpy(),
               latent_dist.variance.data.numpy())
 
-    assert_equal(pyro.param('svgd_z').mean(0), latent_dist.mean, prec=0.01)
+    assert_equal(final_particles.mean(0), latent_dist.mean, prec=0.01)
     prec = 0.05 if mode == 'multivariate' else 0.02
-    assert_equal(pyro.param('svgd_z').var(0), latent_dist.variance, prec=prec)
+    assert_equal(final_particles.var(0), latent_dist.variance, prec=prec)
 
 
 @pytest.mark.parametrize("shape", [(1, 1), (2, 1, 3), (4, 2), (1, 2, 1, 3)])
