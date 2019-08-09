ac37ad1d9373511a51af20e6b3160826cb530be2
Fritz Obermeyer <fritzo@uber.com>
2019-08-09 15:36:13 -0700

    Vectorize LowerCholeskyTransform (#2007)

--- tests/distributions/test_torch_patch.py
+++ tests/distributions/test_torch_patch.py
@@ -1,7 +1,8 @@
+import pytest
 import torch
 
 import pyro.distributions as dist
-from tests.common import requires_cuda
+from tests.common import assert_close, requires_cuda
 
 
 @requires_cuda
@@ -14,3 +15,18 @@ def test_dirichlet_grad_cuda():
 def test_linspace():
     x = torch.linspace(-1., 1., 100, device="cuda")
     assert x.device.type == "cuda"
+
+
+@pytest.mark.parametrize("batch_shape", [(), (5,), (2, 3)], ids=str)
+@pytest.mark.parametrize("dim", [1, 2, 3, 4])
+def test_lower_cholesky_transform(batch_shape, dim):
+    t = torch.distributions.transform_to(torch.distributions.constraints.lower_cholesky)
+    x = torch.randn(batch_shape + (dim, dim))
+    y = t(x)
+    assert y.shape == x.shape
+    actual = y.matmul(y.transpose(-1, -2)).cholesky()
+    assert_close(actual, y)
+    x2 = t.inv(y)
+    assert x2.shape == x.shape
+    y2 = t(x2)
+    assert_close(y2, y)
