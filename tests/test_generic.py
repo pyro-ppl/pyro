4611aedeb941d445819e07d19f9ee1af3e0eb67b
Fritz Obermeyer <fritzo@uber.com>
2019-10-11 16:12:58 -0700

    Exclude seed handler from the PYRO_STACK (#2061)

--- tests/test_generic.py
+++ tests/test_generic.py
@@ -2,6 +2,7 @@ import pytest
 
 from pyro.generic import handlers, infer, pyro, pyro_backend
 from pyro.generic.testing import MODELS
+from tests.common import assert_close, xfail_if_not_implemented
 
 pytestmark = pytest.mark.stage('unit')
 
@@ -30,9 +31,8 @@ def test_not_implemented(backend):
 
 @pytest.mark.parametrize('model', MODELS)
 @pytest.mark.parametrize('backend', ['minipyro', 'pyro'])
-@pytest.mark.xfail(reason='Not supported by backend.')
 def test_model_sample(model, backend):
-    with pyro_backend(backend), handlers.seed(rng_seed=2):
+    with pyro_backend(backend), handlers.seed(rng_seed=2), xfail_if_not_implemented():
         f = MODELS[model]()
         model, model_args = f['model'], f.get('model_args', ())
         model(*model_args)
@@ -40,9 +40,38 @@ def test_model_sample(model, backend):
 
 @pytest.mark.parametrize('model', MODELS)
 @pytest.mark.parametrize('backend', ['minipyro', 'pyro'])
-@pytest.mark.xfail(reason='Not supported by backend.')
+def test_rng_seed(model, backend):
+    with pyro_backend(backend), handlers.seed(rng_seed=2), xfail_if_not_implemented():
+        f = MODELS[model]()
+        model, model_args = f['model'], f.get('model_args', ())
+        with handlers.seed(rng_seed=0):
+            expected = model(*model_args)
+        with handlers.seed(rng_seed=0):
+            actual = model(*model_args)
+        assert_close(actual, expected)
+
+
+@pytest.mark.parametrize('model', MODELS)
+@pytest.mark.parametrize('backend', ['minipyro', 'pyro'])
+def test_rng_state(model, backend):
+    with pyro_backend(backend), handlers.seed(rng_seed=2), xfail_if_not_implemented():
+        f = MODELS[model]()
+        model, model_args = f['model'], f.get('model_args', ())
+        with handlers.seed(rng_seed=0):
+            model(*model_args)
+            expected = model(*model_args)
+        with handlers.seed(rng_seed=0):
+            model(*model_args)
+            with handlers.seed(rng_seed=0):
+                model(*model_args)
+            actual = model(*model_args)
+        assert_close(actual, expected)
+
+
+@pytest.mark.parametrize('model', MODELS)
+@pytest.mark.parametrize('backend', ['minipyro', 'pyro'])
 def test_trace_handler(model, backend):
-    with pyro_backend(backend), handlers.seed(rng_seed=2):
+    with pyro_backend(backend), handlers.seed(rng_seed=2), xfail_if_not_implemented():
         f = MODELS[model]()
         model, model_args, model_kwargs = f['model'], f.get('model_args', ()), f.get('model_kwargs', {})
         # should be implemented
