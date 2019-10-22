c3c88095a42c91de040e1745d1e0ae38b79aa74d
Neeraj Pradhan <npradhan@uber.com>
2019-10-22 16:19:29 -0700

    add ConditionalAutoregressiveNN to docs

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ setup(
         # numpy is necessary for some functionality of PyTorch
         'numpy>=1.7',
         'opt_einsum>=2.3.2',
-        'pyro-api@0.1.0',
+        'pyro-api>=0.1.0',
         'torch>=1.2.0',
         'tqdm>=4.31',
     ],
