f9163218df6ef687be2c4f48fdd9e5ca48c5e97f
Martin Jankowiak <jankowiak@uber.com>
2019-10-23 10:45:30 -0700

    tweak

--- pyro/ops/tensor_utils.py
+++ pyro/ops/tensor_utils.py
@@ -10,9 +10,6 @@ def block_diag(m):
     :returns torch.Tensor: a 2-dimensional block diagonal tensor
     """
     assert m.dim() == 3, "Input to block_diag() must be a 3-dimensional tensor"
-    batch_size = m.size(0)
-    block_shape = m.shape[-2:]
-    eye = torch.eye(batch_size).type_as(m).unsqueeze(-2)
-    eye = eye.reshape(eye.shape + (1,))
-    m = (m.unsqueeze(-2) * eye).reshape(torch.Size(torch.tensor(block_shape) * batch_size))
-    return m
+    B, M, N = m.shape
+    eye = torch.eye(B, dtype=m.dtype, device=m.device).reshape(B, 1, B, 1)
+    return (m.unsqueeze(-2) * eye).reshape(B * M, B * N)
