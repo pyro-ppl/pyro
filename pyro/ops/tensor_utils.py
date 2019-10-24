857567736c3054108921ff5f382c8346f0ff7cc2
Fritz Obermeyer <fritzo@uber.com>
2019-10-23 18:59:29 -0700

    Sketch 1d convolution kernel via fft

--- pyro/ops/tensor_utils.py
+++ pyro/ops/tensor_utils.py
@@ -13,3 +13,30 @@ def block_diag(m):
     B, M, N = m.shape
     eye = torch.eye(B, dtype=m.dtype, device=m.device).reshape(B, 1, B, 1)
     return (m.unsqueeze(-2) * eye).reshape(B * M, B * N)
+
+
+def _complex_mul(a, b):
+    ar, ai = a.unbind(-1)
+    br, bi = b.unbind(-1)
+    # FIXME should b be conjugated?
+    # bi = -bi  # Conjugates b.
+    return torch.stack([ar * br - ai * bi, ar * bi + ai * br], dim=-1)
+
+
+def conv1d_fft(signal, kernel):
+    """
+    Computes the 1-d convolution of signal by kernel using FFTs.
+    The two arguments should have the same rightmost dim, but may otherwise be
+    arbitrarily broadcastable.
+
+    :param torch.Tensor signal: A signal to convolve.
+    :param torch.Tensor kernel: A convolution kernel.
+    :return: A tensor of shape
+        ``broadcast_shape(convolve, kernel.shape)``
+    :rtype torch.Tensor:
+    """
+    assert signal.size(-1) == kernel.size(-1)
+    f_signal = torch.rfft(torch.cat([signal, torch.zeros_like(signal)], dim=-1), 1)
+    f_kernel = torch.rfft(torch.cat([kernel, torch.zeros_like(kernel)], dim=-1), 1)
+    f_result = _complex_mul(f_signal, f_kernel)
+    return torch.irfft(f_result, 1)[..., :signal.size(-1)]
--- tests/ops/test_tensor_utils.py
+++ tests/ops/test_tensor_utils.py
@@ -2,7 +2,7 @@ import pytest
 from tests.common import assert_equal
 
 import torch
-from pyro.ops.tensor_utils import block_diag
+from pyro.ops.tensor_utils import block_diag, conv1d_fft
 
 
 pytestmark = pytest.mark.stage('unit')
@@ -22,3 +22,18 @@ def test_block_diag(batch_size, block_size):
         bottom, top = k * block_size[0], (k + 1) * block_size[0]
         left, right = k * block_size[1], (k + 1) * block_size[1]
         assert_equal(b[bottom:top, left:right], m[k])
+
+
+@pytest.mark.parametrize('T', [2, 3, 4, 5, 10])
+@pytest.mark.parametrize('batch_shape', [(), (4,), (2, 3)], ids=str)
+def test_conv1d_fft(batch_shape, T):
+    signal = torch.randn(T)  # not batched
+    kernel = torch.randn(*batch_shape, T).exp()  # batched
+    actual = conv1d_fft(signal, kernel)
+
+    # FIXME is this correct?
+    padded = torch.cat([torch.zeros_like(signal), signal])
+    expected = torch.stack([kernel @ padded[t:t+T]
+                            for t in range(1, 1+T)], dim=-1)
+    assert actual.shape == expected.shape
+    assert torch.allclose(actual, expected), (actual, expected)
