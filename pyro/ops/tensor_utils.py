d0eed43eee498616a07113dace9826fe66bd25ae
Martin Jankowiak <jankowiak@uber.com>
2019-10-24 08:29:16 -0700

    tweaks

--- pyro/ops/tensor_utils.py
+++ pyro/ops/tensor_utils.py
@@ -3,11 +3,11 @@ import torch
 
 def block_diag(m):
     """
-    Takes a of shape (..., B, M, N) and returns a block diagonal tensor
+    Takes a tensor of shape (..., B, M, N) and returns a block diagonal tensor
     of shape (..., B x M, B x N).
 
-    :param torch.Tensor m: 3-dimensional input tensor
-    :returns torch.Tensor: a 2-dimensional block diagonal tensor
+    :param torch.Tensor m: an input tensor with 3 or more dimensions
+    :returns torch.Tensor: a block diagonal tensor with dimension `m.dim() - 1`
     """
     assert m.dim() > 2, "Input to block_diag() must be of dimension 3 or higher"
     B, M, N = m.shape[-3:]
