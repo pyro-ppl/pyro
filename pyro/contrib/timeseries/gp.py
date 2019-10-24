2119ad164e060ca6ebf05ab2541565c9e29b451f
Martin Jankowiak <jankowiak@uber.com>
2019-10-23 18:44:53 -0700

    add CoupledMaternGP

--- pyro/contrib/timeseries/gp.py
+++ pyro/contrib/timeseries/gp.py
@@ -5,6 +5,7 @@ from torch.distributions import MultivariateNormal
 import pyro.distributions as dist
 from pyro.ops.ssm_gp import MaternKernel
 from pyro.contrib.timeseries.base import TimeSeriesModel
+from pyro.ops.tensor_utils import block_diag
 
 
 class IndependentMaternGP(TimeSeriesModel):
@@ -13,15 +14,14 @@ class IndependentMaternGP(TimeSeriesModel):
     with a univariate Gaussian Process with a Matern kernel. The targets are assumed
     to be evenly spaced in time. Training and inference are logarithmic in the length
     of the time series T.
+
+    :param float nu: The order of the Matern kernel; either 1.5 or 2.5.
+    :param float dt: The time spacing between neighboring observations of the time series.
+    :param int obs_dim: The dimension of the targets at each time step.
     """
     def __init__(self, nu=1.5, dt=1.0, obs_dim=1,
                  log_length_scale_init=None, log_kernel_scale_init=None,
                  log_obs_noise_scale_init=None):
-        """
-        :param float nu: The order of the Matern kernel; either 1.5 or 2.5.
-        :param float dt: The time spacing between neighboring observations of the time series.
-        :param int obs_dim: The dimension of the targets at each time step.
-        """
         self.nu = nu
         self.dt = dt
         self.obs_dim = obs_dim
@@ -117,3 +117,138 @@ class IndependentMaternGP(TimeSeriesModel):
         filtering_state = self._filter(targets)
         predicted_mean, predicted_covar = self._forecast(dts, filtering_state)
         return torch.distributions.Normal(predicted_mean, predicted_covar.sqrt())
+
+
+class CoupledMaternGP(TimeSeriesModel):
+    """
+    A time series model in which each output dimension is modeled as a linear combination
+    of shared univariate Gaussian Processes with Matern kernels.
+
+    In more detail, the generative process is:
+
+        :math:`y_i(t) = \\sum_j A_{ij} f_j(t) + \\epsilon_i`
+
+    The targets :math:`y_i` are assumed to be evenly spaced in time. Training and inference
+    are logarithmic in the length of the time series T.
+
+    :param float nu: The order of the Matern kernel; either 1.5 or 2.5.
+    :param float dt: The time spacing between neighboring observations of the time series.
+    :param int obs_dim: The dimension of the targets at each time step.
+    :param int num_gps: The number of independent GPs that are mixed to model the time series.
+        Typical values might be :math:`\\N_{\\rm gp} \\in [\\D_{\\rm obs} / 2, \\D_{\\rm obs}]`
+    """
+    def __init__(self, nu=1.5, dt=1.0, obs_dim=2, num_gps=1,
+                 log_length_scale_init=None, log_kernel_scale_init=None,
+                 log_obs_noise_scale_init=None):
+        self.nu = nu
+        self.dt = dt
+        assert obs_dim > 1, "If obs_dim==1 you should use IndependentMaternGP"
+        self.obs_dim = obs_dim
+        self.num_gps = num_gps
+
+        if log_obs_noise_scale_init is None:
+            log_obs_noise_scale_init = -2.0 * torch.ones(obs_dim)
+        assert log_obs_noise_scale_init.shape == (obs_dim,)
+
+        self.dt = dt
+        self.obs_dim = obs_dim
+        self.num_gps = num_gps
+
+        super(CoupledMaternGP, self).__init__()
+
+        self.kernel = MaternKernel(nu=nu, num_gps=num_gps,
+                                   log_length_scale_init=log_length_scale_init,
+                                   log_kernel_scale_init=log_kernel_scale_init)
+        self.full_state_dim = num_gps * self.kernel.state_dim
+
+        self.log_obs_noise_scale = nn.Parameter(log_obs_noise_scale_init)
+        self.A = nn.Parameter(0.3 * torch.randn(self.num_gps, self.obs_dim))
+
+    def _get_obs_matrix(self):
+        # (num_gps, obs_dim) => (state_dim * num_gps, obs_dim)
+        return self.A.repeat_interleave(self.kernel.state_dim, dim=0) * \
+            torch.tensor([1.0] + [0.0] * (self.kernel.state_dim - 1),
+                         dtype=self.A.dtype, device=self.A.device).repeat(self.num_gps).unsqueeze(-1)
+
+    def _get_obs_noise_scale(self):
+        return self.log_obs_noise_scale.exp()
+
+    def _stationary_covariance(self):
+        return block_diag(self.kernel.stationary_covariance())
+
+    def _get_init_dist(self):
+        loc = torch.zeros(self.full_state_dim, device=self.A.device, dtype=self.A.dtype)
+        return MultivariateNormal(loc, self._stationary_covariance())
+
+    def _get_obs_dist(self):
+        loc = torch.zeros(self.obs_dim, device=self.A.device, dtype=self.A.dtype)
+        return dist.Normal(loc, self._get_obs_noise_scale()).to_event(1)
+
+    def _get_dist(self):
+        """
+        Get the `GaussianHMM` distribution that corresponds to a `CoupledMaternGP`.
+        """
+        trans_matrix, process_covar = self.kernel.transition_matrix_and_covariance(dt=self.dt)
+        trans_matrix = block_diag(trans_matrix)
+        process_covar = block_diag(process_covar)
+        loc = torch.zeros(self.full_state_dim, dtype=self.A.dtype, device=self.A.device)
+        trans_dist = MultivariateNormal(loc, process_covar)
+        return dist.GaussianHMM(self._get_init_dist(), trans_matrix, trans_dist,
+                                self._get_obs_matrix(), self._get_obs_dist())
+
+    def log_prob(self, targets):
+        """
+        :param torch.Tensor targets: A 2-dimensional tensor of real-valued targets
+            of shape `(T, obs_dim)`, where `T` is the length of the time series and `obs_dim`
+            is the dimension of the real-valued `targets` at each time step
+        :returns torch.Tensor: a (scalar) log probability
+        """
+        assert targets.dim() == 2 and targets.size(-1) == self.obs_dim
+        return self._get_dist().log_prob(targets)
+
+    def _filter(self, targets):
+        """
+        Return the filtering state for the associated state space model.
+        """
+        assert targets.dim() == 2 and targets.size(-1) == self.obs_dim
+        return self._get_dist().filter(targets)
+
+    def _forecast(self, dts, filtering_state, include_observation_noise=True, full_covar=True):
+        """
+        Internal helper for forecasting.
+        """
+        assert dts.dim() == 1
+        dts = dts.unsqueeze(-1).unsqueeze(-1).unsqueeze(-1)
+        trans_mat = self.kernel.transition_matrix(dts)
+        trans_mat = block_diag(trans_mat)
+        trans_obs = torch.matmul(trans_mat, self._get_obs_matrix())  # S x full_state_dim x O
+        predicted_mean = torch.matmul(filtering_state.loc.unsqueeze(-2), trans_obs).squeeze(-2)
+        predicted_function_covar = torch.matmul(trans_obs.transpose(-1, -2),
+                                                torch.matmul(filtering_state.covariance_matrix,
+                                                trans_obs))
+
+        if include_observation_noise:
+            eye = torch.eye(self.obs_dim, dtype=self.A.dtype, device=self.A.device)
+            obs_noise = self._get_obs_noise_scale().pow(2.0) * eye
+            predicted_function_covar = predicted_function_covar + obs_noise
+        if not full_covar:
+            predicted_function_covar = predicted_function_covar.diagonal(dim1=-1, dim2=-2)
+
+        return predicted_mean, predicted_function_covar
+
+    def forecast(self, targets, dts):
+        """
+        :param torch.Tensor targets: A 2-dimensional tensor of real-valued targets
+            of shape `(T, obs_dim)`, where `T` is the length of the time series and `obs_dim`
+            is the dimension of the real-valued targets at each time step. These
+            represent the training data that are conditioned on for the purpose of making
+            forecasts.
+        :param torch.Tensor dts: A 1-dimensional tensor of times to forecast into the future,
+            with zero corresponding to the time of the final target `targets[-1]`.
+        :returns torch.distributions.MultivariateNormal: Returns a predictive MultivariateNormal
+            distribution with batch shape `(S,)` and event shape `(obs_dim,)`,
+            where `S` is the size of `dts`.
+        """
+        filtering_state = self._filter(targets)
+        predicted_mean, predicted_covar = self._forecast(dts, filtering_state)
+        return MultivariateNormal(predicted_mean, predicted_covar)
--- tests/contrib/timeseries/test_gp.py
+++ tests/contrib/timeseries/test_gp.py
@@ -2,45 +2,63 @@ import torch
 
 from tests.common import assert_equal
 import pyro
-from pyro.contrib.timeseries import IndependentMaternGP
+from pyro.contrib.timeseries import IndependentMaternGP, CoupledMaternGP
 import pytest
 
 
+@pytest.mark.parametrize('model', ['cmgp', 'imgp'])
 @pytest.mark.parametrize('nu', [1.5, 2.5])
 @pytest.mark.parametrize('obs_dim', [1, 3])
 @pytest.mark.parametrize('T', [11, 37])
-def test_independent_matern_gp(nu, obs_dim, T):
+def test_independent_matern_gp(model, nu, obs_dim, T):
     torch.set_default_tensor_type('torch.DoubleTensor')
     dt = 0.1 + torch.rand(1).item()
 
-    gp = IndependentMaternGP(nu=nu, obs_dim=obs_dim, dt=dt,
-                             log_length_scale_init=torch.randn(obs_dim),
-                             log_kernel_scale_init=torch.randn(obs_dim),
+    if model=='cmgp':
+        if obs_dim==1:
+            return
+        num_gps = 2
+        gp = CoupledMaternGP(nu=nu, obs_dim=obs_dim, dt=dt, num_gps=num_gps,
+                             log_length_scale_init=torch.randn(num_gps),
+                             log_kernel_scale_init=torch.randn(num_gps),
                              log_obs_noise_scale_init=torch.randn(obs_dim))
+    elif model=='imgp':
+        gp = IndependentMaternGP(nu=nu, obs_dim=obs_dim, dt=dt,
+                                 log_length_scale_init=torch.randn(obs_dim),
+                                 log_kernel_scale_init=torch.randn(obs_dim),
+                                 log_obs_noise_scale_init=torch.randn(obs_dim))
+
     targets = torch.randn(T, obs_dim)
     gp_log_prob = gp.log_prob(targets)
-    assert gp_log_prob.shape == (obs_dim,)
+    if model=='imgp':
+        assert gp_log_prob.shape == (obs_dim,)
+    else:
+        assert gp_log_prob.dim() == 0
 
     times = dt * torch.arange(T).double()
 
-    for dim in range(obs_dim):
-        lengthscale = gp.kernel.log_length_scale.exp()[dim]
-        variance = (2.0 * gp.kernel.log_kernel_scale).exp()[dim]
-        obs_noise = (2.0 * gp.log_obs_noise_scale).exp()[dim]
+    if model=='imgp':
+        for dim in range(obs_dim):
+            lengthscale = gp.kernel.log_length_scale.exp()[dim]
+            variance = (2.0 * gp.kernel.log_kernel_scale).exp()[dim]
+            obs_noise = (2.0 * gp.log_obs_noise_scale).exp()[dim]
 
-        kernel = pyro.contrib.gp.kernels.Matern32 if nu == 1.5 else pyro.contrib.gp.kernels.Matern52
-        kernel = kernel(input_dim=1, lengthscale=lengthscale, variance=variance)
-        kernel = kernel(times) + obs_noise * torch.eye(T)
+            kernel = pyro.contrib.gp.kernels.Matern32 if nu == 1.5 else pyro.contrib.gp.kernels.Matern52
+            kernel = kernel(input_dim=1, lengthscale=lengthscale, variance=variance)
+            kernel = kernel(times) + obs_noise * torch.eye(T)
 
-        mvn = torch.distributions.MultivariateNormal(torch.zeros(T), kernel)
-        mvn_log_prob = mvn.log_prob(targets[:, dim])
-        assert_equal(mvn_log_prob, gp_log_prob[dim])
+            mvn = torch.distributions.MultivariateNormal(torch.zeros(T), kernel)
+            mvn_log_prob = mvn.log_prob(targets[:, dim])
+            assert_equal(mvn_log_prob, gp_log_prob[dim])
 
     for S in [1, 3]:
         dts = torch.rand(S)
         predictive = gp.forecast(targets, dts)
         assert predictive.loc.shape == (S, obs_dim)
-        assert predictive.scale.shape == (S, obs_dim)
+        if model=='imgp':
+            assert predictive.scale.shape == (S, obs_dim)
+        else:
+            assert predictive.covariance_matrix.shape == (S, obs_dim, obs_dim)
 
     # the distant future
     dts = torch.tensor([500.0])
@@ -48,5 +66,10 @@ def test_independent_matern_gp(nu, obs_dim, T):
     # assert mean reverting
     assert_equal(predictive.loc, torch.zeros(1, obs_dim))
     # assert large time covariance
-    expected_scale = gp._get_obs_noise_scale().unsqueeze(0)
-    assert_equal(predictive.scale, expected_scale)
+    if model=='imgp':
+        expected_scale = gp._get_obs_noise_scale().unsqueeze(0)
+        assert_equal(predictive.scale, expected_scale)
+    else:
+        expected_cov = torch.eye(obs_dim) * gp._get_obs_noise_scale().pow(2.0)
+        expected_cov = expected_cov.unsqueeze(0)
+        assert_equal(predictive.covariance_matrix, expected_cov)
