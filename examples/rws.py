d10f1334f582795be2c642e6b4f20bc5d01813ee
Tuan-Anh Le <tuananh@robots.ox.ac.uk>
2019-10-25 11:45:21 -0400

    fix true q_loc and q_scale

--- examples/rws.py
+++ examples/rws.py
@@ -10,19 +10,25 @@ import pyro.distributions as dist
 pyro.set_rng_seed(101)
 
 
-def scale(guess):
-    weight = pyro.sample('weight', dist.Normal(guess, 1.))
-    return pyro.sample('measurement', dist.Normal(weight, 1.), obs=9.5)
+def scale(guess, guess_scale, obs_scale, obs):
+    weight = pyro.sample('weight', dist.Normal(guess, guess_scale))
+    return pyro.sample('measurement', dist.Normal(weight, obs_scale), obs=obs)
 
 
-def scale_parametrized_guide(guess):
-    loc = pyro.param('loc', torch.tensor(guess))
-    log_scale = pyro.param('log_scale', torch.tensor(1.))
+def scale_parametrized_guide(guess, guess_scale, obs_scale, obs):
+    loc = pyro.param('loc', torch.tensor(0.))
+    log_scale = pyro.param('log_scale', torch.tensor(0.))
     return pyro.sample('weight', dist.Normal(loc, torch.exp(log_scale)))
 
 
 if __name__ == '__main__':
     guess = 8.5
+    guess_scale = 1
+    obs_scale = 0.75
+    obs = 9.5
+    true_q_scale = np.sqrt(1 / (1 / guess_scale**2 + 1 / obs_scale**2))
+    true_q_loc = true_q_scale**2 * (guess / guess_scale**2 + obs / obs_scale**2)
+
     num_particles = 7
 
     pyro.clear_param_store()
@@ -32,13 +38,16 @@ if __name__ == '__main__':
                          loss=pyro.infer.ReweightedWakeSleep(num_particles=num_particles))
 
     theta_losses, phi_losses, q_loc, q_log_scale = [], [], [], []
-    num_steps = 2500
+    num_steps = 10000
     for t in range(num_steps):
-        theta_loss, phi_loss = svi.step(guess)
+        theta_loss, phi_loss = svi.step(guess, guess_scale, obs_scale, obs)
         theta_losses.append(theta_loss)
         phi_losses.append(phi_loss)
         q_loc.append(pyro.param('loc').item())
         q_log_scale.append(pyro.param('log_scale').item())
+        if t % 100 == 0:
+            print('Iteration {}: theta loss = {}, phi loss = {}'.format(
+                t, theta_loss, phi_loss))
 
     fig, axs = plt.subplots(4, 1, dpi=200, sharex=True)
     axs[0].plot(theta_losses)
@@ -47,9 +56,9 @@ if __name__ == '__main__':
     axs[1].set_ylabel(r'$\phi$ loss')
     axs[2].plot(q_loc)
     axs[2].set_ylabel('q loc')
-    axs[2].axhline(9.14, color='black')
+    axs[2].axhline(true_q_loc, color='black')
     axs[3].plot(np.exp(q_log_scale))
     axs[3].set_ylabel('q scale')
-    axs[3].axhline(0.6, color='black')
+    axs[3].axhline(true_q_scale, color='black')
     fig.tight_layout()
     plt.show()
