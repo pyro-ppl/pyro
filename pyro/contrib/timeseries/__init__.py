0283dae8f35c9eacbec4a3f25ab7d0f890647ac7
Martin Jankowiak <jankowiak@uber.com>
2019-10-26 17:23:22 -0700

    tests pass

--- pyro/contrib/timeseries/__init__.py
+++ pyro/contrib/timeseries/__init__.py
@@ -2,7 +2,8 @@
 The :mod:`pyro.contrib.timeseries` module provides a collection of Bayesian time series
 models useful for forecasting applications.
 """
-from pyro.contrib.timeseries.gp import IndependentMaternGP, LinearlyCoupledMaternGP, GenericLGSSM
+from pyro.contrib.timeseries.gp import IndependentMaternGP, LinearlyCoupledMaternGP
+from pyro.contrib.timeseries.lgssm import GenericLGSSM
 
 __all__ = [
     "GenericLGSSM",
--- pyro/contrib/timeseries/lgssm.py
+++ pyro/contrib/timeseries/lgssm.py
@@ -4,19 +4,21 @@ from torch.distributions import MultivariateNormal
 
 import pyro.distributions as dist
 from pyro.contrib.timeseries.base import TimeSeriesModel
+from pyro.ops.tensor_utils import repeated_matmul
 
 
 class GenericLGSSM(TimeSeriesModel):
     """
-    A generic Linear Gaussian State Space Model parameterized with arbitrary transition
-    and observation dynamics. The targets are (implicitly) assumed to be evenly spaced in
-    time. Training and inference are logarithmic in the length of the time series T.
+    A generic Linear Gaussian State Space Model parameterized with arbitrary time invariant
+    transition and observation dynamics. The targets are (implicitly) assumed to be evenly
+    spaced in time. Training and inference are logarithmic in the length of the time series T.
 
-    :param int state_dim: The dimension of latent state at each time step.
     :param int obs_dim: The dimension of the targets at each time step.
+    :param int state_dim: The dimension of latent state at each time step.
     """
-    def __init__(self, obs_dim=1, log_obs_noise_scale_init=None):
+    def __init__(self, obs_dim=1, state_dim=2, log_obs_noise_scale_init=None):
         self.obs_dim = obs_dim
+        self.state_dim = state_dim
 
         if log_obs_noise_scale_init is None:
             log_obs_noise_scale_init = -2.0 * torch.ones(obs_dim)
@@ -34,18 +36,18 @@ class GenericLGSSM(TimeSeriesModel):
         return self.log_obs_noise_scale.exp()
 
     def _get_init_dist(self):
-        loc = torch.zeros(self.state_dim, device=self.obs_matrix.device, dtype=self.obs_matrix.device)
-        eye = torch.eye(self.state_dim, device=self.obs_matrix.device, dtype=self.obs_matrix.device)
+        loc = self.obs_matrix.new_zeros(self.state_dim)
+        eye = torch.eye(self.state_dim, device=self.obs_matrix.device, dtype=self.obs_matrix.dtype)
         return MultivariateNormal(loc, self.log_trans_noise_scale_sq.exp() * eye)
 
     def _get_obs_dist(self):
-        loc = torch.zeros(self.obs_dim, device=self.obs_matrix.device, dtype=self.obs_matrix.device)
+        loc = self.obs_matrix.new_zeros(self.obs_dim)
         return dist.Normal(loc, self._get_obs_noise_scale()).to_event(1)
 
     def _get_trans_dist(self):
-        loc = torch.zeros(self.state_dim, device=self.obs_matrix.device, dtype=self.obs_matrix.device)
-        eye = torch.eye(self.state_dim, device=self.obs_matrix.device, dtype=self.obs_matrix.device)
-        return MultivariateNormal(loc, self.log_trans_noise_scale.exp() * eye)
+        loc = self.obs_matrix.new_zeros(self.state_dim)
+        eye = torch.eye(self.state_dim, device=self.obs_matrix.device, dtype=self.obs_matrix.dtype)
+        return MultivariateNormal(loc, self.log_trans_noise_scale_sq.exp() * eye)
 
     def _get_dist(self):
         """
@@ -71,33 +73,46 @@ class GenericLGSSM(TimeSeriesModel):
         assert targets.dim() == 2 and targets.size(-1) == self.obs_dim
         return self._get_dist().filter(targets)
 
-    def _forecast(self, dts, filtering_state, include_observation_noise=True):
+    def _forecast(self, N_timesteps, filtering_state, include_observation_noise=True):
         """
         Internal helper for forecasting.
         """
-        assert dts.dim() == 1
-        dts = dts.unsqueeze(-1).unsqueeze(-1).unsqueeze(-1)
-        predicted_mean = torch.matmul(filtering_state.loc.unsqueeze(-2), trans_matrix).squeeze(-2)[..., 0]
-        predicted_function_covar = torch.matmul(trans_matrix.transpose(-1, -2),
-                                                torch.matmul(filtering_state.covariance_matrix,
-                                                trans_matrix))[..., 0, 0] + process_covar[..., 0, 0]
+        N_trans_matrix = repeated_matmul(self.trans_matrix, N_timesteps)
+        N_trans_obs = torch.matmul(N_trans_matrix, self.obs_matrix)
+        predicted_mean = torch.matmul(filtering_state.loc.unsqueeze(-2), N_trans_obs).squeeze(-2)
+
+        # first compute the contribution from filtering_state.covariance_matrix
+        predicted_covar1 = torch.matmul(N_trans_obs.transpose(-1, -2),
+                                        torch.matmul(filtering_state.covariance_matrix,
+                                        N_trans_obs))  # N O O
+
+        # next compute the contribution from process noise that is injected at each timestep.
+        # (we need to do a cumulative sum to integrate across time)
+        process_covar = self._get_trans_dist().covariance_matrix
+        N_trans_obs_shift = torch.cat([self.obs_matrix.unsqueeze(0), N_trans_obs[0:-1]])
+        predicted_covar2 = torch.matmul(N_trans_obs_shift.transpose(-1, -2),
+                                        torch.matmul(process_covar, N_trans_obs_shift))  # N O O
+
+        predicted_covar = predicted_covar1 + torch.cumsum(predicted_covar2, dim=0)
 
         if include_observation_noise:
-            predicted_function_covar = predicted_function_covar + self._get_obs_noise_scale().pow(2.0)
-        return predicted_mean, predicted_function_covar
+            eye = torch.eye(self.obs_dim, device=self.obs_matrix.device, dtype=self.obs_matrix.dtype)
+            predicted_covar = predicted_covar + self._get_obs_noise_scale().pow(2.0) * eye
 
-    def forecast(self, targets, dts):
+        return predicted_mean, predicted_covar
+
+    def forecast(self, targets, N_timesteps):
         """
         :param torch.Tensor targets: A 2-dimensional tensor of real-valued targets
             of shape `(T, obs_dim)`, where `T` is the length of the time series and `obs_dim`
             is the dimension of the real-valued targets at each time step. These
             represent the training data that are conditioned on for the purpose of making
             forecasts.
-        :param torch.Tensor dts: A 1-dimensional tensor of times to forecast into the future,
-            with zero corresponding to the time of the final target `targets[-1]`.
-        :returns torch.distributions.Normal: Returns a predictive Normal distribution with batch shape `(S,)` and
-            event shape `(obs_dim,)`, where `S` is the size of `dts`.
+        :param int N_timesteps: The number of timesteps to forecast into the future from
+            the final target `targets[-1]`.
+        :returns torch.distributions.MultivariateNormal: Returns a predictive MultivariateNormal distribution
+            with batch shape `(N_timesteps,)` and event shape `(obs_dim,)`
         """
         filtering_state = self._filter(targets)
-        predicted_mean, predicted_covar = self._forecast(dts, filtering_state)
-        return torch.distributions.Normal(predicted_mean, predicted_covar.sqrt())
+        predicted_mean, predicted_covar = self._forecast(N_timesteps, filtering_state)
+        return torch.distributions.MultivariateNormal(predicted_mean, predicted_covar)
