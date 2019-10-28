e37a4fbd29785caa9f2923d2752b72ae19d3774b
Alex Campbell <alexander.jr.campbell@gmail.com>
2019-10-28 15:29:33 +0000

    Add distribution to init

--- pyro/distributions/__init__.py
+++ pyro/distributions/__init__.py
@@ -19,6 +19,7 @@ from pyro.distributions.torch import *  # noqa F403
 from pyro.distributions.torch import __all__ as torch_dists
 from pyro.distributions.torch_distribution import TorchDistribution
 from pyro.distributions.torch_transform import TransformModule
+from pyro.distributions.triangular import Triangular
 from pyro.distributions.util import enable_validation, is_validation_enabled, validation_enabled
 from pyro.distributions.von_mises import VonMises
 from pyro.distributions.von_mises_3d import VonMises3D
@@ -52,6 +53,7 @@ __all__ = [
     "SpanningTree",
     "TorchDistribution",
     "TransformModule",
+    "Triangular",
     "VonMises",
     "VonMises3D",
     "ZeroInflatedPoisson",
