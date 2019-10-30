fd821299d212e17e68ea52f99fe2c56904f54b7d
Fritz Obermeyer <fritzo@uber.com>
2019-10-30 09:46:39 -0700

    Reorganize docs in pyro.contrib.timeseries

--- pyro/contrib/timeseries/__init__.py
+++ pyro/contrib/timeseries/__init__.py
@@ -2,11 +2,13 @@
 The :mod:`pyro.contrib.timeseries` module provides a collection of Bayesian time series
 models useful for forecasting applications.
 """
+from pyro.contrib.timeseries.base import TimeSeriesModel
 from pyro.contrib.timeseries.gp import IndependentMaternGP, LinearlyCoupledMaternGP
 from pyro.contrib.timeseries.lgssm import GenericLGSSM
 
 __all__ = [
     "GenericLGSSM",
-    "LinearlyCoupledMaternGP",
     "IndependentMaternGP",
+    "LinearlyCoupledMaternGP",
+    "TimeSeriesModel",
 ]
