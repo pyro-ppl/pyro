06f98782ead0262a5f8d83e439dd469b8238d6b6
martinjankowiak <martinjankowiak@users.noreply.github.com>
2019-11-04 16:38:03 -0800

    clean-up docs in pyro.contrib.timeseries (#2117)

--- pyro/contrib/timeseries/base.py
+++ pyro/contrib/timeseries/base.py
@@ -8,8 +8,8 @@ class TimeSeriesModel(nn.Module):
     def log_prob(self, targets):
         """
         :param torch.Tensor targets: A 2-dimensional tensor of real-valued targets
-            of shape `(T, obs_dim)`, where `T` is the length of the time series and `obs_dim`
-            is the dimension of the real-valued `targets` at each time step
+            of shape ``(T, obs_dim)``, where ``T`` is the length of the time series and ``obs_dim``
+            is the dimension of the real-valued ``targets`` at each time step
         :returns torch.Tensor: A 0-dimensional log probability for the case of properly
             multivariate time series models in which the output dimensions are correlated;
             otherwise returns a 1-dimensional tensor of log probabilities for batched
@@ -20,14 +20,14 @@ class TimeSeriesModel(nn.Module):
     def forecast(self, targets, dts):
         """
         :param torch.Tensor targets: A 2-dimensional tensor of real-valued targets
-            of shape `(T, obs_dim)`, where `T` is the length of the time series and `obs_dim`
+            of shape ``(T, obs_dim)``, where ``T`` is the length of the time series and ``obs_dim``
             is the dimension of the real-valued targets at each time step. These
             represent the training data that are conditioned on for the purpose of making
             forecasts.
         :param torch.Tensor dts: A 1-dimensional tensor of times to forecast into the future,
-            with zero corresponding to the time of the final target `targets[-1]`.
-        :returns torch.distributions.Distribution: Returns a predictive distribution with batch shape `(S,)` and
-            event shape `(obs_dim,)`, where `S` is the size of `dts`. That is, the resulting
-            predictive distributions do not encode correlations between distinct times in `dts`.
+            with zero corresponding to the time of the final target ``targets[-1]``.
+        :returns torch.distributions.Distribution: Returns a predictive distribution with batch shape ``(S,)`` and
+            event shape ``(obs_dim,)``, where ``S`` is the size of ``dts``. That is, the resulting
+            predictive distributions do not encode correlations between distinct times in ``dts``.
         """
         raise NotImplementedError
