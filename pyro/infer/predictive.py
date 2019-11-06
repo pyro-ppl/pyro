e5073c19e6ea7370c609771f222fb8bac8117745
Du Phan <fehiepsi@gmail.com>
2019-11-06 13:08:59 -0500

    Fix preditive shapes (#2123)

--- pyro/infer/predictive.py
+++ pyro/infer/predictive.py
@@ -28,7 +28,7 @@ def _guess_max_plate_nesting(model, args, kwargs):
 
 
 def _predictive_sequential(model, posterior_samples, model_args, model_kwargs,
-                           num_samples, sample_sites, return_trace=False):
+                           num_samples, return_site_shapes, return_trace=False):
     collected = []
     samples = [{k: v[i] for k, v in posterior_samples.items()} for i in range(num_samples)]
     for i in range(num_samples):
@@ -36,10 +36,13 @@ def _predictive_sequential(model, posterior_samples, model_args, model_kwargs,
         if return_trace:
             collected.append(trace)
         else:
-            collected.append({site: trace.nodes[site]['value'] for site in sample_sites})
+            collected.append({site: trace.nodes[site]['value'] for site in return_site_shapes})
 
-    return collected if return_trace else {site: torch.stack([s[site] for s in collected])
-                                           for site in sample_sites}
+    if return_trace:
+        return collected
+    else:
+        return {site: torch.stack([s[site] for s in collected]).reshape(shape)
+                for site, shape in return_site_shapes.items()}
 
 
 def _predictive(model, posterior_samples, num_samples, return_sites=None,
@@ -61,7 +64,8 @@ def _predictive(model, posterior_samples, num_samples, return_sites=None,
 
     return_site_shapes = {}
     for site in model_trace.stochastic_nodes + model_trace.observation_nodes:
-        site_shape = (num_samples,) + model_trace.nodes[site]['value'].shape
+        append_ndim = max_plate_nesting - len(model_trace.nodes[site]["fn"].batch_shape)
+        site_shape = (num_samples,) + (1,) * append_ndim + model_trace.nodes[site]['value'].shape
         if isinstance(return_sites, (list, tuple, set)):
             if site in return_sites:
                 return_site_shapes[site] = site_shape
@@ -77,7 +81,7 @@ def _predictive(model, posterior_samples, num_samples, return_sites=None,
 
     if not parallel:
         return _predictive_sequential(model, posterior_samples, model_args, model_kwargs, num_samples,
-                                      return_site_shapes.keys(), return_trace=False)
+                                      return_site_shapes, return_trace=False)
 
     trace = poutine.trace(poutine.condition(vectorize(model), reshaped_samples))\
         .get_trace(*model_args, **model_kwargs)
