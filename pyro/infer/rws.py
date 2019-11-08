c5bbcb72a9fab785febc931e43f5b7ba6395d5c9
Siddharth N <siddharth@iffsid.com>
2019-11-08 16:34:56 +0000

    rws: refactor
     - abstract out `_loss' and derive `loss' and `loss_and_grads' from it
     - unify naming of 'particles'

--- pyro/infer/rws.py
+++ pyro/infer/rws.py
@@ -26,7 +26,7 @@ class ReweightedWakeSleep(ELBO):
     :param num_particles: The number of particles/samples used to form the objective
         (gradient) estimator. Default is 2.
     :param insomnia: The scaling between the wake-phi and sleep-phi terms. Default is 1.0 [wake-phi]
-    :param num_sleep_samples: The number of particles/samples used to form the sleep-phi estimator.
+    :param num_sleep_particles: The number of particles used to form the sleep-phi estimator.
         Default is 1 [matching the batch size].
     :param int max_plate_nesting: Bound on max number of nested
         :func:`pyro.plate` contexts. Default is infinity.
@@ -47,7 +47,7 @@ class ReweightedWakeSleep(ELBO):
     def __init__(self,
                  num_particles=2,
                  insomnia=1.,
-                 num_sleep_samples=1,
+                 num_sleep_particles=1,
                  max_plate_nesting=float('inf'),
                  max_iarange_nesting=None,  # DEPRECATED
                  vectorize_particles=True,
@@ -66,7 +66,7 @@ class ReweightedWakeSleep(ELBO):
                                                   vectorize_particles=vectorize_particles,
                                                   strict_enumeration_warning=strict_enumeration_warning)
         self.insomnia = insomnia
-        self.num_sleep_samples = num_sleep_samples
+        self.num_sleep_particles = num_sleep_particles
         assert(insomnia >= 0 and insomnia <= 1), \
             "insomnia should be in [0, 1]"
 
@@ -80,58 +80,14 @@ class ReweightedWakeSleep(ELBO):
             check_if_enumerated(guide_trace)
         return model_trace, guide_trace
 
-    def loss(self, model, guide, *args, **kwargs):
-        """
-        :returns: returns an estimate of the ELBO
-        :rtype: float
-
-        Evaluates RWS with estimators that uses num_particles many samples/particles.
-        """
-        log_joints = []
-        log_qs = []
-
-        # grab a vectorized trace from the generator
-        for model_trace, guide_trace in self._get_traces(model, guide, *args, **kwargs):
-            log_joint = 0.
-            log_q = 0.
-
-            # compute log weights
-            for _, site in model_trace.nodes.items():
-                if site["type"] == "sample":
-                    log_p_site = site["log_prob"].detach()
-                    log_joint = log_joint + log_p_site
-
-            for name, site in guide_trace.nodes.items():
-                if site["type"] == "sample":
-                    log_q_site = site["log_prob"].detach()
-                    log_q = log_q + log_q_site
-
-            log_joints.append(log_joint)
-            log_qs.append(log_q)
-
-        log_joints = log_joints[0] if self.vectorize_particles else torch.stack(log_joints)
-        log_qs = log_qs[0] if self.vectorize_particles else torch.stack(log_qs)
-        log_weights = log_joints - log_qs
-
-        # wake theta = iwae:
-        log_sum_weight = torch.logsumexp(log_weights, dim=0)
-        wake_theta_loss = -(log_sum_weight - math.log(self.num_particles)).sum()
-
-        # wake phi = reweighted csis:
-        normalised_weights = (log_weights - log_sum_weight).exp()
-        wake_phi_loss = -(normalised_weights * log_qs).sum(0).sum(0)
-
-        warn_if_nan(wake_theta_loss, "loss")
-        warn_if_nan(wake_phi_loss, "loss")
-        return wake_theta_loss, wake_phi_loss
-
-    def loss_and_grads(self, model, guide, *args, **kwargs):
+    def _loss(self, model, guide, *args, **kwargs):
         """
         :returns: returns model loss and guide loss
-        :rtype: float
+        :rtype: float, float
 
-        Computes the RWS estimators for the model (wake-theta) and the guide (wake-phi).
-        Performs backward as appropriate on both, using num_particle many samples/particles.
+        Computes the re-weighted wake-sleep estimators for the model (wake-theta) and the
+          guide (insomnia * wake-phi + (1 - insomnia) * sleep-phi).
+        Performs backward as appropriate on both, over the specified number of particles.
         """
         log_joints = []
         log_qs = []
@@ -140,7 +96,6 @@ class ReweightedWakeSleep(ELBO):
             log_joint = 0.
             log_q = 0.
 
-            # compute log_weight and log_q
             for name, site in model_trace.nodes.items():
                 if site["type"] == "sample":
                     log_p_site = site["log_prob"]
@@ -160,36 +115,37 @@ class ReweightedWakeSleep(ELBO):
 
         # wake theta = iwae:
         log_sum_weight = torch.logsumexp(log_weights, dim=0)
-        # TODO: check whether grad est should sum or mean over batch dim (currently sum)
+        # TODO: check whether sum or mean over batch dim (currently sum)
         wake_theta_loss = -(log_sum_weight - math.log(self.num_particles)).sum()
-        wake_theta_loss.backward(retain_graph=True)
         warn_if_nan(wake_theta_loss, "wake theta loss")
 
         if self.insomnia > 0:
             # wake phi = reweighted csis:
             normalised_weights = (log_weights - log_sum_weight).exp().detach()
-            # TODO: check whether grad est should sum or mean over batch dim (currently sum)
+            # TODO: check whether sum or mean over batch dim (currently sum)
             wake_phi_loss = -(normalised_weights * log_qs).sum(0).sum(0)
             warn_if_nan(wake_phi_loss, "wake phi loss")
 
         if self.insomnia < 1:
+            # sleep phi = csis
             _model = pyro.poutine.uncondition(model)
             _guide = guide
             _log_q = 0.
-            if self.vectorize_particles:
+            should_vectorize = self.vectorize_particles and self.num_sleep_particles > 1
+            if should_vectorize:
                 old_num_particles = self.num_particles
-                self.num_particles = self.num_sleep_samples
+                self.num_particles = self.num_sleep_particles
                 _model = self._vectorized_num_particles(_model)
                 _guide = self._vectorized_num_particles(guide)
-            for _ in range(1 if self.vectorize_particles else self.num_sleep_samples):
+            for _ in range(1 if should_vectorize else self.num_sleep_particles):
                 _model_trace = poutine.trace(_model).get_trace(*args, **kwargs)
                 for site in _model_trace.nodes.values():
                     if site["type"] == "sample":
                         site["value"] = site["value"].detach()
                 _guide_trace = self._get_matched_trace(_model_trace, _guide, *args, **kwargs)
-                # TODO: mean is probably the more sensible thing here
+                # TODO: check whether sum or mean over batch dim (currently sum)
                 _log_q += _guide_trace.log_prob_sum()
-            if self.vectorize_particles:
+            if should_vectorize:
                 self.num_particles = old_num_particles
 
             sleep_phi_loss = -_log_q
@@ -198,6 +154,35 @@ class ReweightedWakeSleep(ELBO):
         phi_loss = sleep_phi_loss if self.insomnia == 0 \
             else wake_phi_loss if self.insomnia == 1 \
             else self.insomnia * wake_phi_loss + (1. - self.insomnia) * sleep_phi_loss
+
+        return wake_theta_loss, phi_loss
+
+    def loss(self, model, guide, *args, **kwargs):
+        """
+        :returns: returns model loss and guide loss
+        :rtype: float, float
+
+        Computes the re-weighted wake-sleep estimators for the model (wake-theta) and the
+          guide (insomnia * wake-phi + (1 - insomnia) * sleep-phi).
+        """
+        with torch.no_grad():
+            wake_theta_loss, phi_loss = self._loss(model, guide, *args, **kwargs)
+
+        return wake_theta_loss, phi_loss
+
+    def loss_and_grads(self, model, guide, *args, **kwargs):
+        """
+        :returns: returns model loss and guide loss
+        :rtype: float
+
+        Computes the RWS estimators for the model (wake-theta) and the guide (wake-phi).
+        Performs backward as appropriate on both, using num_particle many samples/particles.
+        """
+        wake_theta_loss, phi_loss = self._loss(model, guide, *args, **kwargs)
+        try:
+            wake_theta_loss.backward(retain_graph=True)
+        except RuntimeError:
+            pass
         phi_loss.backward()
 
         return wake_theta_loss.detach(), phi_loss.detach()
