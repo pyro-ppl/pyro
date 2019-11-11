e0f5c6901178d95a6a19f8fd3db5d8279b446ab1
Fritz Obermeyer <fritzo@uber.com>
2019-11-11 11:57:34 -0800

    Remove obsolete pyro.params.constrained_property (#2136)

--- pyro/params/__init__.py
+++ pyro/params/__init__.py
@@ -1,10 +1,6 @@
-from .constrained_parameter import ConstrainedModule, ConstrainedParameter, constraint
 from .param_store import module_from_param_with_module_name, param_with_module_name, user_param_name
 
 __all__ = [
-    "ConstrainedModule",
-    "ConstrainedParameter",
-    "constraint",
     "module_from_param_with_module_name",
     "param_with_module_name",
     "user_param_name",
