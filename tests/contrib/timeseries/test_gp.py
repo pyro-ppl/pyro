e3ba2ab026f861e2913f0eac7c9551244ceaf8cc
Martin Jankowiak <jankowiak@uber.com>
2019-11-12 10:27:18 -0800

    make most tests pass

--- tests/contrib/timeseries/test_gp.py
+++ tests/contrib/timeseries/test_gp.py
@@ -25,27 +25,27 @@ def test_timeseries_models(model, nu_statedim, obs_dim, T):
     if model == 'lcmgp':
         num_gps = 2
         gp = LinearlyCoupledMaternGP(nu=nu_statedim, obs_dim=obs_dim, dt=dt, num_gps=num_gps,
-                                     log_length_scale_init=torch.randn(num_gps),
-                                     log_kernel_scale_init=torch.randn(num_gps),
-                                     log_obs_noise_scale_init=torch.randn(obs_dim))
+                                     length_scale_init=0.1 + torch.rand(num_gps),
+                                     kernel_scale_init=0.1 + torch.rand(num_gps),
+                                     obs_noise_scale_init=0.1 + torch.rand(obs_dim))
     elif model == 'imgp':
         gp = IndependentMaternGP(nu=nu_statedim, obs_dim=obs_dim, dt=dt,
-                                 log_length_scale_init=torch.randn(obs_dim),
-                                 log_kernel_scale_init=torch.randn(obs_dim),
-                                 log_obs_noise_scale_init=torch.randn(obs_dim))
+                                 length_scale_init=0.1 + torch.rand(obs_dim),
+                                 kernel_scale_init=0.1 + torch.rand(obs_dim),
+                                 obs_noise_scale_init=0.1 + torch.rand(obs_dim))
     elif model == 'glgssm':
         gp = GenericLGSSM(state_dim=nu_statedim, obs_dim=obs_dim,
-                          log_obs_noise_scale_init=torch.randn(obs_dim))
+                          obs_noise_scale_init=0.1 + torch.rand(obs_dim))
     elif model == 'ssmgp':
         state_dim = {0.5: 4, 1.5: 3, 2.5: 2}[nu_statedim]
         gp = GenericLGSSMWithGPNoiseModel(nu=nu_statedim, state_dim=state_dim, obs_dim=obs_dim,
-                                          log_obs_noise_scale_init=torch.randn(obs_dim))
+                                          obs_noise_scale_init=0.1 + torch.rand(obs_dim))
     elif model == 'dmgp':
         gp = DependentMaternGP(nu=nu_statedim, obs_dim=obs_dim, dt=dt,
-                               log_length_scale_init=torch.randn(obs_dim))
+                               length_scale_init=0.1 + torch.rand(obs_dim))
     elif model == 'lcdgp':
         gp = LinearlyCoupledDependentMaternGP(nu=nu_statedim, obs_dim=obs_dim, dt=dt,
-                                              log_length_scale_init=torch.randn(obs_dim))
+                                              length_scale_init=0.1 + torch.rand(obs_dim))
 
     targets = torch.randn(T, obs_dim)
     gp_log_prob = gp.log_prob(targets)
@@ -58,9 +58,9 @@ def test_timeseries_models(model, nu_statedim, obs_dim, T):
     if model == 'imgp':
         times = dt * torch.arange(T).double()
         for dim in range(obs_dim):
-            lengthscale = gp.kernel.log_length_scale.exp()[dim]
-            variance = (2.0 * gp.kernel.log_kernel_scale).exp()[dim]
-            obs_noise = (2.0 * gp.log_obs_noise_scale).exp()[dim]
+            lengthscale = gp.kernel.length_scale[dim]
+            variance = gp.kernel.kernel_scale.pow(2)[dim]
+            obs_noise = gp.obs_noise_scale.pow(2)[dim]
 
             kernel = {0.5: pyro.contrib.gp.kernels.Exponential,
                       1.5: pyro.contrib.gp.kernels.Matern32,
@@ -105,11 +105,11 @@ def test_timeseries_models(model, nu_statedim, obs_dim, T):
 def test_dependent_matern_gp(obs_dim):
     dt = 0.5 + torch.rand(1).item()
     gp = DependentMaternGP(nu=1.5, obs_dim=obs_dim, dt=dt,
-                           log_length_scale_init=torch.randn(obs_dim))
+                           length_scale_init=0.1 + torch.rand(obs_dim))
 
     # make sure stationary covariance matrix satisfies the relevant
     # matrix riccati equation
-    lengthscale = gp.kernel.log_length_scale.exp().unsqueeze(-1).unsqueeze(-1)
+    lengthscale = gp.kernel.length_scale.unsqueeze(-1).unsqueeze(-1)
     F = torch.tensor([[0.0, 1.0], [0.0, 0.0]])
     mask1 = torch.tensor([[0.0, 0.0], [-3.0, 0.0]])
     mask2 = torch.tensor([[0.0, 0.0], [0.0, -math.sqrt(12.0)]])
