f99bafd0be0123d0702057b8dd999cfcf761a8a7
Eli <eli.bingham@uber.com>
2019-11-13 16:52:18 -0800

    add test case and remove obsolete assertion in poutine.condition

--- pyro/poutine/condition_messenger.py
+++ pyro/poutine/condition_messenger.py
@@ -31,8 +31,6 @@ class ConditionMessenger(Messenger):
         name = msg["name"]
 
         if name in self.data:
-            assert not msg["is_observed"], \
-                "should not change values of existing observes"
             if isinstance(self.data, Trace):
                 msg["value"] = self.data.nodes[name]["value"]
             else:
--- tests/poutine/test_counterfactual.py
+++ tests/poutine/test_counterfactual.py
@@ -10,9 +10,13 @@ from tests.common import assert_equal, assert_not_equal
 logger = logging.getLogger(__name__)
 
 
-@pytest.mark.parametrize('intervene', [True, False])
-@pytest.mark.parametrize('observe', [True, False])
-def test_counterfactual_query(intervene, observe):
+@pytest.mark.parametrize('intervene,observe,flip', [
+    (True, False, False),
+    (False, True, False),
+    (True, True, False),
+    (True, True, True),
+])
+def test_counterfactual_query(intervene, observe, flip):
     # x -> y -> z -> w
 
     sites = ["x", "y", "z", "w"]
@@ -26,14 +30,20 @@ def test_counterfactual_query(intervene, observe):
         w = pyro.sample("w", dist.Normal(z, 1))
         return dict(x=x, y=y, z=z, w=w)
 
-    if intervene:
-        model = poutine.do(model, data=interventions)
-    if observe:
-        model = poutine.condition(model, data=observations)
+    if not flip:
+        if intervene:
+            model = poutine.do(model, data=interventions)
+        if observe:
+            model = poutine.condition(model, data=observations)
+    elif flip and intervene and observe:
+        model = poutine.do(
+            poutine.condition(model, data=observations),
+            data=interventions)
 
     tr = poutine.trace(model).get_trace()
     actual_values = tr.nodes["_RETURN"]["value"]
     for name in sites:
+        # case 1: purely observational query like poutine.condition
         if not intervene and observe:
             if observations[name] is not None:
                 assert tr.nodes[name]['is_observed']
@@ -41,12 +51,14 @@ def test_counterfactual_query(intervene, observe):
                 assert_equal(observations[name], tr.nodes[name]['value'])
             if interventions[name] != observations[name]:
                 assert_not_equal(interventions[name], actual_values[name])
+        # case 2: purely interventional query like old poutine.do
         elif intervene and not observe:
             assert not tr.nodes[name]['is_observed']
             if interventions[name] is not None:
                 assert_equal(interventions[name], actual_values[name])
             assert_not_equal(observations[name], tr.nodes[name]['value'])
             assert_not_equal(interventions[name], tr.nodes[name]['value'])
+        # case 3: counterfactual query mixing intervention and observation
         elif intervene and observe:
             if observations[name] is not None:
                 assert tr.nodes[name]['is_observed']
