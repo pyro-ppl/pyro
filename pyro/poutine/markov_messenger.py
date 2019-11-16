87d6fe7448ff61103c346b6b054ab63f06987993
Eli <eli.bingham@uber.com>
2019-11-15 16:30:10 -0800

    add vectorized interface stubs to pyro.markov

--- pyro/poutine/markov_messenger.py
+++ pyro/poutine/markov_messenger.py
@@ -17,11 +17,24 @@ class MarkovMessenger(ReentrantMessenger):
         when branching: if ``keep=True``, neighboring branches at the same
         level can depend on each other; if ``keep=False``, neighboring branches
         are independent (conditioned on their shared ancestors).
+    :param int dim: An optional dimension to use for this independence index.
+        Interface stub, behavior not yet implemented.
+    :param str name: An optional unique name to help inference algorithms match
+        :fn:`pyro.markov` sites between models and guides.
+        Interface stub, behavior not yet implemented.
     """
-    def __init__(self, history=1, keep=False):
+    def __init__(self, history=1, keep=False, dim=None, name=None):
         assert history >= 0
         self.history = history
         self.keep = keep
+        self.dim = dim
+        self.name = name
+        if dim is not None:
+            raise NotImplementedError(
+                "vectorized markov not yet implemented, try setting dim to None")
+        if name is not None:
+            raise NotImplementedError(
+                "vectorized markov not yet implemented, try setting name to None")
         self._iterable = None
         self._pos = -1
         self._stack = []
