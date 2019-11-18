08a70106d038dd760b755ff5227ef5dea2a6a5e4
Siddharth N <siddharth@iffsid.com>
2019-11-18 23:02:55 +0000

    drop minibatch warning

--- pyro/infer/rws.py
+++ pyro/infer/rws.py
@@ -21,8 +21,6 @@ class ReweightedWakeSleep(ELBO):
 
     .. note:: This returns _two_ losses, one each for the model and the guide.
 
-    .. warning:: Mini-batch training might have issues.
-
     :param num_particles: The number of particles/samples used to form the objective
         (gradient) estimator. Default is 2.
     :param insomnia: The scaling between the wake-phi and sleep-phi terms. Default is 1.0 [wake-phi]
