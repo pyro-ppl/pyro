4917df990d8ee9a96093de87a5fe060e6a0c418f
Siddharth N <siddharth@iffsid.com>
2019-11-18 23:21:19 +0000

    add warning for non-sleep case for model_has_params

--- pyro/infer/rws.py
+++ pyro/infer/rws.py
@@ -74,6 +74,8 @@ class ReweightedWakeSleep(ELBO):
 
         assert(insomnia >= 0 and insomnia <= 1), \
             "insomnia should be in [0, 1]"
+        if not (model_has_params or insomnia == 0.):
+            warnings.warn("model_has_params = False is ignored when insomnia != 0.")
 
     def _get_trace(self, model, guide, *args, **kwargs):
         """
