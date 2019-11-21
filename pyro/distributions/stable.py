228ef39c7e7f62677a1436b01da044785d8845c4
Fritz Obermeyer <fritzo@uber.com>
2019-11-20 21:12:58 -0600

    Interpolate around hole at alpha=0

--- pyro/distributions/stable.py
+++ pyro/distributions/stable.py
@@ -7,27 +7,48 @@ from torch.distributions.utils import broadcast_all
 from pyro.distributions.torch_distribution import TorchDistribution
 
 
-def _standard_stable(shape, alpha, beta):
-    # Implements a noisy-reparametrized sampler following the
-    # Chambers-Mallows-Stuck method as corrected by Weron [1,3].
+def _unsafe_standard_stable(shape, alpha, beta):
+    # Implements a noisily reparametrized version of the sampler
+    # Chambers-Mallows-Stuck method as corrected by Weron [1,3] and simplified
+    # by Nolan [4]. This will fail if alpha is close to zero.
+
+    # Draw parameter-free noise.
     with torch.no_grad():
         half_pi = math.pi / 2
         V = alpha.new_empty(shape).uniform_(-half_pi, half_pi)
         W = alpha.new_empty(shape).exponential_()
+
+    # Differentiably transform noise via parameters.
     inv_alpha = alpha.reciprocal()
-    b = beta * alpha.mul(half_pi).tan()
-    B = b.atan() * inv_alpha
-    S = (1 + b * b).pow(0.5 * inv_alpha)
-    v = alpha * (V + B)
-    X = S * v.sin() / V.cos().pow(inv_alpha) \
-        * ((V - v).cos() / W).pow((1-alpha) * inv_alpha)
-    alpha_eq_1 = alpha == 1
-    if alpha_eq_1.any():
-        c = half_pi + beta * V
-        X1 = 1/half_pi * (c * V.tan() - beta * (half_pi * W * V.cos() / c).log())
-        # FIXME make this differentiable.
-        X = torch.where(alpha_eq_1, X1, X)
+    b = beta * (half_pi * alpha).tan()
+    atan_b = b.atan()
+    Z = (atan_b + alpha * V).sin() \
+        / (atan_b.cos() * V.cos()).pow(inv_alpha) \
+        * ((atan_b + (alpha - 1) * V).cos() / W).pow(inv_alpha - 1)
+
+    # Convert to Nolan's parametrization S^0 so that samples depend
+    # continuously on (alpha,beta), allowing us to interpolate around the hole
+    # at alpha=0.
+    X = Z - b
     return X
+
+
+RADIUS = 0.01
+
+
+def _standard_stable(shape, alpha, beta):
+    # Avoids the hole at alpha=0 by interpololating between evenly-weighted
+    # antithetic pairs each of which is at least RADIUS away from zero.
+    shape_ = shape + (1,)
+    alpha_ = alpha.unsqueeze(-1).expand(alpha.shape + (2,)).contiguous()
+    with torch.no_grad():
+        near_zero = alpha.data.abs() <= RADIUS
+        lower, upper = alpha_.unbind(-1)
+        lower.data[near_zero] -= RADIUS
+        upper.data[near_zero] += RADIUS
+    beta_ = beta.unsqueeze(-1)
+    result = _unsafe_standard_stable(shape_, alpha_, beta_)
+    return result.mean(-1)
 
 
 class Stable(TorchDistribution):
@@ -35,14 +56,15 @@ class Stable(TorchDistribution):
     Levy :math:`\alpha`-stable distribution. See [1] for a review.
 
     This uses Nolan's parametrization [2] of the ``loc`` parameter, which is
-    required for differentiability. This corresponds to the notation
-    :math:`S^0_\alpha(\beta,\sigma,\mu_0)` of [1], where :math:`\alpha` =
-    stability, :math:`\beta` = skew, :math:`\sigma` = scale, and :math:`\mu_0`
-    = loc.
+    required for continuity and differentiability. This corresponds to the
+    notation :math:`S^0_\alpha(\beta,\sigma,\mu_0)` of [1], where
+    :math:`\alpha` = stability, :math:`\beta` = skew, :math:`\sigma` = scale,
+    and :math:`\mu_0` = loc.
 
-    This implements a reparametrized sampler, but does not implement
-    :meth:`log_prob` . Use in inference is thus limited to likelihood-free
-    algorithms such as :class:`~pyro.infer.trace_crps.Trace_CRPS`.
+    This implements a reparametrized sampler :meth:`rsample` , but does not
+    implement :meth:`log_prob` . Use in inference is thus limited to
+    likelihood-free algorithms such as
+    :class:`~pyro.infer.trace_crps.Trace_CRPS`.
 
     [1] S. Borak, W. Hardle, R. Weron (2005).
         Stable distributions.
@@ -52,11 +74,15 @@ class Stable(TorchDistribution):
     [3] Rafal Weron (1996).
         On the Chambers-Mallows-Stuck Method for
         Simulating Skewed Stable Random Variables.
+    [4] J.P. Nolan (2017).
+        Stable Distributions: Models for Heavy Tailed Data.
+        http://fs2.american.edu/jpnolan/www/stable/chap1.pdf
 
     :param Tensor stability: Levy stability parameter :math:`\alpha\in(0,2]` .
     :param Tensor skew: Skewness :math:`\beta\in[-1,1]` .
-    :param Tensor scale: Scale :math:`\sigma > 0` .
+    :param Tensor scale: Scale :math:`\sigma > 0` . Defaults to 1.
     :param Tensor loc: Location :math:`\mu_0` in Nolan's parametrization [2].
+        Defaults to 0.
     """
     has_rsample = True
     arg_constraints = {"stability": constraints.interval(0, 2),  # half-open (0, 2]
@@ -64,7 +90,7 @@ class Stable(TorchDistribution):
                        "scale": constraints.positive,
                        "loc": constraints.real}
 
-    def __init__(self, stability, skew, scale, loc, validate_args=None):
+    def __init__(self, stability, skew, scale=1.0, loc=0.0, validate_args=None):
         self.stability, self.skew, self.scale, self.loc = broadcast_all(
             stability, skew, scale, loc)
         super().__init__(self.loc.shape, validate_args=validate_args)
@@ -79,10 +105,9 @@ class Stable(TorchDistribution):
         return new
 
     def log_prob(self, value):
-        raise NotImplementedError
+        raise NotImplementedError("Stable.log_prob() is not implemented")
 
     def rsample(self, sample_shape=torch.Size()):
         shape = self._extended_shape(sample_shape)
         x = _standard_stable(shape, self.stability, self.skew)
-        # Note this differs from [3] by using Nolan's parametrization [2].
-        return self.loc + self.scale * (x - (2 / math.pi) * self.skew * self.scale.log())
+        return self.loc + self.scale * x
--- tests/distributions/test_stable.py
+++ tests/distributions/test_stable.py
@@ -1,5 +1,8 @@
+import numpy as np
 import pytest
 import torch
+from scipy.stats import levy_stable, kstest
+
 import pyro.distributions as dist
 
 
@@ -18,3 +21,22 @@ def test_shape(sample_shape, batch_shape):
     assert x.shape == sample_shape + batch_shape
 
     x.sum().backward()
+
+
+@pytest.mark.parametrize("beta", [-0.8, -0.4, 0.0, 0.4, 0.8])
+@pytest.mark.parametrize("alpha", [0.2, 0.5, 0.8, 0.99, 1.01, 1.2, 1.5, 1.8])
+def test_sample(alpha, beta):
+    num_samples = 100
+    d = dist.Stable(alpha, beta)
+
+    def sampler(size):
+        x = d.sample([size])
+        # Convert from Nolan's parametrization S^0 to scipy parametrization S.
+        z = x + beta * np.tan(np.pi / 2 * alpha)
+        return z
+
+    def cdf(x):
+        return levy_stable.cdf(x, alpha, beta)
+
+    stat, pvalue = kstest(sampler, cdf, N=num_samples)
+    assert pvalue > 0.1, pvalue
