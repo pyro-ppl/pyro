93ecf86c2e7f38973463719bb3f9252cec502d71
Fritz Obermeyer <fritzo@uber.com>
2019-11-23 13:37:53 -0600

    Get training to work on synthetic dataset

--- examples/cevae.py
+++ examples/cevae.py
@@ -5,20 +5,16 @@ import torch.nn as nn
 from torch.utils.data import DataLoader, TensorDataset
 
 import pyro
+import pyro.distributions as dist
 from pyro import poutine
-from pyro.distributions import Bernoulli, Normal
 from pyro.infer import SVI, Trace_ELBO
 from pyro.infer.util import torch_item
 from pyro.nn import PyroModule
 from pyro.optim import ClippedAdam
 
 
-def TODO(*args):
-    raise NotImplementedError("TODO")
-
-
 class FullyConnected(nn.Sequential):
-    def __init__(sizes):
+    def __init__(self, sizes):
         layers = []
         for in_size, out_size in zip(sizes, sizes[1:]):
             layers.append(nn.Linear(in_size, out_size))
@@ -29,18 +25,26 @@ class FullyConnected(nn.Sequential):
 
 class DiagNormalNet(nn.Module):
     def __init__(self, sizes):
+        super().__init__()
         self.fc = FullyConnected(sizes[:-1] + [sizes[-1] * 2])
 
     def forward(self, x):
         loc_scale = self.fc(x)
         d = loc_scale.size(-1) // 2
         loc = loc_scale[..., :d]
-        scale = nn.functional.softplus(loc_scale[..., d:])
+        scale = nn.functional.softplus(loc_scale[..., d:]).clamp(min=1e-10)
         return loc, scale
+
+
+def nn_init(m):
+    if isinstance(m, nn.Linear):
+        m.weight.data.normal_(0, 0.1 / m.in_features ** 0.5)
+        m.bias.data.fill_(0)
 
 
 class BernoulliNet(nn.Module):
     def __init__(self, sizes):
+        super().__init__()
         self.fc = FullyConnected(sizes + [1])
 
     def forward(self, x):
@@ -52,52 +56,52 @@ class Model(PyroModule):
         self.latent_dim = args.latent_dim
         super().__init__()
         self.x_nn = DiagNormalNet([args.latent_dim] +
-                                  [args.hidden_size] * args.num_layers +
+                                  [args.hidden_dim] * args.num_layers +
                                   [args.feature_dim])
         self.y0_nn = BernoulliNet([args.latent_dim] +
-                                  [args.hidden_size] * args.num_layers)
+                                  [args.hidden_dim] * args.num_layers)
         self.y1_nn = BernoulliNet([args.latent_dim] +
-                                  [args.hidden_size] * args.num_layers)
-        self.t_nn = BernoulliNet(args.feature_dim)
+                                  [args.hidden_dim] * args.num_layers)
+        self.t_nn = BernoulliNet([args.latent_dim])
 
     def forward(self, x, t=None, y=None, size=None):
         if size is None:
             size = x.size(0)
         with pyro.plate("data", size, subsample=x):
-            z = pyro.sample("z", self.z_dist)
+            z = pyro.sample("z", self.z_dist())
             x = pyro.sample("x", self.x_dist(z), obs=x)
             t = pyro.sample("t", self.t_dist(z), obs=t)
             y = pyro.sample("y", self.y_dist(t, z), obs=y)
         return y
 
     def z_dist(self):
-        return Normal(0, 1).expand([self.latent_dim]).to_event(1)
+        return dist.Normal(0, 1).expand([self.latent_dim]).to_event(1)
 
     def x_dist(self, z):
         loc, scale = self.x_nn(z)
-        return Normal(loc, scale).to_event(1)
+        return dist.Normal(loc, scale).to_event(1)
 
     def y_dist(self, t, z):
         # Parameters are not shared among t values.
         logits0 = self.y0_nn(z)
         logits1 = self.y1_nn(z)
-        logits = torch.where(t, logits1, logits0)
-        return Bernoulli(logits=logits)
+        logits = torch.where(t.bool(), logits1, logits0)
+        return dist.Bernoulli(logits=logits)
 
-    def t_dist(self, x):
-        logits = self.t_nn(x)
-        return Bernoulli(logits=logits)
+    def t_dist(self, z):
+        logits = self.t_nn(z)
+        return dist.Bernoulli(logits=logits)
 
 
 class Guide(PyroModule):
-    def __init__(self):
+    def __init__(self, args):
         self.latent_dim = args.latent_dim
         super().__init__()
-        self.t_nn = BernoulliNet(self.feature_dim)
-        self.y_nn = FullyConnected([self.feature_dim] +
+        self.t_nn = BernoulliNet([args.feature_dim])
+        self.y_nn = FullyConnected([args.feature_dim] +
                                    [args.hidden_dim] * args.num_layers)
-        self.y0_nn = BernoulliNet(args.hidden_dim)
-        self.y1_nn = BernoulliNet(args.hidden_dim)
+        self.y0_nn = BernoulliNet([args.hidden_dim])
+        self.y1_nn = BernoulliNet([args.hidden_dim])
         self.z0_nn = DiagNormalNet([1 + args.feature_dim] +
                                    [args.hidden_dim] * args.num_layers +
                                    [args.latent_dim])
@@ -115,7 +119,7 @@ class Guide(PyroModule):
 
     def t_dist(self, x):
         logits = self.t_nn(x)
-        return Bernoulli(logits=logits)
+        return dist.Bernoulli(logits=logits)
 
     def y_dist(self, t, x):
         # The first n-1 layers are identical for all t values.
@@ -123,17 +127,17 @@ class Guide(PyroModule):
         # In the final layer params are not shared among t values.
         logits0 = self.y0_nn(hidden)
         logits1 = self.y1_nn(hidden)
-        logits = torch.where(t, logits1, logits0)
-        return Bernoulli(logits=logits)
+        logits = torch.where(t.bool(), logits1, logits0)
+        return dist.Bernoulli(logits=logits)
 
     def z_dist(self, t, y, x):
         # Parameters are not shared among t values.
         y_x = torch.cat([y.unsqueeze(-1), x], dim=-1)
         loc0, scale0 = self.z0_nn(y_x)
         loc1, scale1 = self.z1_nn(y_x)
-        loc = torch.where(t, loc1, loc0)
-        scale = torch.where(t, scale1, scale0)
-        return Normal(loc, scale)
+        loc = torch.where(t.bool().unsqueeze(-1), loc1, loc0)
+        scale = torch.where(t.bool().unsqueeze(-1), scale1, scale0)
+        return dist.Normal(loc, scale).to_event(1)
 
 
 class TraceCausalEffect_ELBO(Trace_ELBO):
@@ -154,7 +158,7 @@ class TraceCausalEffect_ELBO(Trace_ELBO):
 
         # Add log q terms.
         for name in blocked_names:
-            log_q = guide_trace.nodes[name]["log_prob"]
+            log_q = guide_trace.nodes[name]["log_prob_sum"]
             loss = loss - torch_item(log_q)
             surrogate_loss = surrogate_loss - log_q
 
@@ -184,6 +188,8 @@ def ite(model, guide, x, num_samples=100):
 def train(args, x, t, y):
     model = Model(args)
     guide = Guide(args)
+    model.apply(nn_init)
+    guide.apply(nn_init)
 
     dataset = TensorDataset(x, t, y)
     dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
@@ -196,15 +202,17 @@ def train(args, x, t, y):
         loss = 0
         for x, t, y in dataloader:
             loss += svi.step(x, t, y, size=len(dataset))
-        print("epoch {: >3d} loss = {:0.6g}".format(loss / len(dataloader)))
+        print("epoch {: >3d} loss = {:0.6g}".format(epoch, loss / len(dataloader)))
     return model, guide
 
 
 def generate_data(args):
-    z = TODO()
-    x = TODO(z)
-    t = TODO(z)
-    y = TODO(t, z)
+    # args.feature_dim = 1
+    # args.latent_dim = 5
+    z = dist.Bernoulli(0.5).sample([args.num_data])
+    x = dist.Normal(z, 5 * z + 3 * (1 - z)).sample([args.feature_dim]).t()
+    t = dist.Bernoulli(0.75 * z + 0.25 * (1 - z)).sample()
+    y = dist.Bernoulli(logits=3 * (z + 2 * (2 * t - 2))).sample()
     return x, t, y
 
 
@@ -221,19 +229,20 @@ def main(args):
     train(args, x, t, y)
 
     # Evaluate.
-    TODO()
+    raise NotImplementedError("TODO evaluate")
 
 
 if __name__ == "__main__":
     assert pyro.__version__.startswith('1.0.0')
     parser = argparse.ArgumentParser(description="Causal Effect Variational Autoencoder")
+    parser.add_argument("--num-data", default=1000, type=int)
     parser.add_argument("--feature-dim", default=5, type=int)
     parser.add_argument("--latent-dim", default=20, type=int)
+    parser.add_argument("--hidden-dim", default=200, type=int)
     parser.add_argument("--num-layers", default=3, type=int)
-    parser.add_argument("--hidden-size", default=200, type=int)
     parser.add_argument("-n", "--num-epochs", default=1000, type=int)
     parser.add_argument("-b", "--batch-size", default=100, type=int)
-    parser.add_argument("-lr", "--learning-rate", default=0.05, type=float)
+    parser.add_argument("-lr", "--learning-rate", default=1e-3, type=float)
     parser.add_argument("-lrd", "--learning-rate-decay", default=0.1, type=float)
     parser.add_argument("--weight-decay", default=1e-4, type=float)
     parser.add_argument("--seed", default=1234567890, type=int)
