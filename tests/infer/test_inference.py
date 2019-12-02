9828256d2fca90393aa02152b37fa7fdafe1646e
Fritz Obermeyer <fritzo@uber.com>
2019-12-02 14:26:07 -0800

    Implement EnergyDistance likelihood-free inference algorithm (#2178)
    * Sketch Trace_CRPS class
    
    * Add -log p(z) term to loss
    
    * Add smoke tests
    
    * Revert typo
    
    * Fix KL(q,p) computation
    
    * Fix sign error
    
    * Fix bug in max_plate_nesting
    
    * Add failing inference test
    
    * Fix sign error in kl, add inference test
    
    * flake8
    
    * Fix typo
    
    * Address review comments
    
    * Fix mask,scale shape bug
    
    * Add multivariate crps test
    
    * Add more shape tests
    
    * Add beta and tol parameters to Trace_CRPS
    
    * Remove unnecessary numerical hacks
    
    * Ensure dist.Stable works with Trace_CRPS
    
    * Switch from KL to prior regularization
    
    * Rename to Trace_CRPS --> EnergyDistance
    
    * Add math to docs
    
    * Fix math typo

--- tests/infer/test_inference.py
+++ tests/infer/test_inference.py
@@ -1,3 +1,4 @@
+import logging
 import math
 from unittest import TestCase
 
@@ -6,15 +7,18 @@ import torch
 from torch.distributions import constraints
 
 import pyro
+import pyro.contrib.gp.kernels as kernels
 import pyro.distributions as dist
 import pyro.optim as optim
-import pyro.contrib.gp.kernels as kernels
-from pyro.infer.util import torch_item
 from pyro.distributions.testing import fakes
 from pyro.distributions.testing.rejection_gamma import ShapeAugmentedGamma
-from pyro.infer import (SVI, JitTrace_ELBO, JitTraceEnum_ELBO, JitTraceGraph_ELBO, Trace_ELBO, TraceEnum_ELBO,
-                        TraceGraph_ELBO, RenyiELBO, TraceMeanField_ELBO, TraceTailAdaptive_ELBO, Trace_MMD)
-from tests.common import assert_equal, xfail_param, xfail_if_not_implemented
+from pyro.infer import (SVI, EnergyDistance, JitTrace_ELBO, JitTraceEnum_ELBO, JitTraceGraph_ELBO, RenyiELBO,
+                        Trace_ELBO, Trace_MMD, TraceEnum_ELBO, TraceGraph_ELBO, TraceMeanField_ELBO,
+                        TraceTailAdaptive_ELBO)
+from pyro.infer.util import torch_item
+from tests.common import assert_close, assert_equal, xfail_if_not_implemented, xfail_param
+
+logger = logging.getLogger(__name__)
 
 
 def param_mse(name, target):
@@ -606,3 +610,73 @@ class SafetyTests(TestCase):
 
         with pytest.raises(RuntimeError):
             svi.step()
+
+
+@pytest.mark.stage("integration", "integration_batch_1")
+@pytest.mark.parametrize("prior_scale", [0, 1e-4])
+def test_energy_distance_univariate(prior_scale):
+
+    def model(data):
+        loc = pyro.sample("loc", dist.Normal(0, 100))
+        scale = pyro.sample("scale", dist.LogNormal(0, 1))
+        with pyro.plate("data", len(data)):
+            pyro.sample("obs", dist.Normal(loc, scale), obs=data)
+
+    def guide(data):
+        loc_loc = pyro.param("loc_loc", torch.tensor(0.))
+        loc_scale = pyro.param("loc_scale", torch.tensor(1.),
+                               constraint=constraints.positive)
+        log_scale_loc = pyro.param("log_scale_loc", torch.tensor(0.))
+        log_scale_scale = pyro.param("log_scale_scale", torch.tensor(1.),
+                                     constraint=constraints.positive)
+        pyro.sample("loc", dist.Normal(loc_loc, loc_scale))
+        pyro.sample("scale", dist.LogNormal(log_scale_loc, log_scale_scale))
+
+    data = 10.0 + torch.randn(8)
+    adam = optim.Adam({"lr": 0.1})
+    loss_fn = EnergyDistance(num_particles=32, prior_scale=prior_scale)
+    svi = SVI(model, guide, adam, loss_fn)
+    for step in range(2001):
+        loss = svi.step(data)
+        if step % 20 == 0:
+            logger.info("step {} loss = {:0.4g}, loc = {:0.4g}, scale = {:0.4g}"
+                        .format(step, loss, pyro.param("loc_loc").item(),
+                                pyro.param("log_scale_loc").exp().item()))
+
+    expected_loc = data.mean()
+    expected_scale = data.std()
+    actual_loc = pyro.param("loc_loc").detach()
+    actual_scale = pyro.param("log_scale_loc").exp().detach()
+    assert_close(actual_loc, expected_loc, atol=0.05)
+    assert_close(actual_scale, expected_scale, rtol=0.1 if prior_scale else 0.05)
+
+
+@pytest.mark.stage("integration", "integration_batch_1")
+@pytest.mark.parametrize("prior_scale", [0, 1])
+def test_energy_distance_multivariate(prior_scale):
+
+    def model(data):
+        loc = torch.zeros(2)
+        cov = pyro.sample("cov", dist.Normal(0, 100).expand([2, 2]).to_event(2))
+        with pyro.plate("data", len(data)):
+            pyro.sample("obs", dist.MultivariateNormal(loc, cov), obs=data)
+
+    def guide(data):
+        scale_tril = pyro.param("scale_tril", torch.eye(2),
+                                constraint=constraints.lower_cholesky)
+        pyro.sample("cov", dist.Delta(scale_tril @ scale_tril.t(), event_dim=2))
+
+    cov = torch.tensor([[1, 0.8], [0.8, 1]])
+    data = dist.MultivariateNormal(torch.zeros(2), cov).sample([10])
+    loss_fn = EnergyDistance(num_particles=32, prior_scale=prior_scale)
+    svi = SVI(model, guide, optim.Adam({"lr": 0.1}), loss_fn)
+    for step in range(2001):
+        loss = svi.step(data)
+        if step % 20 == 0:
+            logger.info("step {} loss = {:0.4g}".format(step, loss))
+
+    delta = data - data.mean(0)
+    expected_cov = (delta.t() @ delta) / len(data)
+    scale_tril = pyro.param("scale_tril").detach()
+    actual_cov = scale_tril @ scale_tril.t()
+    assert_close(actual_cov, expected_cov, atol=0.2)
