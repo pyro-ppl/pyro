aba68136205a0318d79c0bc0ca03bd825db230b8
Neeraj Pradhan <npradhan@uber.com>
2019-12-28 00:18:23 -0800

    Add simple smoke test for neutra

--- pyro/infer/reparam/neutra.py
+++ pyro/infer/reparam/neutra.py
@@ -2,9 +2,9 @@ from torch.distributions import biject_to
 
 import pyro
 import pyro.distributions as dist
+from pyro import poutine
 from pyro.distributions.util import sum_rightmost
 from pyro.infer.autoguide.guides import AutoContinuous
-
 from .reparam import Reparam
 
 
@@ -42,8 +42,16 @@ class NeuTraReparam(Reparam):
         if not isinstance(guide, AutoContinuous):
             raise TypeError("NeuTraReparam expected an AutoContinuous guide, but got {}"
                             .format(type(guide)))
-        self.guide
+        self.guide = guide
+        self.transform = None
         self.x_unconstrained = []
+
+    def _reparam_config(self, site):
+        if site["name"] in self.guide.prototype_trace:
+            return self
+
+    def reparam(self, fn=None):
+        return poutine.reparam(fn, config=self._reparam_config)
 
     def __call__(self, name, fn, obs):
         if name not in self.guide.prototype_trace.nodes:
@@ -57,14 +65,14 @@ class NeuTraReparam(Reparam):
                 raise ValueError("NeuTraReparam only supports guides whose posteriors are "
                                  "TransformedDistributions but got a posterior of type {}"
                                  .format(type(posterior)))
-            t = dist.transforms.ComposeTransform(posterior.transforms)
+            self.transform = dist.transforms.ComposeTransform(posterior.transforms)
             z_unconstrained = pyro.sample("{}_shared_latent".format(name),
                                           posterior.base_dist.mask(False))
 
             # Differentiably transform.
-            x_unconstrained = t(z_unconstrained)
-            log_density = t.log_abs_det_jacobian(z_unconstrained, x_unconstrained)
-            self.x_unconstrained = list(reversed(self.guide._unpack_latent(x_unconstrained)))
+            x_unconstrained = self.transform(z_unconstrained)
+            log_density = self.transform.log_abs_det_jacobian(z_unconstrained, x_unconstrained)
+            self.x_unconstrained = list(reversed(list(self.guide._unpack_latent(x_unconstrained))))
 
         # Extract a single site's value from the shared latent.
         site, unconstrained_value = self.x_unconstrained.pop()
@@ -76,3 +84,22 @@ class NeuTraReparam(Reparam):
         log_density = log_density + fn.log_prob(value) + logdet
         new_fn = dist.Delta(value, log_density, event_dim=fn.event_dim)
         return new_fn, value
+
+    def transform_sample(self, latent):
+        """
+        Given latent samples from the warped posterior (with a possible batch dimension),
+        return a `dict` of samples from the latent sites in the model.
+
+        :param latent: sample from the warped posterior (possibly batched). Note that the
+            batch dimension must not collide with plate dimensions in the model, i.e.
+            any batch dims `d < - max_plate_nesting`.
+        :return: a `dict` of samples keyed by latent sites in the model.
+        :rtype: dict
+        """
+        x_unconstrained = self.transform(latent)
+        transformed_samples = {}
+        for site, value in self.guide._unpack_latent(x_unconstrained):
+            transform = biject_to(site["fn"].support)
+            x_constrained = transform(value)
+            transformed_samples[site["name"]] = x_constrained
+        return transformed_samples
