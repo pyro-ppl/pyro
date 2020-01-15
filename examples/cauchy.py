726f8946623753cbe7cecab1d04b2c8d4d8cbfb4
Eli <eli.bingham@uber.com>
2020-01-14 18:11:28 -0800

    add exact training to example

--- examples/cauchy.py
+++ examples/cauchy.py
@@ -7,6 +7,7 @@ import torch.distributions.constraints as constraints
 
 import pyro
 import pyro.distributions as dist
+import pyro.poutine as poutine
 from pyro.infer import TraceTMC_ELBO, config_enumerate
 from pyro.ops.ssm_gp import MaternKernel
 from pyro.contrib.timeseries import IndependentMaternGP
@@ -71,13 +72,13 @@ def tmc_run(args, ys):
         for y in pyro.markov(ys):
             z = model.step(z, y)
 
-    @config_enumerate(default="parallel", num_samples=args.num_particles, expand=False)
+    @config_enumerate(default="parallel", num_samples=args.num_particles, expand=False, tmc=args.tmc_strategy)
     def tmc_guide(ys):
         z = guide.init()
         for y in pyro.markov(ys):
             z = guide.step(z, y)
 
-    def exact_eval():
+    def exact_eval(ys):
         gp = IndependentMaternGP(nu=1.5, obs_dim=1,
                                  length_scale_init=model.kernel.length_scale,
                                  kernel_scale_init=model.kernel.kernel_scale,
@@ -86,24 +87,70 @@ def tmc_run(args, ys):
         clear(gp)
         return log_prob
 
-    optim = pyro.optim.ClippedAdam({'lr': 0.001, 'betas': (0.90, 0.999), 'clip_norm': 1.0})
-    svi = pyro.infer.SVI(tmc_model, tmc_guide, optim, tmc)
+    svi_model, svi_guide = None, None
+    if args.train == "iwae":
+        model_optim = pyro.optim.ClippedAdam({'lr': 0.001, 'betas': (0.90, 0.999), 'clip_norm': 1.0})
+        svi_model = pyro.infer.SVI(tmc_model, tmc_guide, model_optim, tmc.differentiable_loss)
+    elif args.train == "rws":
+        model_optim = pyro.optim.ClippedAdam({'lr': 0.001, 'betas': (0.90, 0.999), 'clip_norm': 1.0})
+        svi_model = pyro.infer.SVI(tmc_model, poutine.block(tmc_guide, hide_types=["param"]), model_optim,
+                                   tmc.differentiable_loss)
+        guide_optim = pyro.optim.ClippedAdam({'lr': 0.001, 'betas': (0.90, 0.999), 'clip_norm': 1.0})
+        svi_guide = pyro.infer.SVI(poutine.block(tmc_model, hide_types=["param"]), tmc_guide, guide_optim,
+                                   tmc.wake_phi_loss)
+    elif args.train == "model":  # no guide updates
+        model_optim = pyro.optim.ClippedAdam({'lr': 0.001, 'betas': (0.90, 0.999), 'clip_norm': 1.0})
+        svi_model = pyro.infer.SVI(tmc_model, poutine.block(tmc_guide, hide_types=["param"]), model_optim,
+                                   tmc.differentiable_loss)
+    else:
+        raise ValueError
 
     pyro.param("sigma_obs", torch.tensor(0.1), constraint=constraints.positive)
-    logging.info("Initial exact log prob: {:.6f}".format(exact_eval()))
+    logging.info("Initial exact log prob: {:.6f}".format(exact_eval(ys)))
 
     num_steps = 300
 
     for step in range(num_steps):
-        logp = svi.step(ys)
+        logp = svi_model.step(ys)
+        if svi_guide is not None:
+            svi_guide.step(ys)
         if step % 10 == 0 or step == num_steps - 1:
             frmt = "[Step {}]  loss: {:.4f}  exact: {:.4f}  lengthscale: {:.3f}  kernelscale: {:.3f}  sigmaobs: {:.3f}"
-            logging.info(frmt.format(step, logp, exact_eval(), model.kernel.length_scale.item(),
+            logging.info(frmt.format(step, logp, exact_eval(ys), model.kernel.length_scale.item(),
                                      model.kernel.kernel_scale.item(), pyro.param("sigma_obs").item()))
 
     tmc_estimates = [-tmc.loss(tmc_model, tmc_guide, ys) for _ in range(100)]
     logging.info("TMC-estimated log prob: {:.4f} +- {:.4f}".format(np.mean(tmc_estimates), np.std(tmc_estimates)))
-    logging.info("Final exact log prob: {:.6f}".format(exact_eval()))
+    logging.info("Final exact log prob: {:.6f}".format(exact_eval(ys)))
+
+
+def exact_run(args, ys):
+
+    gp_model = IndependentMaternGP(nu=1.5, obs_dim=1,
+                                   length_scale_init=torch.tensor([1.5]),
+                                   kernel_scale_init=torch.tensor([0.5]),
+                                   obs_noise_scale_init=torch.tensor([0.1]))
+    model_optim = pyro.optim.clipped_adam.ClippedAdam(
+        gp_model.parameters(), **{'lr': 0.001, 'betas': (0.90, 0.999), 'clip_norm': 1.0})
+
+    logging.info("Initial exact log prob: {:.6f}".format(
+        gp_model.log_prob(ys.unsqueeze(-1)).item()))
+
+    num_steps = 300
+
+    for step in range(num_steps):
+        log_prob = -gp_model.log_prob(ys.unsqueeze(-1))
+        log_prob.backward()
+        model_optim.step()
+        logp = log_prob.item()
+        if step % 10 == 0 or step == num_steps - 1:
+            frmt = "[Step {}]  loss: {:.4f}  exact: {:.4f}  lengthscale: {:.3f}  kernelscale: {:.3f}  sigmaobs: {:.3f}"
+            logging.info(frmt.format(step, logp, -logp,
+                                     gp_model.kernel.length_scale.item(),
+                                     gp_model.kernel.kernel_scale.item(),
+                                     gp_model.obs_noise_scale.item()))
+
+    logging.info("Final exact log prob: {:.6f}".format(-logp))
 
 
 def main(args):
@@ -115,13 +162,18 @@ def main(args):
     ts = 3.0 * torch.arange(T).float() / T
     ys = torch.sin(2.0 * ts)
 
-    tmc_run(args, ys)
+    if args.train == "exact":
+        exact_run(args, ys)
+    else:
+        tmc_run(args, ys)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Cauchy process")
-    parser.add_argument("-n", "--num-timesteps", default=10, type=int)
-    parser.add_argument("-p", "--num-particles", default=50, type=int)
+    parser.add_argument("-T", "--num-timesteps", default=10, type=int)
+    parser.add_argument("-K", "--num-particles", default=50, type=int)
     parser.add_argument("--seed", default=0, type=int)
+    parser.add_argument("--tmc-strategy", default="diagonal", type=str)
+    parser.add_argument("--train", default="exact", type=str)
     args = parser.parse_args()
     main(args)
