6c2da702df453a64b5afc6bced2b1db37f60aeb9
Fritz Obermeyer <fritzo@uber.com>
2020-01-16 08:19:32 -0800

    Bump PyTorch and torchvision versions

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ EXTRAS_REQUIRE = [
     'graphviz>=0.8',
     'matplotlib>=1.3',
     'pillow-simd',
-    'torchvision>=0.4.0',
+    'torchvision>=0.5.0',
     'visdom>=0.1.4',
     'pandas',
     'seaborn',
@@ -86,7 +86,7 @@ setup(
         'numpy>=1.7',
         'opt_einsum>=2.3.2',
         'pyro-api>=0.1.1',
-        'torch>=1.3.0',
+        'torch>=1.4.0',
         'tqdm>=4.36',
     ],
     extras_require={
