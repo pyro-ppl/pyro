3da127cb3c5648bd02748516e3ba6da70583fb1b
Yerdos Ordabayev <ordabayev@brandeis.edu>
2020-01-25 23:24:31 -0500

    use MaskedDistribution in MaskMessenger

--- pyro/poutine/mask_messenger.py
+++ pyro/poutine/mask_messenger.py
@@ -31,5 +31,9 @@ class MaskMessenger(Messenger):
         self.mask = mask
 
     def _process_message(self, msg):
+        if msg["done"] or msg["type"] != "sample": # not sure if conditions are right here
+            return None
+        # possibly eliminate msg["mask"] ??
         msg["mask"] = self.mask if msg["mask"] is None else self.mask & msg["mask"]
+        msg["fn"] = MaskedDistribution(msg["fn"], msg["mask"])
         return None
