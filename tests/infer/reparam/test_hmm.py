328237bd8f87b447f7ae8084984320489a393e88
Fritz Obermeyer <fritzo@uber.com>
2020-02-05 05:41:36 -0800

    Revert accidental deletion

--- tests/infer/reparam/test_hmm.py
+++ tests/infer/reparam/test_hmm.py
@@ -8,6 +8,7 @@ import pyro
 import pyro.distributions as dist
 from pyro import poutine
 from pyro.infer.reparam import LinearHMMReparam, StableReparam, StudentTReparam, SymmetricStableReparam
+from tests.common import assert_close
 from tests.ops.gaussian import random_mvn
 
 
@@ -108,6 +109,46 @@ def test_stable_hmm_shape(skew, batch_shape, duration, hidden_dim, obs_dim):
             model(data)
     assert isinstance(tr.trace.nodes["x"]["fn"], dist.GaussianHMM)
     tr.trace.compute_log_prob()  # smoke test only
+
+
+# Test helper to extract a few fractional moments from joint samples.
+# This uses fractional moments because Stable variance is infinite.
+def get_hmm_moments(samples):
+    loc = samples.median(0).values
+    delta = samples - loc
+    cov = (delta.unsqueeze(-1) * delta.unsqueeze(-2)).sqrt().mean(0)
+    scale = cov.diagonal(dim1=-2, dim2=-1)
+    sigma = scale.sqrt()
+    corr = cov / (sigma.unsqueeze(-1) * sigma.unsqueeze(-2))
+    return loc, scale, corr
+
+
+@pytest.mark.parametrize("duration", [1, 2, 3])
+@pytest.mark.parametrize("obs_dim", [1, 2])
+@pytest.mark.parametrize("hidden_dim", [1, 2])
+@pytest.mark.parametrize("stability", [1.9, 1.6])
+@pytest.mark.parametrize("skew", [0, None], ids=["symmetric", "skewed"])
+def test_stable_hmm_distribution(stability, skew, duration, hidden_dim, obs_dim):
+    init_dist = random_stable((hidden_dim,), stability, skew=skew).to_event(1)
+    trans_mat = torch.randn(duration, hidden_dim, hidden_dim)
+    trans_dist = random_stable((duration, hidden_dim), stability, skew=skew).to_event(1)
+    obs_mat = torch.randn(duration, hidden_dim, obs_dim)
+    obs_dist = random_stable((duration, obs_dim), stability, skew=skew).to_event(1)
+    hmm = dist.LinearHMM(init_dist, trans_mat, trans_dist, obs_mat, obs_dist, duration=duration)
+
+    num_samples = 200000
+    expected_samples = hmm.sample([num_samples]).reshape(num_samples, duration * obs_dim)
+    expected_loc, expected_scale, expected_corr = get_hmm_moments(expected_samples)
+
+    rep = SymmetricStableReparam() if skew == 0 else StableReparam()
+    with pyro.plate("samples", num_samples):
+        with poutine.reparam(config={"x": LinearHMMReparam(rep, rep, rep)}):
+            actual_samples = pyro.sample("x", hmm).reshape(num_samples, duration * obs_dim)
+    actual_loc, actual_scale, actual_corr = get_hmm_moments(actual_samples)
+
+    assert_close(actual_loc, expected_loc, atol=0.05, rtol=0.05)
+    assert_close(actual_scale, expected_scale, atol=0.05, rtol=0.05)
+    assert_close(actual_corr, expected_corr, atol=0.01)
 
 
 @pytest.mark.parametrize("duration", [1, 2, 3, 4, 5, 6])
