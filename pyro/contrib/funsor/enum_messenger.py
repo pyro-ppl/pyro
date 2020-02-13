504f639dcedad58e658325b9fba2a381f19e87a7
Eli <eli.bingham@uber.com>
2020-02-12 21:36:22 -0800

    add a more funsor-y example of enumeration

--- pyro/contrib/funsor/enum_messenger.py
+++ pyro/contrib/funsor/enum_messenger.py
@@ -42,14 +42,40 @@ class FunsorTraceMessenger(TraceMessenger):
         return super()._pyro_post_sample(msg)
 
 
-def simple_ve(model, *args):
+def simple_ve_1(model, *args):
 
     import funsor
 
-    with FunsorTraceMessenger() as tr:
+    with FunsorLogJointMessenger() as tr:
         with SimpleEnumMessenger():
             model(*args)
 
     log_joint = sum([site["infer"]["funsor_fn"](value=site["infer"]["funsor_value"])
                      for site in tr.trace.nodes.values()])
     return log_joint.reduce(funsor.ops.logaddexp)
+
+
+class FunsorLogJointMessenger(Messenger):
+
+    def __enter__(self):
+        self.log_joint = to_funsor(0.)
+        return super().__enter__()
+
+    def _pyro_post_sample(self, msg):
+        import funsor
+        with funsor.interpretation(funsor.terms.lazy):
+            self.log_joint += msg["infer"].get("funsor_fn", to_funsor(msg["fn"]))(
+                value=msg["infer"].get("funsor_value", to_funsor(msg["value"]))
+            )
+
+
+def simple_ve_2(model, *args):
+
+    import funsor
+
+    with FunsorLogJointMessenger() as tr:
+        with SimpleEnumMessenger():
+            model(*args)
+
+    with funsor.interpretation(funsor.optimizer.optimize):
+        return tr.log_joint.reduce(funsor.ops.logaddexp)
