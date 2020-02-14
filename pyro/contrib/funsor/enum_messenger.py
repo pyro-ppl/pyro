5c6e1efef08e2909be766736125f94eb70224951
Eli <eli.bingham@uber.com>
2020-02-13 19:41:34 -0800

    match existing API

--- pyro/contrib/funsor/enum_messenger.py
+++ pyro/contrib/funsor/enum_messenger.py
@@ -19,11 +19,13 @@ class IndepMessenger(GlobalNameMessenger):
     """
     Sketch of vectorized plate implementation using to_data instead of _DIM_ALLOCATOR
     """
-    def __init__(self, name=None, size=None):
+    def __init__(self, name=None, size=None, dim=None):
         assert size > 1
+        assert dim is None or dim < 0
         super().__init__()
         self.name = name
         self.size = size
+        self.dim = dim
 
         import funsor
 
@@ -35,7 +37,10 @@ class IndepMessenger(GlobalNameMessenger):
 
     def __enter__(self):
         super().__enter__()  # do this first to take care of globals recycling
-        indices = to_data(self._indices)  # TODO indicate that this dim is user-visible
+        if self.dim is not None:
+            indices = to_data(self._indices, name_to_dim=OrderedDict([(self.name, self.dim)]))
+        else:
+            indices = to_data(self._indices)  # TODO indicate that this dim is user-visible
         self.dim, self.indices = -indices.dim(), indices.squeeze()
         return self
 
@@ -49,6 +54,11 @@ class IndepMessenger(GlobalNameMessenger):
 
 
 class EnumMessenger(GlobalNameMessenger):
+
+    def __init__(self, first_available_dim=None):
+        assert first_available_dim is None or first_available_dim < 0, first_available_dim
+        self.first_available_dim = first_available_dim
+        super().__init__()
 
     def _pyro_sample(self, msg):
 
