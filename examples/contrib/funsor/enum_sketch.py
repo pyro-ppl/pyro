a3c5c8d25fde2cb5263fb2115bb7154b747f3854
Eli <eli.bingham@uber.com>
2020-02-13 18:21:58 -0800

    split enum into components and sketch

---
+++ examples/contrib/funsor/enum_sketch.py
@@ -0,0 +1,49 @@
+# Copyright (c) 2017-2019 Uber Technologies, Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+import torch
+from collections import OrderedDict
+
+from pyro.poutine import Messenger
+
+from pyro.contrib.funsor import to_funsor, to_data
+from pyro.contrib.funsor.enum_messenger import EnumMessenger, FunsorTraceMessenger
+
+
+def simple_ve_1(model, *args):
+
+    import funsor
+
+    with FunsorTraceMessenger() as tr:
+        with EnumMessenger():
+            model(*args)
+
+    log_joint = sum([site["infer"]["funsor_fn"](value=site["infer"]["funsor_value"])
+                     for site in tr.trace.nodes.values()])
+    return log_joint.reduce(funsor.ops.logaddexp)
+
+
+class FunsorLogJointMessenger(Messenger):
+
+    def __enter__(self):
+        self.log_joint = to_funsor(0.)
+        return super().__enter__()
+
+    def _pyro_post_sample(self, msg):
+        import funsor
+        with funsor.interpretation(funsor.terms.lazy):
+            self.log_joint += msg["infer"].get("funsor_fn", to_funsor(msg["fn"]))(
+                value=msg["infer"].get("funsor_value", to_funsor(msg["value"]))
+            )
+
+
+def simple_ve_2(model, *args):
+
+    import funsor
+
+    with FunsorLogJointMessenger() as tr:
+        with EnumMessenger():
+            model(*args)
+
+    with funsor.interpretation(funsor.optimizer.optimize):
+        return tr.log_joint.reduce(funsor.ops.logaddexp)
--- pyro/contrib/funsor/enum_messenger.py
+++ pyro/contrib/funsor/enum_messenger.py
@@ -8,9 +8,15 @@ from pyro.poutine import Messenger
 from pyro.poutine.trace_messenger import TraceMessenger
 
 from pyro.contrib.funsor import named, to_funsor, to_data
+from pyro.contrib.funsor.named_messenger import GlobalNameMessenger
 
 
-class SimpleEnumMessenger(Messenger):
+class IndepMessenger(GlobalNameMessenger):
+    # TODO
+    pass
+
+
+class EnumMessenger(GlobalNameMessenger):
 
     def _pyro_sample(self, msg):
 
@@ -40,42 +46,3 @@ class FunsorTraceMessenger(TraceMessenger):
         if "funsor_value" not in msg["infer"]:
             msg["infer"]["funsor_value"] = to_funsor(msg["value"])
         return super()._pyro_post_sample(msg)
-
-
-def simple_ve_1(model, *args):
-
-    import funsor
-
-    with FunsorLogJointMessenger() as tr:
-        with SimpleEnumMessenger(), named():
-            model(*args)
-
-    log_joint = sum([site["infer"]["funsor_fn"](value=site["infer"]["funsor_value"])
-                     for site in tr.trace.nodes.values()])
-    return log_joint.reduce(funsor.ops.logaddexp)
-
-
-class FunsorLogJointMessenger(Messenger):
-
-    def __enter__(self):
-        self.log_joint = to_funsor(0.)
-        return super().__enter__()
-
-    def _pyro_post_sample(self, msg):
-        import funsor
-        with funsor.interpretation(funsor.terms.lazy):
-            self.log_joint += msg["infer"].get("funsor_fn", to_funsor(msg["fn"]))(
-                value=msg["infer"].get("funsor_value", to_funsor(msg["value"]))
-            )
-
-
-def simple_ve_2(model, *args):
-
-    import funsor
-
-    with FunsorLogJointMessenger() as tr:
-        with SimpleEnumMessenger(), named():
-            model(*args)
-
-    with funsor.interpretation(funsor.optimizer.optimize):
-        return tr.log_joint.reduce(funsor.ops.logaddexp)
