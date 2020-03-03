3658054923c1a2d745a62487fb8671725561ca19
Fritz Obermeyer <fritzo@uber.com>
2020-03-03 10:52:40 -0800

    Address review comments

--- pyro/ops/tensor_utils.py
+++ pyro/ops/tensor_utils.py
@@ -111,20 +111,27 @@ def periodic_cumsum(tensor, period, dim):
 
 def periodic_features(duration, max_period=None, min_period=None, **options):
     r"""
-    Create periodic (sin,cos) features from max_period down to min_period.
+    Create periodic (sin,cos) features from ``max_period`` down to
+    ``min_period``.
 
     This is useful in time series models where long uneven seasonality can be
-    treated via regression, and short seasonality can be treated via the
-    :func:`periodic_repeat` and :func:`periodic_cumsum` helpers. For example,
-    to combine yearly with weekly seasonality one could set
+    treated via regression. When only ``max_period`` is specified this
+    generates periodic features at all length scales. When also ``min_period``
+    is specified this generates periodic features at large length scales, but
+    omits high frequecy features. This is useful when combining regression for
+    long seasonality with other techinques like :func:`periodic_repeat` and
+    :func:`periodic_cumsum` for short time scales. For example, to combine
+    regress yearly seasonality down to the scale of one week one could set
     ``max_period=365.25`` and ``min_period=7``.
 
     :param int duration: Number of discrete time steps.
     :param float max_period: Optional max period, defaults to ``duration``.
-    :param float min_period: Optional min period (exclusive), defaults to 2 = Nyquist cutoff.
-    :param \*\*options: Tensor construction options, e.g. ``dtype`` and ``device``.
-    :returns: A ``(duration, 2 * ceil(max_period - min_period) - 2)``-shaped tensor of features
-        normalized to lie in [-1,1].
+    :param float min_period: Optional min period (exclusive), defaults to
+        2 = Nyquist cutoff.
+    :param \*\*options: Tensor construction options, e.g. ``dtype`` and
+        ``device``.
+    :returns: A ``(duration, 2 * ceil(max_period / min_period) - 2)``-shaped
+        tensor of features normalized to lie in [-1,1].
     :rtype: ~torch.Tensor
     """
     assert isinstance(duration, int) and duration >= 0
