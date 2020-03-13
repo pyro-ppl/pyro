3f8cd57682fc9987a9d4235e669473fc988b77d9
Martin Jankowiak <jankowiak@uber.com>
2020-03-13 14:55:03 -0700

    use safe cholesky

--- pyro/ops/tensor_utils.py
+++ pyro/ops/tensor_utils.py
@@ -330,10 +330,52 @@ def idct(x, dim=-1):
     return torch.stack([y, y.flip(-1)], axis=-1).reshape(x.shape[:-1] + (-1,))[..., :N]
 
 
+def psd_safe_cholesky(A, upper=False, out=None, jitter=None):
+    """Compute the Cholesky decomposition of A. If A is only p.s.d, add a small jitter to the diagonal.
+    Args:
+        :attr:`A` (Tensor):
+            The tensor to compute the Cholesky decomposition of
+        :attr:`upper` (bool, optional):
+            See torch.cholesky
+        :attr:`out` (Tensor, optional):
+            See torch.cholesky
+        :attr:`jitter` (float, optional):
+            The jitter to add to the diagonal of A in case A is only p.s.d. If omitted, chosen
+            as 1e-6 (float) or 1e-8 (double)
+    """
+    try:
+        L = torch.cholesky(A, upper=upper, out=out)
+        return L
+    except RuntimeError as e:
+        isnan = torch.isnan(A)
+        if isnan.any():
+            raise NanError(
+                f"cholesky_cpu: {isnan.sum().item()} of {A.numel()} elements of the {A.shape} tensor are NaN."
+            )
+
+        if jitter is None:
+            jitter = 1e-6 if A.dtype == torch.float32 else 1e-8
+        Aprime = A.clone()
+        jitter_prev = 0
+        for i in range(3):
+            jitter_new = jitter * (10 ** i)
+            Aprime.diagonal(dim1=-2, dim2=-1).add_(jitter_new - jitter_prev)
+            jitter_prev = jitter_new
+            try:
+                L = torch.cholesky(Aprime, upper=upper, out=out)
+                warnings.warn(f"A not p.d., added jitter of {jitter_new} to the diagonal", RuntimeWarning)
+                return L
+            except RuntimeError:
+                continue
+        raise e
+
+
 def cholesky(x):
     if x.size(-1) == 1:
         return x.sqrt()
-    return x.cholesky()
+    print("PSD SAFE CHOLESKY")
+    return psd_safe_cholesky(x)
+    #return x.cholesky()
 
 
 def cholesky_solve(x, y):
