8ad5fcc60cfb85d92acd2117d24dacc8952de3e0
Stefan Webb <info@stefanwebb.me>
2020-03-25 21:57:23 -0700

    Asthetic improvements to transform docstrings

--- pyro/distributions/transforms/generalized_channel_permute.py
+++ pyro/distributions/transforms/generalized_channel_permute.py
@@ -9,35 +9,40 @@ from pyro.distributions.torch_transform import TransformModule
 @copy_docs_from(TransformModule)
 class GeneralizedChannelPermute(TransformModule):
     """
-    A bijection that generalizes a permutation on the channels of a batch of 2D image in :math:`[\\ldots,C,H,W]` format.
-    Specifically this transform performs the operation,
+    A bijection that generalizes a permutation on the channels of a batch of 2D
+    image in :math:`[\\ldots,C,H,W]` format. Specifically this transform performs
+    the operation,
 
         :math:`\\mathbf{y} = \\text{torch.nn.functional.conv2d}(\\mathbf{x}, W)`
 
-    where :math:`\\mathbf{x}` are the inputs, :math:`\\mathbf{y}` are the outputs, and
-    :math:`W\\sim C\\times C\\times 1\\times 1` is the filter matrix for a 1x1 convolution with :math:`C` input and
-    output channels.
+    where :math:`\\mathbf{x}` are the inputs, :math:`\\mathbf{y}` are the outputs,
+    and :math:`W\\sim C\\times C\\times 1\\times 1` is the filter matrix for a 1x1
+    convolution with :math:`C` input and output channels.
 
-    Ignoring the final two dimensions, :math:`W` is restricted to be the matrix product,
+    Ignoring the final two dimensions, :math:`W` is restricted to be the matrix
+    product,
 
         :math:`W = PLU`
 
-    where :math:`P\\sim C\\times C` is a permutation matrix on the channel dimensions, :math:`L\\sim C\\times C` is a
-    lower triangular matrix with ones on the diagonal, and :math:`U\\sim C\\times C` is an upper triangular matrix.
-    :math:`W` is initialized to a random orthogonal matrix. Then, :math:`P` is fixed and the learnable parameters set
-    to :math:`L,U`.
+    where :math:`P\\sim C\\times C` is a permutation matrix on the channel
+    dimensions, :math:`L\\sim C\\times C` is a lower triangular matrix with ones on
+    the diagonal, and :math:`U\\sim C\\times C` is an upper triangular matrix.
+    :math:`W` is initialized to a random orthogonal matrix. Then, :math:`P` is fixed
+    and the learnable parameters set to :math:`L,U`.
 
-    The input :math:`\\mathbf{x}` and output :math:`\\mathbf{y}` both have shape `[...,C,H,W]`, where `C` is the number
-    of channels set at initialization.
+    The input :math:`\\mathbf{x}` and output :math:`\\mathbf{y}` both have shape
+    `[...,C,H,W]`, where `C` is the number of channels set at initialization.
 
-    This operation was introduced in [1] for Glow normalizing flow, and is also known as 1x1 invertible convolution.
-    It appears in other notable work such as [2,3], and corresponds to the class `tfp.bijectors.MatvecLU` of TensorFlow
+    This operation was introduced in [1] for Glow normalizing flow, and is also
+    known as 1x1 invertible convolution. It appears in other notable work such as
+    [2,3], and corresponds to the class `tfp.bijectors.MatvecLU` of TensorFlow
     Probability.
 
     Example usage:
 
     >>> channels = 3
-    >>> base_dist = dist.Normal(torch.zeros(channels, 32, 32), torch.ones(channels, 32, 32))
+    >>> base_dist = dist.Normal(torch.zeros(channels, 32, 32),
+    ... torch.ones(channels, 32, 32))
     >>> inv_conv = GeneralizedChannelPermute(channels=channels)
     >>> flow_dist = dist.TransformedDistribution(base_dist, [inv_conv])
     >>> flow_dist.sample()  # doctest: +SKIP
@@ -45,14 +50,14 @@ class GeneralizedChannelPermute(TransformModule):
     :param channels: Number of channel dimensions in the input.
     :type channels: int
 
-    1. Glow: Generative Flow with Invertible 1x1 Convolutions. [arXiv:1807.03039]
-    Diederik P. Kingma, Prafulla Dhariwal.
+    [1] Diederik P. Kingma, Prafulla Dhariwal. Glow: Generative Flow with Invertible
+    1x1 Convolutions. [arXiv:1807.03039]
 
-    2. WaveGlow: A Flow-based Generative Network for Speech Synthesis. [arXiv:1811.00002]
-    Ryan Prenger, Rafael Valle, Bryan Catanzaro.
+    [2] Ryan Prenger, Rafael Valle, Bryan Catanzaro. WaveGlow: A Flow-based
+    Generative Network for Speech Synthesis. [arXiv:1811.00002]
 
-    3. Neural Spline Flows. [arXiv:1906.04032]
-    Conor Durkan, Artur Bekasov, Iain Murray, George Papamakarios.
+    [3] Conor Durkan, Artur Bekasov, Iain Murray, George Papamakarios. Neural Spline
+    Flows. [arXiv:1906.04032]
 
     """
 
@@ -87,8 +92,8 @@ class GeneralizedChannelPermute(TransformModule):
         :type x: torch.Tensor
 
         Invokes the bijection x=>y; in the prototypical context of a
-        :class:`~pyro.distributions.TransformedDistribution` `x` is a sample from the base distribution (or the output
-        of a previous transform)
+        :class:`~pyro.distributions.TransformedDistribution` `x` is a sample from
+        the base distribution (or the output of a previous transform)
         """
 
         # Extract the lower and upper matrices from the packed LU matrix
@@ -111,8 +116,8 @@ class GeneralizedChannelPermute(TransformModule):
         """
 
         """
-        NOTE: This method is equivalent to the following two lines. Using Tensor.inverse() would be
-        numerically unstable, however.
+        NOTE: This method is equivalent to the following two lines. Using
+        Tensor.inverse() would be numerically unstable, however.
 
         U = self.LU.triu()
         L = self.LU.tril()
@@ -141,7 +146,8 @@ class GeneralizedChannelPermute(TransformModule):
 
     def log_abs_det_jacobian(self, x, y):
         """
-        Calculates the elementwise determinant of the log Jacobian, i.e. log(abs(det(dy/dx))).
+        Calculates the elementwise determinant of the log Jacobian, i.e.
+        log(abs(det(dy/dx))).
         """
 
         h, w = x.shape[-2:]
@@ -152,7 +158,8 @@ class GeneralizedChannelPermute(TransformModule):
 
 def generalized_channel_permute(**kwargs):
     """
-    A helper function to create a :class:`~pyro.distributions.transforms.GeneralizedChannelPermute` object for
+    A helper function to create a
+    :class:`~pyro.distributions.transforms.GeneralizedChannelPermute` object for
     consistency with other helpers.
 
     :param channels: Number of channel dimensions in the input.
