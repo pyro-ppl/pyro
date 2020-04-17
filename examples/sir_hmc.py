1f5f55d4ce72f748f78df8694ca9a68ae375744e
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2020-04-17 08:11:33 -0700

    Add test for init logic

--- examples/sir_hmc.py
+++ examples/sir_hmc.py
@@ -15,6 +15,7 @@ import pyro.poutine as poutine
 from pyro.distributions.transforms.discrete_cosine import DiscreteCosineTransform
 from pyro.infer import MCMC, NUTS, config_enumerate
 from pyro.infer.autoguide import init_to_value
+from pyro.infer.autoguide.initialization import InitMessenger
 from pyro.infer.reparam import DiscreteCosineReparam
 from pyro.ops.tensor_utils import convolve, safe_log
 from pyro.util import warn_if_nan
@@ -207,8 +208,45 @@ def continuous_model(data, population):
 
 
 # Now all latent variables in the continuous_model are either continuous or
-# enumerated, so we can use HMC.
-#
+# enumerated, so we can use HMC. However we need to take special care with
+# constraints because the above Markov reparameterization covers regions of
+# hypothesis space that are infeasible (i.e. whose log_prob is -infinity). We
+# thus heuristically initialize to a feasible point.
+
+def heuristic_init(args, data):
+    """Heuristically initialize to a good feasible point."""
+    S0 = args.population - 1  # Start with a single infection.
+    S2I = data * min(2., S0 / data.sum())  # Assume 50% response rate.
+    S2I[0] += 1  # Account for the single infection.
+    S = (S0 - S2I.cumsum(-1)).clamp_(min=0.5)
+    # Assume infection lasts less than a month.
+    recovery = (1 - args.recovery_rate / 2) ** torch.arange(30.)
+    I = convolve(S2I, recovery)[:len(data)].clamp_(min=0.5)
+    # Also initialize DCT transformed coordinates.
+    t = ComposeTransform([biject_to(constraints.interval(-0.5, args.population + 0.5)).inv,
+                          DiscreteCosineTransform(dim=-1)])
+
+    if __debug__:
+        S_prev = torch.nn.functional.pad(S[:-1], (1, 0), value=args.population - 1)
+        I_prev = torch.nn.functional.pad(I[:-1], (1, 0), value=1)
+        S2I = S_prev - S
+        I2R = I_prev - I + S2I
+        assert (data <= S2I).all()
+        assert (S2I <= S).all()
+        assert (0 <= I2R).all()
+        assert (I2R <= I).all()
+
+    return {
+        "prob_s": torch.tensor(0.5),
+        "prob_i": torch.tensor(0.5),
+        "rho": torch.tensor(0.5),
+        "S_aux": S,
+        "I_aux": I,
+        "S_aux_dct": t(S),
+        "I_aux_dct": t(I),
+    }
+
+
 # One trick to improve inference geometry is to reparameterize the S_aux,I_aux
 # variables via DiscreteCosineReparam. This especially allows HMC's diagonal mass
 # matrix to learn different step sizes for high- and low-frequency directions.
@@ -218,27 +256,7 @@ def infer_hmc_cont(model, args, data):
     if args.dct:
         model = poutine.reparam(model, {"S_aux": DiscreteCosineReparam(),
                                         "I_aux": DiscreteCosineReparam()})
-
-    # Heuristically initialize to a good feasible point.
-    S0 = args.population - 1  # Start with a single infection.
-    S2I = data * min(2., S0 / data.sum())  # Assume 50% response rate.
-    S2I[0] += 1  # Account for the single infection.
-    S_aux = (S0 - S2I.cumsum(-1)).clamp_(min=0.5)
-    # Assume infection lasts a few days.
-    I_aux = convolve(S2I, torch.tensor([1., 0.8, 0.5, 0.3, 0.2]))[:len(data)].clamp_(min=0.5)
-    # Also initialze DCT coordinates.
-    t = ComposeTransform([biject_to(constraints.interval(-0.5, 0.5 + args.population)).inv,
-                          DiscreteCosineTransform(dim=-1)])
-    init_values = {
-        "prob_s": torch.tensor(0.5),
-        "prob_i": torch.tensor(0.5),
-        "rho": torch.tensor(0.5),
-        "S_aux": S_aux,
-        "I_aux": I_aux,
-        "S_aux_dct": t(S_aux),
-        "I_aux_dct": t(I_aux),
-    }
-
+    init_values = heuristic_init(args, data)
     return _infer_hmc(args, data, model, init_values=init_values)
 
 
@@ -314,6 +332,18 @@ def main(args):
 
     data = generate_data(args)
 
+    # Optionally test the goodness of our initialization heuristic.
+    if args.test_init:
+        init_values = heuristic_init(args, data)
+        with InitMessenger(init_to_value(values=init_values)):
+            trace = poutine.trace(vectorized_model).get_trace(data, args.population)
+        log_prob = trace.log_prob_sum()
+        logging.info("log_prob = {:0.6g}".format(log_prob))
+        if not torch.isfinite(log_prob):
+            for key, value in init_values.items():
+                logging.info("{}:\n{}".format(key, value))
+            raise ValueError("infeasible initialization")
+
     # Choose among inference methods.
     if args.enum:
         samples = infer_hmc_enum(args, data)
@@ -352,12 +382,20 @@ if __name__ == "__main__":
     parser.add_argument("-n", "--num-samples", default=200, type=int)
     parser.add_argument("-w", "--warmup-steps", default=100, type=int)
     parser.add_argument("--max-tree-depth", default=4, type=int)
-    parser.add_argument("--rng-seed", nargs='?', default=0, type=int)
+    parser.add_argument("--rng-seed", default=0, type=int)
+    parser.add_argument("--test-init", action="store_true")
+    parser.add_argument("--double", action="store_true")
     parser.add_argument("--jit", action="store_true")
     parser.add_argument("--cuda", action="store_true")
     parser.add_argument("--verbose", action="store_true")
     args = parser.parse_args()
 
-    if args.cuda:
+    if args.double:
+        if args.cuda:
+            torch.set_default_tensor_type(torch.cuda.DoubleTensor)
+        else:
+            torch.set_default_tensor_type(torch.DoubleTensor)
+    elif args.cuda:
         torch.set_default_tensor_type(torch.cuda.FloatTensor)
+
     main(args)
