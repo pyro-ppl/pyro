e284f6402cac52b1e50157a035afcc5c4b24b474
Fritz Obermeyer <fritzo@uber.com>
2020-04-24 09:12:44 -0700

    Allow negative total_count in Extended distributions (#2439)

--- pyro/distributions/extended.py
+++ pyro/distributions/extended.py
@@ -12,9 +12,13 @@ from .torch import Binomial
 class ExtendedBinomial(Binomial):
     """
     EXPERIMENTAL :class:`~pyro.distributions.Binomial` distribution extended to
-    have logical support the entire integers. Numerical support is still the
-    integer interval ``[0, total_count]``
+    have logical support the entire integers and to allow arbitrary integer
+    ``total_count``. Numerical support is still the integer interval ``[0,
+    total_count]``.
     """
+    arg_constraints = {"total_count": constraints.integer,
+                       "probs": constraints.unit_interval,
+                       "logits": constraints.real}
     support = constraints.integer
 
     def log_prob(self, value):
@@ -26,9 +30,13 @@ class ExtendedBinomial(Binomial):
 class ExtendedBetaBinomial(BetaBinomial):
     """
     EXPERIMENTAL :class:`~pyro.distributions.BetaBinomial` distribution
-    extended to have logical support the entire integers. Numerical support is
-    still the integer interval ``[0, total_count]``
+    extended to have logical support the entire integers and to allow arbitrary
+    integer ``total_count``. Numerical support is still the integer interval
+    ``[0, total_count]``.
     """
+    arg_constraints = {"concentration1": constraints.positive,
+                       "concentration0": constraints.positive,
+                       "total_count": constraints.integer}
     support = constraints.integer
 
     def log_prob(self, value):
