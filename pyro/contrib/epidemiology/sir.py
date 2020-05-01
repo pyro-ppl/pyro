5c7f1c369fcb63cb9432a5b326bc7bc769f93c3d
Fritz Obermeyer <fritzo@uber.com>
2020-05-01 09:42:05 -0700

    Add example of sparsely observed SIR model (#2457)
    * Add example of sparsely observed SIR model
    
    * Fix typo

--- pyro/contrib/epidemiology/sir.py
+++ pyro/contrib/epidemiology/sir.py
@@ -22,7 +22,6 @@ class SimpleSIRModel(CompartmentalModel):
     :param int population: Total ``population = S + I + R``.
     :param float recovery_time: Mean recovery time (duration in state
         ``I``). Must be greater than 1.
-    :param iterable data: Time series of new observed infections.
     :param iterable data: Time series of new observed infections. Each time
         step is Binomial distributed between 0 and the number of ``S -> I``
         transitions. This allows false negative but no false positives.
@@ -136,7 +135,6 @@ class OverdispersedSIRModel(CompartmentalModel):
     :param int population: Total ``population = S + I + R``.
     :param float recovery_time: Mean recovery time (duration in state
         ``I``). Must be greater than 1.
-    :param iterable data: Time series of new observed infections.
     :param iterable data: Time series of new observed infections. Each time
         step is Binomial distributed between 0 and the number of ``S -> I``
         transitions. This allows false negative but no false positives.
@@ -211,4 +209,112 @@ class OverdispersedSIRModel(CompartmentalModel):
         # Condition on observations.
         pyro.sample("obs_{}".format(t),
                     dist.ExtendedBinomial(S2I, rho),
+                    obs=self.data[t])
+
+
+class SparseSIRModel(CompartmentalModel):
+    """
+    Susceptible-Infected-Recovered model with sparsely observed infections.
+
+    To customize this model we recommend forking and editing this class.
+
+    This is a stochastic discrete-time discrete-state model with four
+    compartments: "S" for susceptible, "I" for infected, and "R" for
+    recovered individuals (the recovered individuals are implicit: ``R =
+    population - S - I``) with transitions ``S -> I -> R``.
+
+    This model allows observations of **cumulative** infections at uneven time
+    intervals. To preserve Markov structure (and hence tractable inference)
+    this model adds an auxiliary compartment ``O`` denoting the fully-observed
+    cumulative number of observations at each time point. At observed times
+    (when ``mask[t] == True``) ``O`` must exactly match the provided data;
+    between observed times ``O`` stochastically imputes the provided data.
+
+    :param int population: Total ``population = S + I + R``.
+    :param float recovery_time: Mean recovery time (duration in state
+        ``I``). Must be greater than 1.
+    :param iterable data: Time series of **cumulative** observed infections.
+        Whenever ``mask[t] == True``, ``data[t]`` corresponds to an
+        observation; otherwise ``data[t]`` can be arbitrary, e.g. NAN.
+    :param iterable mask: Boolean time series denoting whether an observation
+        is made at each time step. Should satisfy ``len(mask) == len(data)``.
+    """
+
+    def __init__(self, population, recovery_time, data, mask):
+        assert len(data) == len(mask)
+        duration = len(data)
+        compartments = ("S", "I", "O")  # O is auxiliary, R is implicit.
+        super().__init__(compartments, duration, population)
+
+        assert isinstance(recovery_time, float)
+        assert recovery_time > 1
+        self.recovery_time = recovery_time
+
+        self.data = data
+        self.mask = mask
+
+    series = ("S2I", "I2R", "S2O", "obs")
+    full_mass = [("R0", "rho")]
+
+    def global_model(self):
+        tau = self.recovery_time
+        R0 = pyro.sample("R0", dist.LogNormal(0., 1.))
+        rho = pyro.sample("rho", dist.Uniform(0, 1))
+        return R0, tau, rho
+
+    def initialize(self, params):
+        # Start with a single infection.
+        return {"S": self.population - 1, "I": 1, "O": 0}
+
+    def transition_fwd(self, params, state, t):
+        R0, tau, rho = params
+
+        # Sample flows between compartments.
+        S2I = pyro.sample("S2I_{}".format(t),
+                          infection_dist(individual_rate=R0 / tau,
+                                         num_susceptible=state["S"],
+                                         num_infectious=state["I"],
+                                         population=self.population))
+        I2R = pyro.sample("I2R_{}".format(t),
+                          dist.Binomial(state["I"], 1 / tau))
+        S2O = pyro.sample("S2O_{}".format(t),
+                          dist.ExtendedBinomial(S2I, rho))
+
+        # Update compartments with flows.
+        state["S"] = state["S"] - S2I
+        state["I"] = state["I"] + S2I - I2R
+        state["O"] = state["O"] + S2O
+
+        # Condition on cumulative observations.
+        mask_t = self.mask[t] if t < self.duration else False
+        data_t = self.data[t] if t < self.duration else None
+        pyro.sample("obs_{}".format(t),
+                    dist.Delta(state["O"]).mask(mask_t),
+                    obs=data_t)
+
+    def transition_bwd(self, params, prev, curr, t):
+        R0, tau, rho = params
+
+        # Reverse the flow computation.
+        S2I = prev["S"] - curr["S"]
+        I2R = prev["I"] - curr["I"] + S2I
+        S2O = curr["O"] - prev["O"]
+
+        # Condition on flows between compartments.
+        pyro.sample("S2I_{}".format(t),
+                    infection_dist(individual_rate=R0 / tau,
+                                   num_susceptible=prev["S"],
+                                   num_infectious=prev["I"],
+                                   population=self.population),
+                    obs=S2I)
+        pyro.sample("I2R_{}".format(t),
+                    dist.ExtendedBinomial(prev["I"], 1 / tau),
+                    obs=I2R)
+        pyro.sample("S2O_{}".format(t),
+                    dist.ExtendedBinomial(S2I, rho),
+                    obs=S2O)
+
+        # Condition on cumulative observations.
+        pyro.sample("obs_{}".format(t),
+                    dist.Delta(curr["O"]).mask(self.mask[t]),
                     obs=self.data[t])
