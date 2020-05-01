06e82cc6c11a5e768c2c78282d986b12fa758da9
Eli <eli.bingham@uber.com>
2020-05-01 11:02:54 -0700

    consolidate the two tracemessengers

--- pyro/contrib/funsor/handlers/trace_messenger.py
+++ pyro/contrib/funsor/handlers/trace_messenger.py
@@ -12,33 +12,31 @@ from pyro.contrib.funsor.handlers.named_messenger import NamedMessenger
 
 class TraceMessenger(OrigTraceMessenger):
     """
-    This version of TraceMessenger does its packing online instead of after the fact,
+    Setting ``pack_online=True`` packs online instead of after the fact,
     converting all distributions and values to Funsors as soon as they are available.
-    """
-    def _pyro_post_sample(self, msg):
-        if isinstance(msg["fn"], _Subsample):
-            return super()._pyro_post_sample(msg)
-        if "funsor_fn" not in msg["infer"]:
-            msg["infer"]["funsor_fn"] = to_funsor(msg["fn"], funsor.reals())
-        if "funsor_log_prob" not in msg["infer"]:
-            msg["infer"]["funsor_log_prob"] = to_funsor(msg["fn"].log_prob(msg["value"]),
-                                                        funsor.reals())
-        if "funsor_value" not in msg["infer"]:
-            value_output = funsor.reals(*getattr(msg["fn"], "event_shape", ()))
-            msg["infer"]["funsor_value"] = to_funsor(msg["value"], value_output)
-        return super()._pyro_post_sample(msg)
 
-
-class PackTraceMessenger(OrigTraceMessenger):
-    """
-    This version of TraceMessenger records information necessary to do packing after execution.
+    Setting ``pack_online=False`` computes information necessary to do packing after execution.
     Each sample site is annotated with a "dim_to_name" dictionary,
     which can be passed directly to funsor.to_funsor.
     """
+    def __init__(self, graph_type=None, param_only=None, pack_online=None):
+        super().__init__(graph_type=graph_type, param_only=param_only)
+        self.pack_online = True if pack_online is None else pack_online
+
     def _pyro_post_sample(self, msg):
         if isinstance(msg["fn"], _Subsample):
             return super()._pyro_post_sample(msg)
-        msg["infer"]["dim_to_name"] = NamedMessenger._get_dim_to_name(msg["fn"].batch_shape)
-        msg["infer"]["dim_to_name"].update(NamedMessenger._get_dim_to_name(
-            msg["value"].shape[:len(msg["value"]).shape - len(msg["fn"].event_shape)]))
+        if self.pack_online:
+            if "funsor_fn" not in msg["infer"]:
+                msg["infer"]["funsor_fn"] = to_funsor(msg["fn"], funsor.reals())
+            if "funsor_log_prob" not in msg["infer"]:
+                msg["infer"]["funsor_log_prob"] = to_funsor(msg["fn"].log_prob(msg["value"]),
+                                                            funsor.reals())
+            if "funsor_value" not in msg["infer"]:
+                value_output = funsor.reals(*getattr(msg["fn"], "event_shape", ()))
+                msg["infer"]["funsor_value"] = to_funsor(msg["value"], value_output)
+        else:
+            msg["infer"]["dim_to_name"] = NamedMessenger._get_dim_to_name(msg["fn"].batch_shape)
+            msg["infer"]["dim_to_name"].update(NamedMessenger._get_dim_to_name(
+                msg["value"].shape[:len(msg["value"]).shape - len(msg["fn"].event_shape)]))
         return super()._pyro_post_sample(msg)
--- pyro/contrib/funsor/infer.py
+++ pyro/contrib/funsor/infer.py
@@ -3,7 +3,7 @@
 
 import funsor
 
-from pyro.infer import ELBO, SVI
+from pyro.infer import ELBO, SVI  # noqa: F401
 from pyro.poutine.util import prune_subsample_sites
 
 from pyro.contrib.funsor import to_data, to_funsor
