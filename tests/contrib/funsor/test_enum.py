156024dba90973b7e9a32361f3345d553f7c2c12
Eli <eli.bingham@uber.com>
2020-04-30 17:26:33 -0700

    add a new enumeration inference test file and rename test_named to test_valid_models

---
+++ tests/contrib/funsor/test_enum.py
@@ -0,0 +1,183 @@
+# Copyright Contributors to the Pyro project.
+# SPDX-License-Identifier: Apache-2.0
+
+import contextlib
+import logging
+import math
+
+import pytest
+import torch
+from torch.autograd import grad
+from torch.distributions import constraints
+
+import pyro
+import pyro.distributions as dist
+import pyro.poutine as poutine
+from pyro.distributions.testing import fakes
+from pyro.infer import config_enumerate
+from pyro.infer.importance import vectorized_importance_weights
+from pyro.infer.tracetmc_elbo import TraceTMC_ELBO as PyroTMC_ELBO
+from pyro.infer.traceenum_elbo import TraceEnum_ELBO as PyroEnum_ELBO
+from tests.common import assert_equal
+
+from pyro.contrib.funsor import to_data, to_funsor, markov
+from pyro.contrib.funsor.named_messenger import _DIM_STACK, GlobalNamedMessenger
+from pyro.contrib.funsor.enum_messenger import EnumMessenger, PlateMessenger, TraceMessenger, enum_seq
+from pyro.contrib.funsor.infer import TraceTMC_ELBO as FunsorTMC_ELBO
+from pyro.contrib.funsor.infer import TraceEnum_ELBO as FunsorEnum_ELBO
+
+import funsor
+funsor.set_backend("torch")
+torch.set_default_dtype(torch.float32)
+
+
+logger = logging.getLogger(__name__)
+
+
+_ENUM_BACKEND_VERSION = "pyro"
+
+
+@contextlib.contextmanager
+def toggle_backend(backend):
+    global _ENUM_BACKEND_VERSION
+    old_version = _ENUM_BACKEND_VERSION
+    _ENUM_BACKEND_VERSION = backend
+    try:
+        yield
+    finally:
+        _ENUM_BACKEND_VERSION = old_version
+
+
+def pyro_plate(*args, **kwargs):
+    global _ENUM_BACKEND_VERSION
+    # TODO update to plate wrapper from pyro.contrib.funsor.__init__
+    return (PlateMessenger if _ENUM_BACKEND_VERSION == "funsor" else pyro.plate)(*args, **kwargs)
+
+
+def pyro_markov(*args, **kwargs):
+    global _ENUM_BACKEND_VERSION
+    return (markov if _ENUM_BACKEND_VERSION == "funsor" else pyro.markov)(*args, **kwargs)
+
+
+def TraceEnum_ELBO(*args, **kwargs):
+    global _ENUM_BACKEND_VERSION
+    return (FunsorEnum_ELBO if _ENUM_BACKEND_VERSION == "funsor" else PyroEnum_ELBO)(*args, **kwargs)
+
+
+def TraceTMC_ELBO(*args, **kwargs):
+    global _ENUM_BACKEND_VERSION
+    return (FunsorTMC_ELBO if _ENUM_BACKEND_VERSION == "funsor" else PyroTMC_ELBO)(*args, **kwargs)
+
+
+@pytest.mark.parametrize("depth", [1, 2, 3, 4, 5])
+@pytest.mark.parametrize("num_samples", [None, 200])
+@pytest.mark.parametrize("max_plate_nesting", [2, 3])
+@pytest.mark.parametrize("tmc_strategy", ["diagonal"])
+def test_tmc_categoricals(depth, max_plate_nesting, num_samples, tmc_strategy):
+    qs = [pyro.param("q0", torch.tensor([0.4, 0.6], requires_grad=True))]
+    for i in range(1, depth):
+        qs.append(pyro.param(
+            "q{}".format(i),
+            torch.randn(2, 2).abs().detach().requires_grad_(),
+            constraint=constraints.simplex
+        ))
+    qs.append(pyro.param("qy", torch.tensor([0.75, 0.25], requires_grad=True)))
+
+    qs = [q.unconstrained() for q in qs]
+
+    data = (torch.rand(4, 3) > 0.5).to(dtype=qs[-1].dtype, device=qs[-1].device)
+
+    def model():
+        x = pyro.sample("x0", dist.Categorical(pyro.param("q0")))
+        with pyro_plate("local", 3):
+            for i in range(1, depth):
+                x = pyro.sample("x{}".format(i),
+                                dist.Categorical(pyro.param("q{}".format(i))[..., x, :]))
+            with pyro_plate("data", 4):
+                pyro.sample("y", dist.Bernoulli(pyro.param("qy")[..., x]),
+                            obs=data)
+
+    with toggle_backend("pyro"):
+        elbo = TraceTMC_ELBO(max_plate_nesting=max_plate_nesting)
+        enum_model = config_enumerate(model, default="parallel", expand=False, num_samples=None, tmc=tmc_strategy)
+        expected_loss = (-elbo.differentiable_loss(enum_model, lambda: None)).exp()
+        expected_grads = grad(expected_loss, qs)
+
+    with toggle_backend("funsor"):
+        tmc = TraceTMC_ELBO(max_plate_nesting=max_plate_nesting)
+        tmc_model = config_enumerate(model, default="parallel", expand=False, num_samples=num_samples, tmc=tmc_strategy)
+        actual_loss = (-tmc.differentiable_loss(tmc_model, lambda: None)).exp()
+        actual_grads = grad(actual_loss, qs)
+
+    prec = 0.05
+    assert_equal(actual_loss, expected_loss, prec=prec, msg="".join([
+        "\nexpected loss = {}".format(expected_loss),
+        "\n  actual loss = {}".format(actual_loss),
+    ]))
+
+    for actual_grad, expected_grad in zip(actual_grads, expected_grads):
+        assert_equal(actual_grad, expected_grad, prec=prec, msg="".join([
+            "\nexpected grad = {}".format(expected_grad.detach().cpu().numpy()),
+            "\n  actual grad = {}".format(actual_grad.detach().cpu().numpy()),
+        ]))
+
+
+@pytest.mark.parametrize("depth", [1, 2, 3, 4])
+@pytest.mark.parametrize("num_samples,expand", [(200, False)])
+@pytest.mark.parametrize("max_plate_nesting", [1])
+@pytest.mark.parametrize("guide_type", ["prior", "factorized", "nonfactorized"])
+@pytest.mark.parametrize("reparameterized", [False, True])
+@pytest.mark.parametrize("tmc_strategy", ["diagonal"])
+def test_tmc_normals_chain_gradient(depth, num_samples, max_plate_nesting, expand,
+                                    guide_type, reparameterized, tmc_strategy):
+    # compare reparameterized and nonreparameterized gradient estimates
+    q2 = pyro.param("q2", torch.tensor(0.5, requires_grad=True))
+    qs = (q2.unconstrained(),)
+
+    def model(reparameterized):
+        Normal = dist.Normal if reparameterized else fakes.NonreparameterizedNormal
+        x = pyro.sample("x0", Normal(pyro.param("q2"), math.sqrt(1. / depth)))
+        for i in range(1, depth):
+            x = pyro.sample("x{}".format(i), Normal(x, math.sqrt(1. / depth)))
+        pyro.sample("y", Normal(x, 1.), obs=torch.tensor(float(1)))
+
+    def factorized_guide(reparameterized):
+        Normal = dist.Normal if reparameterized else fakes.NonreparameterizedNormal
+        pyro.sample("x0", Normal(pyro.param("q2"), math.sqrt(1. / depth)))
+        for i in range(1, depth):
+            pyro.sample("x{}".format(i), Normal(0., math.sqrt(float(i+1) / depth)))
+
+    def nonfactorized_guide(reparameterized):
+        Normal = dist.Normal if reparameterized else fakes.NonreparameterizedNormal
+        x = pyro.sample("x0", Normal(pyro.param("q2"), math.sqrt(1. / depth)))
+        for i in range(1, depth):
+            x = pyro.sample("x{}".format(i), Normal(x, math.sqrt(1. / depth)))
+
+    with toggle_backend("funsor"):
+        tmc = TraceTMC_ELBO(max_plate_nesting=max_plate_nesting)
+        tmc_model = config_enumerate(
+            model, default="parallel", expand=expand, num_samples=num_samples, tmc=tmc_strategy)
+        guide = factorized_guide if guide_type == "factorized" else \
+            nonfactorized_guide if guide_type == "nonfactorized" else \
+            lambda *args: None
+        tmc_guide = config_enumerate(
+            guide, default="parallel", expand=expand, num_samples=num_samples, tmc=tmc_strategy)
+
+        # convert to linear space for unbiasedness
+        actual_loss = (-tmc.differentiable_loss(tmc_model, tmc_guide, reparameterized)).exp()
+        actual_grads = grad(actual_loss, qs)
+
+    # gold values from Funsor
+    expected_grads = (torch.tensor(
+        {1: 0.0999, 2: 0.0860, 3: 0.0802, 4: 0.0771}[depth]
+    ),)
+
+
+    grad_prec = 0.05 if reparameterized else 0.1
+
+    for actual_grad, expected_grad in zip(actual_grads, expected_grads):
+        print(actual_loss)
+        assert_equal(actual_grad, expected_grad, prec=grad_prec, msg="".join([
+            "\nexpected grad = {}".format(expected_grad.detach().cpu().numpy()),
+            "\n  actual grad = {}".format(actual_grad.detach().cpu().numpy()),
+        ]))
