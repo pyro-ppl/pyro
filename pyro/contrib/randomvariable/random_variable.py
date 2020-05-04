60c2f9c72c3fad7841f79da0fca83212189d098b
ecotner <2.71828cotner@gmail.com>
2020-05-04 18:31:17 -0400

    adding/updating sphinx-style documentation

--- pyro/contrib/randomvariable/random_variable.py
+++ pyro/contrib/randomvariable/random_variable.py
@@ -18,7 +18,7 @@ from pyro.distributions.transforms import (
 
 
 class RVMagicOps:
-    """Mixin class for overloading __magic__ operations on random variables
+    """Mixin class for overloading __magic__ operations on random variables.
     """
 
     def __add__(self, x: Union[float, Tensor]):
@@ -54,7 +54,7 @@ class RVMagicOps:
 
 class RVChainOps:
     """Mixin class for performing common unary/binary operations on/between
-    random variables/constant tensors
+    random variables/constant tensors using method chaining syntax.
     """
 
     def add(self, x):
@@ -95,7 +95,7 @@ class RVChainOps:
 
 
 class RandomVariable(RVMagicOps, RVChainOps):
-    """Random variable container class around a distribution
+    """EXPERIMENTAL random variable container class around a distribution
 
     Representation of a distribution interpreted as a random variable. Rather
     than directly manipulating a probability density by applying pointwise
@@ -104,9 +104,29 @@ class RandomVariable(RVMagicOps, RVChainOps):
     consider using the `transform` method. Note that if you perform a
     non-invertible transform (like `abs(X)` or `X**2`), certain things might
     not work properly.
+
+    Can switch between `RandomVariable` and `Distribution` objects with the
+    convenient `Distribution.rv` and `RandomVariable.dist` properties.
+
+    Supports either chaining operations or arithmetic operator overloading.
+
+    Example usage::
+
+        # This should be equivalent to an Exponential distribution.
+        RandomVariable(Uniform(0, 1)).log().neg().dist
+
+        # These two distributions Y1, Y2 should be the same
+        X = Uniform(0, 1).rv
+        Y1 = X.mul(4).pow(0.5).sub(1).abs().neg().dist
+        Y2 = (-abs((4*X)**(0.5) - 1)).dist
     """
 
     def __init__(self, distribution):
+        """Wraps a distribution as a RandomVariable
+
+        :param distribution: The `Distribution` object to wrap
+        :type distribution: ~pyro.distributions.distribution.Distribution
+        """
         self.distribution = distribution
 
     def transform(self, t: Transform):
@@ -116,11 +136,20 @@ class RandomVariable(RVMagicOps, RVChainOps):
             applied to the distribution. There are many examples to be found in
             `torch.distributions.transforms` and `pyro.distributions.transforms`,
             or you can subclass directly from `Transform`.
-        :type t: `Transform`
+        :type t: ~pyro.distributions.transforms.Transform
+
+        :return: The transformed `RandomVariable`
+        :rtype: ~pyro.contrib.randomvariable.random_variable.RandomVariable
         """
         dist = TransformedDistribution(self.distribution, t)
         return RandomVariable(dist)
 
     @property
     def dist(self):
+        """Convenience property for exposing the distribution underlying the
+        random variable.
+
+        :return: The `Distribution` object underlying the random variable
+        :rtype: ~pyro.distributions.distribution.Distribution
+        """
         return self.distribution
