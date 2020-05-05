dd6b52859cd8eda776b75f2b5ee757a76f17f145
Nick Pawlowski <pawlowski.nick@gmail.com>
2020-05-05 16:46:03 +0100

    use relative import to avoid importing top level pyro

--- pyro/distributions/conditional.py
+++ pyro/distributions/conditional.py
@@ -3,9 +3,10 @@
 
 from abc import ABC, abstractmethod
 
-import pyro
 import torch
 import torch.nn
+
+from .torch import TransformedDistribution
 
 
 class ConditionalDistribution(ABC):
@@ -64,4 +65,4 @@ class ConditionalTransformedDistribution(ConditionalDistribution):
     def condition(self, context):
         base_dist = self.base_dist.condition(context)
         transforms = [t.condition(context) for t in self.transforms]
-        return pyro.distributions.TransformedDistribution(base_dist, transforms)
+        return TransformedDistribution(base_dist, transforms)
