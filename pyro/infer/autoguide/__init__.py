ff5ade40aa99d820d381000be9e0845e505f0f86
Fritz Obermeyer <fritzo@uber.com>
2020-05-15 15:57:03 -0700

    Add initialization strategy that randomly samples values (#2482)
    * Add init_to_generated strategy
    
    * Use init_to_generated in CompartmentalModel
    
    * Address review comments

--- pyro/infer/autoguide/__init__.py
+++ pyro/infer/autoguide/__init__.py
@@ -5,10 +5,9 @@ from pyro.infer.autoguide.guides import (AutoCallable, AutoContinuous, AutoDelta
                                          AutoDiscreteParallel, AutoGuide, AutoGuideList, AutoIAFNormal,
                                          AutoLaplaceApproximation, AutoLowRankMultivariateNormal,
                                          AutoMultivariateNormal, AutoNormal, AutoNormalizingFlow)
-from pyro.infer.autoguide.utils import mean_field_entropy
-from pyro.infer.autoguide.initialization import (init_to_feasible, init_to_mean, init_to_median,
+from pyro.infer.autoguide.initialization import (init_to_feasible, init_to_generated, init_to_mean, init_to_median,
                                                  init_to_sample, init_to_uniform, init_to_value)
-
+from pyro.infer.autoguide.utils import mean_field_entropy
 
 __all__ = [
     'AutoCallable',
@@ -25,6 +24,7 @@ __all__ = [
     'AutoNormal',
     'AutoNormalizingFlow',
     'init_to_feasible',
+    'init_to_generated',
     'init_to_mean',
     'init_to_median',
     'init_to_sample',
