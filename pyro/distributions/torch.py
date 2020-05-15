d1ab1312d102dd0bb989d64968f74af06121d605
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2020-05-15 11:47:36 -0700

    Add binomial approx

--- pyro/distributions/torch.py
+++ pyro/distributions/torch.py
@@ -1,6 +1,9 @@
 # Copyright (c) 2017-2019 Uber Technologies, Inc.
 # SPDX-License-Identifier: Apache-2.0
 
+import math
+import numbers
+
 import torch
 from torch.distributions import constraints
 from torch.distributions.utils import lazy_property
@@ -27,6 +30,48 @@ class Beta(torch.distributions.Beta, TorchDistributionMixin):
 
         log_normalizer = _log_normalizer(self) + _log_normalizer(other) - _log_normalizer(updated)
         return updated, log_normalizer
+
+
+class Binomial(torch.distributions.Binomial, TorchDistributionMixin):
+    def __init__(self, total_count=1, probs=None, logits=None, validate_args=None, *,
+                 approx_sample_thresh=math.inf):
+        assert isinstance(approx_sample_thresh, numbers.Number)
+        assert approx_sample_thresh >= 0
+        super().__init__(total_count=total_count, probs=probs, logits=logits,
+                         validate_args=validate_args)
+        self.approx_sample_thresh = approx_sample_thresh
+
+    def expand(self, batch_shape, _instance=None):
+        new = self._get_checked_instance(Binomial, _instance)
+        batch_shape = torch.Size(batch_shape)
+        new.total_count = self.total_count.expand(batch_shape)
+        if 'probs' in self.__dict__:
+            new.probs = self.probs.expand(batch_shape)
+            new._param = new.probs
+        if 'logits' in self.__dict__:
+            new.logits = self.logits.expand(batch_shape)
+            new._param = new.logits
+        new.approx_sample_thresh = self.approx_sample_thresh
+        super(torch.distributions.Binomial, new).__init__(batch_shape, validate_args=False)
+        new._validate_args = self._validate_args
+        return new
+
+    def sample(self, sample_shape=torch.Size()):
+        if self.approx_sample_thresh < math.inf:
+            if (self.approx_sample_thresh < self.total_count).all():
+                # Return a moment-matched clamped Poisson approximation.
+                with torch.no_grad():
+                    shape = self._extended_shape(sample_shape)
+                    p = self.probs
+                    q = 1 - self.probs
+                    mean = torch.min(p, q) * self.total_count
+                    variance = p * q * self.total_count
+                    shift = (mean - variance).round()
+                    result = torch.poisson(variance.expand(shape)) + shift
+                    result = torch.min(result, self.total_count)
+                    return torch.where((p < q).expand(shape),
+                                       result, self.total_count - result)
+        return super().sample(sample_shape)
 
 
 # This overloads .log_prob() and .enumerate_support() to speed up evaluating
---
+++ tests/distributions/test_binomial.py
@@ -0,0 +1,35 @@
+# Copyright Contributors to the Pyro project.
+# SPDX-License-Identifier: Apache-2.0
+
+import pytest
+
+import pyro.distributions as dist
+from tests.common import assert_close
+
+
+@pytest.mark.parametrize("total_count", [10, 100, 1000, 10000])
+@pytest.mark.parametrize("prob", [0.01, 0.1, 0.5, 0.9, 0.99])
+def test_binomial_approx_sample(total_count, prob):
+    sample_shape = (10000,)
+    d1 = dist.Binomial(total_count, prob)
+    d2 = dist.Binomial(total_count, prob, approx_sample_thresh=200)
+    expected = d1.sample(sample_shape)
+    actual = d2.sample(sample_shape)
+
+    assert_close(expected.mean(), actual.mean(), rtol=0.05)
+    assert_close(expected.std(), actual.std(), rtol=0.05)
+
+
+@pytest.mark.parametrize("total_count", [10, 100, 1000, 1000, 10000])
+@pytest.mark.parametrize("concentration1", [0.1, 0.5, 1.0, 10.])
+@pytest.mark.parametrize("concentration0", [0.1, 0.5, 1.0, 10.])
+def test_beta_binomial_approx_sample(concentration1, concentration0, total_count):
+    sample_shape = (10000,)
+    d1 = dist.BetaBinomial(concentration1, concentration0, total_count)
+    d2 = dist.BetaBinomial(concentration1, concentration0, total_count,
+                           approx_sample_thresh=200)
+    expected = d1.sample(sample_shape)
+    actual = d2.sample(sample_shape)
+
+    assert_close(expected.mean(), actual.mean(), rtol=0.05)
+    assert_close(expected.std(), actual.std(), rtol=0.05)
