Fix ExtendedBetaBinomial gradient issue

tests/ops/test_special.py
@@ -3,8 +3,24 @@
 
 import pytest
 import torch
+from torch.autograd import grad
 
-from pyro.ops.special import log_beta, log_binomial
+from pyro.ops.special import log_beta, log_binomial, safe_log
+from tests.common import assert_equal
+
+
+def test_safe_log():
+    # Test values.
+    x = torch.randn(1000).exp().requires_grad_()
+    expected = x.log()
+    actual = safe_log(x)
+    assert_equal(actual, expected)
+    assert_equal(grad(actual.sum(), [x])[0], grad(expected.sum(), [x])[0])
+
+    # Test gradients.
+    x = torch.tensor(0., requires_grad=True)
+    assert not torch.isfinite(grad(x.log(), [x])[0])
+    assert torch.isfinite(grad(safe_log(x), [x])[0])
 
 
 @pytest.mark.parametrize("tol", [