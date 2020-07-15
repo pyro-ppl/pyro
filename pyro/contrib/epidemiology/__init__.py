acde6b547644c9acb839d2271ce0ce122c2e505e
Fritz Obermeyer <fritzo@uber.com>
2020-07-15 10:55:50 -0700

    Add helper to convert Bio.Phylo trees to PyTorch (#2557)
    * Add helper to convert Bio.Phylo trees to PyTorch
    
    * Fix docs
    
    * Fix parsing format
    
    * Remove debugging statements
    
    * Do not require biopython

--- pyro/contrib/epidemiology/__init__.py
+++ pyro/contrib/epidemiology/__init__.py
@@ -1,6 +1,8 @@
 # Copyright Contributors to the Pyro project.
 # SPDX-License-Identifier: Apache-2.0
 
+from pyro.distributions.coalescent import bio_phylo_to_times
+
 from .compartmental import CompartmentalModel
 from .distributions import beta_binomial_dist, binomial_dist, infection_dist
 
@@ -8,5 +10,6 @@ __all__ = [
     "CompartmentalModel",
     "beta_binomial_dist",
     "binomial_dist",
+    "bio_phylo_to_times",
     "infection_dist",
 ]
