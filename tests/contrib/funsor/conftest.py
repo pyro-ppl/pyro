e4ecef00883be822438987f465ffd56050fbe959
Eli <eli@elibingham.com>
2020-08-05 14:56:33 -0400

    try skipping in test collection if importerror

--- tests/contrib/funsor/conftest.py
+++ tests/contrib/funsor/conftest.py
@@ -7,6 +7,11 @@ import pytest
 def pytest_collection_modifyitems(items):
     for item in items:
         if item.nodeid.startswith("tests/contrib/funsor"):
+            try:
+                import funsor
+                funsor.set_backend("torch")
+            except ImportError:
+                item.add_marker(pytest.mark.skip(reason="funsor is not installed"))
             if "stage" not in item.keywords:
                 item.add_marker(pytest.mark.stage("funsor"))
             if "init" not in item.keywords:
