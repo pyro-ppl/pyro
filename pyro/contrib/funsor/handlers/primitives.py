42ae909c96df8d041f967282ddc32777da11d4e4
Eli <eli@elibingham.com>
2020-08-05 16:02:13 -0400

    move funsor import inside primitives to appease travis

--- pyro/contrib/funsor/handlers/primitives.py
+++ pyro/contrib/funsor/handlers/primitives.py
@@ -1,8 +1,6 @@
 # Copyright Contributors to the Pyro project.
 # SPDX-License-Identifier: Apache-2.0
 
-import funsor
-
 import pyro.poutine.runtime
 
 from pyro.contrib.funsor.handlers.runtime import _DIM_STACK, DimRequest, DimType
@@ -10,6 +8,7 @@ from pyro.contrib.funsor.handlers.runtime import _DIM_STACK, DimRequest, DimType
 
 @pyro.poutine.runtime.effectful(type="to_funsor")
 def to_funsor(x, output=None, dim_to_name=None, dim_type=DimType.LOCAL):
+    import funsor
     if pyro.poutine.runtime.am_i_wrapped() and not dim_to_name:
         dim_to_name = _DIM_STACK.global_frame.dim_to_name.copy()
     assert not dim_to_name or not any(isinstance(name, DimRequest) for name in dim_to_name.values())
@@ -18,6 +17,7 @@ def to_funsor(x, output=None, dim_to_name=None, dim_type=DimType.LOCAL):
 
 @pyro.poutine.runtime.effectful(type="to_data")
 def to_data(x, name_to_dim=None, dim_type=DimType.LOCAL):
+    import funsor
     if pyro.poutine.runtime.am_i_wrapped() and not name_to_dim:
         name_to_dim = _DIM_STACK.global_frame.name_to_dim.copy()
     assert not name_to_dim or not any(isinstance(dim, DimRequest) for dim in name_to_dim.values())
