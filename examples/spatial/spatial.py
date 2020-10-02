8887e4f7b9f555eb97099ecdc9bc68ef85be9920
martin jankowiak <jankowiak@gmail.com>
2020-10-02 18:13:10 +0000

    stabilize

--- examples/spatial/spatial.py
+++ examples/spatial/spatial.py
@@ -15,7 +15,7 @@ import pyro.distributions as dist
 import pyro.poutine as poutine
 from pyro.distributions.util import broadcast_shape
 from pyro.optim import Adam, ClippedAdam
-from pyro.infer import SVI, config_enumerate, TraceEnum_ELBO
+from pyro.infer import SVI, config_enumerate, TraceEnum_ELBO, Trace_ELBO
 
 import matplotlib.pyplot as plt
 from matplotlib.patches import Patch
@@ -55,7 +55,8 @@ class Z2Decoder(nn.Module):
         hidden = self.fc(_z1_y)
         hidden = hidden.reshape(z1_y.shape[:-1] + hidden.shape[-1:])
         loc, scale = split_in_half(hidden)
-        scale = softplus(scale - 2.0)
+        scale = softplus(scale / 20.0)
+        #scale = softplus(scale / 20.0 + 4.0)
         return loc, scale
 
 
@@ -81,8 +82,8 @@ class Z2LEncoder(nn.Module):
     def forward(self, x, s):
         x_s = torch.cat([x, s], dim=-1)
         h1, h2 = split_in_half(self.fc(x_s))
-        z2_loc, z2_scale = h1[..., :-1], softplus(h2[..., :-1] - 2.0)
-        l_loc, l_scale = h1[..., -1:], softplus(h2[..., -1:] - 2.0)
+        z2_loc, z2_scale = h1[..., :-1], softplus(h2[..., :-1] / 20.0 - 2.0)
+        l_loc, l_scale = h1[..., -1:], softplus(h2[..., -1:] / 20.0 - 2.0)
         return z2_loc, z2_scale, l_loc, l_scale
 
 
@@ -100,7 +101,7 @@ class Z1Encoder(nn.Module):
         hidden = self.fc(_z2_y)
         hidden = hidden.reshape(z2_y.shape[:-1] + hidden.shape[-1:])
         loc, scale = split_in_half(hidden)
-        scale = softplus(scale - 2.0)
+        scale = softplus(scale / 20.0 - 2.0)
         return loc, scale
 
 
@@ -117,7 +118,7 @@ class Classifier(nn.Module):
 
 
 class Spatial(nn.Module):
-    def __init__(self, num_genes, num_labels, latent_dim=5, alpha=0.01, scale_factor=1.0):
+    def __init__(self, num_genes, num_labels, latent_dim=5, alpha=0.1, scale_factor=1.0):
         self.num_genes = num_genes
         self.num_labels = num_labels
         self.latent_dim = latent_dim
@@ -134,12 +135,12 @@ class Spatial(nn.Module):
         self.z1_encoder = Z1Encoder(num_labels=num_labels, z1_dim=self.latent_dim,
                                     z2_dim=self.latent_dim, hidden_dims=[50])
 
-        self.epsilon = 1.0e-3
+        self.epsilon = 1.0e-6
 
     def model(self, l_mean, l_scale, x, s, y=None):
         pyro.module("spatial", self)
 
-        theta = pyro.param("inverse_dispersion", x.new_ones(self.num_genes),
+        theta = pyro.param("inverse_dispersion", 10.0 * x.new_ones(self.num_genes),
                            constraint=constraints.positive)
 
         with pyro.plate("batch", len(x)), poutine.scale(scale=self.scale_factor):
@@ -147,6 +148,8 @@ class Spatial(nn.Module):
             y = pyro.sample("y", dist.OneHotCategorical(logits=x.new_zeros(self.num_labels)))
 
             z2_loc, z2_scale = self.z2_decoder(z1, y)
+            #print("MODEL z2_loc", z2_loc.mean().item(), z2_loc.median().item())
+            #print("MODEL z2_scale", z2_scale.mean().item(), z2_scale.median().item(), z2_scale.min().item(), z2_scale.max().item())
             z2 = pyro.sample("z2", dist.Normal(z2_loc, z2_scale).to_event(1))
 
             l_scale = l_scale * x.new_ones(1)
@@ -155,19 +158,19 @@ class Spatial(nn.Module):
             mu = self.x_decoder(z2)
             # TODO revisit this parameterization when https://github.com/pytorch/pytorch/issues/42449 is resolved
             nb_logits = (l * mu + self.epsilon).log() - (theta + self.epsilon).log()
-            #nb_logits = (theta + self.epsilon).log() - (l * mu + self.epsilon).log()
-            print("theta", theta.mean().item(), "nb_logits", nb_logits.mean().item())
+            #print("theta", theta.mean().item(), "nb_logits", nb_logits.mean().item())
             x_dist = dist.NegativeBinomial(total_count=theta, logits=nb_logits)
-            xx = x_dist.sample()
-            print("xx", xx.sum(-1).mean().item(), "x", x.sum(-1).mean().item())
             pyro.sample("x", x_dist.to_event(1), obs=x)
 
     def guide(self, l_mean, l_scale, x, s, y=None):
         with pyro.plate("batch", len(x)), poutine.scale(scale=self.scale_factor):
             z2_loc, z2_scale, l_loc, l_scale = self.z2l_encoder(x, s)
-            print("z2_loc", z2_loc.mean().item(), "z2_scale",z2_scale.mean().item())
+            #print("z2_loc", z2_loc.mean().item(), "z2_scale",z2_scale.mean().item())
             pyro.sample("l", dist.LogNormal(l_loc + l_mean, l_scale).to_event(1))
             z2 = pyro.sample("z2", dist.Normal(z2_loc, z2_scale).to_event(1))
+            #print("GUIDE z2_loc", z2_loc.mean().item(), z2_loc.median().item())
+            #print("GUIDE z2_scale", z2_scale.mean().item(), z2_scale.median().item(), z2_scale.min().item(), z2_scale.max().item())
+            #print("GUIDE z2", z2.mean().item(), z2.median().item(), z2.min().item(), z2.max().item())
 
             y_logits = self.classifier(z2)
             y_dist = dist.OneHotCategorical(logits=y_logits)
@@ -175,7 +178,7 @@ class Spatial(nn.Module):
                 y = pyro.sample("y", y_dist)
             else:
                 classification_loss = y_dist.log_prob(y)
-                #pyro.factor("classification_loss", -self.alpha * classification_loss)
+                pyro.factor("classification_loss", -self.alpha * classification_loss)
 
             z1_loc, z1_scale = self.z1_encoder(z2, y)
             pyro.sample("z1", dist.Normal(z1_loc, z1_scale).to_event(1))
@@ -193,7 +196,7 @@ def main(args):
     spatial = Spatial(num_genes=num_genes, num_labels=76,
                       scale_factor=1.0 / (args.batch_size * num_genes)).cuda()
 
-    optim = ClippedAdam({"lr": args.learning_rate, "clip_norm": 0.02})
+    optim = ClippedAdam({"lr": args.learning_rate, "clip_norm": 10.0})
     guide = config_enumerate(spatial.guide, "parallel", expand=True)
     svi = SVI(spatial.model, guide, optim, TraceEnum_ELBO())
 
@@ -201,12 +204,10 @@ def main(args):
         losses = []
 
         for x, yr, l_mean, l_scale, dataset in dataloader:
-            print("step", dataset, l_mean, l_scale)
             if dataset == "ref":
                 loss = svi.step(l_mean, l_scale, x, x.new_ones(x.size(0), 1), yr)
             elif dataset == "ss":
                 loss = svi.step(l_mean, l_scale, x, x.new_zeros(x.size(0), 1), None)
-            print("loss", loss, dataset)
             losses.append(loss)
 
         print("[Epoch %04d]  Loss: %.4f" % (epoch, np.mean(losses)))
@@ -218,7 +219,7 @@ if __name__ == "__main__":
     parser.add_argument('-s', '--seed', default=0, type=int, help='rng seed')
     parser.add_argument('-n', '--num-epochs', default=200, type=int, help='number of training epochs')
     parser.add_argument('-bs', '--batch-size', default=1024, type=int, help='mini-batch size')
-    parser.add_argument('-lr', '--learning-rate', default=0.0001, type=float, help='learning rate')
+    parser.add_argument('-lr', '--learning-rate', default=0.01, type=float, help='learning rate')
     args = parser.parse_args()
 
     main(args)
