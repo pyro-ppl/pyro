d18e243ee32180bfa9d4d0dbc60b0684768cbae5
Akihiro Nitta <nitta@akihironitta.com>
2020-10-05 23:06:21 +0900

    Revert commits regarding `with_traceback()`

--- pyro/poutine/trace_messenger.py
+++ pyro/poutine/trace_messenger.py
@@ -1,6 +1,8 @@
 # Copyright (c) 2017-2019 Uber Technologies, Inc.
 # SPDX-License-Identifier: Apache-2.0
 
+import sys
+
 from .messenger import Messenger
 from .trace_struct import Trace
 from .util import site_is_subsample
@@ -162,8 +164,11 @@ class TraceHandler:
             try:
                 ret = self.fn(*args, **kwargs)
             except (ValueError, RuntimeError) as e:
+                exc_type, exc_value, traceback = sys.exc_info()
                 shapes = self.msngr.trace.format_shapes()
-                raise type(e)(u"\n{}".format(shapes)) from e
+                exc = exc_type(u"{}\n{}".format(exc_value, shapes))
+                exc = exc.with_traceback(traceback)
+                raise exc from e
             self.msngr.trace.add_node("_RETURN", name="_RETURN", type="return", value=ret)
         return ret
 
