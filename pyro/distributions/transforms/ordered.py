9f7790f35224b9242f30be57f879e4fc9b8bb0b8
ecotner <2.71828cotner@gmail.com>
2020-10-11 16:43:59 -0400

    fix latex problems again...

--- pyro/distributions/transforms/ordered.py
+++ pyro/distributions/transforms/ordered.py
@@ -6,10 +6,10 @@ from pyro.distributions import constraints
 class OrderedTransform(Transform):
     """
     Transforms a real vector into an ordered vector.
-    
+
     Specifically, enforces monotonically increasing order on the last dimension
     of a given tensor via the transformation :math:`y_0 = x_0`,
-    :math:`y_i = \sum_{1 \le j \le i} \exp(x_i)`
+    :math:`y_i = \\sum_{1 \\le j \\le i} \\exp(x_i)`
     """
     domain = constraints.real_vector
     codomain = constraints.ordered_vector
