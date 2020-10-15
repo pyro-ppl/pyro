f985bd812e87b529db15d88909b5060b33292a2e
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2020-10-15 08:09:08 -0400

    Bump version

--- examples/contrib/epidemiology/sir.py
+++ examples/contrib/epidemiology/sir.py
@@ -295,7 +295,7 @@ def main(args):
 
 
 if __name__ == "__main__":
-    assert pyro.__version__.startswith('1.4.0')
+    assert pyro.__version__.startswith('1.5.0')
     parser = argparse.ArgumentParser(
         description="Compartmental epidemiology modeling using HMC")
     parser.add_argument("-p", "--population", default=1000, type=float)
