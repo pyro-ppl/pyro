db888b6ca5e4524b140b05d8c8d460de010bf2ed
ecotner <2.71828cotner@gmail.com>
2020-11-11 22:20:30 -0500

    added tests for input validation and observed distributions with nontrivial event_shape

--- tests/infer/test_log_likelihood.py
+++ tests/infer/test_log_likelihood.py
@@ -14,7 +14,7 @@ def model1(x):
     b = pyro.sample("b", Normal(0., 1.))
     sigma = pyro.sample("sigma", Exponential(1.))
     # gotta do some tricky stuff to support dynamic batch sizes...
-    plates = reversed([pyro.plate(f"obs_{i}", d) for i, d in enumerate(x.shape)])
+    plates = [pyro.plate(f"obs_{i}", d) for i, d in enumerate(x.shape[::-1])]
     with ExitStack() as stack:
         for cm in plates:
             stack.enter_context(cm)
@@ -43,6 +43,7 @@ def test_batch_shape(batch_shape, num_post_samples, parallel):
 
     cond_model = pyro.condition(model1, data={"y": y})
 
+    pyro.clear_param_store()
     lglik = log_likelihood(
         cond_model,
         guide=guide1,
@@ -53,6 +54,23 @@ def test_batch_shape(batch_shape, num_post_samples, parallel):
     assert len(lglik) == 1
     assert "y" in lglik
     assert lglik["y"].shape == (num_post_samples,) + batch_shape
+
+
+def test_input_checking():
+    batch_shape = (10,)
+    x = torch.randn(batch_shape)
+    y = x + 0.1 * torch.randn(batch_shape)
+
+    cond_model = pyro.condition(model1, data={"y": y})
+
+    with pytest.raises(ValueError):
+        log_likelihood(cond_model, guide=guide1)
+
+    with pytest.raises(ValueError):
+        log_likelihood(cond_model, posterior_samples={})
+
+    with pytest.raises(ValueError):
+        log_likelihood(cond_model, posterior_samples={}, guide=guide1)
 
 
 def model2(*Xs):
@@ -89,6 +107,7 @@ def test_obs_sites(num_sites, num_post_samples, parallel):
     data = {f"y_{i}": y[i] for i in range(num_sites)}
     cond_model = pyro.condition(model2, data=data)
 
+    pyro.clear_param_store()
     lglik = log_likelihood(
         cond_model,
         guide=guide2,
@@ -99,3 +118,68 @@ def test_obs_sites(num_sites, num_post_samples, parallel):
     assert len(lglik) == num_sites
     assert set(lglik.keys()) == set(data.keys())
     assert all(lglik[k].shape == (num_post_samples, N) for k in lglik)
+
+
+def model3(x, event_shape):
+    m = pyro.sample("m", Normal(0., 1.).expand(event_shape).to_event(len(event_shape)))
+    sigma = pyro.sample("sigma", Exponential(1.))
+    # use dynamic number of plates to handle dynamic batch dim
+    plates = [pyro.plate(f"obs{i}", d) for i, d in enumerate(x.shape[::-1])]
+    with ExitStack() as stack:
+        for cm in plates:
+            stack.enter_context(cm)
+        # need to reshape all the tensors to make sure everything lines up
+        if sigma.ndim > 0:  # if using vectorized trace
+            N = sigma.shape[0]  # num samples
+            # broadcast all tensors to shape (N,) + x.shape + event_shape
+            M = len((N,) + x.shape + event_shape)
+            while m.ndim < M:
+                m = m.unsqueeze(-(len(event_shape)+1))
+            x = x.unsqueeze(0)
+            while x.ndim < M:
+                x = x.unsqueeze(-1)
+            while sigma.ndim < M:
+                sigma = sigma.unsqueeze(-1)
+        else:  # if using _predictive_serial
+            # broadcast all tensors to shape x.shape + event_shape
+            M = len(x.shape + event_shape)
+            while m.ndim < M:
+                m = m.unsqueeze(0)
+            while x.ndim < M:
+                x = x.unsqueeze(-1)
+            while sigma.ndim < M:
+                sigma = sigma.unsqueeze(-1)
+        # finally add them!
+        y = m * x
+        return pyro.sample("y", Normal(y, sigma).to_event(len(event_shape)))
+
+
+def guide3(x, event_shape):
+    m_mean = pyro.param("m_mean", tt(0.).expand(event_shape))
+    m_std = pyro.param("m_std", tt(1.).expand(event_shape))
+    sigma_mean = pyro.param("sigma_mean", tt(1.))
+    pyro.sample("m", Normal(m_mean, m_std).to_event(len(event_shape)))
+    pyro.sample("sigma", Exponential(sigma_mean))
+
+
+@pytest.mark.parametrize("batch_shape", [(), (1,), (5,), (2, 3), (1, 2, 3, 4)])
+@pytest.mark.parametrize("event_shape", [(), (1,), (5,), (2, 3), (1, 2, 3, 4)])
+@pytest.mark.parametrize("num_post_samples", [1, 5, 10])
+@pytest.mark.parametrize("parallel", [True, False])
+def test_event_shape(batch_shape, event_shape, num_post_samples, parallel):
+    x = torch.randn(batch_shape)
+    y = torch.randn(x.shape + event_shape)
+
+    cond_model = pyro.condition(model3, data={"y": y})
+
+    pyro.clear_param_store()
+    lglik = log_likelihood(
+        cond_model,
+        guide=guide3,
+        num_samples=num_post_samples,
+        parallel=parallel,
+    )(x, event_shape)
+
+    assert len(lglik) == 1
+    assert "y" in lglik
+    assert lglik["y"].shape == (num_post_samples,) + batch_shape
