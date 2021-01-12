8d05f160caa6b48daf0b5b576c3c278076e26200
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2021-01-11 20:35:21 -0500

    Add unit test for chi2sf

--- pyro/distributions/affine_beta.py
+++ pyro/distributions/affine_beta.py
@@ -1,3 +1,6 @@
+# Copyright Contributors to the Pyro project.
+# SPDX-License-Identifier: Apache-2.0
+
 import torch
 from pyro.distributions.torch import Beta, TransformedDistribution
 from torch.distributions import constraints
