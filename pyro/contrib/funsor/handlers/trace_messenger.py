Fix masking in contrib.funsor

pyro/contrib/funsor/handlers/trace_messenger.py
@@ -2,12 +2,19 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import funsor
-
-from pyro.poutine.subsample_messenger import _Subsample
-from pyro.poutine.trace_messenger import TraceMessenger as OrigTraceMessenger
+import torch
 
 from pyro.contrib.funsor.handlers.primitives import to_funsor
 from pyro.contrib.funsor.handlers.runtime import _DIM_STACK
+from pyro.poutine.subsample_messenger import _Subsample
+from pyro.poutine.trace_messenger import TraceMessenger as OrigTraceMessenger
+
+
+def _mask_fn(fn, mask):
+    if mask is not None:
+        mask = torch.as_tensor(mask)  # FIXME support mask=False in funsor.distributions
+        fn = fn.mask(mask)
+    return fn
 
 
 class TraceMessenger(OrigTraceMessenger):
@@ -32,7 +39,7 @@ class TraceMessenger(OrigTraceMessenger):
             return super()._pyro_post_sample(msg)
         if self.pack_online:
             if "fn" not in msg["funsor"]:
-                fn_masked = msg["fn"].mask(msg["mask"]) if msg["mask"] is not None else msg["fn"]
+                fn_masked = _mask_fn(msg["fn"], msg["mask"])
                 msg["funsor"]["fn"] = to_funsor(fn_masked, funsor.Real)(value=msg["name"])
             if "value" not in msg["funsor"]:
                 # value_output = funsor.Reals[getattr(msg["fn"], "event_shape", ())]
@@ -41,7 +48,7 @@ class TraceMessenger(OrigTraceMessenger):
                     not msg["infer"].get("_do_not_trace") and \
                     not msg["infer"].get("_do_not_score", False):
                 # optimization: don't perform this tensor op unless we have to
-                fn_masked = msg["fn"].mask(msg["mask"]) if msg["mask"] is not None else msg["fn"]
+                fn_masked = _mask_fn(msg["fn"], msg["mask"])
                 msg["funsor"]["log_prob"] = to_funsor(fn_masked.log_prob(msg["value"]), output=funsor.Real)
                 # TODO support this pattern which uses funsor directly - blocked by casting issues
                 # msg["funsor"]["log_prob"] = msg["funsor"]["fn"](**{msg["name"]: msg["funsor"]["value"]})