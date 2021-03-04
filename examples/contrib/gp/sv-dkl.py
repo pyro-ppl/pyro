a106882e8ffbfe6ac96f19aef9a218026482ed51
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2021-03-04 18:00:51 -0500

    Bump to 1.6.0 (#2777)
    * Use torch==1.8.0 and torchvision==0.9.0
    
    * Change version
    
    * Update tutorial versions

--- examples/contrib/gp/sv-dkl.py
+++ examples/contrib/gp/sv-dkl.py
@@ -165,7 +165,7 @@ def main(args):
 
 
 if __name__ == '__main__':
-    assert pyro.__version__.startswith('1.5.2')
+    assert pyro.__version__.startswith('1.6.0')
     parser = argparse.ArgumentParser(description='Pyro GP MNIST Example')
     parser.add_argument('--data-dir', type=str, default=None, metavar='PATH',
                         help='default directory to cache MNIST data')
--- examples/contrib/oed/ab_test.py
+++ examples/contrib/oed/ab_test.py
@@ -117,7 +117,7 @@ def main(num_vi_steps, num_bo_steps, seed):
 
 
 if __name__ == "__main__":
-    assert pyro.__version__.startswith('1.5.2')
+    assert pyro.__version__.startswith('1.6.0')
     parser = argparse.ArgumentParser(description="A/B test experiment design using VI")
     parser.add_argument("-n", "--num-vi-steps", nargs="?", default=5000, type=int)
     parser.add_argument('--num-bo-steps', nargs="?", default=5, type=int)
