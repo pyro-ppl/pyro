fix scaling stuff

examples/contrib/funsor/run.py
@@ -1,7 +1,10 @@
 import argparse
+import time
+import math
 import functools
 import logging
 import sys
+from copy import copy
 
 import torch
 import torch.nn as nn
@@ -11,22 +14,15 @@ from pyro.contrib.examples import polyphonic_data_loader as poly
 from pyro.infer.autoguide import AutoDelta
 from pyro.ops.indexing import Vindex
 from pyro.util import ignore_jit_warnings
+from pyro.optim import ClippedAdam
 
 import pyro.contrib.funsor
 from pyroapi import distributions as dist
 from pyroapi import handlers, infer, optim, pyro, pyro_backend
 
 import funsor
-from utils import get_mb_indices
-
-
-logging.basicConfig(format='%(relativeCreated) 9d %(message)s', level=logging.DEBUG)
-
-log = logging.getLogger()
-debug_handler = logging.StreamHandler(sys.stdout)
-debug_handler.setLevel(logging.DEBUG)
-debug_handler.addFilter(filter=lambda record: record.levelno <= logging.DEBUG)
-log.addHandler(debug_handler)
+from utils import get_mb_indices, get_logger
+import uuid
 
 
 # HMM
@@ -46,7 +42,7 @@ class model1(nn.Module):
                              constraint=constraints.unit_interval)
 
         tones_plate = pyro.plate("tones", data_dim, dim=-1)
-        with pyro.plate("sequences", mb.size(0), dim=-3), handlers.scale(scale=args.scale), \
+        with pyro.plate("sequences", mb.size(0), dim=-3), handlers.scale(scale=torch.tensor(args.scale)), \
             handlers.mask(mask=mask.unsqueeze(-1).unsqueeze(-1)):
             lengths = lengths[mb]
             x_prev = 0
@@ -65,9 +61,42 @@ models = {name[len('model'):]: model
 
 
 def main(args):
-    #torch.set_default_tensor_type('torch.cuda.FloatTensor')
+    if args.cuda:
+        torch.set_default_tensor_type('torch.cuda.FloatTensor')
 
-    logging.info('Loading data')
+    N_train = {'jsb': 229, 'piano': 87, 'nottingham': 694, 'muse': 524}[args.dataset]
+    if args.dataset == 'jsb':
+        args.batch_size = 20
+        args.num_steps = 300 # 400
+        NN = args.num_steps * N_train / args.batch_size
+        args.learning_rate_decay = math.exp(math.log(args.learning_rate_decay) / NN)
+    elif args.dataset == 'piano':
+        args.batch_size = 15
+        args.num_steps = 300 # 400
+        NN = args.num_steps * N_train / args.batch_size
+        args.learning_rate_decay = math.exp(math.log(args.learning_rate_decay) / NN)
+    elif args.dataset == 'nottingham':
+        args.batch_size = 30
+        args.num_steps = 200
+        NN = args.num_steps * N_train / args.batch_size
+        args.learning_rate_decay = math.exp(math.log(args.learning_rate_decay) / NN)
+    elif args.dataset == 'muse':
+        args.batch_size = 20
+        args.num_steps = 150
+        NN = args.num_steps * N_train / args.batch_size
+        args.learning_rate_decay = math.exp(math.log(args.learning_rate_decay) / NN)
+
+    log_tag = 'hmm.{}.model{}.num_steps_{}.bs_{}.hd_{}.seed_{}'
+    log_tag += '.lrd_{:.5f}.lr_{:.3f}.nn_{}_{}.scale_{}'
+    log_tag = log_tag.format(args.dataset, args.model, args.num_steps, args.batch_size,
+                             args.hidden_dim, args.seed, args.learning_rate_decay,
+                             args.learning_rate, args.nn_dim, args.nn_channels,
+                             args.scale_loss)
+
+    uid = str(uuid.uuid4())[0:4]
+    log = get_logger('./logs/', log_tag + '.' + uid + '.log')
+    log(args)
+
     data = poly.load_data(poly.JSB_CHORALES)
     train_sequences = data['train']['sequences'].float()
     train_lengths = data['train']['sequence_lengths'].long()
@@ -75,10 +104,9 @@ def main(args):
     test_lengths = data['test']['sequence_lengths'].long()
     data_dim = 88
 
-    logging.info('-' * 40)
     model = models[args.model](args, data_dim)
 
-    logging.info('Training {} on {} sequences'.format("model{}".format(args.model), len(train_sequences)))
+    log('Training {} on {} sequences'.format("model{}".format(args.model), len(train_sequences)))
 
     pyro.set_rng_seed(args.seed)
     pyro.clear_param_store()
@@ -88,21 +116,31 @@ def main(args):
 
     N_train_obs = float(train_lengths.sum())
     N_obs_mb = N_train_obs * args.batch_size / train_sequences.size(0)
-    args.scale = 1.0 / N_obs_mb if args.scale_loss else 1.0
+    N_test_obs = float(test_lengths.sum())
 
-    model = functools.partial(model.model, args)
+    model_eval = functools.partial(model.model, copy(args))
+    args.scale = 1.0 / N_obs_mb if args.scale_loss else 1.0
+    model_train = functools.partial(model.model, copy(args))
     guide = functools.partial(guide, args=args)
 
-    optimizer = optim.Adam({'lr': args.learning_rate})
+    optim = ClippedAdam({'lr': args.learning_rate, 'betas': (0.90, 0.999),
+                         'lrd': args.learning_rate_decay, 'clip_norm': 20.0})
 
     Elbo = infer.JitTraceMarkovEnum_ELBO
     max_plate_nesting = 3
-    elbo = Elbo(max_plate_nesting=max_plate_nesting,
-                strict_enumeration_warning=True,
-                jit_options={"time_compilation": args.time_compilation})
-    svi = infer.SVI(model, guide, optimizer, elbo)
+    elbo = Elbo(max_plate_nesting=max_plate_nesting, strict_enumeration_warning=True)
+    elbo_eval = Elbo(max_plate_nesting=max_plate_nesting, strict_enumeration_warning=True)
+    svi = infer.SVI(model_train, guide, optim, elbo)
 
-    logging.info('Step\tLoss')
+    @torch.no_grad()
+    def evaluate(sequences, lengths, evaluate_batch_size=8):
+        mb_indices, masks = get_mb_indices(sequences.size(0), evaluate_batch_size)
+        return sum([elbo_eval.differentiable_loss(model_eval, guide, sequences, lengths, mb, mask)
+                    for mb, mask in zip(mb_indices, masks)])
+
+    ts = [time.time()]
+    eval_frequency = 5
+
     for epoch in range(args.num_steps):
         epoch_loss = 0.0
         mb_indices, masks = get_mb_indices(train_sequences.size(0), args.batch_size)
@@ -110,10 +148,14 @@ def main(args):
         for mb, mask in zip(mb_indices, masks):
             epoch_loss += svi.step(train_sequences, train_lengths, mb, mask)
 
-        logging.info('{: >5d}\t{}'.format(epoch, epoch_loss))
-
-    if args.time_compilation:
-        logging.debug('time to compile: {} s.'.format(elbo._differentiable_loss.compile_time))
+        ts.append(time.time())
+        log('[epoch %03d]  running train loss: %.4f\t\t (epoch dt: %.2f)' % (epoch, epoch_loss,
+                                                      (ts[-1] - ts[0])/(epoch+1)))
+        if epoch > 3 and epoch % eval_frequency == 0:
+            train_loss = evaluate(train_sequences, train_lengths) / N_train_obs
+            test_loss = evaluate(test_sequences, test_lengths) / N_test_obs
+            log('[epoch %03d]  train loss: %.4f' % (epoch, train_loss))
+            log('[epoch %03d]   test loss: %.4f' % (epoch, test_loss))
 
     # We evaluate on the entire training dataset,
     # excluding the prior term so our results are comparable across models.
@@ -129,21 +171,23 @@ def main(args):
 
 
 if __name__ == '__main__':
-    assert pyro.__version__.startswith('1.6.0')
     parser = argparse.ArgumentParser(description="MAP Baum-Welch learning Bach Chorales")
+    parser.add_argument("--dataset", default="jsb", type=str)
     parser.add_argument("-m", "--model", default="1", type=str,
                         help="one of: {}".format(", ".join(sorted(models.keys()))))
     parser.add_argument("-n", "--num-steps", default=20, type=int)
     parser.add_argument("-b", "--batch-size", default=8, type=int)
-    parser.add_argument("-d", "--hidden-dim", default=16, type=int)
+    parser.add_argument("-d", "--hidden-dim", default=36, type=int)
     parser.add_argument("-nn", "--nn-dim", default=48, type=int)
     parser.add_argument("-nc", "--nn-channels", default=2, type=int)
-    parser.add_argument("-lr", "--learning-rate", default=0.05, type=float)
+    parser.add_argument("-lr", "--learning-rate", default=0.03, type=float)
+    parser.add_argument("--scale", default=1.0, type=float)
+    parser.add_argument("-lrd", "--learning-rate-decay", default=3.0e-5, type=float)
     parser.add_argument("-t", "--truncate", type=int)
     parser.add_argument("-p", "--print-shapes", action="store_true")
     parser.add_argument("--seed", default=0, type=int)
     parser.add_argument('--scale-loss', action='store_true')
-    parser.add_argument('--time-compilation', action='store_true')
+    parser.add_argument('--cuda', action='store_true')
     args = parser.parse_args()
 
     funsor.set_backend("torch")