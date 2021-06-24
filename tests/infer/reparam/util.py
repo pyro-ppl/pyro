c7565d32638425f941577fba6555fde01ae60f59
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2021-06-24 12:09:53 -0600

    Fix stable tests

--- tests/infer/reparam/util.py
+++ tests/infer/reparam/util.py
@@ -1,6 +1,10 @@
 # Copyright Contributors to the Pyro project.
 # SPDX-License-Identifier: Apache-2.0
 
+import warnings
+
+import pytest
+
 from pyro import poutine
 from pyro.infer.autoguide.initialization import InitMessenger, init_to_value
 from pyro.infer.reparam.reparam import Reparam
@@ -18,5 +22,13 @@ def check_init_reparam(model, reparam):
 
         # Check with reparametrizing.
         with poutine.reparam(config={"x": reparam}):
-            actual = model()
+            with warnings.catch_warnings(record=True) as ws:
+                warnings.simplefilter("always", category=RuntimeWarning)
+                actual = model()
+            for w in ws:
+                if w.category == RuntimeWarning and "falling back to default" in str(w):
+                    pytest.skip("overwriting initial value")
+                else:
+                    warnings.warn(str(w.message), category=w.category)
+
             assert_close(actual, init_value)
