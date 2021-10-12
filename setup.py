ee5a5ad29fcf81aa3db9b357a57fe3dc3aec5136
Yerdos Ordabayev <ordabayev@brandeis.edu>
2021-10-12 12:04:56 -0400

    pin funsor to the latest commit

--- setup.py
+++ setup.py
@@ -135,8 +135,7 @@ setup(
         "horovod": ["horovod[pytorch]>=0.19"],
         "funsor": [
             # This must be a released version when Pyro is released.
-            # "funsor[torch] @ git+git://github.com/pyro-ppl/funsor.git@b25eecdaaf134c536f280131bddcf47a475a3c37",
-            "funsor[torch] @ git+git://github.com/pyro-ppl/funsor.git@constant-materialize",
+            "funsor[torch] @ git+git://github.com/pyro-ppl/funsor.git@18a107ccce40de9e3e98db87d116789e0909ce7b",
             # "funsor[torch]==0.4.1",
         ],
     },
