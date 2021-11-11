3c6a591f8d18c7a65f716e9ff1241e0cd898b2da
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2021-11-11 14:03:57 -0500

    Change coordinatization of AutoMultivariateNormal (#2963)
    * Change coordinatization of AutoMultivariateNormal
    
    * lint
    
    * Fix AutoLaplaceApproximation
    
    * Fix AutoLaplaceApproximation
    
    * Fix test
    
    * Move autoguide tests

--- pyro/infer/autoguide/guides.py
+++ pyro/infer/autoguide/guides.py
@@ -862,6 +862,7 @@ class AutoMultivariateNormal(AutoContinuous):
         (unconstrained transformed) latent variable.
     """
 
+    scale_constraint = constraints.softplus_positive
     scale_tril_constraint = constraints.softplus_lower_cholesky
 
     def __init__(self, model, init_loc_fn=init_to_median, init_scale=0.1):
@@ -874,27 +875,31 @@ class AutoMultivariateNormal(AutoContinuous):
         super()._setup_prototype(*args, **kwargs)
         # Initialize guide params
         self.loc = nn.Parameter(self._init_loc())
+        self.scale = PyroParam(
+            torch.full_like(self.loc, self._init_scale), self.scale_constraint
+        )
         self.scale_tril = PyroParam(
-            eye_like(self.loc, self.latent_dim) * self._init_scale,
-            self.scale_tril_constraint,
+            eye_like(self.loc, self.latent_dim), self.scale_tril_constraint
         )
 
     def get_base_dist(self):
         return dist.Normal(
-            torch.zeros_like(self.loc), torch.zeros_like(self.loc)
+            torch.zeros_like(self.loc), torch.ones_like(self.loc)
         ).to_event(1)
 
     def get_transform(self, *args, **kwargs):
-        return dist.transforms.LowerCholeskyAffine(self.loc, scale_tril=self.scale_tril)
+        scale_tril = self.scale[..., None] * self.scale_tril
+        return dist.transforms.LowerCholeskyAffine(self.loc, scale_tril=scale_tril)
 
     def get_posterior(self, *args, **kwargs):
         """
         Returns a MultivariateNormal posterior distribution.
         """
-        return dist.MultivariateNormal(self.loc, scale_tril=self.scale_tril)
+        scale_tril = self.scale[..., None] * self.scale_tril
+        return dist.MultivariateNormal(self.loc, scale_tril=scale_tril)
 
     def _loc_scale(self, *args, **kwargs):
-        return self.loc, self.scale_tril.diag()
+        return self.loc, self.scale * self.scale_tril.diag()
 
 
 class AutoDiagonalNormal(AutoContinuous):
@@ -937,7 +942,7 @@ class AutoDiagonalNormal(AutoContinuous):
 
     def get_base_dist(self):
         return dist.Normal(
-            torch.zeros_like(self.loc), torch.zeros_like(self.loc)
+            torch.zeros_like(self.loc), torch.ones_like(self.loc)
         ).to_event(1)
 
     def get_transform(self, *args, **kwargs):
@@ -1167,15 +1172,23 @@ class AutoLaplaceApproximation(AutoContinuous):
         loss = guide_trace.log_prob_sum() - model_trace.log_prob_sum()
 
         H = hessian(loss, self.loc)
-        cov = H.inverse()
-        loc = self.loc
-        scale_tril = torch.linalg.cholesky(cov)
+        with torch.no_grad():
+            loc = self.loc.detach()
+            cov = H.inverse()
+            scale = cov.diagonal().sqrt()
+            cov /= scale[:, None]
+            cov /= scale[None, :]
+            scale_tril = torch.linalg.cholesky(cov)
 
         gaussian_guide = AutoMultivariateNormal(self.model)
         gaussian_guide._setup_prototype(*args, **kwargs)
-        # Set loc, scale_tril parameters as computed above.
-        gaussian_guide.loc = loc
-        gaussian_guide.scale_tril = scale_tril
+        # Set detached loc, scale, scale_tril parameters as computed above.
+        del gaussian_guide.loc
+        del gaussian_guide.scale
+        del gaussian_guide.scale_tril
+        gaussian_guide.register_buffer("loc", loc)
+        gaussian_guide.register_buffer("scale", scale)
+        gaussian_guide.register_buffer("scale_tril", scale_tril)
         return gaussian_guide
 
 
