d1dc116f0febde72120a84bcfac4fbc3bf78234d
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2021-12-06 09:57:08 -0500

    Load tensors to default location

--- pyro/contrib/examples/nextstrain.py
+++ pyro/contrib/examples/nextstrain.py
@@ -1,3 +1,6 @@
+# Copyright Contributors to the Pyro project.
+# SPDX-License-Identifier: Apache-2.0
+
 import logging
 import os
 import subprocess
@@ -11,7 +14,7 @@ DATA = get_data_directory(__file__)
 URL = "https://github.com/pyro-ppl/datasets/raw/master/nextstrain.data.pt.gz"
 
 
-def load_nextstrain_counts() -> dict:
+def load_nextstrain_counts(map_location=None) -> dict:
     """
     Loads a SARS-CoV-2 dataset.
 
@@ -21,14 +24,21 @@ def load_nextstrain_counts() -> dict:
     (month,location,lineage) and (lineage,mutation) bins by the Broad Institute's
     `preprocessing script <https://github.com/broadinstitute/pyro-cov/blob/master/scripts/preprocess_nextstrain.py>`_.
     """
+    # Download the gzipped file.
     _mkdir_p(DATA)
     basename = URL.split("/")[-1]
     gz_filename = os.path.join(DATA, basename)
     if not os.path.exists(gz_filename):
         logging.debug(f"downloading {URL}")
         urllib.request.urlretrieve(URL, gz_filename)
+
+    # Decompress the file.
     filename = gz_filename.replace(".gz", "")
     if not os.path.exists(filename):
         logging.debug(f"unzipping {gz_filename}")
         subprocess.check_call(["gunzip", "-k", gz_filename])
-    return torch.load(filename)
+
+    # Load tensors to the default location.
+    if map_location is None:
+        map_location = torch.tensor(0.0).device
+    return torch.load(filename, map_location=map_location)
