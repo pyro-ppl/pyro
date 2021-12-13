d21be94d0404680372d79dbbfe7e514314049f0f
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2021-12-13 18:15:11 -0500

    Pin to Funsor 0.4.2

--- setup.py
+++ setup.py
@@ -135,8 +135,8 @@ setup(
         "horovod": ["horovod[pytorch]>=0.19"],
         "funsor": [
             # This must be a released version when Pyro is released.
-            "funsor[torch] @ git+git://github.com/pyro-ppl/funsor.git@93250f9b937639d35cd841f6964b96ffa9848686",
-            # "funsor[torch]==0.4.1",
+            # "funsor[torch] @ git+git://github.com/pyro-ppl/funsor.git@7bb52d0eae3046d08a20d1b288544e1a21b4f461",
+            "funsor[torch]==0.4.2",
         ],
     },
     python_requires=">=3.6",
