c46efa3c27643bd17bfe9101c852bbba7989ec27
Du Phan <fehiepsi@gmail.com>
2022-04-10 08:43:01 -0400

    Fix isort

--- pyro/distributions/distribution.py
+++ pyro/distributions/distribution.py
@@ -2,8 +2,8 @@
 # SPDX-License-Identifier: Apache-2.0
 
 import functools
-from abc import ABCMeta, abstractmethod
 import inspect
+from abc import ABCMeta, abstractmethod
 
 from pyro.distributions.score_parts import ScoreParts
 
