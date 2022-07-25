cdea25653beb63daba4c90b2559dd31a3eb42724
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2022-07-25 18:43:47 -0400

    Implement Resamper for interactive prior tuning (#3118)
    * Implement ResamplingCache
    
    * Vectorize for speed
    
    * lint
    
    * optimization nit
    
    * More optimization
    
    * Completely vectorize
    
    * Generalize to multiple distributions
    
    * Add a tutorial
    
    * Refactor to use models
    
    * Simplified, but introduced a bug :confused:
    
    * fix bug
    
    * Update tutorial
    
    * Implement stable sampling via Gumbel-max trick
    
    * Change nomenclature
    
    * Update prior_predictive.ipynb
    
    Fix colab link
    
    * Install pyro-ppl in colab
    
    * Address review comments

--- tutorial/source/conf.py
+++ tutorial/source/conf.py
@@ -85,7 +85,7 @@ release = version  # eg pyro 0.1.2
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
