f1fd2d6bd20e31387685099240716b31a9ca15ee
Will Tai <wtaisen@gmail.com>
2022-10-28 20:05:13 +0100

    fix: imported Iterator form typing instead

--- pyro/params/param_store.py
+++ pyro/params/param_store.py
@@ -4,9 +4,9 @@
 import re
 import warnings
 import weakref
-from collections.abc import Iterator, KeysView
+from collections.abc import KeysView
 from contextlib import contextmanager
-from typing import Callable, Generator, Optional, Tuple, Union
+from typing import Callable, Generator, Optional, Tuple, Union, Iterator
 
 import torch
 from torch.distributions import constraints, transform_to
