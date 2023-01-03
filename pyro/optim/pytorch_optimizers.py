4d45868d4360f30464ab05080628e2c960ca1b0e
Eli <eli@elibingham.com>
2023-01-03 16:00:07 -0500

    format

--- pyro/optim/pytorch_optimizers.py
+++ pyro/optim/pytorch_optimizers.py
@@ -34,18 +34,16 @@ for _name, _Optim in torch.optim.__dict__.items():
     del _PyroOptim
 
 # Load all schedulers from PyTorch
-try:  # breaking change in torch >= 1.14: LRScheduler is new base class
+# breaking change in torch >= 1.14: LRScheduler is new base class
+if hasattr(torch.optim.lr_scheduler, "LRScheduler"):
     _torch_scheduler_base = torch.optim.lr_scheduler.LRScheduler
-except AttributeError:  # for torch < 1.13, _LRScheduler is base class
+else:  # for torch < 1.13, _LRScheduler is base class
     _torch_scheduler_base = torch.optim.lr_scheduler._LRScheduler
 
 for _name, _Optim in torch.optim.lr_scheduler.__dict__.items():
     if not isinstance(_Optim, type):
         continue
-    if (
-        not issubclass(_Optim, _torch_scheduler_base)
-        and _name != "ReduceLROnPlateau"
-    ):
+    if not issubclass(_Optim, _torch_scheduler_base) and _name != "ReduceLROnPlateau":
         continue
     if _Optim is torch.optim.Optimizer:
         continue
