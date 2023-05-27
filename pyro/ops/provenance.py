3a7b8ee53f6782714d693e5dec6cceaa459cd07d
Ilia Kats <ilia-kats@gmx.net>
2023-05-27 12:32:41 +0200

    simplify further

--- pyro/ops/provenance.py
+++ pyro/ops/provenance.py
@@ -59,8 +59,6 @@ class ProvenanceTensor(torch.Tensor):
         if isinstance(data, ProvenanceTensor):
             provenance |= data._provenance
         self._provenance = provenance
-        if hasattr(data, "unconstrained"):
-            self._t.unconstrained = data.unconstrained
 
     def __repr__(self):
         return "Provenance:\n{}\nTensor:\n{}".format(self._provenance, self._t)
