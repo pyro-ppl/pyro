Fix sign error in JitTraceGraph_ELBO

tests/infer/test_jit.py
@@ -29,7 +29,7 @@ from pyro.infer import (
 from pyro.optim import Adam
 from pyro.poutine.indep_messenger import CondIndepStackFrame
 from pyro.util import ignore_jit_warnings
-from tests.common import assert_equal
+from tests.common import assert_close, assert_equal
 
 
 def constant(*args, **kwargs):
@@ -249,6 +249,49 @@ def test_one_hot_categorical_enumerate(shape, expand):
     log_prob = f(probs)
     batch_shape = shape[:-1]
     assert log_prob.shape == shape[-1:] + batch_shape
+
+
+@pytest.mark.parametrize(
+    "Elbo",
+    [
+        Trace_ELBO,
+        JitTrace_ELBO,
+        TraceGraph_ELBO,
+        JitTraceGraph_ELBO,
+        TraceEnum_ELBO,
+        JitTraceEnum_ELBO,
+        TraceMeanField_ELBO,
+        JitTraceMeanField_ELBO,
+    ],
+)
+def test_loss(Elbo):
+    pyro.clear_param_store()
+    data = torch.tensor(1.0)
+
+    def model(data):
+        loc = pyro.sample("loc", dist.Normal(0, 1))
+        scale = pyro.sample("scale", dist.LogNormal(0, 1))
+        pyro.sample("obs", dist.Normal(loc, scale), obs=data)
+
+    def guide(data):
+        loc_loc = pyro.param("loc_loc", lambda: torch.tensor(1.0))
+        scale_loc = pyro.param("scale_loc", lambda: torch.tensor(-1.0))
+        pyro.sample("loc", dist.Normal(loc_loc, 2))
+        pyro.sample("scale", dist.LogNormal(-1, 0.1))
+
+    elbo = Elbo(
+        num_particles=10_000,
+        vectorize_particles=True,
+        max_plate_nesting=0,
+        strict_enumeration_warning=False,
+    )
+    expected = 18.611
+
+    actual = elbo.loss(model, guide, data)
+    assert_close(actual, expected, rtol=0.1)
+
+    actual = elbo.loss_and_grads(model, guide, data)
+    assert_close(actual, expected, rtol=0.1)
 
 
 @pytest.mark.parametrize("num_particles", [1, 10])