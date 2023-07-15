cacea5aacedecf7537040e9cc2d9a75eb538c25d
François Rozet <francois.rozet@outlook.com>
2023-07-15 16:45:37 +0200

    Revert to support torch>=1.11.0

--- setup.py
+++ setup.py
@@ -102,7 +102,7 @@ setup(
         "numpy>=1.7",
         "opt_einsum>=2.3.2",
         "pyro-api>=0.1.1",
-        "torch>=2.0.1",
+        "torch>=1.11.0",
         "tqdm>=4.36",
     ],
     extras_require={
