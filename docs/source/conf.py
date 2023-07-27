c74b2da6e0a560e0937e026de49f25f07943f210
Du Phan <phandu@google.com>
2023-07-27 08:31:21 -0400

    disable pdf and epub

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -164,9 +164,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    # Disabled pdf builds to unblock readthedocs failed builds;
-    # see https://github.com/pyro-ppl/pyro/issues/3248
-    # (master_doc, "Pyro.tex", u"Pyro Documentation", u"Uber AI Labs", "manual"),
+    (master_doc, "Pyro.tex", u"Pyro Documentation", u"Uber AI Labs", "manual"),
 ]
 
 # -- Options for manual page output ---------------------------------------
