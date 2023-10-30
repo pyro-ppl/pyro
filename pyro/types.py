f8faddf6b7e338a155a54fc01fededf79032fe04
Yerdos Ordabayev <yordabay@broadinstitute.org>
2023-10-30 00:52:59 +0000

    fix make docs

--- pyro/types.py
+++ pyro/types.py
@@ -8,30 +8,22 @@ from typing import Callable, Dict, Optional, Tuple, Union
 import torch
 from typing_extensions import TypedDict
 
-from pyro.params.param_store import (  # noqa: F401
-    _MODULE_NAMESPACE_DIVIDER,
-    ParamStoreDict,
-)
 from pyro.poutine.indep_messenger import CondIndepStackFrame
 
-Message = TypedDict(
-    "Message",
-    {
-        "type": Optional[str],
-        "name": str,
-        "fn": Callable,
-        "is_observed": bool,
-        "args": Tuple,
-        "kwargs": Dict,
-        "value": Optional[torch.Tensor],
-        "scale": float,
-        "mask": Union[bool, torch.Tensor, None],
-        "cond_indep_stack": Tuple[CondIndepStackFrame, ...],
-        "done": bool,
-        "stop": bool,
-        "continuation": Optional[Callable[[Message], None]],
-        "infer": Optional[Dict[str, Union[str, bool]]],
-        "obs": Optional[torch.Tensor],
-    },
-    total=False,
-)
+
+class Message(TypedDict, total=False):
+    type: Optional[str]
+    name: str
+    fn: Callable
+    is_observed: bool
+    args: Tuple
+    kwargs: Dict
+    value: Optional[torch.Tensor]
+    scale: float
+    mask: Union[bool, torch.Tensor, None]
+    cond_indep_stack: Tuple[CondIndepStackFrame, ...]
+    done: bool
+    stop: bool
+    continuation: Optional[Callable[[Message], None]]
+    infer: Optional[Dict[str, Union[str, bool]]]
+    obs: Optional[torch.Tensor]
