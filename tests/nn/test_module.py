863107c1745551f84216c4254372517b3797c7c7
Eli <eli@elibingham.com>
2024-02-15 15:06:00 -0500

    context

--- tests/nn/test_module.py
+++ tests/nn/test_module.py
@@ -768,7 +768,6 @@ def test_bayesian_gru():
 
 
 def test_functorch_pyroparam():
-    pyro.settings.set(module_local_params=True)
 
     class ParamModule(PyroModule):
         def __init__(self):
@@ -785,30 +784,31 @@ def test_functorch_pyroparam():
         def forward(self, x, y):
             return ((self.param_module.a1 + self.param_module.a2) * x + self.b - y) ** 2
 
-    model = Model()
+    with pyro.settings.context(module_local_params=True):
+        model = Model()
 
-    model(torch.tensor(1.3), torch.tensor(0.2))
+        model(torch.tensor(1.3), torch.tensor(0.2))
 
-    params = dict(model.named_parameters())
+        params = dict(model.named_parameters())
 
-    grad_model = torch.func.grad(
-        lambda p, x, y: torch.func.functional_call(model, p, (x, y))
-    )
-    grad_params_func = grad_model(params, torch.tensor(1.3), torch.tensor(0.2))
+        grad_model = torch.func.grad(
+            lambda p, x, y: torch.func.functional_call(model, p, (x, y))
+        )
+        grad_params_func = grad_model(params, torch.tensor(1.3), torch.tensor(0.2))
 
-    gs = torch.autograd.grad(
-        model(torch.tensor(1.3), torch.tensor(0.2)), tuple(params.values())
-    )
-    grad_params_autograd = dict(zip(params.keys(), gs))
+        gs = torch.autograd.grad(
+            model(torch.tensor(1.3), torch.tensor(0.2)), tuple(params.values())
+        )
+        grad_params_autograd = dict(zip(params.keys(), gs))
 
-    assert len(grad_params_autograd) == len(grad_params_func) != 0
-    assert (
-        set(grad_params_autograd.keys())
-        == set(grad_params_func.keys())
-        == set(params.keys())
-    )
-    for k in grad_params_autograd.keys():
-        assert not torch.allclose(
-            grad_params_func[k], torch.zeros_like(grad_params_func[k])
-        ), k
-        assert torch.allclose(grad_params_autograd[k], grad_params_func[k]), k
+        assert len(grad_params_autograd) == len(grad_params_func) != 0
+        assert (
+            set(grad_params_autograd.keys())
+            == set(grad_params_func.keys())
+            == set(params.keys())
+        )
+        for k in grad_params_autograd.keys():
+            assert not torch.allclose(
+                grad_params_func[k], torch.zeros_like(grad_params_func[k])
+            ), k
+            assert torch.allclose(grad_params_autograd[k], grad_params_func[k]), k
