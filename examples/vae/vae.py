a7398461aaead1edc8316b741117e8a34aac65c0
Fritz Obermeyer <fritz.obermeyer@gmail.com>
2024-02-18 13:57:11 -0800

    Bump version to 1.9.0

--- examples/vae/vae.py
+++ examples/vae/vae.py
@@ -216,7 +216,7 @@ def main(args):
 
 
 if __name__ == "__main__":
-    assert pyro.__version__.startswith("1.8.6")
+    assert pyro.__version__.startswith("1.9.0")
     # parse command line arguments
     parser = argparse.ArgumentParser(description="parse args")
     parser.add_argument(
