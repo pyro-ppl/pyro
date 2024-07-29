60008124aa3a23cc4a1dcac95a89a922f3c3f59b
Ben Zickel <ben@vesttoo.com>
2024-07-29 17:51:19 +0300

    Handle support of stacking and concatenation transforms in SplitReparam.

--- pyro/infer/reparam/split.py
+++ pyro/infer/reparam/split.py
@@ -11,9 +11,10 @@ from pyro.distributions.torch_distribution import TorchDistributionMixin
 from .reparam import Reparam
 
 
-def same_support(fn: TorchDistributionMixin):
+def same_support(fn: TorchDistributionMixin, *args):
     """
-    Returns support of the `fn` distribution.
+    Returns support of the `fn` distribution. Used in :class:`SplitReparam` in
+    order to determine the support of the split value.
 
     :param fn: distribution class
     :returns: distribution support
@@ -21,14 +22,43 @@ def same_support(fn: TorchDistributionMixin):
     return fn.support
 
 
-def real_support(fn: TorchDistributionMixin):
+def real_support(fn: TorchDistributionMixin, *args):
     """
     Returns real support with same event dimension as that of the `fn` distribution.
+    Used in :class:`SplitReparam` in order to determine the support of the split value.
 
     :param fn: distribution class
     :returns: distribution support
     """
     return dist.constraints.independent(dist.constraints.real, fn.event_dim)
+
+
+def default_support(fn: TorchDistributionMixin, slice, dim):
+    """
+    Returns support of the `fn` distribution, corrected for split stacking and
+    concatenation transforms. Used in :class:`SplitReparam` in
+    order to determine the support of the split value.
+
+    :param fn: distribution class
+    :param slice: slice for which to return support
+    :param dim: dimension for which to return support
+    :returns: distribution support
+    """
+    support = fn.support
+    # Unwrap support
+    reinterpreted_batch_ndims_vec = []
+    while isinstance(support, dist.constraints.independent):
+        reinterpreted_batch_ndims_vec.append(support.reinterpreted_batch_ndims)
+        support = support.base_constraint
+    # Slice concatenation and stacking transforms
+    if isinstance(support, dist.constraints.stack) and support.dim == dim:
+        support = dist.constraints.stack(support.cseq[slice], dim)
+    elif isinstance(support, dist.constraints.cat) and support.dim == dim:
+        support = dist.constraints.cat(support.cseq[slice], dim, support.lengths[slice])
+    # Wrap support
+    for reinterpreted_batch_ndims in reinterpreted_batch_ndims_vec[::-1]:
+        support = dist.constraints.independent(support, reinterpreted_batch_ndims)
+    return support
 
 
 class SplitReparam(Reparam):
@@ -50,13 +80,14 @@ class SplitReparam(Reparam):
     :type: list(int)
     :param int dim: Dimension along which to split. Defaults to -1.
     :param callable support_fn: Function which derives the split support
-        from the site's sampling function. Default is :func:`same_support`
-        as the sampling function, but in some cases such as sampling functions
-        which are stacked transforms, you would have to explicitly specify
-        the support with :func:`real_support`
+        from the site's sampling function, split size, and split dimension.
+        Default is :func:`default_support` which correctly handles stacking
+        and concatenation transforms. Other options are :func:`same_support`
+        which returns the same support as that of the sampling function, and
+        :func:`real_support` which returns a real support.
     """
 
-    def __init__(self, sections, dim, support_fn=same_support):
+    def __init__(self, sections, dim, support_fn=default_support):
         assert isinstance(dim, int) and dim < 0
         assert isinstance(sections, list)
         assert all(isinstance(size, int) for size in sections)
@@ -80,14 +111,20 @@ class SplitReparam(Reparam):
         dim = fn.event_dim - self.event_dim
         left_shape = fn.event_shape[:dim]
         right_shape = fn.event_shape[1 + dim :]
+        start = 0
         for i, size in enumerate(self.sections):
             event_shape = left_shape + (size,) + right_shape
             value_split[i] = pyro.sample(
                 f"{name}_split_{i}",
-                dist.ImproperUniform(self.support_fn(fn), fn.batch_shape, event_shape),
+                dist.ImproperUniform(
+                    self.support_fn(fn, slice(start, start + size), -self.event_dim),
+                    fn.batch_shape,
+                    event_shape,
+                ),
                 obs=value_split[i],
                 infer={"is_observed": is_observed},
             )
+            start += size
 
         # Combine parts into value.
         if value is None:
