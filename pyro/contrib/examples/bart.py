6b5173c66a21150fb3275473cb6fd33375784489
Juan Orduz <juanitorduz@gmail.com>
2025-03-12 20:43:32 +0100

    fix datapath bart data (#3425)

--- pyro/contrib/examples/bart.py
+++ pyro/contrib/examples/bart.py
@@ -32,7 +32,9 @@ SOURCE_FILES = [
     "date-hour-soo-dest-2018.csv.gz",
     "date-hour-soo-dest-2019.csv.gz",
 ]
-CACHE_URL = "https://d2hg8soec8ck9v.cloudfront.net/datasets/bart_full.pkl.bz2"
+CACHE_URL = (
+    "https://github.com/pyro-ppl/datasets/blob/master/bart_full.pkl.bz2?raw=true"
+)
 
 
 def _load_hourly_od(basename):
