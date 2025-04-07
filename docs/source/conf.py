02e61436b246a4b3dd67ab11e792bdc2cab3540d
Ben Zickel <benzickel@gmail.com>
2025-04-07 16:03:54 +0300

    Fix Sphinx issue (works on local machine).

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -122,7 +122,6 @@ html_favicon = "_static/img/favicon/favicon.ico"
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
